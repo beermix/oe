aee5007b69c09d71695dabee107c15aebfe69766
AV <27rurk@gmail.com>
2016-10-28 02:32:28 +0000

    2

--- packages/addons/official/kodi-addons/pvr.iptvsimple/package.mk
+++ packages/addons/official/kodi-addons/pvr.iptvsimple/package.mk
@@ -31,13 +31,13 @@ PKG_SHORTDESC="pvr.iptvsimple"
 PKG_LONGDESC="pvr.iptvsimple"
 PKG_AUTORECONF="no"
 
-#PKG_IS_ADDON="yes"
-#PKG_ADDON_TYPE="xbmc.pvrclient"
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_MODULE_PATH=$SYSROOT_PREFIX/usr/lib/kodi \
 		       -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr"
-#addon() {
-#  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
-#  cp -PR $PKG_BUILD/.install_pkg/usr/share/kodi/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
-#  cp -PL $PKG_BUILD/.install_pkg/usr/lib/kodi/addons/$PKG_NAME/*.so $ADDON_BUILD/$PKG_ADDON_ID/
-#}
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -PR $PKG_BUILD/.install_pkg/usr/share/kodi/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -PL $PKG_BUILD/.install_pkg/usr/lib/kodi/addons/$PKG_NAME/*.so $ADDON_BUILD/$PKG_ADDON_ID/
+}
