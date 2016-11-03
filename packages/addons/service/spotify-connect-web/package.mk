95f3db74de085d54d7dff688a963d171549f26e6
AV <27rurk@gmail.com>
2016-11-03 16:42:01 +0000

    autocommit on change

--- packages/addons/service/spotify-connect-web/package.mk
+++ packages/addons/service/spotify-connect-web/package.mk
@@ -17,13 +17,9 @@
 ################################################################################
 
 PKG_NAME="spotify-connect-web"
-PKG_VERSION="0.0.3-alpha"
-PKG_REV="103"
-PKG_ARCH="arm"
-PKG_ADDON_PROJECTS="RPi2 WeTek_Core WeTek_Play"
-PKG_LICENSE="prop."
+PKG_VERSION="master"
 PKG_SITE="https://github.com/Fornoth/spotify-connect-web"
-PKG_URL="https://github.com/Fornoth/spotify-connect-web/releases/download/$PKG_VERSION/${PKG_NAME}_$PKG_VERSION.tar.gz"
+PKG_GIT_URL="https://github.com/Fornoth/spotify-connect-web"
 PKG_SOURCE_DIR="spotify-connect-web"
 PKG_DEPENDS_TARGET="toolchain avahi"
 PKG_SECTION="service"
@@ -47,7 +43,7 @@ makeinstall_target() {
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
 
-  cp -P $(get_build_dir avahi)/avahi-utils/.libs/avahi-publish \
+  cp -P $(get_pkg_build avahi)/avahi-utils/.libs/avahi-publish \
         $ADDON_BUILD/$PKG_ADDON_ID/bin/
 
   cp -PR $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID/
--- packages/addons/service/tigervnc/package.mk
+++ packages/addons/service/tigervnc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="tigervnc"
-PKG_VERSION="feca443"
+PKG_VERSION="6c0181c"
 PKG_REV="100"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
