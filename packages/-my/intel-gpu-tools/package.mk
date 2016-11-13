a8d9e2de93c00d19721d4f85a496aa41955da2a3
AV <27rurk@gmail.com>
2016-11-13 12:31:54 +0000

    autocommit on change

--- packages/-my/intel-gpu-tools/package.mk
+++ packages/-my/intel-gpu-tools/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="intel-gpu-tools"
 PKG_VERSION="1.16"
 PKG_URL="http://xorg.freedesktop.org/releases/individual/app/intel-gpu-tools-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain systemd mesa libdrm libpciaccess cairo libXv"
+PKG_DEPENDS_TARGET="toolchain systemd mesa libdrm libpciaccess cairo"
 PKG_PRIORITY="optional"
 PKG_SECTION="tools"
 PKG_SHORTDESC="x11-utils"
--- packages/-my/tar/package.mk
+++
@@ -1,9 +0,0 @@
-PKG_NAME="tar"
-PKG_VERSION="1.29"
-PKG_URL="http://mirrors.kernel.org/gnu/tar/tar-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain pcre acl"
-PKG_PRIORITY="optional"
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
-
-PKG_CONFIGURE_OPTS_TARGET="gl_cv_func_mbrtowc_incomplete_state=no gl_cv_func_wcrtomb_retval=no"
