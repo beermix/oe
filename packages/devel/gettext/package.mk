aeba530bead97a58b3b87cd8b046e61a3baa5b79
AV <27rurk@gmail.com>
2016-11-13 21:13:00 +0000

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -30,16 +30,25 @@ PKG_SHORTDESC="gettext: A program internationalization library and tools"
 PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
+
 
 PKG_CONFIGURE_SCRIPT="gettext-tools/configure"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
-                         --disable-rpath \
-                         --with-gnu-ld \
                          --disable-java \
-                         --disable-curses \
-                         --with-included-libxml \
                          --disable-native-java \
                          --disable-csharp \
-                         --without-emacs"
+                         --enable-static \
+                         --enable-threads=posix \
+                         --without-emacs \
+                         --disable-openmp \
+                         --without-cvs \
+                         --without-git \
+                         --with-included-libcroco \
+                         --with-included-libunistring \
+                         --with-included-libxml \
+                         --with-included-glib \
+                         --disable-silent-rules"
+
+PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
