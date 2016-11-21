11bfaa3dcb1a0d636ca5858a55d093352feed9b4
AV <27rurk@gmail.com>
2016-11-21 23:40:59 +0000

    autocommit on change

--- packages/addons/addon-depends/nss/package.mk
+++ packages/addons/addon-depends/nss/package.mk
@@ -58,7 +58,6 @@ make_target() {
   make BUILD_OPT=1 $TARGET_USE_64 \
      NSPR_INCLUDE_DIR=$SYSROOT_PREFIX/usr/include/nspr \
      USE_SYSTEM_ZLIB=1 ZLIB_LIBS=-lz \
-     NSS_USE_SYSTEM_SQLITE3=1 \
      OS_TEST=$TARGET_ARCH \
      NSS_TESTS="dummy" \
      NSINSTALL=$ROOT/$TOOLCHAIN/bin/nsinstall \
