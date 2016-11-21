7a3ceadea0940ae18c50bb0ae5b6587b6a3f5984
AV <27rurk@gmail.com>
2016-11-21 16:36:05 +0000

    autocommit on change

--- packages/-my/x11vnc/package.mk
+++ packages/-my/x11vnc/package.mk
@@ -2,7 +2,7 @@ PKG_NAME="x11vnc"
 PKG_VERSION="0.9.13"
 PKG_SITE="http://www.karlrunge.com/x11vnc/"
 PKG_URL="http://downloads.sourceforge.net/project/libvncserver/x11vnc/${PKG_VERSION}/x11vnc-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo libvncserver"
+PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo"
 PKG_PRIORITY="optional"
 PKG_SECTION="service/system"
 PKG_AUTORECONF="yes"
@@ -11,35 +11,31 @@ pre_build_target() {
 	mkdir -p $PKG_BUILD/.$TARGET_NAME
 	cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
 	export CFLAGS="$CFLAGS -fPIC -DPIC"
+	export MAKEFLAGS="-j1"
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-      --with-x11vnc \
-      --with-x \
-      --without-xkeyboard \
-      --without-xinerama \
-      --without-xrandr \
-      --without-xfixes \
-      --without-xdamage \
-      --without-xtrap \
-      --without-xrecord \
-      --without-fbpm \
-      --without-dpms \
-      --without-v4l \
-      --without-fbdev \
-      --without-uinput \
-      --without-macosx-native \
-      --without-crypt \
-      --without-crypto \
-      --without-ssl \
-      --without-avahi \
-      --with-jpeg \
-      --without-libz \
-      --without-zlib \
-      --without-gnutls \
-      --without-client-tls"
-
-
-post_makeinstall_target() {
-  debug_strip $INSTALL/usr
-}
+			   --with-x11vnc \
+			   --with-x \
+			   --without-xkeyboard \
+			   --without-xinerama \
+			   --without-xrandr \
+			   --without-xfixes \
+			   --without-xdamage \
+			   --without-xtrap \
+			   --without-xrecord \
+			   --without-fbpm \
+			   --without-dpms \
+			   --without-v4l \
+			   --without-fbdev \
+			   --without-uinput \
+			   --without-macosx-native \
+			   --without-crypt \
+			   --without-crypto \
+			   --without-ssl \
+			   --without-avahi \
+			   --with-jpeg \
+			   --without-libz \
+			   --without-zlib \
+			   --without-gnutls \
+			   --without-client-tls"
