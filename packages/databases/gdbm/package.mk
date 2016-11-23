726e018e7e257d37357b6bc90d72855b43a5a24f
AV <27rurk@gmail.com>
2016-11-23 04:49:48 +0000

    autocommit on change

--- packages/databases/gdbm/package.mk
+++ packages/databases/gdbm/package.mk
@@ -5,15 +5,12 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
 			   --disable-shared \
 			   --enable-static \
 			   --with-gnu-ld \
-			   --disable-nls \
-			   --with-gdbm183-library \
-			   --enable-libgdbm-compat \
-			   --with-sysroot=$SYSROOT_PREFIX"
+			   --with-gdbm183-library"
 			   
 #--enable-libgdbm-compat
--- packages/lang/Python/package - Copy.mk
+++ packages/lang/Python/package - Copy.mk
@@ -24,7 +24,7 @@ PKG_LICENSE="OSS"
 PKG_SITE="http://www.python.org/"
 PKG_URL="http://www.python.org/ftp/python/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="zlib:host bzip2:host"
-PKG_DEPENDS_TARGET="toolchain sqlite expat zlib bzip2 openssl libffi gdbm Python:host"
+PKG_DEPENDS_TARGET="toolchain sqlite expat pcre libz tcl bzip2 readline libedit openssl libffi gdbm Python:host"
 PKG_SECTION="lang"
 PKG_SHORTDESC="python: The Python programming language"
 PKG_LONGDESC="Python is an interpreted object-oriented programming language, and is often compared with Tcl, Perl, Java or Scheme."
@@ -37,7 +37,8 @@ PY_DISABLED_MODULES="_tkinter nis bsddb ossaudiodev"
 PKG_CONFIGURE_OPTS_HOST="--cache-file=config.cache \
                          --without-cxx-main \
                          --with-threads \
-                         --enable-unicode=ucs4"
+                         --enable-unicode=ucs4 \
+                         --disable-ipv6"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
                            ac_cv_file_dev_ptmx=yes \
@@ -62,7 +63,10 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
                            --with-wctype-functions \
                            --without-cxx-main \
                            --with-system-ffi \
-                           --with-system-expat"
+                           --with-system-expat \
+                           --with-system-zlib \
+                           --enable-shared \
+                           --with-lto"
 post_patch() {
   # This is needed to make sure the Python build process doesn't try to
   # regenerate those files with the pgen program. Otherwise, it builds
@@ -89,10 +93,15 @@ makeinstall_host() {
 
 pre_configure_target() {
   export PYTHON_FOR_BUILD=$ROOT/$TOOLCHAIN/bin/python
+  #export CFLAGS=`echo $CFLAGS | sed -e "s|-O2||g"`
+  #export CFLAGS=`echo $CFLAGS | sed -e "s|-O3||g"`
+  strip_lto
+  export CFLAGS="-D_DEFAULT_SOURCE $CFLAGS -fPIC"
 }
 
+
 make_target() {
-  make  -j1 CC="$CC" LDFLAGS="$TARGET_LDFLAGS -L." \
+  make  -j1 CC="$CC" LDFLAGS="$TARGET_LDFLAGS -L. -fPIC" \
         PYTHON_DISABLE_MODULES="$PY_DISABLED_MODULES" \
         PYTHON_MODULES_INCLUDE="$TARGET_INCDIR" \
         PYTHON_MODULES_LIB="$TARGET_LIBDIR"
@@ -113,7 +122,7 @@ makeinstall_target() {
 }
 
 post_makeinstall_target() {
-  EXCLUDE_DIRS="bsddb idlelib lib-tk lib2to3 msilib pydoc_data test unittest"
+  EXCLUDE_DIRS="bsddb idlelib lib-tk lib2to3 msilib pydoc_data"
   for dir in $EXCLUDE_DIRS; do
     rm -rf $INSTALL/usr/lib/python*/$dir
   done
@@ -128,8 +137,4 @@ post_makeinstall_target() {
   cd $INSTALL/usr/lib/python2.7
   python -Wi -t -B $ROOT/$PKG_BUILD/Lib/compileall.py -d /usr/lib/python2.7 -f .
   find $INSTALL/usr/lib/python2.7 -name "*.py" -exec rm -f {} \; &>/dev/null
-
-  # strip
-  chmod u+w $INSTALL/usr/lib/libpython*.so.*
-  debug_strip $INSTALL/usr
 }
