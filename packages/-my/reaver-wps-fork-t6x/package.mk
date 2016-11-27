a007824c754eaae510dcc022368b19d86386b703
AV <27rurk@gmail.com>
2016-11-27 23:43:02 +0000

    autocommit on change

--- packages/-my/reaver-wps-fork-t6x/package.mk
+++ packages/-my/reaver-wps-fork-t6x/package.mk
@@ -5,27 +5,23 @@ PKG_DEPENDS_TARGET="toolchain sqlite libpcap"
 PKG_PRIORITY="optional"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 post_unpack() {
   cp -r $PKG_BUILD/src/* $PKG_BUILD/
 }
 
-
 pre_configure_target() {
    cd $ROOT/$PKG_BUILD
    export MAKEFLAGS="-j1"
    export LDFLAGS="-ldl -lpthread -lsqlite3"
+   mkdir -p $INSTALL_DEV/usr/bin/
+   mkdir -p $INSTALL/usr/bin/
 }
 
-
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_sqlite3_sqlite3_open=yes \
-			   --prefix=/usr \
-			   --sysconfdir=/storage/.config \
-			   --datarootdir=/storage/.config"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_sqlite3_sqlite3_open=yes --prefix=/usr --sysconfdir=/storage/.config --datarootdir=/storage/.config"
 
 post_makeinstall_target() {
-   mkdir -p $INSTALL/usr/bin/
    cp wash $INSTALL/usr/bin/wash
    cp reaver $INSTALL/usr/bin/reaver
 }
