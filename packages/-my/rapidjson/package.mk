a008272ecef793f726b791c7f764ce246c10c0c6
AV <27rurk@gmail.com>
2016-11-27 21:22:13 +0000

    autocommit on change

--- packages/-my/rapidjson/package.mk
+++ packages/-my/rapidjson/package.mk
@@ -1,43 +1,19 @@
-################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
-
 PKG_NAME="rapidjson"
-PKG_VERSION="1.0.2"
+PKG_VERSION="v1.1.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="meh"
-PKG_SITE="https://github.com/miloyip/rapidjson"
-PKG_URL="http://saraev.ca/openelec/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_GIT_URL="https://github.com/miloyip/rapidjson"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="rapidjson: JSON parser/generator"
 PKG_LONGDESC="A fast JSON parser/generator for C++ with both SAX/DOM style API"
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-configure_target() {
-  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        ..
-}
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF"
 
-post_makeinstall_target() {
-  rm -rf $INSTALL/usr/lib/cmake
-}
+#post_makeinstall_target() {
+#  rm -rf $INSTALL/usr/lib/cmake
+#}
