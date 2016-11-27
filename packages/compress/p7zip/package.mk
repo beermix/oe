e98441d94749f0bb058fc76d89fdeccc69feb3da
AV <27rurk@gmail.com>
2016-11-27 18:57:39 +0000

    autocommit on change

--- packages/compress/p7zip/package.mk
+++ packages/compress/p7zip/package.mk
@@ -6,9 +6,9 @@ PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
 
-CFLAGS="-march=corei7-avx -O3 -pipe -fstack-protector-strong"
-LDFLAGS="-Wl,-O1 -Wl,--as-needed"
-CPPFLAGS="-march=corei7-avx -O3 -pipe -D_FORTIFY_SOURCE=2"
+#CFLAGS="-march=corei7-avx -O3 -pipe -fstack-protector-strong"
+#LDFLAGS="-Wl,-O1 -Wl,--as-needed"
+#CPPFLAGS="-march=corei7-avx -O3 -pipe -D_FORTIFY_SOURCE=2"
 
 PKG_CMAKE_SCRIPT_TARGET="CPP/7zip/CMAKE/CMakeLists.txt"
 
