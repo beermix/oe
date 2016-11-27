67fc320da5fa9c3f5a7860417da8ad6ac2d27ad2
AV <27rurk@gmail.com>
2016-11-27 16:12:14 +0000

    autocommit on change

--- packages/graphics/tiff/package.mk
+++ packages/graphics/tiff/package.mk
@@ -18,7 +18,6 @@
 
 PKG_NAME="tiff"
 PKG_VERSION="4.0.7"
-#PKG_VERSION="3.9.7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
@@ -33,15 +32,7 @@ PKG_USE_CMAKE="yes"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-#PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF"
-
-#pre_configure_target() {
-#  #strip_lto
-#  #strip_gold
-#  export LIBS="-lterminfo"
-#  export CFLAGS="$CFLAGS -fPIC -DPIC"
-#  export CXXFLAGS="$CXXFLAGS -fPIC -DPIC"
-#}
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
