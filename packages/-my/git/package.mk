d452f3e6592ac57ab58bffe9b92306b5a9bd0e20
AV <27rurk@gmail.com>
2016-12-01 21:29:37 +0000

    autocommit on change

--- packages/-my/git/package.mk
+++ packages/-my/git/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="git"
-PKG_VERSION="2.10.2"
+PKG_VERSION="2.11.0"
 PKG_URL="https://www.kernel.org/pub/software/scm/git/git-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain curl pcre expat libz libssh2 libiconv"
 PKG_PRIORITY="optional"
