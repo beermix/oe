809eeed6d5b2f4a80f294ccfde995a7ba318c7d4
AV <27rurk@gmail.com>
2016-12-02 00:53:39 +0000

    autocommit on change

--- packages/compress/lrzip/package.mk
+++ packages/compress/lrzip/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="lrzip"
-PKG_VERSION="9de7ccb"
+PKG_VERSION="ac393ef"
 PKG_GIT_URL="https://github.com/ckolivas/lrzip"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
@@ -7,5 +7,4 @@ PKG_SECTION="network"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-gnu-ld --disable-silent-rules"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
