5e0fa0ca0756abd8f4133112cad209bfbb9d0fdc
AV <27rurk@gmail.com>
2016-12-03 07:32:47 +0000

    autocommit on change

--- packages/-my/DisplayCAL/package.mk
+++ packages/-my/DisplayCAL/package.mk
@@ -1,38 +1,22 @@
-
 PKG_NAME="DisplayCAL"
-PKG_VERSION="3.1.3.1"
+PKG_VERSION="3.2.1.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
-PKG_URL="https://sourceforge.net/projects/dispcalgui/files/release/3.1.3.1/DisplayCAL-3.1.3.1.tar.gz"
+PKG_URL="https://sourceforge.net/projects/dispcalgui/files/release/3.2.1.0/DisplayCAL-3.2.1.0.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo"
 PKG_PRIORITY="optional"
 PKG_SECTION="service/system"
 PKG_AUTORECONF="no"
 
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --with-x11vnc"
-
-pre_configure_target() {
-  cd $ROOT/$PKG_BUILD
-  rm -rf .$TARGET_NAME
-
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  export LDFLAGS="$LDFLAGS -L$SYSROOT_PREFIX/usr/lib -L$SYSROOT_PREFIX/lib"
-  export LDSHARED="$CC -shared"
+configure_target() {
+  :
 }
 
-pre_configure_target() {
-  cd $ROOT/$PKG_BUILD
-  rm -rf .$TARGET_NAME
-
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  export LDFLAGS="$LDFLAGS -L$SYSROOT_PREFIX/usr/lib -L$SYSROOT_PREFIX/lib"
+make_target() {
+  python setup.py build
+  python setup.py install
+  
 }
-
-makeinstall_target() {
-  python setup.py install --root=$INSTALL --prefix=/usr
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*/tests  
-} 
