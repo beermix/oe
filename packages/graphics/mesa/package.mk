70dcd81d7cf691e79227207a70c36caa30ce6527
AV <27rurk@gmail.com>
2016-12-05 06:02:12 +0000

    autocommit on change

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -99,7 +99,6 @@ PKG_CONFIGURE_OPTS_TARGET="CC_FOR_BUILD=$HOST_CC \
                            --disable-gallium-tests \
                            --enable-shared-glapi \
                            --enable-shader-cache \
-                           --enable-sysfs \
                            --enable-driglx-direct \
                            --enable-glx-tls \
                            --disable-glx-read-only-text \
