b8feff34a27050855fa9c9fbc5a4235df82062dc
AV <27rurk@gmail.com>
2016-12-06 20:35:12 +0000

    autocommit on change

--- packages/-my/libtorrent-rasterbar/package.mk
+++ packages/-my/libtorrent-rasterbar/package.mk
@@ -1,12 +1,12 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="1.1.1"
-PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_1/libtorrent-rasterbar-1.1.1.tar.gz"
+PKG_VERSION="1.0.10"
+PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_0_10/libtorrent-rasterbar-1.0.10.tar.gz"
 PKG_DEPENDS_HOST="toolchain"
 PKG_DEPENDS_TARGET="toolchain openssl expat libiconv boost"
 PKG_SECTION="devel"
 PKG_USE_CMAKE="no"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 #PKG_CMAKE_OPTS_TARGET="-Dunicode=ON -Dstatic_runtime=ON -Dshared=OFF -Dlibiconv=OFF -Dencryption=ON"
 
