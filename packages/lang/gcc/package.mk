efaae345e831b55f17cb906e1e22bd5472c40c7c
AV <27rurk@gmail.com>
2016-12-06 23:47:45 +0000

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -108,6 +108,7 @@ PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
 pre_configure_host() {
   export CXXFLAGS="$CXXFLAGS -std=gnu++98"
   #export CXXFLAGS="$CXXFLAGS -std=gnu++1z"
+  #export CXXFLAGS="$CXXFLAGS -std=gnu++14"
   unset CPP
 }
 
