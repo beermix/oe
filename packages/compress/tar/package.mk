8e21e9161979cb49158acd32cf346366bbbdf58b
AV <27rurk@gmail.com>
2016-12-07 15:52:11 +0000

    autocommit on change

--- packages/compress/tar/package.mk
+++ packages/compress/tar/package.mk
@@ -2,12 +2,11 @@ PKG_NAME="tar"
 PKG_VERSION="1.29"
 PKG_URL="http://mirrors.kernel.org/gnu/tar/tar-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain pcre acl attr"
-PKG_PRIORITY="optional"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="gl_cv_func_mbrtowc_incomplete_state=no \
-				   gl_cv_func_wcrtomb_retval=no \
-				   --with-posix-acls \
-				   --with-xattrs \
-				   --without-selinux"
+			      gl_cv_func_wcrtomb_retval=no \
+			      --with-posix-acls \
+			      --with-xattrs \
+			      --without-selinux"
