dc13082776dfbe61e7d41f906dd084d85f2f20cf
AV <27rurk@gmail.com>
2016-12-07 05:50:18 +0000

    autocommit on change

--- packages/-my/less/package.mk
+++ packages/-my/less/package.mk
@@ -1,17 +1,13 @@
 PKG_NAME="less"
-PKG_VERSION="481"
+PKG_VERSION="487"
 PKG_URL="http://www.greenwoodsoftware.com/less/less-$PKG_VERSION.tar.gz"
-#PKG_SOURCE_DIR="coreutils-1"
 PKG_BUILD_DEPENDS_TARGET="toolchain netbsd-curses libedir"
-PKG_PRIORITY="optional"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
-export LIBS="-ltermcap -lcurses -lterminfo"
-
-export CFLAGS="-Wall -g -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
-
-
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr --enable-static --with-gnu-ld"
-		
+pre_configure_target() {
+  unset CPPFLAGS
+  export LIBS="-ltermcap -lcurses -lterminfo"
+  export CFLAGS="-Wall -g -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
+}
