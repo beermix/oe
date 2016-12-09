4a8ed4c34c02118ff013bbb3c8dc753bd4684dc7
AV <27rurk@gmail.com>
2016-12-09 23:52:20 +0000

    autocommit on change

--- packages/devel/gmp/package.mk
+++ packages/devel/gmp/package.mk
@@ -33,7 +33,7 @@ PKG_LONGDESC="GNU MP is a library for arbitrary precision arithmetic, operating 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-cxx --enable-static --disable-shared --with-pic --enable-silent-rules"
 
