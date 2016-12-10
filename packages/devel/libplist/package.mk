fc4af188a25227e791b3fc5a541289bfff687411
AV <27rurk@gmail.com>
2016-12-10 07:34:08 +0000

    autocommit on change

--- packages/devel/libplist/package.mk
+++ packages/devel/libplist/package.mk
@@ -24,15 +24,15 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://matt.colyer.name/projects/iphone-linux/"
 PKG_URL="http://www.libimobiledevice.org/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libxml2 glib"
-
 PKG_SECTION="devel"
 PKG_SHORTDESC="libplist: a library for manipulating Apple Binary and XML Property Lists"
 PKG_LONGDESC="libplist is a library for manipulating Apple Binary and XML Property Lists"
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+#PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --without-cython"
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --without-cython"
+
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
