3514bed9528d4baa990cf41e501af621fcee8501
AV <27rurk@gmail.com>
2016-12-10 02:35:02 +0000

    autocommit on change

--- packages/-my/ncdu/package.mk
+++ packages/-my/ncdu/package.mk
@@ -1,10 +1,8 @@
 PKG_NAME="ncdu"
 PKG_VERSION="1.12"
 PKG_URL="https://dev.yorhel.nl/download/ncdu-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain readline"
-
+PKG_DEPENDS_TARGET="toolchain netbsd-curses readline"
 PKG_SECTION="tools"
-PKG_AUTORECONF="yes"
-
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr --enable-largefile"
