e5cdabf8e338ccd383d4225d4336edfa21a4c4bf
AV <27rurk@gmail.com>
2016-12-10 12:10:19 +0000

    autocommit on change

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -25,11 +25,9 @@ PKG_SITE="http://www.libpng.org/"
 PKG_URL="$SOURCEFORGE_SRC/libpng/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="libz:host"
 PKG_DEPENDS_TARGET="toolchain libz"
-
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libpng: Portable Network Graphics (PNG) Reference Library"
 PKG_LONGDESC="PNG (Portable Network Graphics) is an extensible file format for the lossless, portable, well-compressed storage of raster images. PNG provides a patent-free replacement for GIF and can also replace many common uses of TIFF. Indexed-color, grayscale, and truecolor images are supported, plus an optional alpha channel. Sample depths range from 1 to 16 bits."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
