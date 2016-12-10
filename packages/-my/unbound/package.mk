a77bb086d7d59b8f8d71d29f78c140d1880366b4
AV <27rurk@gmail.com>
2016-12-10 03:06:40 +0000

    autocommit on change

--- packages/-my/unbound/package.mk
+++ packages/-my/unbound/package.mk
@@ -1,8 +1,7 @@
 PKG_NAME="unbound"
-PKG_VERSION="8e3c8ef"
+PKG_VERSION="7e91dea"
 PKG_GIT_URL="https://github.com/jedisct1/unbound"
-PKG_DEPENDS_TARGET="toolchain expat flex libevent openssl"
-
+PKG_DEPENDS_TARGET="toolchain expat libevent openssl"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
@@ -13,12 +12,12 @@ PKG_AUTORECONF="yes"
 #PKG_CONFIGURE_OPTS_TARGET="--with-libunbound-only --disable-shared --enable-static --with-pthreads --with-gnu-ld"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-pthreads \
-			   --enable-static \
-			   --disable-shared \
-			   --with-gnu-ld \
-			   --sysconfdir=/storage/.config \
-			   --with-libevent=$SYSROOT_PREFIX/usr \
-			   --with-libunbound-only"
+			      --enable-static \
+			      --disable-shared \
+			      --with-gnu-ld \
+			      --sysconfdir=/storage/.config \
+			      --with-libevent=$SYSROOT_PREFIX/usr \
+			      --with-libunbound-only"
 #		
 #post_makeinstall_target() {
 #  rm -rf $INSTALL/storage
