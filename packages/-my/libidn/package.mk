cf19d534c3de14162d372040e086144dbb80b5f5
AV <27rurk@gmail.com>
2016-12-10 07:07:23 +0000

    autocommit on change

--- packages/-my/libidn/package.mk
+++ packages/-my/libidn/package.mk
@@ -2,10 +2,9 @@ KG_NAME="libidn"
 PKG_VERSION="1.33"
 PKG_URL="http://ftpmirror.gnu.org/libidn/libidn-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
