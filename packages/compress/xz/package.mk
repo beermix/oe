81c63d1e477d4fc7e7848cf89bbe08eb17134946
AV <27rurk@gmail.com>
2016-12-10 19:32:34 +0000

    autocommit on change

--- packages/compress/xz/package.mk
+++ packages/compress/xz/package.mk
@@ -23,13 +23,12 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://tukaani.org/xz/"
 PKG_URL="http://tukaani.org/xz/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_HOST="ccache:host gettext:host"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_SECTION="toolchain/archivers"
 PKG_SHORTDESC="xz: a free general-purpose data compression software with high compression ratio."
 PKG_LONGDESC="XZ Utils is free general-purpose data compression software with high compression ratio. XZ Utils were written for POSIX-like systems, but also work on some not-so-POSIX systems. XZ Utils are the successor to LZMA Utils."
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
 
 PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static \
                          --disable-lzmadec \
