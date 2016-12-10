2f65451ceb2657e3a068ea2e43548c92d71c6d79
AV <27rurk@gmail.com>
2016-12-10 14:15:53 +0000

    autocommit on change

--- packages/-my/lnav/package.mk
+++ packages/-my/lnav/package.mk
@@ -1,11 +1,8 @@
 PKG_NAME="lnav"
-PKG_VERSION="2e68f48"
+PKG_VERSION="master"
 PKG_GIT_URL="https://github.com/tstack/lnav"
 PKG_DEPENDS_HOST="toolchain sqlite"
-
 PKG_SECTION="tools"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="yes"
 
-export CC=$LOCAL_CC
-export CCX=$LOCAL_CCX
