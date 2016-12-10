6cd9a8054c3769195cfd9d186f35952ffee37dc9
AV <27rurk@gmail.com>
2016-12-10 11:06:56 +0000

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -17,18 +17,17 @@
 ################################################################################
 
 PKG_NAME="ninja"
-PKG_VERSION="v1.7.2"
+#PKG_VERSION="v1.7.2"
+PKG_VERSION="7d705a3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Apache"
 PKG_SITE="http://martine.github.io/ninja/"
 PKG_GIT_URL="https://github.com/ninja-build/ninja"
 PKG_DEPENDS_HOST="re2c:host"
-
 PKG_SECTION="devel"
 PKG_SHORTDESC="Small build system with a focus on speed"
 PKG_LONGDESC="Small build system with a focus on speed"
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
