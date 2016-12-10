4e89324815903de26c5b8ba42332ca6dcfc3cf31
AV <27rurk@gmail.com>
2016-12-10 01:41:20 +0000

    autocommit on change

--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="gnutls"
-PKG_VERSION="3.4.16"
+PKG_VERSION="3.4.17"
 PKG_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib gmp libgpg-error lzo nettle"
 PKG_SECTION="security"
@@ -8,26 +8,26 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-				   --disable-shared \
-				   --enable-hardware-acceleration \
-				   --disable-openssl-compatibility \
-				   --enable-cxx \
-				   --without-p11-kit \
-				   --enable-local-libopts \
-				   --with-included-libtasn1 \
-				   --with-sysroot=$SYSROOT_PREFIX/usr \
-				   --with-libz-prefix=$SYSROOT_PREFIX/usr \
-				   --with-librt-prefix=$SYSROOT_PREFIX/usr \
-				   --with-libpthread-prefix=$SYSROOT_PREFIX/usr \
-				   --without-libiconv-prefix \
-				   --without-libintl-prefix \
-				   --disable-libdane \
-				   --disable-doc \
-				   --disable-nls \
-				   --disable-guile \
-				   --disable-valgrind-tests \
-				   --with-lzo \
-				   --with-gnu-ld"
+			      --disable-shared \
+			      --enable-hardware-acceleration \
+			      --disable-openssl-compatibility \
+			      --enable-cxx \
+			      --without-p11-kit \
+			      --enable-local-libopts \
+			      --with-included-libtasn1 \
+			      --with-sysroot=$SYSROOT_PREFIX/usr \
+			      --with-libz-prefix=$SYSROOT_PREFIX/usr \
+			      --with-librt-prefix=$SYSROOT_PREFIX/usr \
+			      --with-libpthread-prefix=$SYSROOT_PREFIX/usr \
+			      --without-libiconv-prefix \
+			      --without-libintl-prefix \
+			      --disable-libdane \
+			      --disable-doc \
+			      --disable-nls \
+			      --disable-guile \
+			      --disable-valgrind-tests \
+			      --with-lzo \
+			      --with-gnu-ld"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
