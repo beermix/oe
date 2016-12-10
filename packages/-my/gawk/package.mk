d6497001cec38599e8a69cb85f8f8d87e041181f
AV <27rurk@gmail.com>
2016-12-10 14:09:06 +0000

    autocommit on change

--- packages/-my/gawk/package.mk
+++ packages/-my/gawk/package.mk
@@ -2,8 +2,6 @@ PKG_NAME="gawk"
 PKG_VERSION="4.1.4"
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline netbsd-curses"
-PKG_DEPENDS_HOST="make:host"
-
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
--- packages/-my/grep/package.mk
+++ packages/-my/grep/package.mk
@@ -1,21 +1,10 @@
 PKG_NAME="grep"
 PKG_VERSION="2.26"
 PKG_URL="http://ftp.gnu.org/gnu/grep/grep-$PKG_VERSION.tar.xz"
-#PKG_SOURCE_DIR="coreutils-1"
 PKG_BUILD_DEPENDS_TARGET="toolchain readline gettext pcre libcap texinfo libxml2"
-
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -Ofast"
-}
-
-PKG_CONFIGURE_OPTS_TARGET="\
-		--prefix=/usr \
-		--with-gnu-ld \
-		--enable-pcre \
-		--disable-shared \
-		--enable-static"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
 		
