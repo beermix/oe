01605b4999243a4cf53bf78f7737c5edac64d1d1
AV <27rurk@gmail.com>
2016-12-14 19:20:11 +0000

    autocommit on change

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="systemd"
-PKG_VERSION="86bcce5"
+PKG_VERSION="fc2371c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
