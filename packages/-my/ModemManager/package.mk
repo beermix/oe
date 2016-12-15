663677b45c4297f6279892dfe3bbdecc54325a28
AV <27rurk@gmail.com>
2016-12-15 13:44:37 +0000

    autocommit on change

--- packages/-my/ModemManager/package.mk
+++ packages/-my/ModemManager/package.mk
@@ -1,13 +1,12 @@
 PKG_NAME="ModemManager"
 PKG_VERSION="1.6.2"
 PKG_URL="https://www.freedesktop.org/software/ModemManager/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain" 
-
+PKG_DEPENDS_TARGET="toolchain libgudev" 
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-			   --disable-shared \
-			   --without-mbim \
-			   --without-qmi"
+			      --disable-shared \
+			      --without-mbim \
+			      --without-qmi"
