fc92535ba16317154ab79f261c0fb734613df367
AV <27rurk@gmail.com>
2016-12-15 16:38:39 +0000

    autocommit on change

--- packages/-my/bash/package.mk
+++ packages/-my/bash/package.mk
@@ -9,8 +9,10 @@ pre_configure_target() {
   export LIBS="-ltermcap -lcurses" 
 }
 
-PKG_CONFIGURE_OPTS_TARGET="bash_cv_getenv_redef=no \
-                           bash_cv_job_control_missing=yes \
+PKG_CONFIGURE_OPTS_TARGET="bash_cv_getcwd_malloc=yes \
+                           bash_cv_sys_named_pipes=present \
+                           bash_cv_func_sigsetjmp=present \
+                           bash_cv_printf_a_format=yes \
                            --enable-static \
                            --disable-shared \
                            --bindir=/bin \
