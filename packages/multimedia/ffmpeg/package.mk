cc25c59dfecba9a76791f717bfcdcee016a77604
AV <27rurk@gmail.com>
2016-12-15 20:18:28 +0000

    autocommit on change

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -216,7 +216,7 @@ configure_target() {
               $FFMPEG_FPU \
               --enable-yasm \
               --disable-symver \
-              --disable-lto \
+              --enable-lto \
               --enable-indev=x11grab_xcb
 }
 
