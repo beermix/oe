77ddff14b57ad1835d6631a8c1c098df0eb8c4ed
AV <27rurk@gmail.com>
2016-12-15 21:21:39 +0000

    autocommit on change

--- packages/-my/polipo/package.mk
+++ packages/-my/polipo/package.mk
@@ -2,14 +2,14 @@ PKG_NAME="polipo"
 PKG_VERSION="ab52932"
 PKG_GIT_URL="https://github.com/jech/polipo"
 PKG_DEPENDS_TARGET="toolchain"
-
 PKG_SECTION="service/proxy"
-PKG_SHORTDESC="Polipo - a caching web proxy"
-PKG_LONGDESC="Polipo - a caching web proxy"
-PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+pre_configure_target() {
+   export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
+}
+   
 make_target() {
 make CC="$CC" AR="$AR" LD="$LD" XCFLAGS="$CFLAGS" RANLIB="$RANLIB" XLDFLAGS="$LDFLAGS" MAKEDEPPROG="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS -pthread" -j1
 }
