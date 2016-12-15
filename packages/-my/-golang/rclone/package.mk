efaba0e20ee3d28b56fa692d32df6ab05bcd348e
AV <27rurk@gmail.com>
2016-12-15 11:23:57 +0000

    autocommit on change

--- packages/-my/-golang/rclone/package.mk
+++ packages/-my/-golang/rclone/package.mk
@@ -11,7 +11,7 @@ pre_make_target() {
   export CGO_ENABLED=1
   export CGO_NO_EMULATION=1
   export CGO_CFLAGS=$CFLAGS
-  export LDFLAGS="-s -w -linkmode external -extldflags -Wl,-O1,--as-needed -extld $CC"
+  export LDFLAGS="-s -w -linkmode external -extld $CC"
   export GOLANG=$ROOT/$TOOLCHAIN/lib/golang/bin/go
   export GOPATH=$ROOT/$PKG_BUILD.gopath:$ROOT/$PKG_BUILD/vendor/
   export GOROOT=$ROOT/$TOOLCHAIN/lib/golang
