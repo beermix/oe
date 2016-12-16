93d78ac6c228932a7dd27fddf39f9e4da29287b0
AV <27rurk@gmail.com>
2016-12-16 17:36:41 +0000

    autocommit on change

--- packages/-my/atomicparsley/package.mk
+++ packages/-my/atomicparsley/package.mk
@@ -1,12 +1,9 @@
 PKG_NAME="atomicparsley"
-PKG_VERSION="master"
-PKG_GIT_URL="https://bitbucket.org/wez/atomicparsley"
-PKG_DEPENDS_TARGET="toolchain expat flex libevent openssl"
-
+PKG_VERSION="0.9.6"
+PKG_URL="https://dl.dropboxusercontent.com/s/nl2qgius1vdijac/atomicparsley-0.9.6.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
-
-
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-gnu-ld"
