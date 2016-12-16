4a466f5e70d0f5adea0c285d3f3f3859f16e1104
AV <27rurk@gmail.com>
2016-12-16 00:22:13 +0000

    autocommit on change

--- packages/-my/aria2/package.mk
+++ packages/-my/aria2/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="aria2"
-PKG_VERSION="release-1.29.0"
+PKG_VERSION="release-1.30.0"
 PKG_GIT_URL="https://github.com/aria2/aria2"
-PKG_DEPENDS_TARGET="toolchain libssh2 expat pcre curl libuv libev xmlstarlet libxml2"
+PKG_DEPENDS_TARGET="toolchain libssh2 expat gmp pcre curl libuv libev xmlstarlet libxml2 c-ares"
 PKG_SECTION="tools"
 PKG_AUTORECONF="yes"
 
@@ -24,5 +24,5 @@ PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
                            --without-sqlite3 \
                            --with-xmltest \
                            --with-libxml2 \
-                           --without-libcares \
+                           --with-libcares=$SYSROOT_PREFIX/usr \
                            --with-ca-bundle=/etc/ssl/cert.pem"
--- packages/-my/squid/package.mk
+++ packages/-my/squid/package.mk
@@ -1,22 +1,18 @@
 PKG_NAME="squid"
-#PKG_VERSION="3.5.20"
-#PKG_URL="http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.20.tar.xz"
-PKG_VERSION="4.0.15"
-PKG_URL="http://www.squid-cache.org/Versions/v4/squid-$PKG_VERSION.tar.xz"
+PKG_VERSION="3.5.22"
+PKG_URL="http://www.squid-cache.org/Versions/v3/3.5/squid-$PKG_VERSION.tar.xz"
+#PKG_VERSION="4.0.15"
+#PKG_URL="http://www.squid-cache.org/Versions/v4/squid-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain fuse netbsd-curses openssl glib libcap libnetfilter_conntrack"
-
 PKG_SECTION="tools"
 PKG_AUTORECONF="yes"
 
 
-PKG_CONFIGURE_OPTS_TARGET="squid_cv_gnu_atomics=yes \
+PKG_CONFIGURE_OPTS_TARGET="squid_cv_gnu_atomics=no \
                            --enable-static \
-			   --enable-async-io=8 \
-			   --enable-linux-netfilter \
-                           --disable-optimizations \
-                           --sysconfdir=/storage/.config/squid \
-                           --datarootdir=/storage/.config/squid \
-                           --libdir=/storage/.config/squid \
+                           --enable-async-io=8 \
+                           --enable-linux-netfilter \
+                           --enable-optimizations \
                            --enable-auth-digest \
                            --enable-auth-basic="getpwnam,NCSA,SMB,SMB_LM,RADIUS" \
                            --enable-epoll \
@@ -29,15 +25,26 @@ PKG_CONFIGURE_OPTS_TARGET="squid_cv_gnu_atomics=yes \
                            --without-nettle \
                            --without-gnutls \
                            --with-libcap \
-                           --enable-loadable-modules \
+                           --disable-loadable-modules \
                            --disable-htcp \
                            --disable-eui  \
                            --enable-snmp \
                            --disable-select \
                            --disable-epoll \
                            --disable-http-violations \
-                           --disable-db"
+                           --disable-db \
+                           --sysconfdir=/storage/.config/squid/ \
+                           --datadir=/storage/.config/squid \
+                           --libdir=/storage/.config/squid \
+                           --libexecdir=/storage/.config/squid \
+                           --sharedstatedir=/storage/.config/squid \
+                           --localstatedir=/storage/.config/squid \
+                           --includedir=/storage/.config/squid \
+                           --oldincludedir=/storage/.config/squid \
+                           --datarootdir=/storage/.config/squid \
+                           --infodir=/storage/.config/squid \     
+                           --localedir=/storage/.config/squid"
                            
-post_makeinstall_target() {
-  rm -rf $INSTALL/storage
-}
+#post_makeinstall_target() {
+#  rm -rf $INSTALL/storage
+#}
