3071c6490a531bff52fb912c5b430ddf037cf32c
AV <27rurk@gmail.com>
2016-12-17 00:40:46 +0000

    autocommit on change

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -33,13 +33,13 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_ALL="ac_cv_header_ansidecl_h=no \
-             --enable-static \
-             --enable-shared \
-             --disable-silent-rules \
-             --enable-ipv6 \
-             --without-python \
-             --with-zlib=$ROOT/$TOOLCHAIN \
-             --without-lzma"
+			  --enable-static \
+			  --enable-shared \
+			  --disable-silent-rules \
+			  --enable-ipv6 \
+			  --without-python \
+			  --with-zlib=$ROOT/$TOOLCHAIN \
+			  --without-lzma"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$ROOT/$TOOLCHAIN"
 
