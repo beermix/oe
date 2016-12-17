d3a44f1245cc2c0be11f3a0dec093033883e653f
AV <27rurk@gmail.com>
2016-12-17 02:47:00 +0000

    autocommit on change

--- packages/-my/aufs-util/package.mk
+++ packages/-my/aufs-util/package.mk
@@ -2,8 +2,7 @@ PKG_NAME="aufs-util"
 PKG_VERSION="e345b59"
 PKG_GIT_URL="git://git.code.sf.net/p/aufs/aufs-util"
 #PKG_SOURCE_DIR="aufs2-util"
-PKG_DEPENDS_TARGET="toolchain"
-
+PKG_DEPENDS
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
---
+++ packages/-my/ca-certificates/package.mk
@@ -0,0 +1,13 @@
+PKG_NAME="ca-certificates"
+PKG_VERSION="20161130"
+PKG_URL="http://ftp.ru.debian.org/debian/pool/main/c/ca-certificates/ca-certificates_$PKG_VERSION.tar.xz"
+PKG_SOURCE_DIR="ca-certificates"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_AUTORECONF="no"
+
+pre_configure_target() {
+  mkdir -p $INSTALL/usr/share/ca-certificates/mozilla/
+  mkdir -p $INSTALL_DEV/usr/share/ca-certificates/mozilla/
+  mkdir -p $INSTALL/usr/sbin
+  mkdir -p $INSTALL_DEV/sbin/
+}
