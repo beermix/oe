5b389fed55701a51480fa00caa171ac246a86575
AV <27rurk@gmail.com>
2016-12-21 20:00:59 +0000

    autocommit on change

--- packages/web/libmicrohttpd/package.mk
+++ packages/web/libmicrohttpd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libmicrohttpd"
-PKG_VERSION="0.9.49"
+PKG_VERSION="0.9.51"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1"
@@ -28,7 +28,7 @@ PKG_SECTION="web"
 PKG_SHORTDESC="libmicrohttpd: a small webserver C library"
 PKG_LONGDESC="GNU libmicrohttpd is a small C library that is supposed to make it easy to run an HTTP server as part of another application."
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
