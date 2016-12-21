e4cf0c9a5e070eb3bb758df3da43a7adaa70fee0
AV <27rurk@gmail.com>
2016-12-21 22:26:16 +0000

    autocommit on change

--- packages/addons/addon-depends/gdk-pixbuf/package.mk
+++ packages/addons/addon-depends/gdk-pixbuf/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gdk-pixbuf"
-PKG_VERSION="2.36.0"
+PKG_VERSION="2.34.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
@@ -30,7 +30,7 @@ PKG_SECTION="x11/toolkits"
 PKG_SHORTDESC="gdk-pixbuf: a GNOME library for image loading and manipulation."
 PKG_LONGDESC="gdk-pixbuf (GdkPixbuf) is a GNOME library for image loading and manipulation. The GdkPixbuf documentation contains both the programmer's guide and the API reference."
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="gio_can_sniff=yes \
                            --disable-gtk-doc \
