a0732e9fe8137db18b66be01e0dc0e82c82dff53
AV <27rurk@gmail.com>
2016-12-21 23:58:38 +0000

    autocommit on change

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -14,17 +14,17 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################ parted
+################################################################################
 
 PKG_NAME="busybox"
-PKG_VERSION="1.26.0"
+PKG_VERSION="1.25.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.busybox.net"
 PKG_URL="http://busybox.net/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs zip unzip libaio less tar pciutils usbutils parted procps-ng coreutils findutils bash"
+PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs zip unzip less tar pciutils usbutils parted procps-ng coreutils findutils bash"
 PKG_DEPENDS_INIT="toolchain"
 PKG_SECTION="system"
 PKG_SHORTDESC="BusyBox: The Swiss Army Knife of Embedded Linux"
