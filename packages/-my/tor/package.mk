5c36eaec41db33e09e575db0f191482db1b2adb2
AV <27rurk@gmail.com>
2016-12-21 19:41:28 +0000

    autocommit on change

--- packages/-my/tor/package.mk
+++ packages/-my/tor/package.mk
@@ -1,18 +1,23 @@
 PKG_NAME="tor"
 PKG_VERSION="0.2.9.8"
 PKG_URL="https://archive.torproject.org/tor-package-archive/tor-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain openssl libz libevent"
+PKG_DEPENDS_TARGET="toolchain openssl libz libevent libcap"
 PKG_SECTION="security"
 PKG_AUTORECONF="yes"
 
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -std=gnu99"
+}
+
 PKG_CONFIGURE_OPTS_TARGET="--with-openssl-dir=$SYSROOT_PREFIX/usr \
-			      --enable-gcc-hardening \
+			      --disable-gcc-hardening \
 			      --disable-unittests \
 			      --disable-seccomp \
 			      --sysconfdir=/storage/.config \
 			      --datarootdir=/storage/.cache/tor \
 			      --datadir=/storage/.cache/tor \
 			      --with-zlib-dir=$ROOT/$TOOLCHAIN \
+			      --with-libevent-dir=$SYSROOT_PREFIX/usr \
 			      --disable-asciidoc"
 
 
