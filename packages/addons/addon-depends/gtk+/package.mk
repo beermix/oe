9f721c17f953336bb1ad5ed88dd702a0c957efb6
AV <27rurk@gmail.com>
2016-12-21 18:56:38 +0000

    autocommit on change

--- packages/addons/addon-depends/gtk+/package.mk
+++ packages/addons/addon-depends/gtk+/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gtk+"
-PKG_VERSION="2.24.31"
+PKG_VERSION="2.24.30"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
@@ -32,7 +32,6 @@ PKG_LONGDESC="This is GTK+. GTK+, which stands for the Gimp ToolKit, is a librar
 PKG_IS_ADDON="no"
 
 PKG_AUTORECONF="yes"
-
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_path_GLIB_GENMARSHAL=$ROOT/$TOOLCHAIN/bin/glib-genmarshal \
                            --disable-glibtest \
