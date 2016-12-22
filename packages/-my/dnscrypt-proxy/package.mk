94f87014a6739715bae452bbfcc0a9ebf09ee16d
AV <27rurk@gmail.com>
2016-12-22 09:22:31 +0000

    autocommit on change

--- packages/-my/dnscrypt-proxy/package.mk
+++ packages/-my/dnscrypt-proxy/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="dnscrypt-proxy"
-PKG_VERSION="a325112"
+PKG_VERSION="bab85c2"
 PKG_GIT_URL="https://github.com/jedisct1/dnscrypt-proxy"
 PKG_DEPENDS_TARGET="toolchain systemd libsodium"
 PKG_SECTION="my"
@@ -10,15 +10,14 @@ pre_configure_target() {
   cd $ROOT/$PKG_BUILD
 }
 
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
-			   --datarootdir=/storage/.config/dnscrypt-proxy \
-			   --libdir=/storage/.config/dnscrypt-proxy/lib \
-			   --enable-static \
-			   --disable-shared \
-			   --enable-ssp \
-			   --with-gnu-ld \
-			   --with-systemd \
-			   --disable-plugins"
+PKG_CONFIGURE_OPTS_TARGET="--datarootdir=/storage/.config/dnscrypt-proxy \
+			      --libdir=/storage/.config/dnscrypt-proxy/lib \
+			      --enable-static \
+			      --disable-shared \
+			      --disable-ssp \
+			      --with-gnu-ld \
+			      --with-systemd \
+			      --disable-plugins"
 			   
 #post_makeinstall_target() {
 #  rm -rf $INSTALL/storage/
