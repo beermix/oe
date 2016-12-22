c4d5c6d76be028bad44a91a63d963d3bde530004
AV <27rurk@gmail.com>
2016-12-22 18:13:34 +0000

    autocommit on change

--- packages/-my/tg/package.mk
+++ packages/-my/tg/package.mk
@@ -1,36 +1,38 @@
 PKG_NAME="tg"
 PKG_VERSION="master"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain readline libevent jansson zlib libconfig openssl"
-
+PKG_DEPENDS_TARGET="toolchain readline libevent jansson zlib libconfig openssl lua"
 PKG_SECTION="debug/tools"
 PKG_AUTORECONF="yes"
 
 unpack() {
-  git clone --recursive https://github.com/vysheng/tg $PKG_BUILD
+  git clone --recursive -v --depth 1 https://github.com/vysheng/tg $PKG_BUILD
   cd $PKG_BUILD
   git reset --hard $PKG_VERSION
   rm -rf .git
   cd $ROOT
 }
+
 pre_configure_target() {
   cd $ROOT/$PKG_BUILD
   rm -rf .$TARGET_NAME
-  export LIBS="-lreadline -lterminfo"
+  export LIBS="-lreadline -lterminfo -llua -lm"
+  export LUA_LIBS="-L$SYSROOT_PREFIX/usr/lib -llua -lm"
 }
 
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
-			   ac_cv_func_realloc_0_nonnull=yes \
-			   --with-zlib=$ROOT/$TOOLCHAIN \
-			   --enable-openssl \
-			   --enable-json \
-			   --enable-static \
-			   --enable-libevent \
-			   --enable-libconfig \
-			   --enable-extf \
-			   --enable-python \
-			   --disable-liblua"
+			      ac_cv_func_realloc_0_nonnull=yes \
+			      --with-zlib=$ROOT/$TOOLCHAIN \
+			      --enable-openssl \
+			      --enable-json \
+			      --enable-static \
+			      --enable-libevent \
+			      --enable-libconfig \
+			      --enable-extf \
+			      --disable-python \
+			      --disable-valgrind \
+			      --enable-liblua"
 		
 		
 post_make_target() {
