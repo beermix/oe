bbae6d10b05dd3a82aba3030f6398297f456a86b
AV <27rurk@gmail.com>
2016-12-24 23:41:54 +0000

    autocommit on change

--- packages/-my/transmission/package.mk
+++ packages/-my/transmission/package.mk
@@ -1,16 +1,11 @@
 PKG_NAME="transmission"
-PKG_VERSION="f48c0cc"
+PKG_VERSION="3cda09e"
 PKG_GIT_URL="https://github.com/transmission/transmission"
 PKG_DEPENDS_TARGET="toolchain zlib openssl libpcap pcre libevent curl miniupnpc libdaemon"
 PKG_AUTORECONF="no"
 
-CONCURRENCY_MAKE_LEVEL=1
-strip_lto
-
 PKG_CMAKE_OPTS_TARGET="-DWITH_SYSTEMD=ON \
 			  -DINSTALL_LIB=OFF \
-			  -DCMAKE_C_FLAGS="$CFLAGS" \
-			  -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
 			  -DINSTALL_DOC=OFF \
 			  -DENABLE_TESTS=OFF \
 			  -DENABLE_UTP=ON \
