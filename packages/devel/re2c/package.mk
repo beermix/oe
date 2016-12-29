3a6510b24a59461ef8e949316809e6d646e9e893
AV <27rurk@gmail.com>
2016-12-29 14:31:22 +0000

    autocommit on change

--- packages/devel/re2c/package.mk
+++ packages/devel/re2c/package.mk
@@ -4,7 +4,7 @@ PKG_GIT_URL="https://github.com/skvadrik/re2c"
 PKG_DEPENDS_HOST="bison:host"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 post_unpack() {
   cp -r $PKG_BUILD/re2c/* $PKG_BUILD/
