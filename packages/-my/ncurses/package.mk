560a5d98a764ae12a1872db27d2036322a1adc8b
AV <27rurk@gmail.com>
2016-12-30 04:51:13 +0000

    autocommit on change

--- packages/-my/ncurses/package.mk
+++ packages/-my/ncurses/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ncurses"
-PKG_VERSION="6.0-20161210"
+PKG_VERSION="6.0-20161224"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
