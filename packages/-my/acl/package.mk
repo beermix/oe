a9d1fdb97b1ff46e753d0445abde5075e2a69850
AV <27rurk@gmail.com>
2016-12-30 01:04:47 +0000

    autocommit on change

--- packages/-my/acl/package.mk
+++ packages/-my/acl/package.mk
@@ -1,23 +1,8 @@
 PKG_NAME="acl"
-PKG_VERSION="2.2.52"
-PKG_URL="http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz"
+PKG_VERSION="ea3c6bb711e76d91759f8bf5475e1900362a3142"
+PKG_GIT_URL="git://git.sv.gnu.org/acl.git"
 PKG_DEPENDS_TARGET="toolchain attr"
-
 PKG_SECTION="tools"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  cd $ROOT/$PKG_BUILD
-  rm -rf .$TARGET_NAME
-  	#libtoolize --force && aclocal -I m4 && autoconf && autoheader
-	sed -i \
-		-e '/^as_dummy=/s:=":="$PATH$PATH_SEPARATOR:' \
-		configure # hack PATH with AC_PATH_PROG
-	sed -i \
-		-e "/^PKG_DOC_DIR/s:@pkg_name@:$pkgname:" \
-		-e '/HAVE_ZIPPED_MANPAGES/s:=.*:=false:' \
-		include/builddefs.in \
-		|| return 1
-}
-
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr --disable-shared --enable-static --disable-gettext"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
