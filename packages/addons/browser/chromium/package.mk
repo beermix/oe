af2b92dfdc2b671030cc6875c8823c749fde4b53
AV <27rurk@gmail.com>
2017-01-18 18:41:57 +0000

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -116,8 +116,8 @@ make_target() {
   ./tools/gn/bootstrap/bootstrap.py --gn-gen-args "${_flags[*]}"
   ./out/Release/gn gen out/Release --args="${_flags[*]}" --script-executable=$ROOT/$TOOLCHAIN/bin/python
 
-  ninja -j4 -C out/Release chrome chrome_sandbox
-
+  ninja -j3 -C out/Release chrome chrome_sandbox
+}
 makeinstall_target() {
   :
 }
