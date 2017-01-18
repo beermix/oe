cc3dc0d8b5b2c9f956a1063f53ebc6c0d02e9222
AV <27rurk@gmail.com>
2017-01-18 18:06:29 +0000

    autocommit on change

--- packages/-my/aircrack-ng/package.mk
+++ packages/-my/aircrack-ng/package.mk
@@ -1,13 +1,14 @@
 PKG_NAME="aircrack-ng"
-PKG_VERSION="0c70d5f"
+#PKG_VERSION="0c70d5f"
+PKG_VERSION="1.2-rc4"
 PKG_GIT_URL="https://github.com/aircrack-ng/aircrack-ng"
-PKG_KEEP_CHECKOUT="yes"
 PKG_DEPENDS_TARGET="toolchain openssl libdnet libnl libpcap openssl pcre libgpg-error usbutils iw"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 pre_configure_target() {
    export LDFLAGS="-ldl -lpthread -lsqlite3"
+   strip_lto
 }
 
 PKG_MAKE_OPTS_TARGET="prefix=/usr sqlite=true experimental=true ext_scripts=true pcre=true libnl=true"
