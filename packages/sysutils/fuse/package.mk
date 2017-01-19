a54b163d808285d9f073d4f92afd857f3801a7d6
AV <27rurk@gmail.com>
2017-01-19 01:37:54 +0000

    autocommit on change

--- packages/sysutils/fuse/package.mk
+++ packages/sysutils/fuse/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fuse"
-PKG_VERSION="2.9.7"
+PKG_VERSION="3.0.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -37,10 +37,6 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-lib \
                            --enable-mtab \
                            --disable-rpath \
                            --with-gnu-ld"
-pre_configure_target() {
-# fuse fails to build with GOLD linker on gcc-4.9
-  strip_gold
-}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/etc/init.d
