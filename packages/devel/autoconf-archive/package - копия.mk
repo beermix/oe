d4dbe32c8cc6848284de642211395b987cd16b7b
AV <27rurk@gmail.com>
2017-01-19 03:06:17 +0000

    autocommit on change

--- packages/devel/autoconf-archive/package - копия.mk
+++ packages/devel/autoconf-archive/package - копия.mk
@@ -16,26 +16,27 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="autoconf"
-PKG_VERSION="2.69"
+PKG_NAME="autoconf-archive"
+PKG_VERSION="2016.09.16"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://sources.redhat.com/autoconf/"
-PKG_URL="http://ftp.gnu.org/gnu/autoconf/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="ccache:host m4:host gettext:host libtool:host"
+PKG_SITE="http://ftp.gnu.org/gnu/autoconf-archive"
+PKG_URL="http://ftp.gnu.org/gnu/autoconf-archive/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_SECTION="toolchain/devel"
-PKG_SHORTDESC="autoconf: A GNU tool for automatically configuring source code"
-PKG_LONGDESC="Autoconf is an extensible package of m4 macros that produce shell scripts to automatically configure software source code packages. These scripts can adapt the packages to many kinds of UNIX-like systems without manual user intervention. Autoconf creates a configuration script for a package from a template file that lists the operating system features that the package can use, in the form of m4 macro calls."
+PKG_SHORTDESC="autoconf-archive: macros for autoconf"
+PKG_LONGDESC="autoconf-archive is an package of m4 macros"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="EMACS=no \
-                         ac_cv_path_M4=$ROOT/$TOOLCHAIN/bin/m4 \
-                         ac_cv_prog_gnu_m4_gnu=no \
-                         --target=$TARGET_NAME"
+PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME --prefix=$ROOT/$TOOLCHAIN"
 
-post_makeinstall_host() {
+makeinstall_host() {
+# make install
   make prefix=$SYSROOT_PREFIX/usr install
+
+# remove problematic m4 file
+  rm -rf $SYSROOT_PREFIX/usr/share/aclocal/ax_prog_cc_for_build.m4
 }
