810eca073f30464c55fc29edcf81571faba68071
AV <27rurk@gmail.com>
2017-01-19 00:49:02 +0000

    autocommit on change

--- packages/-my/transmission/package.mk
+++ packages/-my/transmission/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="transmission"
-PKG_VERSION="c1f08ed"
+PKG_VERSION="c199eef"
 PKG_GIT_URL="https://github.com/transmission/transmission"
 PKG_DEPENDS_TARGET="toolchain zlib openssl libpcap pcre libevent curl miniupnpc libdaemon"
 PKG_AUTORECONF="no"
@@ -10,6 +10,6 @@ PKG_CMAKE_OPTS_TARGET="-DWITH_SYSTEMD=ON \
 			  -DENABLE_TESTS=OFF \
 			  -DENABLE_UTP=ON \
 			  -DENABLE_DAEMON=ON \
-			  -DENABLE_CLI=OFF \
+			  -DENABLE_CLI=ON \
 			  -DUSE_SYSTEM_MINIUPNPC=YES \
 			  -DENABLE_LIGHTWEIGHT=OFF"
