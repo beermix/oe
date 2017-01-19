98d37c6606c1029c366a914754247904a8975bab
AV <27rurk@gmail.com>
2017-01-19 11:31:41 +0000

    autocommit on change

--- packages/-my/reaver/package.mk
+++ packages/-my/reaver/package.mk
@@ -5,28 +5,25 @@ PKG_URL="https://storage.googleapis.com/google-code-archive-downloads/v2/code.go
 PKG_DEPENDS_TARGET="toolchain sqlite libcap"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 post_unpack() {
   cp -r $PKG_BUILD/src/* $PKG_BUILD/
 }
 
-
 pre_configure_target() {
    cd $ROOT/$PKG_BUILD
    export MAKEFLAGS="-j1"
-   export LDFLAGS="-ldl -lpthread -lsqlite3"
+   export LDFLAGS="-ldl -lpthread -lsqlite3 -lpcap"
+   mkdir -p $INSTALL_DEV/usr/bin/
+   mkdir -p $INSTALL/usr/bin/
 }
 
-
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_pcap_pcap_open_live=yes \
-			   ac_cv_lib_sqlite3_sqlite3_open=yes \
-			   --prefix=/usr \
-			   --sysconfdir=/storage/.config \
-			   --datarootdir=/storage/.config"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_sqlite3_sqlite3_open=yes \
+			      --sysconfdir=/storage/.config \
+			      --datarootdir=/storage/.config"
 
 post_makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin/
-  #cp wash $INSTALL/usr/bin/
-  cp reaver $INSTALL/usr/bin/
+   cp wash $INSTALL/usr/bin/wash2
+   cp reaver $INSTALL/usr/bin/reaver2
 }
