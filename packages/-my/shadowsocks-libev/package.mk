02b583e73330f7ae5a5661fb7d3b6914d761f138
AV <27rurk@gmail.com>
2017-01-19 21:04:00 +0000

    autocommit on change

--- packages/-my/shadowsocks-libev/package.mk
+++ packages/-my/shadowsocks-libev/package.mk
@@ -1,16 +1,16 @@
 PKG_NAME="shadowsocks-libev"
 PKG_VERSION="v2.6.2"
 PKG_GIT_URL="https://github.com/shadowsocks/shadowsocks-libev"
-PKG_DEPENDS_TARGET="toolchain openssl pcre libsodium udns libev"
-#PKG_DEPENDS_TARGET="toolchain openssl pcre"
+#PKG_DEPENDS_TARGET="toolchain openssl pcre libsodium udns libev"
+PKG_DEPENDS_TARGET="toolchain openssl pcre"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
 			      --disable-ssp \
 			      --enable-silent-rules \
 			      --disable-documentation \
-			      --enable-system-shared-lib \
+			      --disable-system-shared-lib \
 			      --with-pcre=$SYSROOT_PREFIX/usr"
