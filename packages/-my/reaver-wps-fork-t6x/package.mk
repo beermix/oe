5ebaa8073013229fc30c2f96cb213e40b65f51ff
AV <27rurk@gmail.com>
2017-01-24 13:09:29 +0000

    autocommit on change

--- packages/-my/reaver-wps-fork-t6x/package.mk
+++ packages/-my/reaver-wps-fork-t6x/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="reaver-wps-fork-t6x"
-PKG_VERSION="707c574"
+PKG_VERSION="deefc6a"
 PKG_GIT_URL="https://github.com/t6x/reaver-wps-fork-t6x"
 PKG_DEPENDS_TARGET="toolchain sqlite libpcap"
 PKG_SECTION="my"
