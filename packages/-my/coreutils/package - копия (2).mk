fc0d8401ff3abe82ffe60e188c4e43fade5a3376
AV <27rurk@gmail.com>
2017-01-24 23:47:36 +0000

    autocommit on change

--- packages/-my/coreutils/package - копия (2).mk
+++ packages/-my/coreutils/package - копия (2).mk
@@ -1,14 +1,15 @@
 PKG_NAME="coreutils"
 PKG_VERSION="8.25"
 PKG_URL="http://ftpmirror.gnu.org/coreutils/coreutils-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain acl attr libcap readline gmp openssl"
+PKG_DEPENDS_TARGET="toolchain acl attr libcap readline openssl"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
-			      --with-gmp \
+			      --without-gmp \
 			      --enable-no-install-program=hostname,su,kill,uptime \
 			      --without-selinux \
 			      --with-openssl \
-			      --disable-rpath"
+			      --disable-rpath \
+			      --enable-install-program=cat,chgrp,chmod,chown,cp,date,dd"
