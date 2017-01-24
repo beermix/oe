294488f8ff1fd60ca9f7a9b99e9144e0ad1a9af3
AV <27rurk@gmail.com>
2017-01-24 19:22:56 +0000

    autocommit on change

--- packages/devel/libcec/package - копия.mk
+++ packages/devel/libcec/package - копия.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libcec.pulse-eight.com/"
 PKG_GIT_URL="https://github.com/Pulse-Eight/libcec"
-PKG_DEPENDS_TARGET="toolchain systemd lockdev platform"
+PKG_DEPENDS_TARGET="toolchain systemd lockdev p8-platform"
 PKG_SECTION="system"
 PKG_SHORTDESC="libCEC is an open-source dual licensed library designed for communicating with the Pulse-Eight USB - CEC Adaptor"
 PKG_LONGDESC="libCEC is an open-source dual licensed library designed for communicating with the Pulse-Eight USB - CEC Adaptor."
@@ -32,8 +32,10 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
-                       -DCMAKE_INSTALL_LIBDIR:STRING=lib \
-                       -DCMAKE_INSTALL_LIBDIR_NOARCH:STRING=lib"
+                       -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+                       -DCMAKE_INSTALL_LIBDIR_NOARCH=/usr/lib \
+                       -DCMAKE_INSTALL_PREFIX_TOOLCHAIN=$SYSROOT_PREFIX/usr \
+                       -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr"
 
 if [ "$KODIPLAYER_DRIVER" = "bcm2835-firmware" ]; then
   PKG_DEPENDS_TARGET+=" bcm2835-firmware"
