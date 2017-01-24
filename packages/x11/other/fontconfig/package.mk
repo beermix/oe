0985422e7f8e7b99a1ca8680d758fd051b2803d0
AV <27rurk@gmail.com>
2017-01-24 12:00:14 +0000

    autocommit on change

--- packages/x11/other/fontconfig/package.mk
+++ packages/x11/other/fontconfig/package.mk
@@ -21,14 +21,16 @@ PKG_VERSION="2.12.1"
 PKG_SITE="http://www.fontconfig.org"
 PKG_URL="http://www.freedesktop.org/software/fontconfig/release/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain util-macros freetype libxml2 zlib expat"
+PKG_PRIORITY="optional"
 PKG_SECTION="x11/other"
 PKG_SHORTDESC="fontconfig: A library for font customization and configuration"
 PKG_LONGDESC="Fontconfig is a library for font customization and configuration."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-arch=$TARGET_ARCH \
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static \
+                           --with-arch=$TARGET_ARCH \
                            --with-cache-dir=/storage/.cache/fontconfig \
                            --with-default-fonts=/usr/share/fonts \
                            --without-add-fonts \
