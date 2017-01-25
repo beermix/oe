ca5c33571e4cbd579ffe1c3efacb8adf747fd49b
AV <27rurk@gmail.com>
2017-01-25 00:43:39 +0000

    autocommit on change

--- packages/-my/libtorrent-rasterbar/package - копия.mk
+++ packages/-my/libtorrent-rasterbar/package - копия.mk
@@ -1,12 +1,12 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="d2f6104"
-PKG_GIT_URL="https://github.com/arvidn/libtorrent"
+PKG_VERSION="1.0.10"
+PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_0_10/libtorrent-rasterbar-1.0.10.tar.gz"
 PKG_DEPENDS_HOST="toolchain"
 PKG_DEPENDS_TARGET="toolchain openssl expat boost"
 PKG_SECTION="devel"
 PKG_USE_CMAKE="no"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 pre_configure_target() {
   cd $ROOT/$PKG_BUILD
