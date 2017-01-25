3bdc516be704ef57677fe29c512cfe49cadebc67
AV <27rurk@gmail.com>
2017-01-25 22:42:06 +0300

    autocommit on change

--- packages/-my/libtorrent-rasterbar/package.mk
+++ packages/-my/libtorrent-rasterbar/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="1.0.10"
-PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_0_10/libtorrent-rasterbar-1.0.10.tar.gz"
-#PKG_VERSION="d2f6104"
-#PKG_GIT_URL="https://github.com/arvidn/libtorrent"
+#PKG_VERSION="1.0.10"
+#PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_0_10/libtorrent-rasterbar-1.0.10.tar.gz"
+PKG_VERSION="d2f6104"
+PKG_GIT_URL="https://github.com/arvidn/libtorrent"
 PKG_DEPENDS_TARGET="toolchain openssl expat boost libiconv"
 PKG_SECTION="devel"
 PKG_USE_CMAKE="no"
@@ -10,12 +10,13 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 pre_configure_target() {
-  #cd $ROOT/$PKG_BUILD
-  #sh autotool.sh
+  cd $ROOT/$PKG_BUILD
+  sh autotool.sh
   #export CXXFLAGS="$CXXFLAGS -std=gnu++98"
-  #./autotool.sh
+  #sh $ROOT/$PKG_BUILD/autotool.sh
   #strip_lto
-  export CFLAGS="$CFLAGS -O3 -ffast-math"
+  strip_lto
+  export CFLAGS="$CFLAGS -O3 -pipe -ffast-math"
   sed -iv 's/$PKG_CONFIG openssl --libs-only-/$PKG_CONFIG openssl --static --libs-only-/' $ROOT/$PKG_BUILD/configure
   sed -iv -e s/Windows.h/windows.h/ -e s/Wincrypt.h/wincrypt.h/ $ROOT/$PKG_BUILD/ed25519/src/seed.cpp
 }
