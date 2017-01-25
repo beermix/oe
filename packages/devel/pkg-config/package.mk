28242ff5bf0181b73c9f5ec85ee313d3388ffb36
AV <27rurk@gmail.com>
2017-01-25 18:12:43 +0000

    autocommit on change

--- packages/devel/pkg-config/package.mk
+++ packages/devel/pkg-config/package.mk
@@ -30,9 +30,7 @@ PKG_LONGDESC="pkg-config is a system for managing library compile/link flags tha
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-silent-rules \
-                         --with-internal-glib --disable-dtrace \
-                         --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="--disable-silent-rules --with-internal-glib --disable-dtrace --with-gnu-ld"
 
 post_makeinstall_host() {
   mkdir -p $SYSROOT_PREFIX/usr/share/aclocal
