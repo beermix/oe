52c3679f1dc08ae855088abea85fcb9c4d12d7ec
AV <27rurk@gmail.com>
2017-01-25 02:37:02 +0000

    autocommit on change

--- packages/addons/dockerz/containerd/package - копия.mk
+++ packages/addons/dockerz/containerd/package - копия.mk
@@ -1,6 +1,5 @@
 PKG_NAME="containerd"
-#PKG_VERSION="v0.2.5"
-PKG_VERSION="03e5862"
+PKG_VERSION="v0.2.5"
 PKG_SITE="https://containerd.tools/"
 PKG_GIT_URL="https://github.com/docker/containerd"
 PKG_DEPENDS_HOST="toolchain go"
