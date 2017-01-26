aa9e9e871afeaab1582428a63b691326fe2e5f5d
AV <27rurk@gmail.com>
2017-01-26 15:27:18 +0000

    autocommit on change

--- packages/compress/tar/package.mk
+++ packages/compress/tar/package.mk
@@ -1,7 +1,8 @@
 PKG_NAME="tar"
 PKG_VERSION="1.29"
 PKG_URL="http://mirrors.kernel.org/gnu/tar/tar-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain pcre acl attr"
+#PKG_DEPENDS_TARGET="toolchain pcre acl attr"
+PKG_DEPENDS_TARGET="toolchain pcre"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
--- packages/sysutils/fuse/package.mk
+++ packages/sysutils/fuse/package.mk
@@ -35,6 +35,10 @@ PKG_CONFIGURE_OPTS_TARGET="MOUNT_FUSE_PATH=/usr/sbin \
                            --enable-mtab \
                            --disable-rpath \
                            --with-gnu-ld"
+pre_configure_target() {
+# fuse fails to build with GOLD linker on gcc-4.9
+  strip_gold
+}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/etc/init.d
