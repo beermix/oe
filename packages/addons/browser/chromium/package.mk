eea55a3454154e5838e09972ee6eba29daf1362d
AV <27rurk@gmail.com>
2017-01-26 08:14:08 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -113,7 +113,7 @@ make_target() {
   ./tools/gn/bootstrap/bootstrap.py --gn-gen-args "${_flags[*]}"
   ./out/Release/gn gen out/Release --args="${_flags[*]}" --script-executable=$ROOT/$TOOLCHAIN/bin/python
 
-  ninja -j4 -C out/Release chrome chrome_sandbox widevinecdmadapter
+  ninja -j6 -C out/Release chrome chrome_sandbox widevinecdmadapter
 }
 makeinstall_target() {
   :
