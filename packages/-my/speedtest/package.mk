c682126ab63b79577717c45318e3b57a5b6f5227
AV <27rurk@gmail.com>
2017-01-26 11:43:07 +0000

    autocommit on change

--- packages/-my/speedtest/package.mk
+++ packages/-my/speedtest/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="speedtest"
 PKG_VERSION="4ecabc8"
 PKG_GIT_URL="https://github.com/taganaka/SpeedTest"
-PKG_DEPENDS_TARGET="toolchain confuse"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_AUTORECONF="no"
 
 CONCURRENCY_MAKE_LEVEL=1
