7e178e70d1636e7701fffa0f0e78f2e1fd5becce
AV <27rurk@gmail.com>
2017-01-26 17:49:11 +0000

    autocommit on change

--- packages/-my/bash/oth/package - Copy.mk
+++ packages/-my/bash/oth/package - Copy.mk
@@ -2,7 +2,7 @@ PKG_NAME="bash"
 PKG_VERSION="4.3.48"
 PKG_URL="https://dl.dropboxusercontent.com/s/afsixaktnwv42b7/bash-4.3.48.tar.gz"
 #PKG_URL="ftp://ftp.cwru.edu/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain netbsd-curses"
+PKG_DEPENDS_TARGET="toolchain netbsd-curses readline"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
@@ -11,7 +11,6 @@ pre_configure_target() {
 }
 
 PKG_CONFIGURE_OPTS_TARGET="bash_cv_getenv_redef=no \
-                           bash_cv_job_control_missing=yes \
                            --enable-static \
                            --disable-shared \
                            --bindir=/bin \
@@ -37,8 +36,11 @@ PKG_CONFIGURE_OPTS_TARGET="bash_cv_getenv_redef=no \
                            --enable-array-variables \
                            --enable-glob-asciiranges-default \
                            --enable-restricted \
-                           --enable-job-control"   
-			   
+                           --enable-job-control"
+                           
+
 post_makeinstall_target() {
-  rm -rf $INSTALL/bin/bashbug
-}
+  ln -sf bash $INSTALL/bin/rbash
+  ln -sf bash $INSTALL/bin/sh
+}			   
+
