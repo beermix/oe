14c27f57e22c4dbcb0d220e258a2e41776b6e215
AV <27rurk@gmail.com>
2017-01-26 20:41:11 +0000

    autocommit on change

--- packages/-my/bash/package.mk
+++ packages/-my/bash/package.mk
@@ -9,14 +9,24 @@ pre_configure_target() {
   export LIBS="-lterminfo -ltermcap"
 }
 
-PKG_CONFIGURE_OPTS_TARGET="--bindir=/bin \
+PKG_CONFIGURE_OPTS_TARGET="bash_cv_getenv_redef=no \
+			      bash_cv_job_control_missing=yes \
+			      --enable-static \
+                           --disable-shared \
+                           --bindir=/bin \
                            --with-curses \
                            --enable-readline \
                            --without-bash-malloc \
                            --with-installed-readline \
-                           --disable-static-link"
+                           --disable-static-link \
+                           --disable-rpath \
+                           --cache-file=/dev/null \
+                           --enable-history \
+                           --enable-alias \
+                           --enable-job-control"
 			   
 post_makeinstall_target() {
   mkdir -p $INSTALL/bin
   ln -sfv bash $INSTALL/bin/rbash
+  ln -sfv bash $INSTALL/bin/sh
 }
