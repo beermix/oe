e61f66737db97c3c2fcb02478818ed2a8635ec1c
AV <27rurk@gmail.com>
2017-01-26 13:06:42 +0000

    autocommit on change

--- packages/-my/bash/package.mk
+++ packages/-my/bash/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="bash"
 PKG_VERSION="4.4"
 PKG_URL="ftp://ftp.cwru.edu/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain netbsd-curses"
+PKG_DEPENDS_TARGET="toolchain netbsd-curses readline"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
@@ -9,23 +9,17 @@ pre_configure_target() {
   export LIBS="-ltermcap -lcurses" 
 }
 
-PKG_CONFIGURE_OPTS_TARGET="bash_cv_getcwd_malloc=yes \
-			      bash_cv_sys_named_pipes=present \
-			      bash_cv_func_sigsetjmp=present \
-			      bash_cv_printf_a_format=yes \
-			      --bindir=/bin \
-			      --enable-static \
+PKG_CONFIGURE_OPTS_TARGET="--bindir=/bin \
+			      --disable-static \
                            --disable-shared \
                            --with-curses \
                            --without-bash-malloc \
                            --with-installed-readline \
-                           --enable-static-link \
-                           --disable-rpath \
                            --cache-file=/dev/null \
                            --enable-history \
                            --enable-alias \
                            --enable-job-control"   
-			   
+
 post_makeinstall_target() {
   rm -rf $INSTALL/bin/bashbug
   ln -sf /bin/bash $INSTALL/bin/sh
