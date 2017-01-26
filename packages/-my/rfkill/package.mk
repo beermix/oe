5b635de0de4998d4132585e2aa73e0aa80c22860
AV <27rurk@gmail.com>
2017-01-26 15:05:04 +0000

    autocommit on change

---
+++ packages/-my/rfkill/package.mk
@@ -0,0 +1,11 @@
+PKG_NAME="rfkill"
+PKG_VERSION="0.5"
+PKG_URL="https://dl.dropboxusercontent.com/s/dlkrm1b8iz48rwh/rfkill-0.5.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="libs"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  make CC="$CC" AR="$AR" LD="$LD" XCFLAGS="$CFLAGS" RANLIB="$RANLIB" XLDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS" -j1
+}
