ccde6d3ba8f24796dc2b6949aefcbd6edffa5662
AV <27rurk@gmail.com>
2017-02-02 05:07:36 +0000

    autocommit on change

--- packages/-my/unbound/package.mk
+++ packages/-my/unbound/package.mk
@@ -6,12 +6,18 @@ PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+#export LIBS="-lexpat"
+#export LDFLAGS="-s -lexpat"
+
+#PKG_CONFIGURE_OPTS_TARGET="--with-libunbound-only --disable-shared --enable-static --with-pthreads --with-gnu-ld"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-pthreads \
 			      --enable-static \
 			      --disable-shared \
+			      --with-gnu-ld \
 			      --sysconfdir=/storage/.config \
-			      --with-libevent=$SYSROOT_PREFIX/usr"
+			      --with-libevent=$SYSROOT_PREFIX/usr \
+			      --with-libunbound-only"
 #		
 #post_makeinstall_target() {
 #  rm -rf $INSTALL/storage
