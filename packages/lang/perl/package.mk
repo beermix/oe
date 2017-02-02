6ee5fb5399a15f1915fbbc875d0f107f3fca7b88
AV <27rurk@gmail.com>
2017-02-02 08:54:22 +0000

    autocommit on change

--- packages/lang/perl/package.mk
+++ packages/lang/perl/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="perl"
-PKG_VERSION="5.25.8"
+PKG_VERSION="5.24.1"
 PKG_URL="http://www.cpan.org/src/5.0/perl-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl gdbm"
 PKG_SECTION="my"
@@ -7,24 +7,23 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 configure_target() {
-  ./Configure -des -Duselargefiles \
-  		   -Duse64bitint \
-  		   -Dusethreads \
-  		   -Duseshrplib \
-  		   -Dprefix=/usr \
-  		   -Dvendorprefix=/usr \
-  		   -Dusedevel \
-  		   -Accflags="$CFLAGS -fPIC -DPIC" \
-  		   -Dld="$LD" \
-  		   -Dar="$AR" \
-  		   -Dcc="$CC" \
-  		   -Dldflags="$LDFLAGS -fPIC" \
-  		   -Dlibs="-lm -lcrypt -pthread" \
-  		   -Doptimize="-fno-stack-protector -O3 -ffunction-sections -fdata-sections -finline-limit=8 -ffast-math" \
-  		   
+  ./Configure -des \
+  		-Duselargefiles \
+  		-Duse64bitint \
+  		-Dusethreads \
+  		-Duseshrplib \
+  		-Dprefix=/usr \
+  		-Dvendorprefix=/usr \
+  		-Dusedevel \
+  		-Accflags="$CFLAGS -fPIC -DPIC" \
+  		-Dld="$LD" \
+  		-Dar="$AR" \
+  		-Dcc="$CC" \
+  		-Dldflags="$LDFLAGS -fPIC" \
+  		-Dlibs="-lm -lcrypt -pthread"
 }
 
                
 post_makeinstall_target() {
-  mv $INSTALL/usr/bin/perl5.25.5 $INSTALL/usr/bin/perl
+  ln -sf perl$PKG_VERSION $INSTALL/usr/bin/perl
 }
