e0e8252ff344783f15e2d724af96075c2c60d12e
AV <27rurk@gmail.com>
2017-02-02 00:46:47 +0000

    autocommit on change

--- packages/sysutils/libusb/package.mk
+++ packages/sysutils/libusb/package.mk
@@ -17,7 +17,8 @@
 ################################################################################
 
 PKG_NAME="libusb"
-PKG_VERSION="v1.0.21"
+#PKG_VERSION="v1.0.21"
+PKG_VERSION="ffdf3c0"
 PKG_SITE="http://libusb.info/"
 PKG_GIT_URL="https://github.com/libusb/libusb"
 PKG_DEPENDS_TARGET="toolchain systemd"
