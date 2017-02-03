cf96a5585ab6a0ce6f8d4564440892b6ca481682
AV <27rurk@gmail.com>
2017-02-03 04:27:19 +0000

    autocommit on change

--- packages/-my/wxPython/package.mk
+++ packages/-my/wxPython/package.mk
@@ -2,27 +2,17 @@ PKG_NAME="wxPython"
 PKG_VERSION="src-3.0.2.0"
 PKG_URL="https://sourceforge.net/projects/wxpython/files/wxPython/3.0.2.0/wxPython-src-3.0.2.0.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libX11 setuptools:host libpng libjpeg-turbo gtk+ pango gstreamer"
-
 PKG_SECTION="service/system"
 PKG_AUTORECONF="no"
 
+pre_configure_target() {
+  strip_gold
+}
 
- 
-PKG_CONFIGURE_OPTS_TARGET="--libdir=/usr/lib \
-			      --with-gtk=2 \
-			      --with-opengl \
-			      --enable-unicode \
-			      --enable-graphics_ctx \
-			      --enable-mediactrl \
-			      --disable-precomp-headers \
-			      --with-regex=sys \
+
+PKG_CONFIGURE_OPTS_TARGET="--with-regex=sys \
 			      --with-libpng=sys \
 			      --with-libxpm=sys \
 			      --with-libjpeg=sys \
 			      --with-libtiff=sys \
-			      --enable-static \
-			      --disable-shared"
-			     
-make_target() {
-  python2 setup.py WXPORT=gtk2 UNICODE=1 build
-} 
+			      --disable-mediactrl"
