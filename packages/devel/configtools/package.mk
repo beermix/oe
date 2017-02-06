852328839223a9c61fa180f822bfc87322fd22c4
AV <27rurk@gmail.com>
2017-02-06 18:20:15 +0000

    autocommit on change

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -17,8 +17,7 @@
 ################################################################################
 
 PKG_NAME="configtools"
-PKG_VERSION="f41aabd"
-#PKG_VERSION="6a82322"
+PKG_VERSION="6a82322"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
 PKG_GIT_URL="http://git.savannah.gnu.org/r/config.git"
 PKG_GIT_BRANCH="master"
