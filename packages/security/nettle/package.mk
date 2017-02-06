5b13fc281280ba822813f15fef423a37693af5a1
AV <27rurk@gmail.com>
2017-02-06 05:08:52 +0000

    autocommit on change

--- packages/security/nettle/package.mk
+++ packages/security/nettle/package.mk
@@ -2,17 +2,13 @@ PKG_NAME="nettle"
 PKG_VERSION="3.3"
 PKG_SITE="http://www.lysator.liu.se/~nisse/nettle"
 PKG_URL="https://ftp.gnu.org/gnu/nettle/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain gmp"
 PKG_PRIORITY="optional"
 PKG_SECTION="xmedia/depends"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-   export MAKEFLAGS="-j1"
-}
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --enable-mini-gmp"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --disable-mini-gmp"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
