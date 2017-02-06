e9cdec7e9464098ffdd4ee1810a55490f3782a7d
AV <27rurk@gmail.com>
2017-02-06 13:33:48 +0000

    autocommit on change

--- packages/devel/pkg-config/package.mk
+++ packages/devel/pkg-config/package.mk
@@ -30,7 +30,6 @@ PKG_LONGDESC="pkg-config is a system for managing library compile/link flags tha
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-MAKEFLAGS=-j1
 
 PKG_CONFIGURE_OPTS_HOST="--disable-silent-rules \
                          --with-internal-glib --disable-dtrace \
