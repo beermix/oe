146725d86b1512e6465133bd85f7cb157c4b3b42
AV <27rurk@gmail.com>
2017-02-06 06:01:46 +0000

    autocommit on change

--- packages/-my/lnav/package.mk
+++ packages/-my/lnav/package.mk
@@ -1,8 +1,17 @@
 PKG_NAME="lnav"
-PKG_VERSION="master"
+PKG_VERSION="c606e11"
 PKG_GIT_URL="https://github.com/tstack/lnav"
 PKG_DEPENDS_HOST="toolchain sqlite"
 PKG_SECTION="tools"
 PKG_USE_CMAKE="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
+CC_FOR_BUILD="$HOST_CC"
+CFLAGS_FOR_BUILD="$HOST_CFLAGS"
+
+pre_configure_target() {
+  # gdb could fail on runtime if build with LTO support
+    strip_lto
+    cd $ROOT/$PKG_BUILD
+    sh autogen.sh
+}
