dd0680732e5d7cb7bcc8d96670a37e43229803f8
AV <27rurk@gmail.com>
2017-02-06 09:23:13 +0000

    autocommit on change

--- packages/mediacenter/kodi-platform/package.mk
+++ packages/mediacenter/kodi-platform/package.mk
@@ -33,9 +33,7 @@ PKG_LONGDESC="kodi-platform:"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_INSTALL_LIBDIR:STRING=lib \
-                       -DCMAKE_INSTALL_LIBDIR_NOARCH:STRING=lib \
-                       -DCMAKE_INSTALL_PREFIX_TOOLCHAIN=$SYSROOT_PREFIX/usr \
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_MODULE_PATH=$SYSROOT_PREFIX/usr/share/kodi \
                        -DBUILD_SHARED_LIBS=0"
 
 post_makeinstall_target() {
