383860647a05a10b89fc1c9597b633da163a2b83
AV <27rurk@gmail.com>
2017-02-06 08:50:30 +0000

    autocommit on change

--- packages/devel/netbsd-curses/package.mk
+++ packages/devel/netbsd-curses/package.mk
@@ -17,9 +17,10 @@
 ################################################################################
 
 PKG_NAME="netbsd-curses"
-PKG_VERSION="22acb33"
-PKG_SITE="https://github.com/sabotage-linux/netbsd-curses"
-PKG_GIT_URL="git://github.com/sabotage-linux/netbsd-curses"
+PKG_VERSION="0.2.0"
+#PKG_VERSION="22acb33"
+#PKG_SITE="https://github.com/sabotage-linux/netbsd-curses"
+PKG_URL="http://ftp.barfooze.de/pub/sabotage/tarballs/netbsd-curses-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_SECTION="devel"
 PKG_SHORTDESC="netbsd-curses: netbsd-libcurses portable edition"
