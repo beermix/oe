8889bde7174433e0e9da4932fa20307749358b99
AV <27rurk@gmail.com>
2017-02-07 18:30:38 +0000

    autocommit on change

--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="make"
-PKG_VERSION="4.2.1"
+PKG_VERSION="4.1"
 PKG_SITE="https://www.gnu.org/software/make/"
 PKG_URL="https://ftp.gnu.org/gnu/make/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
