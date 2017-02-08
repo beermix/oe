81d42ce21dc79b8e3dd4eb4155a6292f2c3a8928
AV <27rurk@gmail.com>
2017-02-08 00:06:52 +0000

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -17,9 +17,9 @@
 ################################################################################
 
 PKG_NAME="binutils"
-PKG_VERSION="2.27.90.20170124"
+PKG_VERSION="2.27-patches"
 PKG_SITE="http://www.gnu.org/software/binutils/binutils.html"
-PKG_URL="https://dl.dropboxusercontent.com/s/pb38uifdaiy0d9o/binutils-2.27.90.20170124.tar.xz"
+PKG_URL="https://dl.dropboxusercontent.com/s/5727jbjbzu5em6r/binutils-2.27-patches.tar.xz"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="binutils: A GNU collection of binary utilities"
 PKG_LONGDESC="The GNU binutils are utilities of use when dealing with object files. the packages includes ld - the GNU linker, as - the GNU assembler, addr2line - converts addresses into filenames and line numbers, ar - a utility for creating, modifying and extracting from archives, c++filt - filter to demangle encoded C++ symbols, gprof - displays profiling information, nlmconv - converts object code into an NLM, nm - lists symbols from object files, objcopy - Copys and translates object files, objdump - displays information from object files, ranlib - generates an index to the contents of an archive, readelf - displays information from any ELF format object file, size - lists the section sizes of an object or archive file, strings - lists printable strings from files, strip - discards symbols as well as windres - a compiler for Windows resource files."
