e9e8633b427857b216f49fae430f5cd03944bb95
AV <27rurk@gmail.com>
2017-02-10 22:22:42 +0300

    autocommit on change

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -20,7 +20,7 @@ PKG_NAME="cmake"
 PKG_VERSION="3.7.2"
 PKG_SITE="http://www.cmake.org/"
 PKG_URL="http://www.cmake.org/files/v3.7/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host attr:host acl:host bzip2:host libuv:host"
+PKG_DEPENDS_HOST="ccache:host attr:host acl:host zlib:host bzip2:host libuv:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="cmake: A cross-platform, open-source make system"
 PKG_LONGDESC="CMake is used to control the software compilation process using simple platform and compiler independent configuration files. CMake generates native makefiles and workspaces that can be used in the compiler environment of your choice. CMake is quite sophisticated: it is possible to support complex environments requiring system configuration, preprocessor generation, code generation, and template instantiation."
@@ -31,8 +31,8 @@ configure_host() {
   ../configure --prefix=$ROOT/$TOOLCHAIN \
                --no-qt-gui --no-system-libs \
                -- \
-               -DCMAKE_C_FLAGS="-march=corei7-avx -O2 -Wall -pipe -Wno-format-security" \
-               -DCMAKE_CXX_FLAGS="-march=corei7-avx -O2 -Wall -pipe -Wno-format-security" \
+               -DCMAKE_C_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
+               -DCMAKE_CXX_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
                -DCMAKE_EXE_LINKER_FLAGS="$HOST_LDFLAGS" \
                -DCMAKE_USE_OPENSSL=ON \
                -DBUILD_CursesDialog=0
