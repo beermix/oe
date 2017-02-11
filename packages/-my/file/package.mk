2fdb8aea84ba828aa30d6657ea3a06edbb1522e2
AV <27rurk@gmail.com>
2017-02-11 07:14:19 +0300

    autocommit on change

--- packages/-my/file/package.mk
+++ packages/-my/file/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="file"
-PKG_VERSION="5.29"
+PKG_VERSION="5.30"
 PKG_SITE="http://www.darwinsys.com/file/"
 PKG_URL="ftp://ftp.astron.com/pub/file/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib attr expat file:host"
