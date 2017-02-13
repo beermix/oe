d29da1abf191d188987103952dee06d4a83c5ee6
AV <27rurk@gmail.com>
2017-02-13 19:47:14 +0300

    autocommit on change

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -20,7 +20,7 @@ PKG_NAME="readline"
 PKG_VERSION="7.0"
 PKG_SITE="http://www.gnu.org/software/readline/"
 PKG_URL="http://ftpmirror.gnu.org/readline/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain netbsd-curses"
+PKG_DEPENDS_TARGET="toolchain netbsd-curses libedit"
 PKG_SECTION="devel"
 PKG_SHORTDESC="readline: The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in."
 PKG_LONGDESC="The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in."
@@ -32,6 +32,7 @@ PKG_CONFIGURE_OPTS_TARGET="bash_cv_wcwidth_broken=no \
                            --disable-shared \
                            --enable-static \
                            --with-curses \
+                           --with-gnu-ld \
                            --without-purify"
 
 post_makeinstall_target() {
