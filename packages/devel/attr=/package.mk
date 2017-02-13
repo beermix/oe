b165065a50c99d78845354ad21677c74f5f77414
AV <27rurk@gmail.com>
2017-02-13 15:48:28 +0300

    autocommit on change

--- packages/devel/attr=/package.mk
+++ packages/devel/attr=/package.mk
@@ -18,18 +18,15 @@
 ################################################################################
 
 PKG_NAME="attr"
-PKG_VERSION="2.4.47"
-PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE=""
-PKG_URL="http://download.savannah.gnu.org/releases-noredirect/attr/$PKG_NAME-$PKG_VERSION.src.tar.gz"
+PKG_VERSION="315af30d0c1a198dc662a90e6e6ae2456abf2489"
+PKG_GIT_URL="git://git.savannah.nongnu.org/attr.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="accessibility"
 PKG_SHORTDESC="attr: Extended Attributes Of Filesystem Objects"
 PKG_LONGDESC="Extended attributes are name:value pairs associated permanently with files and directories, similar to the environment strings associated with a process. An attribute may be defined or undefined. If it is defined, its value may be empty or non-empty. Extended attributes are extensions to the normal attributes which are associated with all inodes in the system (i.e. the stat(2) data). They are often used to provide additional functionality to a filesystem - for example, additional security features such as Access Control Lists (ACLs) may be implemented using extended attributes."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="OPTIMIZER= \
                            CONFIG_SHELL=/bin/bash \
@@ -49,8 +46,8 @@ pre_configure_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $SYSROOT_PREFIX/usr/lib/
-    cp libattr/.libs/libattr.a $SYSROOT_PREFIX/usr/lib/
+  #mkdir -p $SYSROOT_PREFIX/usr/lib/
+  #  cp libattr/.libs/libattr.a $SYSROOT_PREFIX/usr/lib/
 
   mkdir -p $SYSROOT_PREFIX/usr/include/attr
     cp include/*.h $SYSROOT_PREFIX/usr/include/attr
