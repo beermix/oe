9bc6b9233446d73c54965b2a28d8c8590976311b
AV <27rurk@gmail.com>
2017-02-14 22:07:24 +0300

    autocommit on change

--- packages/devel/libffi/package.mk
+++ packages/devel/libffi/package.mk
@@ -18,9 +18,13 @@
 
 PKG_NAME="libffi"
 PKG_VERSION="3.2.1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
 PKG_SITE="http://sourceware.org/$PKG_NAME/"
 PKG_URL="ftp://sourceware.org/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="Foreign Function Interface Library"
 PKG_LONGDESC="The libffi library provides a portable, high level programming interface to various calling conventions. This allows a programmer to call any function specified by a call interface description at run-time."
@@ -35,6 +39,3 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-debug \
                            --enable-raw-api \
                            --disable-purify-safety \
                            --with-gnu-ld"
-
-
-#PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
--- packages/devel/make=/package.mk
+++ packages/devel/make=/package.mk
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
@@ -18,29 +16,21 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-. config/options $1
+PKG_NAME="make"
+PKG_VERSION="4.1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://www.gnu.org/software/make/"
+PKG_URL="https://ftp.gnu.org/gnu/make/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_HOST=""
+PKG_SECTION="toolchain/devel"
+PKG_SHORTDESC="make: GNU make utility to maintain groups of programs"
+PKG_LONGDESC="The 'make' utility automatically determines which pieces of a large program need to be recompiled, and issues commands to recompile them. This is GNU 'make', which was implemented by Richard Stallman and Roland McGrath. GNU 'make' conforms to section 6.2 of EEE Standard 1003.2-1992' (POSIX.2)."
 
-if [ -z "$1" ]; then
-  echo "usage: $0 package_name"
-  exit 1
-fi
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
-if [ ! -f $PKG_DIR/package.mk ]; then
-  printf "${boldred}$1: no package.mk file found${endcolor}\n"
-  exit 1
-fi
-
-STAMP=$PKG_BUILD/.openelec-unpack
-
-if [ -f $STAMP ] ; then
-  printf "%${BUILD_INDENT}c ${boldcyan}SKIP_UNPACK${endcolor}   $1\n" ' '>&$SILENT_OUT
-  export BUILD_INDENT=$((${BUILD_INDENT:-1}+$BUILD_INDENT_SIZE))
-
-  rm -f $STAMP
-  STAMP_DEPENDS="$PKG_DIR $PKG_NEED_UNPACK $PROJECT_DIR/$PROJECT/patches/$PKG_NAME"
-  PKG_DEEPMD5=$(find $STAMP_DEPENDS -exec md5sum {} \; 2>/dev/null | sort | md5sum | cut -d" " -f1)
-  for i in PKG_NAME PKG_DEEPMD5; do
-    eval val=\$$i
-    echo "STAMP_$i=\"$val\"" >> $STAMP
-  done
-fi
+post_makeinstall_host() {
+  ln -sf make $ROOT/$TOOLCHAIN/bin/gmake
+}
