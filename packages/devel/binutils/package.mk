0a381f04f979d3dfeb2d18681312900272e79952
AV <27rurk@gmail.com>
2017-02-18 04:11:35 +0300

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="binutils"
-PKG_VERSION="6c20b44"
+PKG_VERSION="749984d"
 #PKG_URL="https://fossies.org/linux/misc/binutils-$PKG_VERSION.tar.xz"
 PKG_GIT_URL="git://sourceware.org/git/binutils-gdb.git"
-PKG_GIT_BRANCH="binutils-2_28-branch"
+PKG_GIT_BRANCH="binutils-2_27-branch"
 PKG_DEPENDS_HOST="ccache:host bison:host flex:host linux:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="binutils: A GNU collection of binary utilities"
@@ -58,4 +58,5 @@ makeinstall_host() {
 
 #pre_configure_host() {
 #  sed -i "/ac_cpp=/s/\$CPPFLAGS/\$CPPFLAGS -O2/" $ROOT/$PKG_BUILD/libiberty/configure
+#
 #}
