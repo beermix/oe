c1a000036d7fc2aca257f2fc1f58263a81474132
AV <27rurk@gmail.com>
2017-02-18 09:18:49 +0300

    autocommit on change

--- packages/network/libnetfilter_queue/package.mk
+++ packages/network/libnetfilter_queue/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libnetfilter_queue"
-PKG_VERSION="981025"
-PKG_GIT_URL="git://git.netfilter.org/libnetfilter_queue"
+PKG_VERSION="1.0.2"
+PKG_URL="http://ftp.netfilter.org/pub/libnetfilter_queue/libnetfilter_queue-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libnfnetlink libnl"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
