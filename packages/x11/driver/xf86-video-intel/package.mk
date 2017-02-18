9856244a3cb4ebb9a47465e7c3d3ff8ab33b0b58
AV <27rurk@gmail.com>
2017-02-18 10:04:37 +0300

    autocommit on change

--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-video-intel"
-PKG_VERSION="93942b7"
+PKG_VERSION="860c366"
 PKG_ARCH="x86_64"
 PKG_SITE="http://intellinuxgraphics.org/"
 PKG_GIT_URL="git://anongit.freedesktop.org/xorg/driver/xf86-video-intel"
