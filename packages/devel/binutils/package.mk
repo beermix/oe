f38f3b6106f94dbbee7b32e499ac6864ad02f8d7
AV <27rurk@gmail.com>
2017-02-18 12:27:55 +0300

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -43,6 +43,8 @@ PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --enable-ld=default \
                          --enable-lto \
                          --disable-nls \
+                         --enable-static \
+                         --disable-shared \
                          --enable-poison-system-directories"
 
 makeinstall_host() {
