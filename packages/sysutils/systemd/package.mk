e41ecad6173d204fe324cc7fb247435bbfd101e8
AV <27rurk@gmail.com>
2017-02-18 16:14:57 +0300

    autocommit on change

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="systemd"
-PKG_VERSION="925c81c"
+PKG_VERSION="cc39016"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_GIT_URL="https://github.com/systemd/systemd"
 PKG_DEPENDS_TARGET="toolchain libcap kmod util-linux entropy acl expat xz zlib bzip2 lz4 openssl"
