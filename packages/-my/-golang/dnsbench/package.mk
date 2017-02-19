033209b63f0b3d8b79c163b2ef4fad38b04ae72b
AV <27rurk@gmail.com>
2017-02-19 20:17:45 +0300

    autocommit on change

--- packages/-my/-golang/dnsbench/package.mk
+++ packages/-my/-golang/dnsbench/package.mk
@@ -1,19 +1,20 @@
 PKG_NAME="dnsbench"
 PKG_VERSION="f2c5ef0"
 PKG_GIT_URL="https://github.com/LunaNode/dnsbench"
-PKG_DEPENDS_HOST="toolchain go"
+PKG_DEPENDS_TARGET="toolchain go:host"
 PKG_SECTION="system"
 PKG_SHORTDESC="containerd is a daemon to control runC"
 PKG_LONGDESC="containerd is a daemon to control runC, built for performance and density. containerd leverages runC's advanced features such as seccomp and user namespace support as well as checkpoint and restore for cloning and live migration of containers."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+strip_lto
 
 pre_make_target() {
   export GOOS=linux
   export GOARCH=amd64
-  export CGO_ENABLED=1
-  export CGO_NO_EMULATION=1
+  export CGO_ENABLED=0
+  export CGO_NO_EMULATION=0
   export CGO_CFLAGS=$CFLAGS
   export LDFLAGS="-s -w -linkmode external -extld $CC"
   export GOLANG=$ROOT/$TOOLCHAIN/lib/golang/bin/go
@@ -24,8 +25,7 @@ pre_make_target() {
 
 make_target() {
   mkdir -p bin
-  $GOLANG get -u -v github.com/LunaNode/dnsbench
-  $GOLANG build -v -o bin/dnsbench -a -tags "static_build" -ldflags "$LDFLAGS" .
+  go get -u -v -buildmode=exe -ldflags "-s -w"  github.com/LunaNode/dnsbench 
 }
 
 makeinstall_target() {
