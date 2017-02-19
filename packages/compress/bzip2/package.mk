711c9c8d09d839eb33d43d5fde136b0671670782
AV <27rurk@gmail.com>
2017-02-20 00:11:33 +0300

    autocommit on change

--- packages/compress/bzip2/package.mk
+++ packages/compress/bzip2/package.mk
@@ -59,21 +59,20 @@ pre_make_target() {
 
 make_target() {
   make -f Makefile libbz2.a bzip2 bzip2recover CC=$CC CFLAGS="$CFLAGS -fPIC -DPIC" LDFLAGS="-Wl,-z,relro" -j1
-  make -f Makefile-libbz2_so CC=$CC CFLAGS="$CFLAGS -fPIC -DPIC" LDFLAGS="-Wl,-z,relro" -j1
+  #make -f Makefile-libbz2_so CC=$CC CFLAGS="$CFLAGS -fPIC -DPIC" LDFLAGS="-Wl,-z,relro" -j1
 }
 
 makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib
-  
-  cp libbz2.a $SYSROOT_PREFIX/usr/lib
-  cp -P libbz2.so* $SYSROOT_PREFIX/usr/lib
-  cp bzlib.h $SYSROOT_PREFIX/usr/include
-  
-  
   mkdir -p $INSTALL/usr/bin
   mkdir -p $INSTALL/usr/lib
   
+  cp libbz2.a $SYSROOT_PREFIX/usr/lib
+  
+  #cp -P libbz2.so* $SYSROOT_PREFIX/usr/lib
+  #cp bzlib.h $SYSROOT_PREFIX/usr/include
+  #cp -P libbz2.so* $INSTALL/usr/lib
+  
   cp bzip2 $INSTALL/usr/bin
   cp bzip2recover $INSTALL/usr/bin
-  cp -P libbz2.so* $INSTALL/usr/lib
 }
