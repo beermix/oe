b847eaae7e91643c68cd44fa125c7a9b6157f224
AV <27rurk@gmail.com>
2017-02-19 06:51:35 +0300

    autocommit on change

---
+++ packages/security/gnutls/package - копия (2).mk
@@ -0,0 +1,36 @@
+PKG_NAME="gnutls"
+PKG_VERSION="3.5.9"
+PKG_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain libidn nettle gmp openssl"
+PKG_SECTION="security"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-ld-version-script \
+			      --enable-static \
+			      --without-tpm \
+			      --disable-heartbeat-support \
+			      --disable-shared \
+			      --enable-hardware-acceleration \
+			      --enable-openssl-compatibility \
+			      --enable-cxx \
+			      --without-p11-kit \
+			      --enable-local-libopts \
+			      --with-included-libtasn1 \
+			      --with-included-unistring \
+			      --disable-libdane \
+			      --disable-doc \
+			      --disable-guile \
+			      --enable-tools \
+			      --disable-openpgp-authentication \
+			      --disable-rpath \
+			      --without-nettle-mini \
+			      --with-gnu-ld \
+			      --disable-valgrind-tests \
+			      --without-lzo \
+			      --enable-silent-rules"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -1,35 +1,21 @@
 PKG_NAME="gnutls"
-PKG_VERSION="3.5.9"
+PKG_VERSION="3.4.14"
 PKG_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libidn nettle gmp openssl"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="security"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-ld-version-script \
-			      --enable-static \
-			      --without-tpm \
-			      --disable-heartbeat-support \
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_vfork_works=no 
+			      ac_cv_func_fork=no \
 			      --disable-shared \
-			      --enable-hardware-acceleration \
-			      --enable-openssl-compatibility \
-			      --enable-cxx \
 			      --without-p11-kit \
-			      --enable-local-libopts \
+			      --disable-nls \
 			      --with-included-libtasn1 \
-			      --with-included-unistring \
-			      --disable-libdane \
+			      --enable-local-libopts \
 			      --disable-doc \
-			      --disable-guile \
-			      --enable-tools \
-			      --disable-openpgp-authentication \
-			      --disable-rpath \
-			      --without-nettle-mini \
-			      --with-gnu-ld \
-			      --disable-valgrind-tests \
-			      --without-lzo \
-			      --enable-silent-rules"
+			      --disable-tests"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
