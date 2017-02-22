a40172bbf0a660d3f3bdc7cf33d62e3db999032c
AV <27rurk@gmail.com>
2017-02-22 19:38:59 +0300

    autocommit on change

--- packages/-my/smartmontools/package.mk
+++ packages/-my/smartmontools/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="smartmontools"
-PKG_VERSION="993e781"
+PKG_VERSION="cd4ac41"
 PKG_GIT_URL="https://github.com/mirror/smartmontools"
 PKG_DEPENDS_TARGET="toolchain netbsd-curses"
 PKG_SECTION="my"
