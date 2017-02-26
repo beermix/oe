13f0e3624b01d8294250c2150a8e4c509de30361
AV <27rurk@gmail.com>
2017-02-26 12:52:59 +0300

    autocommit on change

--- packages/devel/pkgconf/package.mk
+++ packages/devel/pkgconf/package.mk
@@ -3,10 +3,14 @@ PKG_VERSION="1.3.0"
 PKG_URL="https://distfiles.dereferenced.org/pkgconf/pkgconf-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host gettext:host autotools:host"
 PKG_SECTION="toolchain/devel"
+
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 
-post_makeinstall_host() {
-  ln -sfv pkgconf $ROOT/$TOOLCHAIN/bin/pkg-config
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
+
+
+#post_makeinstall_host() {
+#  ln -sfv pkgconf $ROOT/$TOOLCHAIN/bin/pkg-config
 }
