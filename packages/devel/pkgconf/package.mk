a142be4073fe2ba50bfb20ede2ab8524ee1a5ce6
AV <27rurk@gmail.com>
2017-02-26 12:57:25 +0300

    autocommit on change

--- packages/devel/pkgconf/package.mk
+++ packages/devel/pkgconf/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="pkgconf"
 PKG_VERSION="1.3.0"
 PKG_URL="https://distfiles.dereferenced.org/pkgconf/pkgconf-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="ccache:host gettext:host autotools:host"
+PKG_DEPENDS_HOST="ccache:host gettext:host"
 PKG_SECTION="toolchain/devel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
