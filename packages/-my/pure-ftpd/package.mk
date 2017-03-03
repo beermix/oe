66ca7dce1c56c9c9cd2c20a1a71e9eec1484dc54
AV <27rurk@gmail.com>
2017-03-04 00:09:59 +0300

    autocommit on change

--- packages/-my/pure-ftpd/package.mk
+++ packages/-my/pure-ftpd/package.mk
@@ -4,11 +4,10 @@ PKG_URL="ftp://ftp.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-$PKG_VERSION.ta
 PKG_DEPENDS_TARGET="toolchain libsodium libevent"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
-			      --with-gnu-ld \
 			      --disable-ssp \
 			      --with-nonroot \
 			      --with-rfc2640 \
