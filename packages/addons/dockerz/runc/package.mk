d1551b74393e6bd0a362ed48ae4e222594a8f13d
AV <27rurk@gmail.com>
2017-03-11 22:12:51 +0300

    autocommit on change

--- packages/addons/dockerz/runc/package.mk
+++ packages/addons/dockerz/runc/package.mk
@@ -17,11 +17,11 @@
 ################################################################################
 
 PKG_NAME="runc"
-PKG_VERSION="a01dafd48bc1c7cc12bdb01206f9fea7dd6feb70"
+PKG_VERSION="a01dafd"
 PKG_ARCH="any"
 PKG_LICENSE="APL"
 PKG_SITE="https://github.com/opencontainers/runc"
-PKG_URL="https://dl.dropboxusercontent.com/s/f28uqmljdwni5b7/runc-a01dafd48bc1c7cc12bdb01206f9fea7dd6feb70.tar.gz"
+PKG_URL="https://dl.dropboxusercontent.com/s/1ub8umc6a75ej1k/runc-a01dafd.tar.xz"
 PKG_DEPENDS_TARGET="toolchain go:host"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
