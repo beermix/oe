e1a06c19a873ebe7b9c17d217a8ae488f5efa2de
AV <27rurk@gmail.com>
2017-03-11 22:48:36 +0300

    autocommit on change

--- packages/-my/fping/package.mk
+++ packages/-my/fping/package.mk
@@ -1,8 +1,7 @@
 PKG_NAME="fping"
-PKG_VERSION="0f36ddd"
+PKG_VERSION="3.16"
 PKG_GIT_URL="https://github.com/schweikert/fping"
 PKG_DEPENDS_TARGET="toolchain libnfnetlink libnl" 
-
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
