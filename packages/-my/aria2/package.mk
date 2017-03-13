976b8abacd8e5f6cc6334497fc31f9d6db1671cd
AV <27rurk@gmail.com>
2017-03-13 06:08:52 +0300

    autocommit on change

--- packages/-my/aria2/package.mk
+++ packages/-my/aria2/package.mk
@@ -1,16 +1,12 @@
 PKG_NAME="aria2"
-PKG_VERSION="1.31.0"
-PKG_URL="https://github.com/aria2/aria2/releases/download/release-$PKG_VERSION/aria2-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain expat gmp pcre curl libuv libev openssl xmlstarlet libxml2"
+PKG_VERSION="release-1.31.0"
+PKG_GIT_URL="https://github.com/aria2/aria2"
+PKG_DEPENDS_TARGET="toolchain expat gmp pcre curl libuv libev openssl xmlstarlet libxml2 libssh2"
 PKG_SECTION="tools"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-                           --disable-option-checking \
                            --disable-shared \
-                           --disable-nls \
-                           --with-gnu-ld \
-                           --with-sysroot=$SYSROOT_PREFIX \
                            --sysconfdir=/storage/.config \
                            --datadir=/storage/.config \
                            --libdir=/storage/.config \
@@ -19,8 +15,7 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --with-openssl \
                            --disable-ipv6 \
                            --with-libgmp \
-                           --enable-gnutls-system-crypto-policy
-                           --without-libssh2 \
+                           --with-libssh2 \
                            --with-libexpat \
                            --with-zlib \
                            --with-libz \
@@ -30,4 +25,5 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --with-xmltest \
                            --with-libxml2 \
                            --without-libcares \
-                           LT_SYS_LIBRARY_PATH=$SYSROOT_PREFIX/lib:$SYSROOT_PREFIX/usr/lib"
+                           --with-libexpat \
+                           --enable-largefile"
