dfc1cff7824aa38e3c53c188ce3ff20c729a27ff
AV <27rurk@gmail.com>
2017-03-20 19:34:37 +0300

    autocommit on change

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="libpng"
-PKG_VERSION="1.6.28"
+PKG_VERSION="1.6.29"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.libpng.org/"
-PKG_URL="$SOURCEFORGE_SRC/libpng/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
