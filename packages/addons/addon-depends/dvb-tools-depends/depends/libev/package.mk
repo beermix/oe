a7d4580066f6dbf1a91cc393390bf54fe271282c
AV <27rurk@gmail.com>
2017-03-20 22:24:31 +0300

    autocommit on change

--- packages/addons/addon-depends/dvb-tools-depends/depends/libev/package.mk
+++ packages/addons/addon-depends/dvb-tools-depends/depends/libev/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libev"
-PKG_VERSION="4.22"
+PKG_VERSION="4.24"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://software.schmorp.de/pkg/libev.html"
@@ -28,6 +28,6 @@ PKG_SHORTDESC="libev: a full-featured and high-performance event loop"
 PKG_LONGDESC="A full-featured and high-performance event loop that is loosely modelled after libevent, but without its limitations and bugs."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
