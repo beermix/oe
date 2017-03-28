8468b7b25178e68d2240885ec7222240e1b2297b
AV <27rurk@gmail.com>
2017-03-28 05:38:35 +0300

    autocommit on change

--- packages/addons/addon-depends/system-tools-depends/mrxvt/package.mk
+++ packages/addons/addon-depends/system-tools-depends/mrxvt/package.mk
@@ -21,7 +21,7 @@ PKG_VERSION="0.5.4"
 PKG_ARCH="i386 x86_64"
 PKG_SITE="http://materm.sourceforge.net/"
 PKG_URL="$SOURCEFORGE_SRC/materm/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libX11"
+PKG_DEPENDS_TARGET="toolchain libX11 libXpm libXrender libXau fontconfig freetype libICE"
 PKG_SECTION="tools"
 PKG_SHORTDESC="mrxvt: Lightweight Xterm replacement"
 PKG_LONGDESC="Mrxvt (previously named as materm) is a lightweight and powerful multi-tabbed X terminal emulator based on the popular rxvt and aterm. It implements many useful features seen in some modern X terminal emulators, like gnome-terminal and konsole, but keep to be lightweight and independent from the GNOME and KDE desktop environment."
@@ -29,14 +29,14 @@ PKG_AUTORECONF="yes"
 
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_setpgrp_void=no \
-			      --enable-minimal \
+			      --disable-minimal \
 			      --disable-frills \
 			      --enable-keepscrolling \
 			      --disable-selectionscrolling \
 			      --enable-mousewheel \
 			      --disable-mouseslipwheel \
 			      --enable-rxvt-scroll \
-			      --disable-half-shadow \
+			      --enable-half-shadow \
 			      --enable-lastlog \
 			      --enable-sessionmgr \
 			      --enable-linespace \
@@ -44,14 +44,14 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_setpgrp_void=no \
 			      --enable-256colors \
 			      --enable-cursor-blink \
 			      --enable-pointer-blank \
-			      --disable-text-shadow \
-			      --disable-menubar \
+			      --enable-text-shadow \
+			      --enable-menubar \
 			      --disable-transparency \
 			      --disable-tinting \
 			      --disable-xrender \
-			      --disable-xpm \
-			      --disable-jpeg \
-			      --disable-png \
+			      --enable-xpm \
+			      --enable-jpeg \
+			      --enable-png \
 			      --disable-xft \
 			      --enable-ttygid \
 			      --enable-backspace-key \
