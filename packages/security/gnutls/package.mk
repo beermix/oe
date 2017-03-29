125755b543b24fff3f05b5c5093462029ad08fbd
AV <27rurk@gmail.com>
2017-03-29 04:21:28 +0300

    autocommit on change

--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -1,14 +1,14 @@
 PKG_NAME="gnutls"
 PKG_VERSION="3.5.10"
-PKG_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain nettle gmp"
 PKG_SECTION="security"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_vfork_works=no \
-				ac_cv_func_fork=no \
+			      ac_cv_func_fork=no \
 				--disable-shared \
 				--without-p11-kit \
 				--disable-nls \
