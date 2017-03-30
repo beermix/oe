19cc2a6eb9c83d5a5a1a3a3093c29b5e7219ac98
AV <27rurk@gmail.com>
2017-03-30 19:09:48 +0300

    autocommit on change

--- packages/security/openssl-old/package.mk
+++ packages/security/openssl-old/package.mk
@@ -1,12 +1,15 @@
 PKG_NAME="openssl"
-PKG_VERSION="1.1.0e"
+PKG_VERSION="1.0.2k"
 PKG_URL="https://www.openssl.org/source/openssl-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain zlib pcre"
+PKG_DEPENDS_TARGET="toolchain zlib pcre gmp"
 PKG_SECTION="security"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-MAKEFLAGS=-j1
+pre_configure_target() {
+  export MAKEFLAGS="-j1"
+  sed -i -e '/^"linux-x86_64"/ s/-m64 -DL_ENDIAN -O3 -Wall/-m64 -DL_ENDIAN -O2 -pipe -Wall -fstack-protector-strong -Wa,--noexecstack/' $ROOT/$PKG_BUILD/Configure
+}
 
 configure_target() {
   ./Configure --prefix=/usr \
@@ -24,11 +27,11 @@ configure_target() {
               enable-ec \
               enable-ecdh \
               enable-ecdsa \
-              no-rc5 \
               no-ssl3-method \
+              no-idea \
               no-heartbeats \
               enable-ec_nistp_64_gcc_128 \
-              debian-amd64
+              linux-x86_64
 }
 
 make_target() {
@@ -39,28 +42,27 @@ make_target() {
        RANLIB="$RANLIB" \
        XLDFLAGS="$LDFLAGS" \
        MAKEDEPPROG="$CC" \
-       all -j1
+       depend all build-shared
 }
 
 makeinstall_target() {
-  make -f Makefile install_sw DESTDIR=$SYSROOT_PREFIX
-  make -f Makefile DESTDIR=$INSTALL install_sw
+  make INSTALL_PREFIX=$SYSROOT_PREFIX install_sw
+  make INSTALL_PREFIX=$INSTALL install_sw
   chmod 755 $INSTALL/usr/lib/*.so*
-  #chmod 755 $INSTALL/usr/lib/engines/*.so
+  chmod 755 $INSTALL/usr/lib/engines/*.so
 }
 
 post_makeinstall_target() {
-  #rm -rf $INSTALL/etc/pki/tls/misc
-  #rm -rf $INSTALL/usr/bin/c_rehash
+  rm -rf $INSTALL/etc/pki/tls/misc
+  rm -rf $INSTALL/usr/bin/c_rehash
   #$STRIP $INSTALL/usr/bin/openssl
   
 # ca-certification: provides a tool to download and create ca-bundle.crt
 # download url: http://curl.haxx.se
 # create new cert: perl ./mk-ca-bundle.pl
-  #mkdir -p $INSTALL/$SSL_CERTIFICATES
   mkdir -p $INSTALL/etc/ssl
-  #perl $PKG_DIR/cert/mk-ca-bundle.pl
-  #cp $ROOT/$PKG_BUILD/ca-bundle.crt $INSTALL/etc/ssl/cert.pem
+  perl $PKG_DIR/cert/mk-ca-bundle.pl
+  cp $ROOT/$PKG_BUILD/ca-bundle.crt $INSTALL/etc/ssl/cert.pem
     
-  cp $PKG_DIR/cert/ca-bundle.crt $INSTALL/etc/ssl/cert.pem
+  #cp $PKG_DIR/cert/ca-bundle.crt $INSTALL/etc/ssl/cert.pem
 }
