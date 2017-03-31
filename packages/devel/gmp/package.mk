891c6e775fcc1cde7e3665e1aab5dbb616df39bf
AV <27rurk@gmail.com>
2017-03-31 17:01:02 +0300

    autocommit on change

--- packages/devel/gmp/package.mk
+++ packages/devel/gmp/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ PKG_SHORTDESC="gmp: The GNU MP (multiple precision arithmetic) library"
 PKG_LONGDESC="GNU MP is a library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating point numbers. It has a rich set of functions, and the functions have a regular interface. GNU MP is designed to be as fast as possible, both for small operands and for huge operands. The speed is achieved by using fullwords as the basic arithmetic type, by using fast algorithms, by carefully optimized assembly code for the most common inner loops for a lots of CPUs, and by a general emphasis on speed (instead of simplicity or elegance). The speed of GNU MP is believed to be faster than any other similar library. The advantage for GNU MP increases with the operand sizes for certain operations, since GNU MP in many cases has asymptotically faster algorithms."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 pre_configure_host() {
   export LDFLAGS="$LDFLAGS -Wl,-Bsymbolic-functions"
@@ -38,6 +38,6 @@ pre_configure_target() {
   export CPPFLAGS="$CPPFLAGS -fexceptions"
 }
 
-PKG_CONFIGURE_OPTS_HOST="--enable-cxx --enable-static --disable-shared  --with-pic"
+PKG_CONFIGURE_OPTS_HOST="--enable-cxx --enable-static --disable-shared --with-pic"
 
 PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -18,12 +18,7 @@
 
 PKG_NAME="make"
 PKG_VERSION="4.2.1"
-PKG_REV="1"
-PKG_ARCH="any"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://www.gnu.org/software/make/"
 PKG_URL="https://ftp.gnu.org/gnu/make/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_HOST=""
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="make: GNU make utility to maintain groups of programs"
