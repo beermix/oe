ad195bd28d621f112bf02187c153eab5c7c33391
AV <27rurk@gmail.com>
2017-04-01 11:45:22 +0300

    autocommit on change

---
+++ packages/audio/libtheora/package.mk
@@ -0,0 +1,8 @@
+PKG_NAME="libtheora"
+PKG_VERSION="1.2.0alpha1"
+PKG_URL="http://downloads.xiph.org/releases/theora/libtheora-1.2.0alpha1.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --disable-examples"
---
+++ packages/audio/oggfwd/package.mk
@@ -0,0 +1,23 @@
+PKG_NAME="oggfwd"
+PKG_VERSION="506088e"
+PKG_GIT_URL="https://r-w-x.org/oggfwd.git"
+PKG_DEPENDS_TARGET="toolchain libshout libogg libvorbis libtheora"
+PKG_SECTION="tools"
+PKG_AUTORECONF="no"
+
+make_target() {
+  strip_lto
+  make CC="$CC" CXX="$CXX" AR="$AR" CFLAGS="-O2 -pipe -fstack-protector-strong -DDEBUG=0" CPPFLAGS="$CPPFLAGS" -j1
+}
+
+
+post_make_target() {
+  mkdir -p $INSTALL/usr/sbin/
+  mkdir -p $INSTALL_DEV/usr/sbin/
+  cp $ROOT/$PKG_BUILD/ps3remote $INSTALL/usr/sbin/
+}
+
+
+makeinstall_target() {
+  :
+}
