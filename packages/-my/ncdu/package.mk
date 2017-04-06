097e974ec243dab5d195215d9f301f8751115b2d
AV <27rurk@gmail.com>
2017-04-06 04:57:03 +0300

    autocommit on change

--- packages/-my/ncdu/package.mk
+++ packages/-my/ncdu/package.mk
@@ -5,10 +5,6 @@ PKG_URL="https://dev.yorhel.nl/download/ncdu-$PKG_VERSION.tar.gz"
 #PKG_GIT_URL="git://g.blicky.net/ncdu.git"
 PKG_DEPENDS_TARGET="toolchain netbsd-curses readline"
 PKG_SECTION="tools"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
-pre_configure_target() {
- export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
-}
-
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr --enable-largefile"
+PKG_CONFIGURE_OPTS_TARGET="--enable-largefile"
