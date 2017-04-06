141b89850e67aef053f5edd62cd2d4f24c945ba8
AV <27rurk@gmail.com>
2017-04-06 21:57:07 +0300

    autocommit on change

--- packages/-my/DisplayCAL/package.mk
+++ packages/-my/DisplayCAL/package.mk
@@ -1,10 +1,10 @@
 PKG_NAME="DisplayCAL"
 PKG_VERSION="3.2.1.0"
 PKG_SITE="http://www.X.org"
-PKG_URL="https://sourceforge.net/projects/dispcalgui/files/release/3.2.1.0/DisplayCAL-3.2.1.0.tar.gz"
+PKG_URL="https://sourceforge.net/projects/dispcalgui/files/release/$PKG_VERSION/DisplayCAL-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo"
-
 PKG_SECTION="service/system"
+
 PKG_AUTORECONF="no"
 
 
