e0e6b3220a91d1a8059a3d804837a1492c2588ab
AV <27rurk@gmail.com>
2017-04-07 22:25:43 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -100,10 +100,8 @@ pre_configure_target() {
 
   unset LD_LIBRARY_PATH
 
-  # set some CFLAGS we need
+# set some CFLAGS we need
   export CFLAGS="$CFLAGS -g"
-
-  export BUILD_CC=$HOST_CC
   export OBJDUMP_FOR_HOST=objdump
 
 cat >config.cache <<EOF
