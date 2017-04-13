5a53a0014bbb205ae220256c2ea891bbe62930d9
AV <27rurk@gmail.com>
2017-04-13 11:52:54 +0300

    autocommit on change

--- packages/network/aircrack-ng/package.mk
+++ packages/network/aircrack-ng/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="aircrack-ng"
-PKG_VERSION="4ffc6f7"
+PKG_VERSION="1.2-rc3"
 PKG_GIT_URL="https://github.com/aircrack-ng/aircrack-ng"
 PKG_DEPENDS_TARGET="toolchain openssl libdnet libnl libpcap openssl pcre libgpg-error usbutils iw"
 PKG_IS_ADDON="no"
