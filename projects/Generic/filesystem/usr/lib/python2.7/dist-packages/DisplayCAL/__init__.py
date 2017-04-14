a060c8854860d801d1989fdd836746c139d84e91
AV <27rurk@gmail.com>
2017-04-15 00:58:18 +0300

    autocommit on change

--- projects/Generic/filesystem/usr/lib/python2.7/dist-packages/DisplayCAL/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-try:
-	import __version__
-except ImportError:
-	__version__ = "0.0.0.0"
-else:
-	__version__ = __version__.VERSION_STRING
--- projects/Generic/filesystem/usr/lib/python2.7/dist-packages/DisplayCAL/tempfile.py
+++
@@ -1,6 +0,0 @@
-# WRAPPER FOR PYTHON 2.5
-# This module exists only to make sure that other module's imports of tempfile
-# end up with a version that has the SpooledTemporaryFile class (introduced in
-# Python 2.6)
-
-from tempfile26 import *
