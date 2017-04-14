39618d68ca8d438bc92d9e21ffe9cbfa2a6e4ebc
AV <27rurk@gmail.com>
2017-04-14 17:00:03 +0300

    autocommit on change

--- packages/-my/MPlayer/package.mk
+++ packages/-my/MPlayer/package.mk
@@ -1,14 +1,18 @@
 PKG_NAME="MPlayer"
-PKG_VERSION="ch0414"
-PKG_URL="https://dl.dropboxusercontent.com/s/3ht4a92bbgk5zj0/MPlayer-ch0414.tar.xz"
+PKG_VERSION="1.3.0"
+PKG_URL="ftp://ftp.mplayerhq.hu/MPlayer/releases/MPlayer-1.3.0.tar.xz"
 #PKG_SOURCE_DIR="mplayer-export-2017-04-14"
 PKG_DEPENDS_TARGET="toolchain ffmpeg faad2"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-configure_target() {
+pre_configure_target() {
   cd $ROOT/$PKG_BUILD
   strip_lto
+  strip_gold
+}
+
+configure_target() {  
   ./configure --enable-runtime-cpudetection \
   		--disable-inet6 \
   		--enable-big-endian \
