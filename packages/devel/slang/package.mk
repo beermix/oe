d50d99847caa1f0c3f2e58d5d2c975a8ff07fb8b
AV <27rurk@gmail.com>
2017-04-15 09:31:14 +0300

    autocommit on change

--- packages/devel/slang/package.mk
+++ packages/devel/slang/package.mk
@@ -1,3 +1,21 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 PKG_NAME="slang"
 PKG_VERSION="e51a17c"
 PKG_REV="1"
@@ -6,48 +24,35 @@ PKG_LICENSE="LGPL"
 PKG_SITE="http://www.jedsoft.org/slang/"
 PKG_GIT_URL="git://git.jedsoft.org/git/slang.git"
 PKG_DEPENDS_TARGET="toolchain pcre libpng"
-PKG_DEPENDS_TARGET="pcre:host libpng:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
+PKG_SHORTDESC="slang: library for the S-Lang extension language"
+PKG_LONGDESC="S-Lang is an interpreted language and a programming library.  The S-Lang language was designed so that it can be easily embedded into a program to provide the program with a powerful extension language.  The S-Lang library, provided in this package, provides the S-Lang extension language.  S-Lang's syntax resembles C, which makes it easy to recode S-Lang procedures in C if you need to."
+PKG_IS_ADDON="no"
+
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+MAKEFLAGS="-j1"
+
+PKG_CONFIGURE_OPTS_TARGET="--without-iconv \
+                           --without-onig \
+                           --with-pcre \
+                           --with-png \
+                           --without-z \
+                           --without-x"
+
+PKG_MAKE_OPTS_TARGET="-C src static "
 
 pre_configure_target() {
-  mkdir -p $ROOT/$PKG_BUILD/.$TARGET_NAME
-  cp -a $ROOT/$PKG_BUILD/* $ROOT/$PKG_BUILD/.$TARGET_NAME/
-
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|g"`
-}
-
-configure_target() {
-  cd $ROOT/$PKG_BUILD/.$TARGET_NAME
-  
-  ./configure --host=$TARGET_NAME --build=$HOST_NAME --prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc \
-  --libexecdir=/usr/lib --localstatedir=/var --without-iconv --without-onig --with-pcre --with-png --without-z --without-x
-}
-
-make_target() {
-  make -C src static -j1
+ # slang fails to build in subdirs
+ cd $ROOT/$PKG_BUILD
+   rm -rf .$TARGET_NAME
+   
+   export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|g"`
 }
 
 makeinstall_target() {
   make DESTDIR="$SYSROOT_PREFIX" -C src install-static
   make DESTDIR="$SYSROOT_PREFIX" install-pkgconfig
-}
-
-######################################
-
-pre_configure_host() {
-  mkdir -p $ROOT/$PKG_BUILD/.$HOST_NAME
-  cp -a $ROOT/$PKG_BUILD/* $ROOT/$PKG_BUILD/.$HOST_NAME/
-}
-
-configure_host() {
-  cd $ROOT/$PKG_BUILD/.$HOST_NAME
-  ./configure --prefix=/ --without-iconv --without-onig --without-pcre --without-png --without-z --without-x
-}
-
-makeinstall_host() {
-  make -j1 DESTDIR=$ROOT/$TOOLCHAIN -C src install
 }
