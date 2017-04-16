a2a116a13ae512723ecfa95c9db241932aa325c0
AV <27rurk@gmail.com>
2017-04-16 12:49:32 +0300

    autocommit on change

--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -5,7 +5,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnutls.org"
 PKG_URL="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/gnutls-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain zlib openssl libgcrypt libtasn1 gmp liblzo nettle libunistring"
+PKG_DEPENDS_TARGET="toolchain zlib openssl libgcrypt libtasn1 gmp lzo nettle libunistring"
 PKG_SECTION="xmedia/tools"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
@@ -20,6 +20,7 @@ PKG_CONFIGURE_OPTS_TARGET="gl_cv_socket_ipv6=no \
 			      --enable-openssl-compatibility \
 			      --enable-static \
 			      --disable-crywrap \
+			      --disable-tools \
 			      --enable-libdane \
 			      --without-tpm \
 			      --disable-guile \
