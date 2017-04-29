52e705590acf8dfa0d3308339e597a928c0a4184
AV <27rurk@gmail.com>
2017-04-29 05:30:09 +0300

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -5,9 +5,10 @@
 ################################################################################
 
 PKG_NAME="binutils"
-PKG_VERSION="d81c51a"
-#PKG_URL="https://fossies.org/linux/misc/binutils-$PKG_VERSION.tar.xz"
-PKG_GIT_URL="git://sourceware.org/git/binutils-gdb.git"
+PKG_VERSION="2.28"
+#PKG_VERSION="d81c51a"
+PKG_URL="https://fossies.org/linux/misc/binutils-$PKG_VERSION.tar.xz"
+#PKG_GIT_URL="git://sourceware.org/git/binutils-gdb.git"
 PKG_DEPENDS_HOST="ccache:host bison:host flex:host linux:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="binutils: A GNU collection of binary utilities"
@@ -25,7 +26,7 @@ PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --disable-werror \
                          --disable-multilib \
                          --disable-libada \
-                         --disable-libssp \
+                         --enable-libssp \
                          --enable-version-specific-runtime-libs \
                          --enable-plugins \
                          --enable-gold \
@@ -34,7 +35,6 @@ PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --with-pic \
                          --disable-nls \
                          --with-system-zlib \
-                         --disable-gdb \
                          --disable-shared \
                          --enable-poison-system-directories"
 
