051b0e7560dcf9da43a47de41736facf202c82c4
AV <27rurk@gmail.com>
2017-04-30 09:36:52 +0300

    autocommit on change

--- packages/devel/swig/package.mk
+++ packages/devel/swig/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="swig"
-PKG_VERSION="3.0.12"
+PKG_VERSION="3.0.10"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.swig.org"
-PKG_URL="https://master.dl.sourceforge.net/project/swig/swig/swig-$PKG_VERSION/swig-$PKG_VERSION.tar.gz"
+PKG_URL="ftp://root:openelec@192.168.1.4:2121/www/swig-3.0.10.tar.gz"
 PKG_DEPENDS_HOST="ccache:host pcre:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
@@ -32,4 +32,28 @@ PKG_LONGDESC="SWIG is a software development tool that connects programs written
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared"
+PKG_CONFIGURE_OPTS_HOST="--with-boost=no \
+                         --without-pcre \
+                         --without-x \
+                         --without-tcl \
+                         --without-python \
+                         --without-python3 \
+                         --without-perl5 \
+                         --without-octave \
+                         --without-java \
+                         --without-gcj \
+                         --without-android \
+                         --without-guile \
+                         --without-mzscheme \
+                         --without-ruby \
+                         --without-php \
+                         --without-ocaml \
+                         --without-pike \
+                         --without-chicken \
+                         --without-csharp \
+                         --without-lua \
+                         --without-allegrocl \
+                         --without-clisp \
+                         --without-r \
+                         --without-go \
+                         --without-d"
