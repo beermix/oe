2633ce5bc879dcb184f7517e216f1ca8d44fccdf
AV <27rurk@gmail.com>
2017-05-01 04:08:27 +0300

    autocommit on change

---
+++ packages/x11/lib/atkmm/package.mk
@@ -0,0 +1,30 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="atkmm"
+PKG_VERSION="2.53.3"
+PKG_SITE="http://www.X.org"
+PKG_GIT_URL="https://github.com/GNOME/atkmm"
+PKG_DEPENDS_TARGET="toolchain util-macros"
+PKG_SECTION="x11/lib"
+PKG_SHORTDESC="xtrans: Abstract network code for X"
+PKG_LONGDESC="Abstract network code for X."
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
