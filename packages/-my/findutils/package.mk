d01095e66beb2c1bdd4c2adb85c5f8b44d49266a
AV <27rurk@gmail.com>
2017-05-01 18:01:31 +0300

    autocommit on change

--- packages/-my/findutils/package.mk
+++ packages/-my/findutils/package.mk
@@ -4,12 +4,13 @@ PKG_URL="http://ftpmirror.gnu.org/findutils/findutils-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="network"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 pre_configure_target() {
+  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
+  MAKEFLAGS=-j1
   strip_lto
-  #export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
-  #MAKEFLAGS=-j1
+  strip_gold
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--without-selinux \
