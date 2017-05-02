d90b6aa166f4ffb92ec8666e8da7fe9f431d3740
AV <27rurk@gmail.com>
2017-05-02 13:26:09 +0300

    autocommit on change

--- packages/-my/mc/package.mk
+++ packages/-my/mc/package.mk
@@ -1,13 +1,11 @@
 PKG_NAME="mc"
-PKG_VERSION="83127bd"
-PKG_GIT_URL="git://github.com/MidnightCommander/mc.git"
+PKG_VERSION="4.8.19"
+PKG_URL="http://ftp.midnight-commander.org/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+#PKG_VERSION="83127bd"
+#PKG_GIT_URL="git://github.com/MidnightCommander/mc.git"
 PKG_DEPENDS_TARGET="toolchain libtool:host gettext:host e2fsprogs util-linux glib pcre fuse libssh2 libevent expat slang"
 PKG_SECTION="tools"
 PKG_AUTORECONF="yes"
-
-pre_configure_target() {
-  sh autogen.sh
-}
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config \
                            --datadir=/storage/.config \
