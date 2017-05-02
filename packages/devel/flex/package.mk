f70587cb6e6c08d1f8d1f52b35d96641ec231995
AV <27rurk@gmail.com>
2017-05-03 01:35:02 +0300

    autocommit on change

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -17,16 +17,16 @@
 ################################################################################
 
 PKG_NAME="flex"
-PKG_VERSION="2.6.3"
+PKG_VERSION="88b8e2a"
 PKG_SITE="https://github.com/westes/flex/releases"
-PKG_URL="https://github.com/westes/flex/releases/download/v$PKG_VERSION/flex-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host"
+PKG_GIT_URL="https://github.com/westes/flex"
+PKG_DEPENDS_HOST="ccache:host autotools:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="flex: Fast lexical analyzer generator"
 PKG_LONGDESC="flex is a tool for generating programs that perform pattern-matching on text."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
 
