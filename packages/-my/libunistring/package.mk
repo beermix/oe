ee0e7f7da2612cdfe6df5844e39e64401a6e6f10
AV <27rurk@gmail.com>
2017-05-03 11:13:53 +0300

    autocommit on change

--- packages/-my/libunistring/package.mk
+++ packages/-my/libunistring/package.mk
@@ -1,13 +1,10 @@
 PKG_NAME="libunistring"
 PKG_VERSION="0.9.7"
 PKG_URL="https://ftp.gnu.org/gnu/libunistring/libunistring-$PKG_VERSION.tar.xz"
-PKG_BUILD_DEPENDS_TARGET="toolchain openssl"
+PKG_BUILD_DEPENDS_TARGET="toolchain openssl icu"
 PKG_SECTION="devel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-			      --enable-static \
-			      --enable-threads=posix \
-			      --with-gnu-ld"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-threads=posix --with-gnu-ld"
 			   
