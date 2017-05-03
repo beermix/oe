bf51e2c755745f3e24f12e08c24ca33a5d3d49bf
AV <27rurk@gmail.com>
2017-05-03 08:45:23 +0300

    autocommit on change

--- packages/-my/psmisc/package.mk
+++ packages/-my/psmisc/package.mk
@@ -1,8 +1,8 @@
-PKG_NAME="psmisc"
-PKG_VERSION="22.21"
-PKG_URL="http://downloads.sourceforge.net/psmisc/psmisc-$PKG_VERSION.tar.gz"
+PKG_NAME="net-tools"
+PKG_VERSION="479bb4a"
+PKG_GIT_URL="git://git.code.sf.net/p/net-tools/code"
 PKG_DEPENDS_TARGET="toolchain netbsd-curses"
 PKG_SECTION="tools"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared"
