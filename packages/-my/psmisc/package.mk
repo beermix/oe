aa496072b972117f7673b6c9785f7ba921c6e41b
AV <27rurk@gmail.com>
2017-05-03 08:58:58 +0300

    autocommit on change

--- packages/-my/psmisc/package.mk
+++ packages/-my/psmisc/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="net-tools"
 PKG_VERSION="479bb4a"
 PKG_GIT_URL="git://git.code.sf.net/p/net-tools/code"
-PKG_DEPENDS_TARGET="toolchain netbsd-curses"
+PKG_DEPENDS_TARGET="toolchain netbsd-curses pcre libdnet"
 PKG_SECTION="tools"
 PKG_AUTORECONF="yes"
 
