fe0d4801dd4753753a0882510c0398fc39b68146
AV <27rurk@gmail.com>
2017-05-07 17:30:43 +0300

    autocommit on change

--- packages/-my/bash/package.mk
+++ packages/-my/bash/package.mk
@@ -9,10 +9,7 @@ pre_configure_target() {
   export LIBS="-ltermcap -lcurses"
 }
 
-PKG_CONFIGURE_OPTS_TARGET="bash_cv_job_control_missing=present \
-			      bash_cv_sys_named_pipes=present \
-			      bash_cv_func_sigsetjmp=present \
-                           --bindir=/bin \
+PKG_CONFIGURE_OPTS_TARGET="--bindir=/bin \
                            --with-curses \
                            --enable-readline \
                            --without-bash-malloc \
