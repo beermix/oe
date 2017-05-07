41a305b7a06c32423c6a3dbe5c8fad5a29773a75
AV <27rurk@gmail.com>
2017-05-07 18:43:47 +0300

    autocommit on change

--- packages/mediacenter/p8-platform/package.mk
+++ packages/mediacenter/p8-platform/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="p8-platform"
-PKG_VERSION="38343e0"
+PKG_VERSION="p8-platform-2.1.0.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
