c0daaf1c714ee5fe495a7655094281d28e3b4c31
AV <27rurk@gmail.com>
2017-05-07 20:43:03 +0300

    autocommit on change

--- packages/addons/addon-depends/protobuf/package.mk
+++ packages/addons/addon-depends/protobuf/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="protobuf"
-PKG_VERSION="3.1.0"
+PKG_VERSION="3.3.0"
 PKG_SITE="https://developers.google.com/protocol-buffers/"
 PKG_URL="https://github.com/google/$PKG_NAME/releases/download/v$PKG_VERSION/$PKG_NAME-cpp-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="toolchain zlib:host Python:host"
---
+++ packages/devel/re2c/package - копия (2).mk
@@ -0,0 +1,15 @@
+PKG_NAME="re2c"
+PKG_VERSION="0.16"
+PKG_GIT_URL="https://github.com/skvadrik/re2c"
+PKG_DEPENDS_HOST="bison:host"
+PKG_SECTION="devel"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+post_unpack() {
+  cp -r $PKG_BUILD/re2c/* $PKG_BUILD/
+}
+
+PKG_CONFIGURE_OPTS_HOST="--disable-docs"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-docs"
