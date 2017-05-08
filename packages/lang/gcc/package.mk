93e8d5bb9c0d08337be5bb1b15b40ea5431ed389
AV <27rurk@gmail.com>
2017-05-08 16:03:53 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -77,7 +77,7 @@ PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
                          --enable-languages=c,c++ \
                          --enable-__cxa_atexit \
                          --enable-decimal-float \
-                         --disable-libssp \
+                         --enable-libssp \
                          --enable-tls \
                          --enable-shared \
                          --disable-static \
