df71c90278e4a4ff10c52b41601caa8edc7214e4
AV <27rurk@gmail.com>
2017-05-08 11:51:24 +0300

    autocommit on change

--- packages/textproc/rapidjson/package.mk
+++ packages/textproc/rapidjson/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="rapidjson"
-PKG_VERSION="fe2b360"
+PKG_VERSION="v1.1.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
