dcd5687e8ef6095d1a04eaffcb54680a9999256c
AV <27rurk@gmail.com>
2017-05-08 20:58:34 +0300

    autocommit on change

--- packages/x11/app/setxkbmap/package.mk
+++ packages/x11/app/setxkbmap/package.mk
@@ -26,4 +26,4 @@ PKG_SHORTDESC="setxkbmap: Sets the keyboard using the X Keyboard Extension"
 PKG_LONGDESC="Setxkbmap sets the keyboard using the X Keyboard Extension."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
