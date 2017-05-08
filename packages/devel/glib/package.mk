9f05dbdb5a8a9195a3b64d13ae1a57c3a0d655d1
AV <27rurk@gmail.com>
2017-05-08 23:11:05 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -18,9 +18,9 @@
 ################################################################################
 
 PKG_NAME="glib"
-PKG_VERSION="2.53.1"
+PKG_VERSION="2.50.3"
 PKG_SITE="http://www.gtk.org/"
-PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/2.53/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/2.50/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib libffi util-linux attr expat pcre Python:host"
 PKG_DEPENDS_HOST="pcre:host"
 PKG_SECTION="devel"
