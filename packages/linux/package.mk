14e396cbf22886440f1b3f0da7423812bcd542b1
AV <27rurk@gmail.com>
2017-05-15 20:32:23 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -65,7 +65,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="linux-4.10"
     ;;
   *)
-    PKG_VERSION="4.11"
+    PKG_VERSION="4.11.1"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="linux-4.11"
     ;;
