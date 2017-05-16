afcfd9b079e922ca943ba6258c47ad461be3df7b
AV <27rurk@gmail.com>
2017-05-16 23:06:13 +0300

    autocommit on change

--- packages/devel/swig/package.mk
+++ packages/devel/swig/package.mk
@@ -32,28 +32,4 @@ PKG_LONGDESC="SWIG is a software development tool that connects programs written
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--with-boost=no \
-                         --with-pcre \
-                         --without-x \
-                         --without-tcl \
-                         --with-python \
-                         --without-python3 \
-                         --without-perl5 \
-                         --without-octave \
-                         --without-java \
-                         --without-gcj \
-                         --without-android \
-                         --without-guile \
-                         --without-mzscheme \
-                         --without-ruby \
-                         --without-php \
-                         --without-ocaml \
-                         --without-pike \
-                         --without-chicken \
-                         --without-csharp \
-                         --with-lua \
-                         --without-allegrocl \
-                         --without-clisp \
-                         --without-r \
-                         --with-go \
-                         --without-d"
+PKG_CONFIGURE_OPTS_HOST="--disable-shared"
