fbd33d4b2f5fc5b1b501e1b323b1d58d359f3eda
AV <27rurk@gmail.com>
2017-05-16 22:17:11 +0300

    autocommit on change

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -21,7 +21,7 @@ PKG_VERSION="b014fa2"
 PKG_ARCH="any"
 PKG_GIT_URL="https://github.com/FFmpeg/FFmpeg.git"
 PKG_GIT_BRANCH="release/3.1"
-PKG_DEPENDS_TARGET="toolchain yasm:host zlib bzip2 openssl speex fdk-aac flac"
+PKG_DEPENDS_TARGET="toolchain yasm:host zlib bzip2 openssl speex fdk-aac flac libvpx"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="FFmpeg is a complete, cross-platform solution to record, convert and stream audio and video."
@@ -199,7 +199,7 @@ configure_target() {
               --disable-libtheora \
               --disable-libvo-amrwbenc \
               --disable-libvorbis \
-              --disable-libvpx \
+              --enable-libvpx \
               --disable-libx264 \
               --disable-libxavs \
               --disable-libxvid \
