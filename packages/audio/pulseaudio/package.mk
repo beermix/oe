d89b5fc23ff9bde547002699f30460fae3351b35
AV <27rurk@gmail.com>
2017-05-17 20:42:06 +0300

    autocommit on change

--- packages/audio/pulseaudio/package.mk
+++ packages/audio/pulseaudio/package.mk
@@ -100,6 +100,9 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-silent-rules \
                            --with-soxr \
                            --with-module-dir=/usr/lib/pulse"
 
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -fopenmp"
+}
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/esdcompat
   rm -rf $INSTALL/usr/include
