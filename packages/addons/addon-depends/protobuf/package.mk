6038485c8e2f09ccf2cbc50b926c1e4a7b5551d5
AV <27rurk@gmail.com>
2017-05-18 00:27:29 +0300

    autocommit on change

--- packages/addons/addon-depends/protobuf/package.mk
+++ packages/addons/addon-depends/protobuf/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="protobuf"
-PKG_VERSION="3.3.0"
+PKG_VERSION="3.1.0"
 PKG_SITE="https://developers.google.com/protocol-buffers/"
 PKG_URL="https://github.com/google/$PKG_NAME/releases/download/v$PKG_VERSION/$PKG_NAME-cpp-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="toolchain zlib:host Python:host"
@@ -27,10 +27,6 @@ PKG_USE_CMAKE="yes"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=None -DBUILD_SHARED_LIBS=OFF"
-
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=None -DBUILD_SHARED_LIBS=OFF"
-
 PKG_CMAKE_SCRIPT_TARGET="cmake/CMakeLists.txt"
 PKG_CMAKE_SCRIPT_HOST="cmake/CMakeLists.txt"
 
@@ -38,9 +34,10 @@ PKG_CMAKE_OPTS_HOST="-DCMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
                      -DBUILD_SHARED_LIBS=0 \
                      -Dprotobuf_BUILD_TESTS=0 \
                      -Dprotobuf_BUILD_EXAMPLES=0 \
-                     -Dprotobuf_WITH_ZLIB=1"
+                     -Dprotobuf_WITH_ZLIB=1 \
+                     -DCMAKE_BUILD_TYPE=Release"
 
-PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_HOST"
+PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_HOST -DCMAKE_BUILD_TYPE=Release"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
