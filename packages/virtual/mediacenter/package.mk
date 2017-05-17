17bd749269e24aefd9fd910f9de18d6091e8197b
AV <27rurk@gmail.com>
2017-05-17 09:41:07 +0300

    autocommit on change

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -42,7 +42,7 @@ if [ "$MEDIACENTER" = "kodi" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inputstream.rtmp"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inputstream.adaptive"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET visualization.shadertoy"
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imagedecoder.raw"
+  #PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imagedecoder.raw"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET audioencoder.flac"
   #PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET audioencoder.vorbis"
 
