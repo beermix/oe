4a6918666f9b5e32695af421b8385f38aa3acd6e
AV <27rurk@gmail.com>
2017-05-17 07:07:49 +0300

    autocommit on change

--- packages/mediacenter/kodi-binary-addons/imagedecoder.raw/package.mk
+++ packages/mediacenter/kodi-binary-addons/imagedecoder.raw/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="imagedecoder.raw"
-PKG_VERSION="37ef22e"
+PKG_VERSION="7f6e9dd"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/notspiff/imagedecoder.raw"
-PKG_URL="https://github.com/notspiff/imagedecoder.raw/archive/$PKG_VERSION.tar.gz"
+PKG_GIT_URL="https://github.com/notspiff/imagedecoder.raw"
 PKG_DEPENDS_TARGET="toolchain kodi-platform libraw"
 PKG_SECTION=""
 PKG_SHORTDESC="imagedecoder.raw"
@@ -34,8 +34,8 @@ PKG_ADDON_TYPE="kodi.imagedecoder"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
-  cp -R $PKG_BUILD/.install_pkg/usr/share/$MEDIACENTER/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -R $ROOT/$PKG_BUILD/.install_pkg/usr/share/$MEDIACENTER/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
 
   ADDONSO=$(xmlstarlet sel -t -v "/addon/extension/@library_linux" $ADDON_BUILD/$PKG_ADDON_ID/addon.xml)
-  cp -L $PKG_BUILD/.install_pkg/usr/lib/$MEDIACENTER/addons/$PKG_NAME/$ADDONSO $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -L $ROOT/$PKG_BUILD/.install_pkg/usr/lib/$MEDIACENTER/addons/$PKG_NAME/$ADDONSO $ADDON_BUILD/$PKG_ADDON_ID/
 }
