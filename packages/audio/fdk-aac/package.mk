c02ba3765f9b77eb1763bdcd0020adb6d63100ca
AV <27rurk@gmail.com>
2017-05-17 11:34:52 +0300

    autocommit on change

--- packages/audio/fdk-aac/package.mk
+++ packages/audio/fdk-aac/package.mk
@@ -1,14 +1,14 @@
 PKG_NAME="fdk-aac"
-PKG_VERSION="5eb6f0d"
+PKG_VERSION="v0.1.5"
 PKG_GIT_URL="https://github.com/mstorsjo/fdk-aac"
 PKG_DEPENDS_TARGET="toolchain libxml2 sqlite"
 PKG_SECTION="tools"
 PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -fPIC"
-  export CXXFLAGS="$CXXFLAGS -fPIC"
-  export LDFLAGS="$LDFLAGS -fPIC"
-}
+#pre_configure_target() {
+#  export CFLAGS="$CFLAGS -fPIC"
+#  export CXXFLAGS="$CXXFLAGS -fPIC"
+#  export LDFLAGS="$LDFLAGS -fPIC"
+#}
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
