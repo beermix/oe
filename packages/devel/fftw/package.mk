1d8510f50ac09a755826fca2fba35d6b3a479254
AV <27rurk@gmail.com>
2017-05-19 07:54:47 +0300

    autocommit on change

--- packages/devel/fftw/package.mk
+++ packages/devel/fftw/package.mk
@@ -1,19 +1,13 @@
 PKG_NAME="fftw"
 PKG_VERSION="3.3.6-pl2"
 PKG_URL="ftp://ftp.fftw.org/pub/fftw/fftw-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libnl hwloc"
+PKG_DEPENDS_TARGET="toolchain libnl"
 PKG_SECTION="service/system"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math -fopenmp"
-}
-
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-threads \
-			      --enable-openmp \
-			      --enable-fortran \
 			      --enable-float \
 			      --enable-sse2 \
 			      --enable-sse \
