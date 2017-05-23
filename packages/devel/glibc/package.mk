3f4cbe3e1878820994c4055735b9110140425b59
AV <27rurk@gmail.com>
2017-05-23 13:57:34 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -115,7 +115,7 @@ EOF
 
   echo "sbindir=/usr/bin" >> configparms
   echo "rootsbindir=/usr/bin" >> configparms
-  echo "build-programs=no" >> configparms
+  #echo "build-programs=no" >> configparms
 }
 
 post_makeinstall_target() {
