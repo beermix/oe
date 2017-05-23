29e3bd6219cdd5b43be81cc817ef419764d8df72
AV <27rurk@gmail.com>
2017-05-23 12:54:20 +0300

    autocommit on change

--- packages/-my/diffutils/package.mk
+++ packages/-my/diffutils/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="diffutils"
-PKG_VERSION="3.5"
+PKG_VERSION="3.6"
 PKG_SITE="http://www.gnu.org/software/diffutils/"
 PKG_URL="ftp://ftp.gnu.org/gnu/diffutils/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
