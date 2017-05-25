9710faaa92fce2714f9beab53f030b1fe432ba0c
AV <27rurk@gmail.com>
2017-05-26 00:09:21 +0300

    autocommit on change

--- packages/-my/bwm-ng/package.mk
+++ packages/-my/bwm-ng/package.mk
@@ -15,7 +15,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
 			      --disable-shared \
 			      --with-time \
 			      --with-getifaddrs \
-			      --with-sysctl \
+			      --without-sysctl \
 			      --with-sysctldisk \
 			      --with-procnetdev \
 			      --with-partitions \ 
