19fd4f74746e766e107b9b8214c6a76e4212dfed
AV <27rurk@gmail.com>
2017-05-26 04:47:26 +0300

    autocommit on change

--- packages/-my/htop/package.mk
+++ packages/-my/htop/package.mk
@@ -1,9 +1,14 @@
 PKG_NAME="htop"
-PKG_VERSION="2.0.2"
+PKG_VERSION="b1028e0"
 PKG_GIT_URL="https://github.com/hishamhm/htop"
 PKG_DEPENDS_TARGET="toolchain hwloc"
 PKG_SECTION="debug/tools"
 PKG_AUTORECONF="yes"
+
+pre_configure_target() {
+  export LIBS="$LIBS -ludev -lhwloc -lxml2 -lpciaccess"
+}
+
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-cgroup \
 			      --enable-vserver \
