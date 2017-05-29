d29a87b1a898f7c0466352f9d5858e7b5f2dd370
AV <27rurk@gmail.com>
2017-05-30 00:03:29 +0300

    autocommit on change

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -17,12 +17,14 @@
 ################################################################################
 
 PKG_NAME="busybox"
-PKG_VERSION="1.26.2"
+#PKG_VERSION="1.26.2"
+PKG_VERSION="6683d1c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.busybox.net"
-PKG_URL="http://busybox.net/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
+#PKG_URL="http://busybox.net/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_GIT_URL="git://git.busybox.net/busybox"
 PKG_DEPENDS_HOST=""
 PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs libiconv zip unzip unrar pcre expat zlib bzip2 xz lz4 lzo lrzip openssl icu libuv libev tar pciutils usbutils parted procps-ng coreutils time bash findutils less gptfdisk libarchive re2"
 PKG_DEPENDS_INIT="toolchain"
