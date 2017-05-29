66aa526a36a059f06ccca6c7231ba04f272f0123
AV <27rurk@gmail.com>
2017-05-30 00:43:50 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -17,9 +17,10 @@
 ################################################################################
 
 PKG_NAME="gcc"
-PKG_VERSION="754d7e5"
-PKG_GIT_URL="git://gcc.gnu.org/git/gcc.git"
-#PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_VERSION="6.3.0"
+#PKG_VERSION="754d7e5"
+#PKG_GIT_URL="git://gcc.gnu.org/git/gcc.git"
+PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_BOOTSTRAP="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host isl:host"
 PKG_DEPENDS_TARGET="gcc:host"
 PKG_DEPENDS_HOST="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host isl:host glibc"
