4be5ed9cc1f9d96a25a98301281115bdfbaa0cd1
AV <27rurk@gmail.com>
2017-05-30 02:54:33 +0300

    autocommit on change

--- packages/-my/nuttcp/package.mk
+++ packages/-my/nuttcp/package.mk
@@ -1,11 +1,9 @@
 PKG_NAME="nuttcp"
-PKG_VERSION="8.1.3"
-PKG_URL="http://nuttcp.net/nuttcp/nuttcp-$PKG_VERSION/nuttcp-$PKG_VERSION.c"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
-  $CC $CFLAGS -o nuttcp nuttcp-$PKG_VERSION.c
+  $CC $CFLAGS -o nuttcp $PKG_DIR/nuttcp-8.1.3.c
 }
