fe4823eb8cd65855affbd97426c2e2c20644e38c
AV <27rurk@gmail.com>
2017-05-30 08:51:58 +0300

    autocommit on change

--- packages/devel/swig/package.mk
+++ packages/devel/swig/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="swig"
-PKG_VERSION="a4d01cd"
-PKG_GIT_URL="https://github.com/swig/swig"
+#PKG_VERSION="a4d01cd"
+#PKG_GIT_URL="https://github.com/swig/swig"
 PKG_VERSION="3.0.12"
 PKG_URL="$SOURCEFORGE_SRC/swig/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="ccache:host pcre:host"
@@ -30,9 +30,9 @@ PKG_LONGDESC="SWIG is a software development tool that connects programs written
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_configure_host() {
-  ./autogen.sh
-}
+#pre_configure_host() {
+#  ./autogen.sh
+#}
 
 PKG_CONFIGURE_OPTS_HOST="--with-pcre-prefix=$TOOLCHAIN \
                          --with-boost=no \
