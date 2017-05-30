f8c72dd73364146afadf7593b8bf0d920fa4a9f9
AV <27rurk@gmail.com>
2017-05-30 07:11:28 +0300

    autocommit on change

---
+++ packages/-my/lsof/package.mk
@@ -0,0 +1,12 @@
+PKG_NAME="lsof"
+PKG_VERSION="4.89"
+PKG_URL="https://dl.dropboxusercontent.com/s/c3u89lk1pqk2gar/lsof-4.89.tar.xz"
+PKG_DEPENDS_TARGET="toolchain netbsd-curses"
+PKG_AUTORECONF="no"
+
+pre_configure_target() {
+  export LIBS="-lterminfo"
+}
+PKG_CONFIGURE_SCRIPT="Configure"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
