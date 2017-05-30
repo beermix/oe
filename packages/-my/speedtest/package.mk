1721e20a77d7102eba3b706f59e5c5d939df995c
AV <27rurk@gmail.com>
2017-05-30 05:37:45 +0300

    autocommit on change

--- packages/-my/speedtest/package.mk
+++ packages/-my/speedtest/package.mk
@@ -1,8 +1,9 @@
 PKG_NAME="speedtest"
-PKG_VERSION="v1.11"
+PKG_VERSION="54a2998"
 PKG_GIT_URL="https://github.com/taganaka/SpeedTest"
 PKG_DEPENDS_TARGET="toolchain curl libxml2 openssl"
-PKG_AUTORECONF="no"
+PKG_USE_CMAKE="no"
+PKG_AUTORECONF="yes"
 
 pre_configure_target() {
   export LIBS="-lterminfo"
