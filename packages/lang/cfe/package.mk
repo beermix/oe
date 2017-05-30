c1edcb4c6768e80ad0a264e1385ff992dcdf3771
AV <27rurk@gmail.com>
2017-05-30 04:13:38 +0300

    autocommit on change

--- packages/lang/cfe/package.mk
+++ packages/lang/cfe/package.mk
@@ -5,14 +5,22 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
 PKG_URL="http://llvm.org/releases/$PKG_VERSION/${PKG_NAME}-${PKG_VERSION}.src.tar.xz"
 PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}.src"
-PKG_DEPENDS_HOST="toolchain"
-PKG_DEPENDS_TARGET="toolchain llvm:host zlib"
+PKG_DEPENDS_HOST="toolchain llvm"
+PKG_DEPENDS_TARGET="toolchain llvm"
 PKG_SECTION="lang"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=MinSizeRel \
+pre_configure_host() {
+  export LLVM_CONFIG="$SYSROOT_PREFIX/usr/bin/llvm-config-host"
+}
+
+pre_configure_target() {
+  export LLVM_CONFIG="$SYSROOT_PREFIX/usr/bin/llvm-config-host"
+}
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
                        -DCMAKE_C_FLAGS="$CFLAGS" \
                        -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
                        -DLLVM_INCLUDE_TOOLS=ON \
@@ -27,19 +35,11 @@ PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=MinSizeRel \
                        -DLLVM_INCLUDE_DOCS=OFF \
                        -DLLVM_ENABLE_DOXYGEN=OFF \
                        -DLLVM_ENABLE_SPHINX=OFF \
-                       -DLLVM_TARGETS_TO_BUILD="AMDGPU" \
+                       -DLLVM_TARGETS_TO_BUILD="X86" \
                        -DLLVM_ENABLE_TERMINFO=OFF \
                        -DLLVM_ENABLE_ASSERTIONS=OFF \
                        -DLLVM_ENABLE_WERROR=OFF \
                        -DLLVM_TARGET_ARCH="$TARGET_ARCH" \
-                       -DLLVM_ENABLE_ZLIB=ON \
-                       -DLLVM_BUILD_LLVM_DYLIB=ON \
-                       -DLLVM_LINK_LLVM_DYLIB=ON \
-                       -DLLVM_OPTIMIZED_TABLEGEN=ON \
-                       -DLLVM_TABLEGEN=$TOOLCHAIN/bin/llvm-tblgen"
-
-post_makeinstall_target() {
-  rm -rf $INSTALL/usr/bin
-  rm -rf $INSTALL/usr/lib/LLVMHello.so
-  rm -rf $INSTALL/usr/lib/libLTO.so
-}
+                       -DLLVM_TABLEGEN=$ROOT/$TOOLCHAIN/bin/llvm-tblgen"
+                       
+PKG_CMAKE_OPTS_HOST="$PKG_CMAKE_OPTS_TARGET"
