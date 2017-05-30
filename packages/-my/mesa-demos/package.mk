53c2e4f5b54330fe97e7346b40c4c6ce0c122de3
AV <27rurk@gmail.com>
2017-05-30 10:09:46 +0300

    autocommit on change

--- packages/-my/mesa-demos/package.mk
+++
@@ -1,9 +0,0 @@
-PKG_NAME="mesa-demos"
-PKG_VERSION="8.3.0"
-PKG_URL="ftp://ftp.freedesktop.org/pub/mesa/demos/8.3.0/mesa-demos-8.3.0.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain libX11"
-PKG_SECTION="service/system"
-PKG_AUTORECONF="yes"
-
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --with-gnu-ld"
