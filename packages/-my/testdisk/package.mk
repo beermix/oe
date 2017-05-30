9afe387300ee36ae58fd2480cdabf9f941dce184
AV <27rurk@gmail.com>
2017-05-30 05:28:18 +0300

    autocommit on change

--- packages/-my/testdisk/package.mk
+++ packages/-my/testdisk/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="testdisk"
 PKG_VERSION="0db61bd"
-#PKG_URL="https://dl.dropboxusercontent.com/s/02huv7ugt9ofkis/testdisk-989.tar.xz"
+PKG_SITE="https://git.cgsecurity.org/cgit/testdisk/log/"
 PKG_GIT_URL="https://git.cgsecurity.org/testdisk.git"
 PKG_DEPENDS_TARGET="toolchain readline netbsd-curses"
 PKG_IS_ADDON="no"
