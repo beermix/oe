33ab338095ffdeee37309d02fcc305a189c63246
AV <27rurk@gmail.com>
2017-05-30 09:03:33 +0300

    autocommit on change

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="elfutils"
-PKG_VERSION="0.168"
+PKG_VERSION="0.169"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
