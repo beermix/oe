ac3413ceec96123fa4864548b608e6cd75e00918
AV <27rurk@gmail.com>
2017-05-31 05:04:19 +0300

    autocommit on change

---
+++ packages/devel/ragel/package.mk
@@ -0,0 +1,8 @@
+PKG_NAME="ragel"
+PKG_VERSION="6.10"
+PKG_URL="http://www.colm.net/files/ragel/ragel-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_SECTION="devel"
+PKG_IS_ADDON="no"
+PKG_USE_CMAKE="no"
+PKG_AUTORECONF="no"
