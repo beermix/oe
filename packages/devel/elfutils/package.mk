a8a6e51cac2040922a4af475cbb49952369f8e5a
AV <27rurk@gmail.com>
2017-05-31 07:13:07 +0300

    autocommit on change

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="elfutils"
-PKG_VERSION="0.166"
+PKG_VERSION="0.169"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://sourceware.org/elfutils/"
-PKG_URL="https://dl.dropboxusercontent.com/s/ns57hi305367ksd/elfutils-0.166.tar.xz"
+PKG_URL="https://sourceware.org/elfutils/ftp/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
@@ -30,7 +30,7 @@ PKG_SHORTDESC="elfutils: collection of utilities to handle ELF objects"
 PKG_LONGDESC="Elfutils is a collection of utilities, including eu-ld (a linker), eu-nm (for listing symbols from object files), eu-size (for listing the section sizes of an object or archive file), eu-strip (for discarding symbols), eu-readelf (to see the raw ELF file structures), and eu-elflint (to check for well-formed ELF files)."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="utrace_cv_cc_biarch=false \
                            --disable-werror \
@@ -43,3 +43,22 @@ PKG_CONFIGURE_OPTS_TARGET="utrace_cv_cc_biarch=false \
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC -DPIC"
 }
+
+make_target() {
+  make V=1 -C libelf libelf.a
+  make V=1 -C libebl libebl.a
+  make V=1 -C libdwfl libdwfl.a
+  make V=1 -C libdw libdw.a
+}
+
+makeinstall_target() {
+  make DESTDIR="$SYSROOT_PREFIX" -C libelf install-includeHEADERS install-pkgincludeHEADERS
+  make DESTDIR="$SYSROOT_PREFIX" -C libdw install-includeHEADERS install-pkgincludeHEADERS
+
+  mkdir -p $SYSROOT_PREFIX/usr/lib
+    cp libelf/libelf.a $SYSROOT_PREFIX/usr/lib
+    cp libdw/libdw.a $SYSROOT_PREFIX/usr/lib
+}
+
+
+PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
