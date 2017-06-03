756ff2598dac254f50b2dd3115cd8e4f888e6745
AV <27rurk@gmail.com>
2017-06-03 16:24:26 +0300

    autocommit on change

--- packages/databases/gdbm/package.mk
+++ packages/databases/gdbm/package.mk
@@ -4,9 +4,9 @@ PKG_URL="http://mirror.switch.ch/ftp/mirror/gnu/gdbm/gdbm-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --with-gnu-ld"
 
 post_makeinstall_target() {
   rm -rf $INSTALL
