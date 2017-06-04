7efbf12b9e93e3641298ec3ecd207b85ed18f89a
AV <27rurk@gmail.com>
2017-06-04 18:35:42 +0300

    autocommit on change

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -33,7 +33,7 @@ PKG_AUTORECONF="no"
 
 configure_host() {
   ../configure --prefix=$ROOT/$TOOLCHAIN \
-               --no-qt-gui --no-system-libs --no-system-jsoncpp \
+               --no-qt-gui --no-system-libs \
                -- \
                -DCMAKE_C_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
                -DCMAKE_CXX_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
