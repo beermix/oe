c46c069cdeac6673d77fdcbd2b09c9716de2d56f
AV <27rurk@gmail.com>
2017-06-05 22:01:20 +0300

    autocommit on change

--- packages/security/nettle/package.mk
+++ packages/security/nettle/package.mk
@@ -15,9 +15,7 @@ PKG_SECTION="xmedia/tools"
 PKG_SHORTDESC="nettle: a cryptographic library"
 PKG_LONGDESC="Nettle is a cryptographic library that is designed to fit easily in more or less any context: In crypto toolkits for object-oriented languages (C++, Python, Pike, ...), in applications like LSH or GNUPG, or even in kernel space. In most contexts, you need more than the basic cryptographic algorithms, you also need some way to keep track of available algorithms, their properties and variants. You often have some algorithm selection process, often dictated by a protocol you want to implement."
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
-
-#MAKEFLAGS=-j1
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --disable-openssl --disable-documentation"
 
