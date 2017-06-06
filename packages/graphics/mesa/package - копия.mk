ac7c16b73edd4b398bed8d915e900fa2d6d42969
AV <27rurk@gmail.com>
2017-06-06 21:48:51 +0300

    autocommit on change

--- packages/graphics/mesa/package - копия.mk
+++ packages/graphics/mesa/package - копия.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,18 +17,32 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="17.1.1"
+PKG_VERSION="17.1.2"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="ftp://freedesktop.org/pub/mesa/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain Python:host expat glproto dri2proto presentproto libdrm libXext libXdamage libXfixes libXxf86vm libxcb libX11 dri3proto libxshmfence zlib"
+PKG_DEPENDS_TARGET="toolchain Python:host expat glproto dri2proto presentproto libdrm libXext libXdamage libXfixes libXxf86vm libxcb libX11 systemd dri3proto libxshmfence openssl Mako:host"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="mesa: 3-D graphics library with OpenGL API"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API which is very similar to that of OpenGL*. To the extent that Mesa utilizes the OpenGL command syntax or state machine, it is being used with authorization from Silicon Graphics, Inc. However, the author makes no claim that Mesa is in any way a compatible replacement for OpenGL or associated with Silicon Graphics, Inc. Those who want a licensed implementation of OpenGL should contact a licensed vendor. While Mesa is not a licensed OpenGL implementation, it is currently being tested with the OpenGL conformance tests. For the current conformance status see the CONFORM file included in the Mesa distribution."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  export DRI_DRIVER_INSTALL_DIR=$XORG_PATH_DRI
+  export DRI_DRIVER_SEARCH_DIR=$XORG_PATH_DRI
+  export X11_INCLUDES=
+  MESA_DRI="--enable-dri --enable-dri3"
+  MESA_GLX="--enable-glx --enable-driglx-direct --enable-glx-tls"
+  MESA_EGL_PLATFORMS="--with-egl-platforms=x11,drm"
+else
+  PKG_DEPENDS_TARGET="toolchain Python:host expat libdrm"
+  MESA_DRI="--enable-dri --disable-dri3"
+  MESA_GLX="--disable-glx --disable-driglx-direct --disable-glx-tls"
+  MESA_EGL_PLATFORMS="--with-egl-platforms=drm"
+fi
 
 # configure GPU drivers and dependencies:
   get_graphicdrivers
@@ -46,8 +60,17 @@ if [ "$VDPAU_SUPPORT" = "yes" -a "$DISPLAYSERVER" = "x11" ]; then
   MESA_VDPAU="--enable-vdpau"
 else
   MESA_VDPAU="--disable-vdpau"
+fi
 
+XA_CONFIG="--disable-xa"
+for drv in $GRAPHIC_DRIVERS; do
+  [ "$drv" = "vmware" ] && XA_CONFIG="--enable-xa"
+done
 
+if [ "$OPENGLES_SUPPORT" = "yes" ]; then
+  MESA_GLES="--disable-gles1 --enable-gles2"
+else
+  MESA_GLES="--disable-gles1 --disable-gles2"
 fi
 
 PKG_CONFIGURE_OPTS_TARGET="CC_FOR_BUILD=$HOST_CC \
@@ -55,58 +78,53 @@ PKG_CONFIGURE_OPTS_TARGET="CC_FOR_BUILD=$HOST_CC \
                            CFLAGS_FOR_BUILD= \
                            CXXFLAGS_FOR_BUILD= \
                            LDFLAGS_FOR_BUILD= \
-                           X11_INCLUDES= \
-                           DRI_DRIVER_INSTALL_DIR=/usr/lib/dri \
-                           DRI_DRIVER_SEARCH_DIR=/usr/lib/dri \
-                           --enable-silent-rules \
                            --disable-debug \
                            --disable-mangling \
                            --enable-texture-float \
                            --enable-asm \
                            --disable-selinux \
                            --enable-opengl \
-                           --disable-gles1 \
-                           --enable-gles2 \
-                           --enable-dri \
-                           --disable-gallium-extra-hud \
-                           --enable-dri3 \
-                           --enable-glx \
+                           $MESA_GLES \
+                           $MESA_DRI \
+                           $MESA_GLX \
                            --disable-osmesa \
                            --disable-gallium-osmesa \
-                           --enable-egl --with-egl-platforms=x11,drm \
-                           --disable-xa \
+                           --enable-egl \
+                           $MESA_EGL_PLATFORMS \
+                           $XA_CONFIG \
                            --enable-gbm \
                            --disable-nine \
                            --disable-xvmc \
                            $MESA_VDPAU \
                            --disable-omx \
                            --disable-va \
-                           --disable-opencl --disable-opencl-icd \
+                           --disable-opencl \
+                           --enable-opencl-icd \
                            --disable-gallium-tests \
                            --enable-shared-glapi \
-                           --enable-driglx-direct \
-                           --enable-glx-tls \
-                           --disable-glx-read-only-text \
+                           --enable-shader-cache \
                            $MESA_GALLIUM_LLVM \
-                           --disable-valgrind \
-                           --with-sysroot=$SYSROOT_PREFIX \
+                           --disable-silent-rules \
                            --with-gl-lib-name=GL \
                            --with-osmesa-lib-name=OSMesa \
                            --with-gallium-drivers=$GALLIUM_DRIVERS \
-                           --with-dri-driverdir=/usr/lib/dri \
-                           --with-dri-searchpath=/usr/lib/dri \
                            --with-dri-drivers=$DRI_DRIVERS \
-                           --without-vulkan-drivers"
+                           --with-vulkan-drivers=no \
+                           --with-sysroot=$SYSROOT_PREFIX"
 
 pre_configure_target() {
-  export LIBS="-lxcb-dri3 -lxcb-dri2 -lxcb-xfixes -lxcb-present -lxcb-sync -lxshmfence -lz"
+  if [ "$DISPLAYSERVER" = "x11" ]; then
+    export LIBS="-lxcb-dri3 -lxcb-dri2 -lxcb-xfixes -lxcb-present -lxcb-sync -lxshmfence -lz"
+  fi
 }
 
 post_makeinstall_target() {
-  # rename and relink for cooperate with nvidia drivers
+  if [ "$DISPLAYSERVER" = "x11" ]; then
+    # rename and relink for cooperate with nvidia drivers
     rm -rf $INSTALL/usr/lib/libGL.so
     rm -rf $INSTALL/usr/lib/libGL.so.1
     ln -sf libGL.so.1 $INSTALL/usr/lib/libGL.so
     ln -sf /var/lib/libGL.so $INSTALL/usr/lib/libGL.so.1
     mv $INSTALL/usr/lib/libGL.so.1.2.0 $INSTALL/usr/lib/libGL_mesa.so.1
+  fi
 }
