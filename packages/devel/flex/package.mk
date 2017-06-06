0bb8a644edbe3cd24d48338e5c556eb3084a2e13
AV <27rurk@gmail.com>
2017-06-07 02:26:51 +0300

    autocommit on change

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="flex"
-PKG_VERSION="2.6.4"
+PKG_VERSION="2.6.1"
 PKG_SITE="http://flex.sourceforge.net/"
 PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
@@ -27,7 +27,7 @@ PKG_LONGDESC="flex is a tool for generating programs that perform pattern-matchi
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="ac_cv_lib_util_getloadavg=no --disable-shared"
 
 post_makeinstall_host() {
   cat > $ROOT/$TOOLCHAIN/bin/lex << "EOF"
