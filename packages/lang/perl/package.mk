1b949e6a9b1c606349ef6bc2cd6a7053a13cefbb
AV <27rurk@gmail.com>
2017-06-15 00:29:36 +0300

    autocommit on change

--- packages/lang/perl/package.mk
+++ packages/lang/perl/package.mk
@@ -1,51 +1,35 @@
 PKG_NAME="perl"
-PKG_VERSION="5.24.1"
+PKG_VERSION="5.26.0"
 PKG_URL="http://www.cpan.org/src/5.0/perl-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain openssl gdbm db"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-CCACHE_DISABLE=1
-
 configure_target() {
   ./Configure -des \
-		-Dcccdlflags='-fPIC' \
-		-Dccdlflags='-rdynamic' \
-		-Dprefix=/usr \
-		-Dprivlib=$_privlib \
-		-Darchlib=$_archlib \
-		-A ccflags="$CFLAGS -fPIC -DPIC" \
-		-Dcc="$CC" \
-		-Duse64bitint \
-		-Dldflags="$LDFLAGS -fPIC" \
-		-Dlibs="-lm -lssl -lcrypto -pthread" \
-		-Doptimize="$CFLAGS -ffunction-sections -fdata-sections -finline-limit=8 -ffast-math" \
+  		-Duselargefiles \
+  		-Duse64bitint \
+  		-A ccflags="$CFLAGS -fPIC -DPIC" \
+  		-Dcc="$CC" \
+  		-Dldflags="$LDFLAGS -fPIC" \
+  		-Dlibs="-lm -lcrypt -pthread" \
 		-Dvendorprefix=/usr \
-		-Dvendorlib=/usr/share/perl5/vendor_perl \
-		-Dvendorarch=/usr/lib/perl5/vendor_perl \
-		-Dsiteprefix=/usr/local \
-		-Dsitelib=/usr/local/share/perl5/site_perl \
-		-Dsitearch=/usr/local/lib/perl5/site_perl \
 		-Dlocincpth=' ' \
 		-Duselargefiles \
 		-Dusethreads \
 		-Duseshrplib \
-		-Dd_semctl_semun \
-		-Dman1dir=/usr/share/man/man1 \
-		-Dman3dir=/usr/share/man/man3 \
-		-Dinstallman1dir=/usr/share/man/man1 \
-		-Dinstallman3dir=/usr/share/man/man3 \
-		-Dman1ext='1' \
-		-Dman3ext='3pm' \
-		-Dcf_by='OE' \
-		-Ud_csh \
-		-Dusenm \
-		|| return 1
-		make; make
+		-DEBUGGING=none \
+		-Uusevendorprefix \
+		-Duseithreads \
+		-Duseperlio \
+		-Uman1dir \
+		-Uman3dir \
+		-Usiteman1dir \
+		-Usiteman3dir
 }
 
-               
+
 post_makeinstall_target() {
   ln -sf perl$PKG_VERSION $INSTALL/usr/bin/perl
 }
