3b20b6c2a72c64ce69171fa9832936d91c9c63e0
AV <27rurk@gmail.com>
2017-06-15 09:57:32 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="expat"
-#PKG_VERSION="2.2.0"
-#PKG_URL="$SOURCEFORGE_SRC/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_VERSION="bfa1152"
-PKG_GIT_URL="https://github.com/libexpat/libexpat"
+PKG_VERSION="2.2.0"
+PKG_URL="$SOURCEFORGE_SRC/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+#PKG_VERSION="bfa1152"
+#PKG_GIT_URL="https://github.com/libexpat/libexpat"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="textproc"
 
@@ -10,11 +10,11 @@ PKG_IS_ADDON="no"
 PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
 
-post_unpack() {
-  cp -r $PKG_BUILD/expat/* $PKG_BUILD/
-}
+#post_unpack() {
+#  cp -r $PKG_BUILD/expat/* $PKG_BUILD/
+#}
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
 
 pre_make_target() {
   # fix builderror when building in subdirs
