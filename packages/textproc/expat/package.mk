8481d131be1547dfa4049344d0f491bbf3d35072
AV <27rurk@gmail.com>
2017-06-15 07:28:30 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -9,15 +9,17 @@ PKG_SHORTDESC="expat: XML parser library"
 PKG_LONGDESC="Expat is an XML parser library written in C. It is a stream-oriented parser in which an application registers handlers for things the parser might find in the XML document (like start tags). An introductory article on using Expat is available on xml.com."
 
 PKG_IS_ADDON="no"
-PKG_USE_CMAKE="no"
-PKG_AUTORECONF="yes"
+PKG_USE_CMAKE="yes"
+PKG_AUTORECONF="no"
+
 CONCURRENCY_MAKE_LEVEL=1
 
-post_unpack() {
- rm $ROOT/$PKG_BUILD/m4/libtool.m4
+#CFLAGS="$CFLAGS -Wall -Wextra -pedantic -Wno-overlength-strings"
+CFLAGS="$CFLAGS -DXML_UNICODE"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
+
+pre_make_target() {
+  # fix builderror when building in subdirs
+  cp -r ../doc .
 }
-
-CFLAGS="$CFLAGS -Wall -Wextra -pedantic -Wno-overlength-strings"
-#CFLAGS="$CFLAGS -DXML_UNICODE"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-shared --enable-static --with-gnu-ld --with-pic"
