0097f438171d5ae42f42d206fa85d8dcf0cc3953
AV <27rurk@gmail.com>
2017-06-16 01:17:42 +0300

    autocommit on change

--- packages/compress/lrzip/package.mk
+++ packages/compress/lrzip/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="lrzip"
-PKG_VERSION="ac393ef"
+PKG_VERSION="1a30639"
 PKG_GIT_URL="https://github.com/ckolivas/lrzip"
 PKG_DEPENDS_TARGET="toolchain zlib lzo xz bzip2"
 PKG_DEPENDS_HOST="zlib:host lzo:host xz:host bzip2:host"
@@ -7,7 +7,7 @@ PKG_SECTION="compress"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --with-gnu-ld --enable-largefile --disable-doc"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-largefile --disable-doc"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-largefile --disable-doc --with-zlib=$ROOT/$TOOLCHAIN"
 
