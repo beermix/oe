bf5e7f7c0d580ded2588374a47b61021cd0a9888
AV <27rurk@gmail.com>
2017-06-22 00:02:44 +0300

    autocommit on change

--- packages/-my/DisplayCAL/package.mk
+++ packages/-my/DisplayCAL/package.mk
@@ -3,8 +3,7 @@ PKG_VERSION="3.3.1.0"
 PKG_SITE="http://www.X.org"
 PKG_URL="https://dl.dropboxusercontent.com/s/i14sk2shsh9umy6/DisplayCAL-3.3.1.0.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo wxPython"
-PKG_SECTION="service/system"
-
+PKG_SECTION="system"
 PKG_AUTORECONF="no"
 
 make_target() {
--- packages/-my/wxPython/package.mk
+++ packages/-my/wxPython/package.mk
@@ -1,9 +1,8 @@
 PKG_NAME="wxPython"
-PKG_VERSION="wxPy-3.0.2.0"
-PKG_GIT_URL="https://github.com/wxWidgets/wxPython"
-PKG_DEPENDS_TARGET="toolchain libX11 setuptools:host libpng libjpeg-turbo gtk3+ pango"
+PKG_VERSION="wxPython-4.0.0a3"
+PKG_GIT_URL="https://github.com/wxWidgets/Phoenix"
+PKG_DEPENDS_TARGET="toolchain libX11 setuptools:host libpng libjpeg-turbo gtk+ pango"
 PKG_SECTION="service/system"
 PKG_AUTORECONF="no"
 
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-gui"
+PKG_CONFIGURE_OPTS_TARGET="--enable-gui"
