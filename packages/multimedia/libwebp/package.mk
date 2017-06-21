0f8f8aba997b5fb71f38746668be2f2c472f0b10
AV <27rurk@gmail.com>
2017-06-21 19:15:28 +0300

    autocommit on change

--- packages/multimedia/libwebp/package.mk
+++ packages/multimedia/libwebp/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="libwebp"
-PKG_VERSION="7b01298"
+PKG_VERSION="7ca0df1"
 PKG_GIT_URL="https://github.com/webmproject/libwebp"
 PKG_DEPENDS_TARGET="toolchain intel-vaapi-driver libva"
 PKG_PRIORITY="optional"
@@ -9,3 +9,7 @@ PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL
+}
