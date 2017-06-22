78f1d7a1d2e3ca1b73bf27be5df6891b59032639
AV <27rurk@gmail.com>
2017-06-22 10:54:40 +0300

    autocommit on change

--- packages/multimedia/intel-gpu-tools/package.mk
+++ packages/multimedia/intel-gpu-tools/package.mk
@@ -1,18 +1,13 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="1.19"
+PKG_VERSION="1.18"
 PKG_URL="http://xorg.freedesktop.org/releases/individual/app/intel-gpu-tools-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain systemd mesa libdrm libpciaccess cairo dri2proto swig:host procps-ng"
 PKG_SECTION="tools"
 PKG_SHORTDESC="x11-utils"
 PKG_AUTORECONF="yes"
 
-#pre_configure_target() {
-#  strip_lto
-#}
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shader-debugger \
-			      --disable-dumper \
-			      --disable-tests \
+PKG_CONFIGURE_OPTS_TARGET="--disable-tests \
 			      --without-libunwind \
 			      --disable-gtk-doc \
 			      --disable-gtk-doc-html \
--- packages/wayland/wayland/package.mk
+++ packages/wayland/wayland/package.mk
@@ -26,9 +26,10 @@ PKG_SHORTDESC="The mtdev is a stand-alone library which transforms all variants 
 PKG_LONGDESC="The mtdev is a stand-alone library which transforms all variants of kernel MT events to the slotted type B protocol. The events put into mtdev may be from any MT device, specifically type A without contact tracking, type A with contact tracking, or type B with contact tracking. See the kernel documentation for further details."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --disable-documentation"
 
 pre_configure_target() {
-  strip_lto
+  CFLAGS="$CFLAGS -fPIC"
 }
-PKG_CONFIGURE_OPTS_TARGET="--disable-documentation --disable-shared"
