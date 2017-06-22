c404e548efcdd08ab781d22b9ab9656e29ba29f8
AV <27rurk@gmail.com>
2017-06-22 05:12:28 +0300

    autocommit on change

--- packages/-my/-python/numpy/package.mk
+++ packages/-my/-python/numpy/package.mk
@@ -1,22 +1,16 @@
 PKG_NAME="numpy"
-PKG_VERSION="v1.12.1"
-PKG_SITE="https://pypi.python.org/pypi/PyAMF/"
-PKG_GIT_URL="https://github.com/numpy/numpy.git"
+PKG_VERSION="1.13.0"
+PKG_SITE="https://pypi.python.org/pypi/numpy/#downloads"
+PKG_URL="https://pypi.python.org/packages/05/84/0feb999c05f252af50a5fbc463268044feda92cdaad8cb0d0a6073d76057/numpy-1.13.0.zip"
 PKG_DEPENDS_TARGET="toolchain Python distutilscross:host cython"
 PKG_SECTION="python/system"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-
 make_target() {
-  python setup.py build
+  python setup.py build 
 }
 
 makeinstall_target() {
-  python setup.py install --prefix=$INSTALL/usr
-}
-
-post_makeinstall_target() {
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/$PKG_NAME-*.egg-info
+  python setup.py install --root=$INSTALL --prefix=/usr
 }
--- packages/-my/DisplayCAL/package.mk
+++ packages/-my/DisplayCAL/package.mk
@@ -1,13 +1,13 @@
 PKG_NAME="DisplayCAL"
 PKG_VERSION="3.3.1.0"
-PKG_SITE="http://www.X.org"
+PKG_SITE="https://sourceforge.net/projects/dispcalgui"
 PKG_URL="https://sourceforge.net/projects/dispcalgui/files/release/$PKG_VERSION/DisplayCAL-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo wxPython"
+PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo numpy wxPython"
 PKG_SECTION="system"
 PKG_AUTORECONF="no"
 
 make_target() {
-  python setup.py
+  python setup.py build
 }
 
 makeinstall_target() {
