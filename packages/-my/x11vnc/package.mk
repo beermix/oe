7359e4056c53287b952e81ba1d1a2190ac641f92
27rurk <27rurk@gmail.com>
2017-06-22 11:34:38 +0300

    autocommit on change

--- packages/-my/x11vnc/package.mk
+++ packages/-my/x11vnc/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="x11vnc"
 PKG_VERSION="0.9.13"
 PKG_SITE="http://www.karlrunge.com/x11vnc/#downloading"
-PKG_URL="https://sourceforge.net/projects/libvncserver/files/x11vnc/0.9.13/x11vnc-0.9.13.tar.gz"
+PKG_URL="https://sourceforge.net/projects/libvncserver/files/x11vnc/$PKG_VERSION/x11vnc-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo"
 PKG_SECTION="service/system"
 PKG_AUTORECONF="yes"
@@ -9,7 +9,7 @@ PKG_AUTORECONF="yes"
 pre_build_target() {
    mkdir -p $ROOT/$PKG_BUILD/.$TARGET_NAME
    cp -RP $ROOT/$PKG_BUILD/* $ROOT/$PKG_BUILD/.$TARGET_NAME
-   export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
+   #export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
