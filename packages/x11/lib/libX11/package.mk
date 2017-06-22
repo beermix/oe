11276511a2f35700312ae7dacb990dd7e0147c3e
27rurk <27rurk@gmail.com>
2017-06-23 00:20:33 +0300

    autocommit on change

--- packages/x11/lib/libX11/package.mk
+++ packages/x11/lib/libX11/package.mk
@@ -29,7 +29,7 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-secure-rpc \
-                           --disable-loadable-i18n \
+                           --enable-loadable-i18n \
                            --disable-loadable-xcursor \
                            --enable-xthreads \
                            --disable-xcms \
