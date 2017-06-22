9b6c3053122d2da38d64f5cf22bab14e42adcb4a
AV <27rurk@gmail.com>
2017-06-22 04:53:30 +0300

    autocommit on change

--- packages/-my/wxPython/package - копия.mk
+++
@@ -1,9 +0,0 @@
-PKG_NAME="wxPython"
-PKG_VERSION="wxPy-3.0.2.0"
-PKG_GIT_URL="https://github.com/wxWidgets/wxPython"
-PKG_DEPENDS_TARGET="toolchain libX11 setuptools:host libpng libjpeg-turbo gtk3+ pango"
-PKG_SECTION="service/system"
-PKG_AUTORECONF="no"
-
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-gui"
--- packages/-my/wxPython/package.mk
+++ packages/-my/wxPython/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="wxPython"
-PKG_VERSION="wxPython-4.0.0a3"
-PKG_GIT_URL="https://github.com/wxWidgets/Phoenix"
+PKG_VERSION="wxPy-3.0.2.0"
+PKG_GIT_URL="https://github.com/wxWidgets/wxPython"
 PKG_DEPENDS_TARGET="toolchain libX11 setuptools:host libpng libjpeg-turbo gtk+ pango"
-PKG_SECTION="service/system"
+PKG_SECTION="system"
 PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-gui"
