0b110a02870365b1eb81d559cb85cfdadaa70eda
27rurk <27rurk@gmail.com>
2017-06-23 03:55:12 +0300

    autocommit on change

--- packages/-my/nano/package.mk
+++ packages/-my/nano/package.mk
@@ -1,17 +1,16 @@
 PKG_NAME="nano"
-PKG_VERSION="2.8.4"
-PKG_URL="https://www.nano-editor.org/dist/v2.8/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain readline file"
+PKG_VERSION="2.7.3"
+PKG_URL="https://www.nano-editor.org/dist/v2.7/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain readline file sed"
 PKG_SECTION="shell/texteditor"
 PKG_SHORTDESC="nano: Pico editor clone with enhancements"
 PKG_LONGDESC="GNU nano (Nano's ANOther editor, or Not ANOther editor) is an enhanced clone of the Pico text editor."
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_ncursesw___set_escdelay=no \
-                           --enable-utf8 \
-                           --disable-nls \
-			      --enable-nanorc \
+PKG_CONFIGURE_OPTS_TARGET="--enable-utf8 \
+                           --sysconfdir=/storage/.config/nano \
+                           --disable-wrapping-as-root \
 			      --without-slang \
 			      --enable-color"
 
