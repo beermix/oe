8b9fcc84affc6f19fef712a544993f5d64ce5fd7
27rurk <27rurk@gmail.com>
2017-06-23 16:45:10 +0300

    autocommit on change

--- packages/x11/lib/libXfont2/package.mk
+++ packages/x11/lib/libXfont2/package.mk
@@ -26,10 +26,9 @@ PKG_SHORTDESC="libxfont: X font Library"
 PKG_LONGDESC="X font Library"
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared \
-                           --disable-ipv6 \
+PKG_CONFIGURE_OPTS_TARGET="--disable-ipv6 \
                            --enable-freetype \
                            --enable-builtins \
                            --disable-pcfformat \
