fede357c451e7ebf201e287126d2c8e1bbfea62c
27rurk <27rurk@gmail.com>
2017-07-03 14:12:22 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="expat"
-PKG_VERSION="2.2.0"
-PKG_URL="$SOURCEFORGE_SRC/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_VERSION="R_2_2_1"
+PKG_GIT_URL="https://github.com/libexpat/libexpat"
 #PKG_VERSION="bfa1152"
 #PKG_GIT_URL="https://github.com/libexpat/libexpat"
 PKG_DEPENDS_TARGET="toolchain"
@@ -10,9 +10,9 @@ PKG_IS_ADDON="no"
 PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
 
-#post_unpack() {
-#  cp -r $PKG_BUILD/expat/* $PKG_BUILD/
-#}
+post_unpack() {
+  cp -r $PKG_BUILD/expat/* $PKG_BUILD/
+}
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
 
