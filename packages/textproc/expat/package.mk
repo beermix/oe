27d6c14b666815da1d10513903cb51d888644e4b
27rurk <27rurk@gmail.com>
2017-07-03 15:15:03 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -1,11 +1,8 @@
 PKG_NAME="expat"
 PKG_VERSION="R_2_2_1"
 PKG_GIT_URL="https://github.com/libexpat/libexpat"
-#PKG_VERSION="bfa1152"
-#PKG_GIT_URL="https://github.com/libexpat/libexpat"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="textproc"
-
 PKG_IS_ADDON="no"
 PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
