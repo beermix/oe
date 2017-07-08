48195c32b64d06faef2bba4c61acb31ade1d6d88
27rurk <27rurk@gmail.com>
2017-07-08 21:45:31 +0300

    autocommit on change

--- packages/graphics/libjpeg-turbo/package.mk
+++ packages/graphics/libjpeg-turbo/package.mk
@@ -17,12 +17,8 @@
 ################################################################################
 
 PKG_NAME="libjpeg-turbo"
-PKG_VERSION="1.5.1"
-PKG_REV="1"
-PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="http://libjpeg-turbo.virtualgl.org/"
-PKG_URL="$SOURCEFORGE_SRC/libjpeg-turbo/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_VERSION="e5c1613"
+PKG_GIT_URL="https://github.com/libjpeg-turbo/libjpeg-turbo"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="graphics"
