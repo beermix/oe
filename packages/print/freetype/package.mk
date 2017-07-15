1410d36e3156b82f21d95f469bf38bd1b34a37aa
27rurk <27rurk@gmail.com>
2017-07-15 22:21:25 +0300

    autocommit on change

--- packages/print/freetype/package.mk
+++ packages/print/freetype/package.mk
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="freetype"
-PKG_VERSION="2.8"
-PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-#PKG_VERSION="390048f"
-#PKG_GIT_URL="git://git.sv.nongnu.org/freetype/freetype2.git"
+#PKG_VERSION="2.8"
+#PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_VERSION="38bdf22"
+PKG_GIT_URL="git://git.sv.nongnu.org/freetype/freetype2.git"
 PKG_DEPENDS_TARGET="toolchain zlib libpng bzip2"
 PKG_DEPENDS_HOST="zlib:host libpng:host bzip2:host"
 PKG_SECTION="print"
