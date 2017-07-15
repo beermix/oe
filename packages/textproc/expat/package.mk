bb9f6a3e78ca32012ef77eae8a95570c7e7bf2c3
27rurk <27rurk@gmail.com>
2017-07-16 00:40:34 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -1,17 +1,12 @@
 PKG_NAME="expat"
 PKG_VERSION="R_2_2_1"
 PKG_GIT_URL="https://github.com/libexpat/libexpat"
-PKG_DEPENDS_TARGET="toolchain libxml2"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="textproc"
 PKG_IS_ADDON="no"
 PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
 
-#post_unpack() {
-#  rm $PKG_BUILD/expat/README
-#  cp -r $PKG_BUILD/expat/* $PKG_BUILD/
-#}
-
 PKG_CMAKE_SCRIPT_TARGET="expat/CMakeLists.txt"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
