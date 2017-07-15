5995b68e44c2c98c77e0b4b0f1d6290e8a764683
27rurk <27rurk@gmail.com>
2017-07-15 19:04:34 +0300

    autocommit on change

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="9260e0a"
+PKG_VERSION="cc1f21c"
 PKG_GIT_URL="https://github.com/xbmc/xbmc.git"
 PKG_GIT_BRANCH="Krypton"
 PKG_KEEP_CHECKOUT="no"
