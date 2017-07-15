706094695eb1f2ef3a2fc8c56ed73ff6e250e269
27rurk <27rurk@gmail.com>
2017-07-15 19:31:53 +0300

    autocommit on change

--- packages/-my/vlc/package.mk
+++ packages/-my/vlc/package.mk
@@ -5,7 +5,7 @@
 
 PKG_NAME="vlc"
 PKG_VERSION="3.0.0-git"
-PKG_REV="20170621-0239"
+PKG_REV="20170715-0247"
 PKG_SITE="https://nightlies.videolan.org/build/source/"
 PKG_URL="https://nightlies.videolan.org/build/source/vlc-3.0.0-$PKG_REV-git.tar.xz"
 PKG_DEPENDS_TARGET="toolchain dbus ffmpeg libdvbpsi libmpeg2 libogg libvorbis libsamplerate zlib lua:host lua gnutls"
