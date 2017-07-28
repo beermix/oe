88b14eb019b54b2b41b7a95fc47d008ddecf1937
27rurk <27rurk@gmail.com>
2017-07-28 19:01:31 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="gcc"
 #PKG_VERSION="6.4.0"
 #PKG_URL="ftp://gcc.gnu.org/pub/gcc/releases/gcc-$PKG_VERSION/gcc-$PKG_VERSION.tar.xz"
-PKG_VERSION="6-20170719"
+PKG_VERSION="6-20170726"
 PKG_URL="ftp://gcc.gnu.org/pub/gcc/snapshots/LATEST-6/gcc-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_BOOTSTRAP="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host"
 PKG_DEPENDS_TARGET="gcc:host"
