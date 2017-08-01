bd3674d60a3fdb10f82cc7243c5e09cc8bc7ea41
27rurk <27rurk@gmail.com>
2017-08-01 14:57:24 +0300

    autocommit on change

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="17.2.0-rc1"
+PKG_VERSION="17.2.0-rc2"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
