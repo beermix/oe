5cd4d4b9273f4aec015a2e76fdd2d9be6a402f40
27rurk <27rurk@gmail.com>
2017-08-03 21:40:47 +0300

    autocommit on change

--- packages/-my/libidn2/package.mk
+++ packages/-my/libidn2/package.mk
@@ -1,6 +1,6 @@
 KG_NAME="libidn2"
-PKG_VERSION="2.0.2"
-PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_VERSION="2.0.3"
+PKG_URL="https://ftp.gnu.org/gnu/libidn/libidn2-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
