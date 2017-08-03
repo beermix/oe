c7152bf34b090b742297c1627b4647bd820bf468
27rurk <27rurk@gmail.com>
2017-08-03 19:18:29 +0300

    autocommit on change

--- packages/devel/icu/package.mk
+++ packages/devel/icu/package.mk
@@ -1,21 +1,37 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 PKG_NAME="icu"
-PKG_VERSION="59.1"
+PKG_VERSION="57.1"
 PKG_ARCH="any"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.icu-project.org"
-PKG_URL="http://download.icu-project.org/files/${PKG_NAME}4c/${PKG_VERSION}/${PKG_NAME}4c-${PKG_VERSION//./_}-src.tgz"
+PKG_URL="https://dl.dropboxusercontent.com/s/weeqpr1qdzhd2dc/icu4c-57_1-src.tgz"
 PKG_SOURCE_DIR="icu"
 PKG_DEPENDS_TARGET="toolchain icu:host"
 PKG_SECTION="textproc"
+PKG_SHORTDESC="International Components for Unicode library"
+PKG_LONGDESC="International Components for Unicode library"
+
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_configure_target() {
-  export LIBS="$LIBS -latomic"
-}
-
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
-
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-shared \
                            --disable-samples \
@@ -24,6 +40,10 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
 
 PKG_CONFIGURE_SCRIPT="source/configure"
 
-post_makeinstall_target() {
-  rm -rf $INSTALL
+make_host() {
+  make VERBOSE=1
+}
+
+make_target() {
+  make VERBOSE=1
 }
