a911a86a40007bf7c8602c14fd57e22a7aeb1d4a
27rurk <27rurk@gmail.com>
2017-08-03 21:35:09 +0300

    autocommit on change

--- packages/-my/hwloc/package.mk
+++ packages/-my/hwloc/package.mk
@@ -3,15 +3,14 @@ PKG_VERSION="1.11.7"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib libevent libpciaccess"
 PKG_SECTION="security"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
 			      --without-x \
 			      --disable-cuda \
 			      --disable-nvml \
-			      --enable-pci \
-			      --enable-malloc0returnsnull"
+			      --enable-pci"
 			      
 post_makeinstall_target() {
   rm -rf $INSTALL
