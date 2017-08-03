2a5ffe79a50e03be8a87055c1518d8bff17fba34
27rurk <27rurk@gmail.com>
2017-08-03 16:32:05 +0300

    autocommit on change

--- aCC/elfutils-0.168/backends/libebl_arm.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIBEBL_ARM_H
-#define _LIBEBL_ARM_H 1
-
-#include <libdw.h>
-
-extern int arm_return_value_location_soft(Dwarf_Die *, const Dwarf_Op **locp);
-extern int arm_return_value_location_hard(Dwarf_Die *, const Dwarf_Op **locp);
-
-#endif
