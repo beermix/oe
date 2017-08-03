5313f036b16eccdb3e2a06d99739a69ad962b490
27rurk <27rurk@gmail.com>
2017-08-03 22:04:33 +0300

    autocommit on change

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -42,8 +42,8 @@ if [ "$MEDIACENTER" = "kodi" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inputstream.rtmp"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inputstream.adaptive"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET visualization.shadertoy"
-  #PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET audioencoder.flac"
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET kodi-theme-AeonNox"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET audioencoder.flac"
+  #PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET kodi-theme-AeonNox"
 
 # other packages
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET OpenELEC-settings"
