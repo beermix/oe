afbb9e4ba165e37a07603d4f4cfea851a7b15002
27rurk <27rurk@gmail.com>
2017-08-04 11:48:42 +0300

    autocommit on change

--- packages/devel/libedit/package.mk
+++ packages/devel/libedit/package.mk
@@ -5,6 +5,6 @@ PKG_DEPENDS_TARGET="toolchain netbsd-curses"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-pic"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
