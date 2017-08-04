dc9b620afb723a493e6d0a06ed0a4ace5d580613
27rurk <27rurk@gmail.com>
2017-08-04 17:47:49 +0300

    autocommit on change

--- packages/-my/x11vnc/package.mk
+++ packages/-my/x11vnc/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="x11vnc"
 PKG_VERSION="0.9.14"
 PKG_SITE="http://www.karlrunge.com/x11vnc/#downloading"
-PKG_URL="https://github.com/LibVNC/x11vnc/archive/0.9.14.tar.gz"
+PKG_URL="https://dl.dropboxusercontent.com/s/laj3ddkdi2v2nwi/x11vnc-0.9.14.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext libXtst libjpeg-turbo"
 PKG_SECTION="service/system"
 PKG_AUTORECONF="yes"
