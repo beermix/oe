c9bf71275c3fd019688d5143c10cf4cbccef295f
27rurk <27rurk@gmail.com>
2017-08-04 15:28:14 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -11,6 +11,4 @@ pre_configure_target() {
   export CFLAGS="$CFLAGS -DXML_POOR_ENTROPY"
 }
 
-PKG_CMAKE_SCRIPT_TARGET="expat/CMakeLists.txt"
-
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
