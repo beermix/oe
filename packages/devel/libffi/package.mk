0be936c47992eada82dcc0fa1219e1d49bae445c
27rurk <27rurk@gmail.com>
2017-08-04 10:53:29 +0300

    autocommit on change

--- packages/devel/libffi/package.mk
+++ packages/devel/libffi/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,13 +18,11 @@
 
 PKG_NAME="libffi"
 PKG_VERSION="3.2.1"
-PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://sourceware.org/$PKG_NAME/"
 PKG_URL="ftp://sourceware.org/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="Foreign Function Interface Library"
 PKG_LONGDESC="The libffi library provides a portable, high level programming interface to various calling conventions. This allows a programmer to call any function specified by a call interface description at run-time."
@@ -33,6 +31,8 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-debug \
+                           --enable-static --disable-shared \
+                           --with-pic \
                            --enable-structs \
                            --enable-raw-api \
                            --disable-purify-safety \
