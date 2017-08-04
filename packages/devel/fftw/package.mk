ec6387ea5801e03a30170d8ec88a17bdf4712f39
27rurk <27rurk@gmail.com>
2017-08-04 13:01:45 +0300

    autocommit on change

--- packages/devel/fftw/package.mk
+++ packages/devel/fftw/package.mk
@@ -1,13 +1,13 @@
 PKG_NAME="fftw"
 PKG_VERSION="3.3.6-pl2"
 PKG_URL="ftp://ftp.fftw.org/pub/fftw/fftw-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain hwloc"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service/system"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="no"
 
 pre_configure_target() {
-  export CFLAGS="$CFLAGS -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math -fopenmp"
+  export CFLAGS="$CFLAGS -O3 -malign-double -fstrict-aliasing -ffast-math -fopenmp -fPIC"
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
@@ -15,8 +15,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-openmp \
 			      --enable-silent-rules \
 			      --enable-sse2 \
-			      --enable-avx \
-			      --with-pic"
+			      --enable-avx"
 
 post_makeinstall_target() {
   rm -rf $INSTALL
