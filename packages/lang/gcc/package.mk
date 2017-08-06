7226241371f91fedde66035a7e53553434ce51dc
27rurk <27rurk@gmail.com>
2017-08-06 11:14:46 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -17,11 +17,11 @@
 ################################################################################
 
 PKG_NAME="gcc"
-PKG_VERSION="7.1.0"
+PKG_VERSION="6-20170802"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://gcc.gnu.org/"
-PKG_URL="ftp://gcc.gnu.org/pub/gcc/releases/$PKG_NAME-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="ftp://gcc.gnu.org/pub/gcc/snapshots/6-20170802/gcc-6-20170802.tar.xz"
 PKG_DEPENDS_BOOTSTRAP="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host isl:host"
 PKG_DEPENDS_TARGET="gcc:host"
 PKG_DEPENDS_HOST="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host isl:host glibc"
