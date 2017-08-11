edca632b00d77f06fbdc07b1bbbbc3a315e8cbff
27rurk <27rurk@gmail.com>
2017-08-11 20:35:17 +0300

    autocommit on change

--- packages/devel/pcre/package.mk
+++ packages/devel/pcre/package.mk
@@ -17,32 +17,24 @@
 ################################################################################
 
 PKG_NAME="pcre"
-PKG_VERSION="8.40"
+PKG_VERSION="8.41"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.pcre.org/"
 PKG_URL="http://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain pcre2"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="devel"
 PKG_SHORTDESC="pcre: Perl Compatible Regulat Expressions"
 PKG_LONGDESC="The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5. PCRE has its own native API, as well as a set of wrapper functions that correspond to the POSIX regular expression API. The PCRE library is free, even for building commercial software."
 
 PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN \
-             --enable-utf8 \
-             --enable-unicode-properties \
-             --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN --enable-utf8 --enable-unicode-properties --with-gnu-ld"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-             --enable-static \
-             --enable-utf8 \
-             --enable-pcre16 \
-             --enable-unicode-properties \
-             --with-gnu-ld"
+PKG_CONFIGURE_OPTS_TARGET="--enable-utf8 --enable-pcre16 --enable-unicode-properties --with-gnu-ld"
 
 pre_configure_target() {
   CFLAGS="$CFLAGS -fPIC"
