4af3d45fc7722b50b92a4bac719449768cd26b70
27rurk <27rurk@gmail.com>
2017-08-14 21:49:23 +0300

    autocommit on change

--- packages/-my/less/package.mk
+++ packages/-my/less/package.mk
@@ -6,8 +6,4 @@ PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  export LIBS="-lterminfo"
-}
-
 PKG_CONFIGURE_OPTS_TARGET="--with-regex=pcre"
--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -366,11 +366,13 @@ post_makeinstall_target() {
 }
 
 post_install() {
-# enable default services
+  enable_service kodi.target
   enable_service kodi-autostart.service
+  enable_service kodi-cleanlogs.service
   enable_service kodi-halt.service
   enable_service kodi-poweroff.service
   enable_service kodi-reboot.service
+  enable_service kodi-waitonnetwork.service
   enable_service kodi.service
   enable_service kodi-lirc-suspend.service
 }
