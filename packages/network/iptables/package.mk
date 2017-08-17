6ec08c62849090e2e08b6222c0d0ec2e965a37f2
27rurk <27rurk@gmail.com>
2017-08-17 23:16:51 +0300

    autocommit on change

--- packages/network/iptables/package.mk
+++ packages/network/iptables/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.netfilter.org/"
 PKG_URL="http://www.netfilter.org/projects/iptables/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain linux libmnl libnftnl"
+PKG_DEPENDS_TARGET="toolchain linux libmnl libnftnl libnetfilter_acct libnetfilter_conntrack libnetfilter_queue"
 PKG_PRIORITY="optional"
 PKG_SECTION="network"
 PKG_SHORTDESC="iptables: IP packet filter administration"
@@ -31,6 +31,5 @@ PKG_LONGDESC="Iptables is used to set up, maintain, and inspect the tables of IP
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
-
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-ipv6 --with-kernel=$(get_pkg_build linux)"
