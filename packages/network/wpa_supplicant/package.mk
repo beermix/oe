7af3dd7b49008451f7a8f489b198fe71ad1e1a8a
27rurk <27rurk@gmail.com>
2017-08-18 02:22:22 +0300

    autocommit on change

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -30,13 +30,11 @@ PKG_LONGDESC="The wpa_supplicant is a free software implementation of an IEEE 80
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-CONCURRENCY_MAKE_LEVEL=1
-
 PKG_MAKE_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 PKG_MAKEINSTALL_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 
 configure_target() {
-  LDFLAGS="$LDFLAGS -lpthread -lm"
+#  LDFLAGS="$LDFLAGS -lpthread -lm"
 
   cp $PKG_DIR/config/makefile.config wpa_supplicant/.config
 # echo "CONFIG_TLS=gnutls" >> .config
