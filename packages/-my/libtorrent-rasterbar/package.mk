7783e0ff54202df9055a37aa913a0a07455f7c31
27rurk <27rurk@gmail.com>
2017-08-18 00:45:06 +0300

    autocommit on change

--- packages/-my/libtorrent-rasterbar/package.mk
+++ packages/-my/libtorrent-rasterbar/package.mk
@@ -1,5 +1,6 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="b065536"
+PKG_VERSION="c074e87"
+PKG_SITE="https://github.com/arvidn/libtorrent/tree/RC_1_0"
 PKG_GIT_URL="https://github.com/arvidn/libtorrent"
 PKG_DEPENDS_TARGET="toolchain openssl expat boost"
 PKG_SECTION="devel"
@@ -10,13 +11,7 @@ PKG_AUTORECONF="no"
 pre_configure_target() {
   cd $ROOT/$PKG_BUILD
   sh autotool.sh
-  export CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE"
-  sed -i 's/$PKG_CONFIG openssl --libs-only-/$PKG_CONFIG openssl --static --libs-only-/' $ROOT/$PKG_BUILD/configure
-  sed -i -e s/Windows.h/windows.h/ -e s/Wincrypt.h/wincrypt.h/ $ROOT/$PKG_BUILD/ed25519/src/seed.cpp
-  
-  #export CFLAGS="-O3 -pipe -fstack-protector-strong"
-  #CPPFLAGS="-D_FORTIFY_SOURCE=2"
-  #LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
+
   strip_lto
 }
 
