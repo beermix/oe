c7ccf5cdacef999367c657fec01aef5285d0ada6
27rurk <27rurk@gmail.com>
2017-08-18 00:56:31 +0300

    autocommit on change

--- packages/-my/libsigc++/package.mk
+++ packages/-my/libsigc++/package.mk
@@ -1,16 +1,16 @@
 PKG_NAME="libsigc++"
-PKG_VERSION="2.99.8"
+PKG_VERSION="2.99.9"
 PKG_SITE="http://libsigc.sourceforge.net"
 PKG_URL="http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.99/libsigc++-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain boost"
 PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="no"
 
-
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
 			      --disable-shared \
 			      --disable-documentation \
+			      --with-gnu-ld \
 			      --with-pic \
 			      --with-boost-libdir=$SYSROOT_PREFIX/usr/lib"
 			   
--- packages/devel/icu/package.mk
+++ packages/devel/icu/package.mk
@@ -28,14 +28,14 @@ PKG_LONGDESC="International Components for Unicode library"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
-PKG_CONFIGURE_OPTS_TARGET="--with-cross-build=$ROOT/$PKG_BUILD/.$HOST_NAME"
+PKG_CONFIGURE_OPTS_HOST="--disable-samples --disable-tests --enable-static --disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--disable-samples --disable-tests --with-cross-build=$ROOT/$PKG_BUILD/.$HOST_NAME"
 
 PKG_CONFIGURE_SCRIPT="source/configure"
 
-#pre_configure_target() {
-#  export LIBS="$LIBS -latomic" --disable-samples --disable-tests 
-#}
+pre_configure_target() {
+  export LIBS="$LIBS -latomic" 
+}
 
 #post_makeinstall_target() {
 #  rm -rf $INSTALL
