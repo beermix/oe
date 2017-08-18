58bc6cdff94710cb71aa92a540f20c8aea509246
27rurk <27rurk@gmail.com>
2017-08-18 03:58:20 +0300

    autocommit on change

--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -1,9 +1,8 @@
-# Made by github.com/escalade
 PKG_NAME="gnutls"
 PKG_VERSION="3.5.14"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://www.gnutls.org"
+PKG_SITE="https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain nettle"
 PKG_SECTION="escalade/depends"
@@ -13,9 +12,9 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-included-libtasn1 \
-			   --with-included-unistring \
-			   --enable-local-libopts \
-			   --without-p11-kit \
-			   --with-libgcrypt \
-			   --disable-tools \
-			   --disable-doc"
+			      --with-included-unistring \
+			      --enable-local-libopts \
+			      --without-p11-kit \
+			      --with-libgcrypt \
+			      --disable-tools \
+			      --disable-doc"
