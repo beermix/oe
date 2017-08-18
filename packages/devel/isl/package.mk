0f3921c1da279412678a30e8d78627e819680aee
27rurk <27rurk@gmail.com>
2017-08-18 04:40:41 +0300

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -7,9 +7,9 @@ PKG_DEPENDS_HOST="gmp:host libtool:host intltool:host"
 PKG_DEPENDS_TARGET="toolchain gmp"
 PKG_SECTION="devel"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --without-clang --with-gmp=$ROOT/$TOOLCHAIN"
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --without-clang --with-gnu-ld --with-gmp=$ROOT/$TOOLCHAIN"
 
 pre_configure_host() {
   export CFLAGS="$CFLAGS -fPIC"
