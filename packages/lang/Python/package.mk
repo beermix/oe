9621caad69ad28e108323aa01de2f0a46b68540b
27rurk <27rurk@gmail.com>
2017-08-18 05:38:24 +0300

    autocommit on change

--- packages/lang/Python/package.mk
+++ packages/lang/Python/package.mk
@@ -26,7 +26,7 @@ PKG_NAME="Python"
 #PKG_GIT_URL="https://github.com/python/cpython"
 PKG_VERSION="2.7.13"
 PKG_URL="http://www.python.org/ftp/python/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="zlib:host bzip2:host sqlite:host"
+PKG_DEPENDS_HOST="zlib:host bzip2:host expat:host sqlite:host"
 PKG_DEPENDS_TARGET="toolchain sqlite expat zlib bzip2 openssl libffi readline Python:host bluez"
 PKG_PRIORITY="optional"
 PKG_SECTION="lang"
@@ -40,6 +40,8 @@ PKG_CONFIGURE_OPTS_HOST="--cache-file=config.cache \
                          --without-cxx-main \
                          --with-threads \
                          --enable-unicode=ucs4 \
+                         --enable-optimizations \
+                         --without-ensurepip \
                          --disable-ipv6"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
@@ -52,6 +54,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
                            ac_cv_file__dev_ptc=no \
                            ac_cv_have_long_long_format=yes \
                            --with-threads \
+                           --enable-optimizations \
                            --enable-unicode=ucs4 \
                            --disable-ipv6 \
                            --disable-profiling \
@@ -62,8 +65,9 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
                            --without-fpectl \
                            --with-wctype-functions \
                            --without-cxx-main \
+                           --with-system-expat \
                            --with-system-ffi \
-                           --with-system-expat"
+                           --without-ensurepip"
 
 post_patch() {
   # This is needed to make sure the Python build process doesn't try to
