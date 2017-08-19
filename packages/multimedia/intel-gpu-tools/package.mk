3dcb43167c87b509d495b01751dcb57cda3fb909
27rurk <27rurk@gmail.com>
2017-08-19 11:48:25 +0300

    autocommit on change

--- packages/multimedia/intel-gpu-tools/package.mk
+++ packages/multimedia/intel-gpu-tools/package.mk
@@ -1,9 +1,9 @@
 PKG_NAME="intel-gpu-tools"
 PKG_VERSION="1.18"
 PKG_URL="http://xorg.freedesktop.org/releases/individual/app/intel-gpu-tools-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain systemd mesa libdrm libpciaccess cairo dri2proto swig:host procps-ng"
+PKG_DEPENDS_TARGET="toolchain systemd procps-ng mesa libdrm libpciaccess cairo dri2proto swig:host"
 PKG_SECTION="tools"
 PKG_SHORTDESC="x11-utils"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no --disable-tests --without-libunwind"
