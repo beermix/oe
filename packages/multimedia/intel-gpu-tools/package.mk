14a9c07e69c0a9f97f68df21fb619e45dfc06d9b
AV <27rurk@gmail.com>
2017-09-08 02:23:50 +0300

    autocommit on change

--- packages/multimedia/intel-gpu-tools/package.mk
+++ packages/multimedia/intel-gpu-tools/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="5ce65a9"
+PKG_VERSION="d5de89e"
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
 PKG_GIT_URL="git://anongit.freedesktop.org/xorg/app/intel-gpu-tools"
 #PKG_URL="http://xorg.freedesktop.org/releases/individual/app/intel-gpu-tools-${PKG_VERSION}.tar.gz"
@@ -8,4 +8,4 @@ PKG_SECTION="tools"
 PKG_SHORTDESC="x11-utils"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no --disable-tests --without-libunwind --disable-shared --enable-static"
+PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no --disable-tests --without-libunwind --disable-shared --enable-static --with-gnu-ld"
