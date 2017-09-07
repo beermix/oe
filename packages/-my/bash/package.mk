782de5f4077aa27e6b1c5adeb4ed94e079d48117
AV <27rurk@gmail.com>
2017-09-08 01:08:17 +0300

    autocommit on change

--- packages/-my/bash/package.mk
+++ packages/-my/bash/package.mk
@@ -5,6 +5,8 @@ PKG_DEPENDS_TARGET="toolchain ncurses readline"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+CONCURRENCY_MAKE_LEVEL=1
+
 PKG_CONFIGURE_OPTS_TARGET="--bindir=/bin
 			      --with-curses \
 			      --enable-readline \
