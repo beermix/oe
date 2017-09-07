1abc232046bd531912d4b5b2925696c24ae1b4fa
AV <27rurk@gmail.com>
2017-09-08 02:34:48 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -18,7 +18,7 @@ pre_configure_host() {
 PKG_CMAKE_SCRIPT_TARGET="expat/CMakeLists.txt"
 PKG_CMAKE_SCRIPT_HOST="expat/CMakeLists.txt"
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
 
 PKG_CMAKE_OPTS_HOST="-DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
 
