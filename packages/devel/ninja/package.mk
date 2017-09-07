52fc0ab207e45e8cab6d29ce95655b134104db26
AV <27rurk@gmail.com>
2017-09-07 21:52:01 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -17,8 +17,7 @@
 ################################################################################
 
 PKG_NAME="ninja"
-#PKG_VERSION="v1.7.2"
-PKG_VERSION="327d6ce"
+PKG_VERSION="v1.8.1"
 PKG_GIT_URL="https://github.com/ninja-build/ninja"
 PKG_DEPENDS_HOST="re2c:host"
 PKG_SECTION="devel"
