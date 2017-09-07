558518a4e2560c9201081846dc486ea24d3e45a6
AV <27rurk@gmail.com>
2017-09-07 22:24:10 +0300

    autocommit on change

--- packages/compress/libzip/package.mk
+++ packages/compress/libzip/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libzip"
-PKG_VERSION="1.2.0"
+PKG_VERSION="1.3.0"
 PKG_SITE="http://www.nih.at/libzip/"
 PKG_URL="http://www.nih.at/libzip/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
