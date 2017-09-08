bba9cdebe05900cf8d105b471db5a58d6a000aea
AV <27rurk@gmail.com>
2017-09-08 15:36:40 +0300

    autocommit on change

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -81,6 +81,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
                            --disable-localed \
                            --disable-coredump \
                            --disable-polkit \
+                           --disable-kdbus \
                            --disable-resolved \
                            --disable-networkd \
                            --disable-efi \
