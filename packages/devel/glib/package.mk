7aaa548fd65986b90f71cf8b4e23fbbd89eed9e1
AV <27rurk@gmail.com>
2017-09-08 09:13:39 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -54,7 +54,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_snprintf_c99=yes \
                            --with-gnu-ld \
                            --enable-static \
                            --with-threads=posix \
-                           --with-pcre=system"
+                           --with-pcre=internal"
 
 post_makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
