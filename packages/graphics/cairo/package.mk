d2370d87acaa3c414215755a129a303786690fa6
AV <27rurk@gmail.com>
2017-09-08 07:43:09 +0300

    autocommit on change

--- packages/graphics/cairo/package.mk
+++ packages/graphics/cairo/package.mk
@@ -60,8 +60,8 @@ fi
 
 PKG_CONFIGURE_OPTS_TARGET="$PKG_CAIRO_CONFIG \
                            --enable-silent-rules \
-                           --disable-shared \
-                           --enable-static \
+                           --enable-shared \
+                           --disable-static \
                            --disable-gtk-doc \
                            --enable-largefile \
                            --enable-atomic \
