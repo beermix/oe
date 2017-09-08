13f7974d497ef51c12aeac187da4c02da96bf8e7
AV <27rurk@gmail.com>
2017-09-08 11:59:50 +0300

    autocommit on change

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="fluidsynth"
-PKG_VERSION="1.1.6"
+PKG_VERSION="v1.1.7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://fluidsynth.org/"
-PKG_URL="$SOURCEFORGE_SRC/project/fluidsynth/fluidsynth-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_GIT_URL="https://github.com/FluidSynth/fluidsynth"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
