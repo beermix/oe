a5e6671e71c21d4c68893eb4cc94e2966e5e9888
AV <27rurk@gmail.com>
2017-09-08 23:32:03 +0300

    autocommit on change

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -10,7 +10,7 @@ PKG_LONGDESC="The Open Source toolkit for Secure Sockets Layer and Transport Lay
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-CONCURRENCY_MAKE_LEVEL=1
+#CONCURRENCY_MAKE_LEVEL=1
 
 PKG_CONFIGURE_OPTS_SHARED="--openssldir=/etc/ssl \
                            --libdir=lib \
