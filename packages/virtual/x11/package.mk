b9f5e247eb339249f97d2f8052190830c597d778
AV <27rurk@gmail.com>
2017-09-08 16:30:43 +0300

    autocommit on change

--- packages/virtual/x11/package.mk
+++ packages/virtual/x11/package.mk
@@ -54,6 +54,6 @@ else
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xf86-input-evdev"
 fi
 
-#for drv in $XORG_DRIVERS; do
-#  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xf86-video-$drv"
-#done
+for drv in $XORG_DRIVERS; do
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xf86-video-$drv"
+done
