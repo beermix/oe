380fdee7c5011d74ccaa5a3eb6712018b2698fde
AV <27rurk@gmail.com>
2017-09-08 13:13:24 +0300

    autocommit on change

--- packages/devel/libelf/package.mk
+++ packages/devel/libelf/package.mk
@@ -26,7 +26,7 @@ PKG_SHORTDESC="mpfr: A C library for multiple-precision floating-point computati
 PKG_LONGDESC="The MPFR library is a C library for multiple-precision floating-point computations with exact rounding (also called correct rounding). It is based on the GMP multiple-precision library. The main goal of MPFR is to provide a library for multiple-precision floating-point computation which is both efficient and has well-defined semantics. It copies the good ideas from the ANSI/IEEE-754 standard for double-precision floating-point arithmetic (53-bit mantissa)."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="-enable-elf64=yes --disable-sanity-checks --disable-shared --enable-elf64"
 			 
@@ -35,13 +35,11 @@ PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
 pre_configure_host() {
   export CFLAGS="$CFLAGS -fPIC"
   export CXXFLAGS="$CXXFLAGS -fPIC"
-  export LDFLAGS="$LDFLAGS -fPIC"
 }
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
   export CXXFLAGS="$CXXFLAGS -fPIC"
-  export LDFLAGS="$LDFLAGS -fPIC"
 }
 
 post_make_target() {
