2587ef67a2312ed620a6d9c423444faef634c520
AV <27rurk@gmail.com>
2017-09-10 02:09:45 +0300

    autocommit on change

--- packages/-my/coreutils/package.mk
+++ packages/-my/coreutils/package.mk
@@ -4,7 +4,7 @@ PKG_URL="http://ftpmirror.gnu.org/coreutils/coreutils-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain attr libcap pcre readline openssl"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_c_restrict=no \
 			      ac_cv_func_chown_works=yes \
