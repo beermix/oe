df0b092082a6649e26e738bb8ce3d59e155b0e51
AV <27rurk@gmail.com>
2017-09-09 17:14:23 +0300

    autocommit on change

--- packages/-my/coreutils/package.mk
+++ packages/-my/coreutils/package.mk
@@ -6,6 +6,8 @@ PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+find $ROOT/$PKG_BUILD -type f -exec sed -i 's/1.15.1/1.15/g' {} \;
+
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_c_restrict=no \
 			      ac_cv_func_chown_works=yes \
 			      ac_cv_func_euidaccess=no \
