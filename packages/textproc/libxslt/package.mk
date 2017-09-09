f1861755c487404a7bd49a2f673ef6cdc1bcc100
AV <27rurk@gmail.com>
2017-09-09 04:20:14 +0300

    autocommit on change

--- packages/textproc/libxslt/package.mk
+++ packages/textproc/libxslt/package.mk
@@ -17,15 +17,18 @@
 ################################################################################
 
 PKG_NAME="libxslt"
-PKG_VERSION="ac341cb"
+PKG_VERSION="1.1.29"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org/xslt/"
-PKG_GIT_URL="git://git.gnome.org/libxslt"
+PKG_URL="ftp://xmlsoft.org/libxml2/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="libxml2:host"
 PKG_DEPENDS_TARGET="toolchain libxml2"
 PKG_SECTION="textproc"
 PKG_SHORTDESC="libxslt"
 PKG_LONGDESC="libxslt"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_HOST="  ac_cv_header_ansidecl_h=no \
                            ac_cv_header_xlocale_h=no \
