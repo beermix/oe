bd6959945f7a4ef89c36ca5dc48f52e82588c706
AV <27rurk@gmail.com>
2017-09-09 04:29:10 +0300

    autocommit on change

--- packages/security/libksba/package.mk
+++ packages/security/libksba/package.mk
@@ -6,9 +6,8 @@ PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_configure_target() {
-  CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
-  CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
-}
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --with-gpg-error-prefix=$SYSROOT_PREFIX/lib"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --with-gnu-ld --with-pic"
+pre_configure_target() {
+  CFLAGS="$CFLAGS -fPIC"
+}
