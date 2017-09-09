068a04e1606c54e0255e241c053ee2968bac6062
AV <27rurk@gmail.com>
2017-09-09 05:42:31 +0300

    autocommit on change

---
+++ packages/devel/gettext-tiny/package.mk
@@ -0,0 +1,38 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="gettext-tiny"
+PKG_VERSION="0.2.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.gnu.org/s/gettext/"
+PKG_URL="http://ftp.barfooze.de/pub/sabotage/tarballs/gettext-tiny-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="toolchain/devel"
+PKG_SHORTDESC="gettext: A program internationalization library and tools"
+PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+makeinstall_host() {
+  make LIBINTL=FLAVOR
+  make DESTDIR=$ROOT/$TOOLCHAIN prefix=/ install
+}
