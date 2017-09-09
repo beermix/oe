8473eb5d14a08ea0a3236ea550bc9a83c84f8ca2
AV <27rurk@gmail.com>
2017-09-09 07:33:41 +0300

    autocommit on change

--- packages/devel/libelf-compat/package.mk
+++ packages/devel/libelf-compat/package.mk
@@ -1,15 +1,11 @@
 PKG_NAME="libelf-compat"
 PKG_VERSION="328643b"
-PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_GIT_URL="https://github.com/sabotage-linux/libelf-compat"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
-PKG_SHORTDESC="gettext: A program internationalization library and tools"
-PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
@@ -23,11 +19,11 @@ makeinstall_host() {
 }
 
 make_target() {
-  make -C CC=$CC prefix=$SYSROOT_PREFIX/usr install
+  make CC=$CC prefix=$SYSROOT_PREFIX/usr install
 }
 
 makeinstall_target() {
-  make -C CC=$CC prefix=$SYSROOT_PREFIX/usr install
+  make CC=$CC prefix=$SYSROOT_PREFIX/usr install
 }
 
 
