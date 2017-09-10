2cc7e4461a630f3c957c7fce8339eff79c0b28a0
AV <27rurk@gmail.com>
2017-09-10 07:06:58 +0300

    autocommit on change

--- packages/compress/tar/package.mk
+++ packages/compress/tar/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="tar"
 PKG_VERSION="1.29"
 PKG_URL="http://mirrors.kernel.org/gnu/tar/tar-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain pcre attr"
+PKG_DEPENDS_TARGET="toolchain pcre acl"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--bindir=/bin --without-selinux --without-posix-acls --with-xattrs"
+PKG_CONFIGURE_OPTS_TARGET="--bindir=/bin --without-selinux"
