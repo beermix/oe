4df8f3e5b45e7a981ad0cd4de003620d1c298120
AV <27rurk@gmail.com>
2017-09-12 19:06:12 +0300

    autocommit on change

--- packages/tools/psplash/package.mk
+++ packages/tools/psplash/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,10 +17,14 @@
 ################################################################################
 
 PKG_NAME="psplash"
-PKG_VERSION="5b3c1cc"
+PKG_VERSION="2015f70"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
 PKG_SITE="http://git.yoctoproject.org/cgit/cgit.cgi/psplash"
 PKG_GIT_URL="http://git.yoctoproject.org/git/psplash"
 PKG_DEPENDS_INIT="toolchain"
+PKG_PRIORITY="optional"
 PKG_SECTION="tools"
 PKG_SHORTDESC="psplash: Boot splash screen based on Fedora's Plymouth code"
 PKG_LONGDESC="PSplash is a userspace graphical boot splash screen for mainly embedded Linux devices supporting a 16bpp or 32bpp framebuffer."
