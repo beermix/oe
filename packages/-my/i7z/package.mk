7b40f76c931254e74019ef29fb1b36ed8ec5feb4
AV <27rurk@gmail.com>
2017-09-12 20:04:16 +0300

    autocommit on change

--- packages/-my/i7z/package.mk
+++ packages/-my/i7z/package.mk
@@ -6,7 +6,7 @@ PKG_SECTION="tools"
 PKG_AUTORECONF="no"
 
 make_target() {
-  LIBS="$LIBS -lcurses -lterminfo" make CC="$CC" AR="$AR" LD="$LD" XCFLAGS="$CFLAGS" RANLIB="$RANLIB" XLDFLAGS="$LDFLAGS" MAKEDEPPROG="$CC" CFLAGS="$CFLAGS" -j1
+  LIBS="$LIBS -lncursesw -ltinfo" make CC="$CC" AR="$AR" LD="$LD" XCFLAGS="$CFLAGS" RANLIB="$RANLIB" XLDFLAGS="$LDFLAGS" MAKEDEPPROG="$CC" CFLAGS="$CFLAGS" -j1
 }
 
 pre_configure_target() {
