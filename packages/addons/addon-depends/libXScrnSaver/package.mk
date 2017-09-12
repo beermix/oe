05105c4ed42833193d76b706c5fb47ad2963d531
AV <27rurk@gmail.com>
2017-09-12 20:44:13 +0300

    autocommit on change

--- packages/addons/addon-depends/libXScrnSaver/package.mk
+++ packages/addons/addon-depends/libXScrnSaver/package.mk
@@ -25,12 +25,6 @@ PKG_SECTION="x11/lib"
 PKG_SHORTDESC=""
 PKG_LONGDESC=""
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --enable-malloc0returnsnull"
-
-pre_configure_target() {
-  CFLAGS="$CFLAGS -fPIC"
-  CXXFLAGS="$CXXFLAGS -fPIC"
-}
-
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-pic --enable-malloc0returnsnull"
