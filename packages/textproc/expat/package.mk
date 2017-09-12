6384db0ccaf88bfd769cd370525c84697149a386
AV <27rurk@gmail.com>
2017-09-12 17:30:11 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -7,21 +7,6 @@ PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -DXML_POOR_ENTROPY"
-}
-
-pre_configure_host() {
-  export CFLAGS="$CFLAGS -DXML_POOR_ENTROPY"
-}
-
-PKG_CMAKE_SCRIPT_TARGET="expat/CMakeLists.txt"
-PKG_CMAKE_SCRIPT_HOST="expat/CMakeLists.txt"
-
-PKG_CMAKE_OPTS_TARGET="-DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
-
-PKG_CMAKE_OPTS_HOST="-DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
-
 pre_make_host() {
   # fix builderror when building in subdirs
   cp -r ../doc .
