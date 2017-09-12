70ff3a7c695d0b7a23a0dac6dfea910338eb9075
AV <27rurk@gmail.com>
2017-09-12 17:25:00 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="expat"
 PKG_VERSION="2.2.4"
-PKG_GIT_URL="http://sources.lede-project.org/expat-$PKG_VERSION.tar.bz2"
+PKG_URL="http://sources.lede-project.org/expat-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="textproc"
 PKG_IS_ADDON="no"
