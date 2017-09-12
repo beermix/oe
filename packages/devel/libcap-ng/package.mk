e79ede411d68bd0d30052ca70c3a0f3b818456a8
AV <27rurk@gmail.com>
2017-09-12 23:04:54 +0300

    autocommit on change

--- packages/devel/libcap-ng/package.mk
+++ packages/devel/libcap-ng/package.mk
@@ -6,11 +6,6 @@ PKG_SECTION="devel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_configure_target() {
-  CFLAGS="$CFLAGS -fPIC"
-  CXXFLAGS="$CXXFLAGS -fPIC"
-}
-
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_swig_found=no --enable-shared=no --with-python=no"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_swig_found=no --enable-shared=no --with-python=no --with-pic"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
