9fbed5acec4fa74301e90c523db51ff794b8e8e9
AV <27rurk@gmail.com>
2017-09-12 22:19:50 +0300

    autocommit on change

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,11 +17,11 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="b4473dd"
+PKG_VERSION="17.2.0"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
-PKG_GIT_URL="https://anongit.freedesktop.org/git/mesa/mesa.git"
+PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain Python:host expat glproto dri2proto presentproto libdrm libXext libXdamage libXfixes libXxf86vm libxcb libX11 systemd dri3proto libxshmfence openssl Mako:host"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="mesa: 3-D graphics library with OpenGL API"
