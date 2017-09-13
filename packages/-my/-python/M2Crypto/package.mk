b5a2b50cfa0ba9e9f7004631b716187e3b27ae9d
AV <27rurk@gmail.com>
2017-09-14 01:13:01 +0300

    autocommit on change

--- packages/-my/-python/M2Crypto/package.mk
+++ packages/-my/-python/M2Crypto/package.mk
@@ -1,26 +1,10 @@
-################################################################################
-#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
-################################################################################
-
 PKG_NAME="M2Crypto"
-PKG_VERSION="0.25.1"
-PKG_REV="1"
-PKG_ARCH="any"
-PKG_LICENSE="OSS"
-PKG_SITE="https://pypi.python.org/pypi/M2Crypto"
-PKG_URL="https://pypi.python.org/packages/9c/58/7e8d8c04995a422c3744929721941c400af0a2a8b8633f129d92f313cfb8/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
-PKG_SECTION="xmedia/torrent"
-PKG_SHORTDESC="M2Crypto is the most complete Python wrapper for OpenSSL"
-PKG_LONGDESC="M2Crypto is the most complete Python wrapper for OpenSSL."
+PKG_VERSION="0.25.0"
+#PKG_VERSION="0.24.0"
+PKG_GIT_URL="https://gitlab.com/m2crypto/m2crypto"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host openssl swig:host"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-pre_configure_target() {
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-  export LDSHARED="$CC -shared"
-}
 
 make_target() {
   python setup.py build build_ext --openssl=$LIB_PREFIX
