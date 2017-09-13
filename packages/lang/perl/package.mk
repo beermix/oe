74616425ef6f504c46dd2a7c8254b98494992d31
AV <27rurk@gmail.com>
2017-09-13 09:26:18 +0300

    autocommit on change

--- packages/lang/perl/package.mk
+++ packages/lang/perl/package.mk
@@ -19,21 +19,20 @@ configure_target() {
 ./Configure -Dusedevel \
 		   -Doptimize="$CFLAGS" \
 		   -Dcc=$CC \
-		   -Dprefix=/usr \
-		   -Dinstallprefix=${startdir}/pkg/usr \
+		   -Dprefix=/usr  \
 		   -Dvendorprefix=/usr \
 		   -Dprivlib=/usr/share/perl5/core_perl \
 		   -Darchlib=/usr/lib/perl5/core_perl \
-		   -Dsitelib=/usr/share/perl5/site_perl/${pkgver} \
-		   -Dsitearch=/usr/lib/perl5/site_perl/${pkgver} \
+		   -Dsitelib=/usr/share/perl5/site_perl/${PKG_VERSION} \
+		   -Dsitearch=/usr/lib/perl5/site_perl/${PKG_VERSION} \
 		   -Dvendorlib=/usr/share/perl5/vendor_perl \
 		   -Dvendorarch=/usr/lib/perl5/vendor_perl \
 		   -Dotherlibdirs=/usr/lib/perl5/current:/usr/lib/perl5/site_perl/current \
 		   -Dscriptdir='/usr/bin/perlbin/core' \
 		   -Dsitescript='/usr/bin/perlbin/site' \
 		   -Dvendorscript='/usr/bin/perlbin/vendor' \
-		   -Dman1ext=1perl -Dman3ext=3perl ${arch_opts} \
-		   -Duseshrplib -Dlibperl=libperl.so.${pkgver} \
+		   -Dman1ext=1perl -Dman3ext=3perl \
+		   -Duseshrplib -Dlibperl=libperl.so.${PKG_VERSION} \
 		   -Dlibswanted="dl m c crypt db ndbm gdbm" \
 		   -des -Dusethreads \
 		   -Dccdlflags="-rdynamic -Wl,-O1" -Dlddlflags="-shared -Wl,-O1" -Dldflags="-Wl,-O1" \
@@ -44,5 +43,5 @@ configure_target() {
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/share/man
-  ln -sfv perl $INSTALL/bin/perl$PKG_VERSION
+  ln -sf perl $INSTALL/usr/bin/perl${PKG_VERSION}
 }
