c4b88c7fd5d8ce258a615d966c76bcc7e80d1def
AV <27rurk@gmail.com>
2017-09-13 08:17:38 +0300

    autocommit on change

--- packages/-my/libsodium/package.mk
+++ packages/-my/libsodium/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="libsodium"
-PKG_VERSION="26c81e8"
+PKG_VERSION="af97ac7"
 PKG_SITE="https://github.com/jedisct1/libsodium/tree/stable"
 PKG_GIT_URL="https://github.com/jedisct1/libsodium"
 PKG_GIT_BRANCH="stable"
@@ -8,4 +8,4 @@ PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-minimal --with-gnu-ld --with-pic"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-minimal --with-pic"
