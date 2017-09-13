cad9a04b9f8de5f525c03c399238d2bc48c7ae8e
AV <27rurk@gmail.com>
2017-09-13 14:37:32 +0300

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="isl"
-PKG_VERSION="0.16"
+PKG_VERSION="0.18"
 PKG_ARCH="any"
 PKG_LICENSE="Custom"
 PKG_URL="http://isl.gforge.inria.fr/isl-$PKG_VERSION.tar.xz"
@@ -9,4 +9,4 @@ PKG_SECTION="devel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --without-clang --with-gmp=$ROOT/$TOOLCHAIN"
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --without-clang --with-gnu-ld --with-gmp=$ROOT/$TOOLCHAIN"
