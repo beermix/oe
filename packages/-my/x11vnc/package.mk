e542a46f3d65800a3a137628d44fb57169b18550
AV <27rurk@gmail.com>
2017-09-13 08:51:21 +0300

    autocommit on change

--- packages/-my/x11vnc/package.mk
+++ packages/-my/x11vnc/package.mk
@@ -9,14 +9,41 @@ PKG_AUTORECONF="yes"
 pre_build_target() {
    mkdir -p $ROOT/$PKG_BUILD/.$TARGET_NAME
    cp -RP $ROOT/$PKG_BUILD/* $ROOT/$PKG_BUILD/.$TARGET_NAME
-   #export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
+   export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
 			      --with-x11vnc \
 			      --with-x \
+			      --without-xkeyboard \
+			      --without-xinerama \
+			      --without-xrandr \
+			      --without-xfixes \
+			      --without-xdamage \
+			      --without-xtrap \
+			      --without-xrecord \
+			      --without-fbpm \
+			      --without-dpms \
+			      --without-v4l \
+			      --without-fbdev \
+			      --without-uinput \
+			      --without-macosx-native \
+			      --without-crypt \
+			      --without-crypto \
+			      --without-ssl \
+			      --without-avahi \
+			      --with-jpeg \
+			      --without-libz \
+			      --without-zlib \
+			      --without-gnutls \
+			      --without-client-tls \
+			      --with-gnu-ld \
 			      --disable-nls"
 			      
 post_install () {
   enable_service x11vnc.service
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/share
 }
