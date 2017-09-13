6de7ea59e582548438d1a5a308cf99374c90c691
AV <27rurk@gmail.com>
2017-09-13 21:28:47 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -7,7 +7,7 @@
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,                           --enable-experimental-malloc --enable-stackguard-randomization \
+#  OpenELEC is distributed |||| --enable-experimental-malloc --enable-stackguard-randomization
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
