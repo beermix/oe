d7bb2563c88d7342af34da61db70042e3d8dd681
AV <27rurk@gmail.com>
2017-09-16 20:05:09 +0300

    autocommit on change

--- packages/databases/mariadb/package.mk
+++ packages/databases/mariadb/package.mk
@@ -126,7 +126,7 @@ configure_host() {
         -DWITH_LIBWRAP=OFF \
         -DWITH_WSREP=OFF \
         -DWITH_SYSTEMD=no \
-        -DSECURITY_HARDENED=0 \
+        -DSECURITY_HARDENED=1 \
         ..
 }
 
@@ -174,7 +174,7 @@ configure_target() {
         -DWITHOUT_FEDERATED_STORAGE_ENGINE=1 \
         -DWITHOUT_PBXT_STORAGE_ENGINE=1 \
         -DWITH_SSL=$SYSROOT_PREFIX/usr \
-        -DSECURITY_HARDENED=0 \
+        -DSECURITY_HARDENED=1 \
         -$MARIADB_OPTS \
         ..
 }
