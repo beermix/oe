1483b5236376beffdd7aad10fc4e6ed61f9d0203
AV <27rurk@gmail.com>
2017-09-16 19:30:12 +0300

    autocommit on change

--- packages/compress/libarchive/package.mk
+++ packages/compress/libarchive/package.mk
@@ -1,12 +1,12 @@
 PKG_NAME="libarchive"
 PKG_VERSION="3.3.2"
 PKG_URL="http://www.libarchive.org/downloads/libarchive-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain openssl expat pcre zlib lz4 xz bzip2"
+PKG_DEPENDS_TARGET="toolchain openssl expat pcre zlib lz4 xz bzip2 libxml2"
 PKG_DEPENDS_HOST="openssl:host zlib:host lzo:host xz:host bzip2:host lz4:host"
 PKG_SECTION="compress"
 PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-shared --enable-static --disable-rpath --disable-xattr --disable-acl --with-gnu-ld --with-pic"
 			   
