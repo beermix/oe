6b6c59652195ae1f57e0c98ab971505837cb0e31
AV <27rurk@gmail.com>
2017-09-17 09:06:27 +0300

    autocommit on change

--- packages/databases/mariadb/package.mk
+++ packages/databases/mariadb/package.mk
@@ -166,6 +166,7 @@ configure_target() {
         -DWITH_SYSTEMD=no \
         -DWITH_LIBWRAP=OFF \
         -DWITH_SSL=$SYSROOT_PREFIX/usr \
+        -DSECURITY_HARDENED=0 \
         $MARIADB_OPTS \
         ..
 }
