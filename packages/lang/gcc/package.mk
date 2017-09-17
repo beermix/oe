32bc02c68143644208d57c32eb22e76237aa38cf
AV <27rurk@gmail.com>
2017-09-17 11:11:57 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -81,8 +81,6 @@ PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
                          --disable-libstdcxx-pch \
                          --enable-libstdcxx-time=yes \
                          --enable-clocale=gnu \
-                         --enable-gnu-unique-object \
-                         --enable-linker-build-id \
                          --enable-default-pie \
                          --enable-default-ssp \
                          $GCC_OPTS"
