7c2e195e75637864dc470d86e499e008eb6d2063
AV <27rurk@gmail.com>
2017-09-17 07:34:51 +0300

    autocommit on change

--- packages/-my/putty/package.mk
+++ packages/-my/putty/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="putty"
-PKG_VERSION="0.69"
+PKG_VERSION="0.70"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
 PKG_SECTION="devel"
