26bc8ad1827d2ac0d9db09c0db9db578fb99fc67
AV <27rurk@gmail.com>
2017-09-17 20:00:55 +0300

    autocommit on change

--- packages/mediacenter/kodi-binary-addons/inputstream.rtmp/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.rtmp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="inputstream.rtmp"
-PKG_VERSION="d93d32d"
+PKG_VERSION="1e64d4a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -34,8 +34,6 @@ PKG_AUTORECONF="no"
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="kodi.inputstream"
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_MODULE_PATH=$SYSROOT_PREFIX/usr/lib/kodi \
-                       -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
