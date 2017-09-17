0cf39fb9f9bcfce9e0f890a7ac83766c6ba044d1
AV <27rurk@gmail.com>
2017-09-17 05:04:26 +0300

    autocommit on change

--- packages/network/libnl/package.mk
+++ packages/network/libnl/package.mk
@@ -18,14 +18,14 @@
 
 PKG_NAME="libnl"
 PKG_VERSION="3.3.0"
-PKG_SITE="http://people.suug.ch/~tgr/libnl/"
+PKG_SITE="https://github.com/thom311/libnl"
 PKG_URL="https://github.com/thom311/$PKG_NAME/releases/download/${PKG_NAME}${PKG_VERSION//./_}/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="network"
 PKG_SHORTDESC="libnl: libnl - netlink library"
 PKG_LONGDESC="libnl is a library for applications dealing with netlink socket. It provides an easy to use interface for raw netlink message but also netlink family specific APIs."
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --disable-cli"
 
