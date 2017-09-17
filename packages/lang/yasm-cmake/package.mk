15f9f16c0f432139a43817c12cf906adb42f3b7d
AV <27rurk@gmail.com>
2017-09-17 14:07:30 +0300

    autocommit on change

--- packages/lang/yasm-cmake/package.mk
+++ packages/lang/yasm-cmake/package.mk
@@ -17,29 +17,17 @@
 ################################################################################
 
 PKG_NAME="yasm"
-PKG_VERSION="1.3.0"
-PKG_ARCH="any"
-PKG_LICENSE="BSD"
-PKG_SITE="http://www.tortall.net/projects/yasm/"
-PKG_URL="http://www.tortall.net/projects/yasm/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST=""
+PKG_VERSION="6caf151"
+PKG_GIT_URL="https://github.com/yasm/yasm"
 PKG_SECTION="toolchain/lang"
 PKG_SHORTDESC="yasm: A complete rewrite of the NASM assembler"
 PKG_LONGDESC="Yasm is a complete rewrite of the NASM assembler under the new BSD License (some portions are under other licenses, see COPYING for details). It is designed from the ground up to allow for multiple assembler syntaxes to be supported (eg, NASM, TASM, GAS, etc.) in addition to multiple output object formats and even multiple instruction sets. Another primary module of the overall design is an optimizer module."
 
 PKG_IS_ADDON="no"
-PKG_USE_CMAKE="no"
+PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-debug \
-                         --disable-warnerror \
-                         --disable-profiling \
-                         --disable-gcov \
-                         --disable-python \
-                         --disable-python-bindings \
-                         --enable-nls \
-                         --disable-rpath \
-                         --without-dmalloc \
-                         --with-gnu-ld \
-                         --without-libiconv-prefix \
-                         --without-libintl-prefix"
+PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DYASM_BUILD_TESTS=OFF"
+
+PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_HOST"
+
