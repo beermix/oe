8b6d4ac1f8060bc4b48457b8aa3f3f47e95e2a99
AV <27rurk@gmail.com>
2017-09-17 04:49:16 +0300

    autocommit on change

--- packages/escalade/sysutils/xfsprogs/package.mk
+++ packages/escalade/sysutils/xfsprogs/package.mk
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="xfsprogs"
-PKG_VERSION="v4.12.0"
+PKG_VERSION="v4.13.0-rc1"
 PKG_SITE="https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git"
 PKG_GIT_URL="git://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git"
-PKG_DEPENDS_TARGET="toolchain util-linux readline"
+PKG_DEPENDS_TARGET="toolchain util-linux libedit"
 PKG_DEPENDS_INIT="xfsprogs"
 PKG_SECTION="tools"
 PKG_SHORTDESC="xfsprogs: Utilities for use with the xfs filesystem"
@@ -31,8 +31,8 @@ PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
 			      --exec-prefix=/ \
 			      --enable-shared=no \
 			      --with-gnu-ld \
-			      --enable-editline=no \
-			      --enable-readline=yes \
+			      --enable-editline=yes \
+			      --enable-readline=no \
 			      --enable-termcap=no \
 			      --enable-blkid=yes \
 			      --enable-librt=yes \
