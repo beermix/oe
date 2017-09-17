513b9933c311e14e1380c5dbbb4e481f4318b9ae
AV <27rurk@gmail.com>
2017-09-17 03:54:40 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -63,10 +63,10 @@ case "$LINUX" in
     PKG_GIT_URL="http://github.com/zen-kernel/zen-kernel"
     PKG_PATCH_DIRS="linux-4.12"
     ;;
-  4.12)
-    PKG_VERSION="4.12.11"
-    PKG_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$PKG_VERSION.tar.xz"
-    PKG_PATCH_DIRS="linux-4.12"
+  4.14)
+    PKG_VERSION="4.14-rc1"
+    PKG_URL="https://git.kernel.org/torvalds/t/linux-$PKG_VERSION.tar.gz"
+    PKG_PATCH_DIRS="linux-4.14"
     ;;
   *)
     PKG_VERSION="4.13.2"
