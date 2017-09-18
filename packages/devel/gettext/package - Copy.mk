a630d73d313fb623f488840a5af3da4d5b0691bb
AV <27rurk@gmail.com>
2017-09-18 20:39:25 +0300

    autocommit on change

--- packages/devel/gettext/package - Copy.mk
+++ packages/devel/gettext/package - Copy.mk
@@ -16,20 +16,30 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="gettext-tiny"
-PKG_VERSION="0.2.0"
+PKG_NAME="gettext"
+PKG_VERSION="0.19.8.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
-PKG_URL="http://ftp.barfooze.de/pub/sabotage/tarballs/gettext-tiny-$PKG_VERSION.tar.xz"
+PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
+PKG_SHORTDESC="gettext: A program internationalization library and tools"
+PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-makeinstall_host() {
-  make LIBINTL=NONE DESTDIR=$ROOT/$TOOLCHAIN prefix=/ all install
-}
+PKG_CONFIGURE_SCRIPT="gettext-tools/configure"
+
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
+                         --disable-rpath \
+                         --with-gnu-ld \
+                         --disable-java \
+                         --disable-curses \
+                         --with-included-libxml \
+                         --disable-native-java \
+                         --disable-csharp \
+                         --without-emacs"
