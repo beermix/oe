05453fe12e151b876d31b1b35ba7689fef5c7cdc
AV <27rurk@gmail.com>
2017-09-18 15:50:24 +0300

    autocommit on change

--- packages/-my/smartmontools/package.mk
+++ packages/-my/smartmontools/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="smartmontools"
-PKG_VERSION="d55b429"
+PKG_VERSION="4db810c"
 PKG_GIT_URL="https://github.com/mirror/smartmontools"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_SECTION="my"
@@ -8,7 +8,6 @@ PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/smartmontools \
 			      --datarootdir=/storage/.config/smartmontools/share \
-			      --with-systemdsystemunitdir=/usr/lib/systemd/system \
 			      --disable-silent-rules"
 
 #post_makeinstall_target() {
