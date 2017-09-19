af9866cff5550516dbcdb601c4f1374122654ae2
AV <27rurk@gmail.com>
2017-09-19 07:07:09 +0300

    autocommit on change

--- packages/-my/tor/package.mk
+++ packages/-my/tor/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="tor"
-PKG_VERSION="0.3.0.10"
+PKG_VERSION="0.3.1.7"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib libevent libcap"
 PKG_SECTION="security"
