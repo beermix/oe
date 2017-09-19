1d1f8e500ce002dd6d6c0e7554457f0af3b4113e
AV <27rurk@gmail.com>
2017-09-19 20:51:33 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -55,7 +55,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_snprintf_c99=yes \
                            --enable-Bsymbolic \
                            --with-gnu-ld \
                            --with-threads=posix \
-                           --with-pcre=system"
+                           --with-pcre=internal"
 
 post_makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
