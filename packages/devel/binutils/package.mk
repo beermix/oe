99269d6827274b87711a105fe9937036d2e7e6c0
AV <27rurk@gmail.com>
2017-09-19 11:06:19 +0300

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -30,8 +30,7 @@ PKG_LONGDESC="The GNU binutils are utilities of use when dealing with object fil
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="MAKEINFO=true \
-                         --target=$TARGET_NAME \
+PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --with-sysroot=$SYSROOT_PREFIX \
                          --with-lib-path=$SYSROOT_PREFIX/lib:$SYSROOT_PREFIX/usr/lib \
                          --without-ppl \
