e3496507ec636cd06d1468841a7956ba0753d930
AV <27rurk@gmail.com>
2017-09-19 12:43:38 +0300

    autocommit on change

--- packages/compress/libarchive/package.mk
+++ packages/compress/libarchive/package.mk
@@ -5,8 +5,14 @@ PKG_DEPENDS_TARGET="toolchain gmp acl openssl expat pcre zlib lz4 xz bzip2 nettl
 PKG_DEPENDS_HOST="openssl:host zlib:host lzo:host xz:host bzip2:host lz4:host"
 PKG_SECTION="compress"
 PKG_IS_ADDON="no"
-PKG_USE_CMAKE="no"
+PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
+
+PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTING=OFF \
+			  -DENABLE_CAT_SHARED=OFF \
+			  -DENABLE_CPIO_SHARED=OFF \
+			  -DENABLE_LibGCC=ON \
+			  -DENABLE_TEST=OFF"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-shared --enable-static --disable-rpath --disable-xattr --disable-acl --with-pic"
 			   
--- packages/devel/gmp/package.mk
+++ packages/devel/gmp/package.mk
@@ -30,4 +30,4 @@ PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-cxx --enable-static --disable-shared --enable-fat --with-pic"
 
-PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
+PKG_CONFIGURE_OPTS_TARGET="--enable-cxx --enable-fat"
