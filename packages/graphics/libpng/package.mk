d07da39754be0898f87a083318ce23085792caab
AV <27rurk@gmail.com>
2017-09-19 10:48:28 +0300

    autocommit on change

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -32,7 +32,7 @@ PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_z_zlibVersion=yes --enable-static --disable-shared --enable-intel-sse"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_z_zlibVersion=yes --enable-intel-sse"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --enable-intel-sse"
 
