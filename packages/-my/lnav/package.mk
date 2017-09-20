fe35c08b71cd12d81c26a325b41d6093d3933d72
AV <27rurk@gmail.com>
2017-09-20 05:44:06 +0300

    autocommit on change

--- packages/-my/lnav/package.mk
+++ packages/-my/lnav/package.mk
@@ -1,22 +1,12 @@
 PKG_NAME="lnav"
-PKG_VERSION="v0.6.2"
-PKG_GIT_URL="https://github.com/tstack/lnav"
+PKG_VERSION="0.8.2"
+PKG_URL="https://github.com/tstack/lnav/releases/download/v0.8.2/lnav-0.8.2.tar.gz"
 PKG_DEPENDS_HOST="toolchain sqlite"
 PKG_SECTION="tools"
 PKG_USE_CMAKE="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  cd $ROOT/$PKG_BUILD
-  rm -rf .$TARGET_NAME
-  NOCONFIGURE=1 ./autogen.sh
-}
 
-pre_configure_host() {
-  cd $ROOT/$PKG_BUILD
-  rm -rf .$TARGET_NAME
-  NOCONFIGURE=1 ./autogen.sh
-}
 
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-dependency-tracking --enable-silent-rules --with-pcre --disable-shared --enable-static"
