78e89d2afb02ffdf5e4a89baf605fda83cad37b6
AV <27rurk@gmail.com>
2017-09-24 00:32:11 +0300

    autocommit on change

--- packages/x11/other/fontconfig/package.mk
+++ packages/x11/other/fontconfig/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fontconfig"
-PKG_VERSION="2.12.4"
+PKG_VERSION="2.12.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
