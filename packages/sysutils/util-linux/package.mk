bba6e3b4e9a1f97ee8df5805e756a9bd56a7c172
AV <27rurk@gmail.com>
2017-09-24 10:08:51 +0300

    autocommit on change

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.30.1"
+PKG_VERSION="2.30.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
