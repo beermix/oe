384a1cec4d72145230be47fa4b0003cb2edb2e59
AV <27rurk@gmail.com>
2017-09-24 05:45:24 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="c647760f"
+PKG_VERSION="7f97a4ae"
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
 PKG_GIT_URL="git://anongit.freedesktop.org/xorg/app/intel-gpu-tools"
 #PKG_URL="http://xorg.freedesktop.org/releases/individual/app/intel-gpu-tools-${PKG_VERSION}.tar.gz"
