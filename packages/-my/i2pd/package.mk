7815cc8fed8c3613f2f0428d40c93b069bdc9658
AV <27rurk@gmail.com>
2017-09-24 23:28:14 +0300

    autocommit on change

--- packages/-my/i2pd/package.mk
+++ packages/-my/i2pd/package.mk
@@ -1,11 +1,18 @@
 PKG_NAME="i2pd"
-PKG_VERSION="4d23de9"
+PKG_VERSION="2.15.0"
 PKG_GIT_URL="https://github.com/PurpleI2P/i2pd"
 PKG_DEPENDS_TARGET="toolchain boost zlib openssl miniupnpc boost"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-strip_lto
-strip_gold
+PKG_CMAKE_SCRIPT_TARGET="build/CMakeLists.txt"
 
+PKG_CMAKE_OPTS_TARGET="-DWITH_LIBRARY=ON \
+			  -DWITH_PCH=OFF \
+			  -DWITH_MESHNET=OFF \
+			  -DWITH_STATIC=OFF \
+			  -DWITH_UPNP=ON \
+			  -DTHREADS_PTHREAD_ARG=4 \
+			  -DWITH_HARDENING=OFF \
+			  -DWITH_AESNI=ON"
