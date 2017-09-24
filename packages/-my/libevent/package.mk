eb80693b6433f89a15f15d2498693f0bbdae4fd4
AV <27rurk@gmail.com>
2017-09-24 07:35:03 +0300

    autocommit on change

--- packages/-my/libevent/package.mk
+++ packages/-my/libevent/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="libevent"
-PKG_VERSION="2.0.22-stable"
+PKG_VERSION="2.1.8-stable"
 PKG_SITE="https://github.com/libevent/libevent/releases/"
 PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION/libevent-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
