65d46661b2bc5f9e5b1d98e98c2cc97a5cec14d5
AV <27rurk@gmail.com>
2017-10-01 23:33:08 +0300

    autocommit on change

--- packages/-my/git/package.mk
+++ packages/-my/git/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="git"
 PKG_VERSION="2.14.2"
 PKG_URL="https://www.kernel.org/pub/software/scm/git/git-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain curl pcre expat zlib openssl"
+PKG_DEPENDS_TARGET="toolchain curl pcre2 expat zlib openssl"
 PKG_SECTION="devel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
@@ -32,7 +32,7 @@ PKG_CONFIGURE_OPTS_TARGET="--with-curl=$SYSROOT_PREFIX/usr \
 			      --with-zlib=$SYSROOT_PREFIX/usr \
 			      --with-editor=/usr/bin/nano \
 			      --with-expat=$SYSROOT_PREFIX/usr \
-			      --with-libpcre=$SYSROOT_PREFIX/usr \
+			      --with-libpcre2=$SYSROOT_PREFIX/usr \
 			      --disable-option-checking"
 
 
