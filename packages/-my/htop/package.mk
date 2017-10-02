3b226f91bc9612125f8d6f42a5e05a3ec394ebf7
AV <27rurk@gmail.com>
2017-10-02 12:00:27 +0300

    autocommit on change

--- packages/-my/htop/package.mk
+++ packages/-my/htop/package.mk
@@ -1,10 +1,13 @@
 PKG_NAME="htop"
 PKG_VERSION="e3f65c8"
 PKG_GIT_URL="https://github.com/hishamhm/htop"
-PKG_DEPENDS_TARGET="toolchain ncurses libpciaccess libxml2 udevil"
+PKG_DEPENDS_TARGET="toolchain ncurses libpciaccess libxml2 udevil hwloc"
 PKG_SECTION="debug/tools"
 PKG_AUTORECONF="yes"
 
+pre_configure_target() {
+  export LIBS="$LIBS -lm -ludev -lltdl -lpthread -ldl -lhwloc"
+}
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/htop \
 			      --datarootdir=/storage/.config/htop \
@@ -12,4 +15,5 @@ PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/htop \
                            --enable-taskstats \
                            --enable-unicode \
                            --enable-linux-affinity \
-                           --enable-setuid"
+                           --enable-setuid \
+                           --enable-hwloc"
--- packages/-my/hwloc/package - Copy.mk
+++ packages/-my/hwloc/package - Copy.mk
@@ -3,15 +3,15 @@ PKG_VERSION="1.11.8"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib libevent libpciaccess"
 PKG_SECTION="security"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
-   export CXXFLAGS="$CXXFLAGS -fPIC"
+  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-			      --with-x \
+			      --without-x \
 			      --disable-cuda \
 			      --disable-nvml \
 			      --enable-plugins \
