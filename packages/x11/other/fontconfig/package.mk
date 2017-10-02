ef2e18b30d5005346d22fc35f0470db63a79a53e
AV <27rurk@gmail.com>
2017-10-02 07:13:21 +0300

    autocommit on change

--- packages/x11/other/fontconfig/package.mk
+++ packages/x11/other/fontconfig/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fontconfig"
-PKG_VERSION="2.12.5"
+PKG_VERSION="2.12.6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
@@ -30,7 +30,7 @@ PKG_SHORTDESC="fontconfig: A library for font customization and configuration"
 PKG_LONGDESC="Fontconfig is a library for font customization and configuration."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-arch=$TARGET_ARCH \
                            --with-cache-dir=/storage/.cache/fontconfig \
