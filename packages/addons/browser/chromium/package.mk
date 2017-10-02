297f2bc7711f3ceefa0387fdc41985777fb21f4c
AV <27rurk@gmail.com>
2017-10-02 06:05:12 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -157,14 +157,15 @@ makeinstall_target() {
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  
+  $STRIP $PKG_BUILD/out/Release/chrome
+  
   cp -P  $PKG_BUILD/out/Release/chrome $ADDON_BUILD/$PKG_ADDON_ID/bin/chromium.bin
   cp -P  $PKG_BUILD/out/Release/chrome_sandbox $ADDON_BUILD/$PKG_ADDON_ID/bin/chrome-sandbox
   cp -P  $PKG_BUILD/out/Release/{*.pak,*.dat,*.bin,libwidevinecdmadapter.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
   cp -PR $PKG_BUILD/out/Release/locales $ADDON_BUILD/$PKG_ADDON_ID/bin/
   cp -PR $PKG_BUILD/out/Release/gen/content/content_resources.pak $ADDON_BUILD/$PKG_ADDON_ID/bin/
   
-  $STRIP $ADDON_BUILD/$PKG_ADDON_ID/bin/chromium.bin
-
   # config
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config
   cp -P $PKG_DIR/config/* $ADDON_BUILD/$PKG_ADDON_ID/config
