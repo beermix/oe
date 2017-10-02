ecbc9efbf28a460e6bad5409d9687da891c551c0
AV <27rurk@gmail.com>
2017-10-02 05:44:02 +0300

    autocommit on change

--- packages/graphics/libglvnd/package.mk
+++ packages/graphics/libglvnd/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="libglvnd"
-PKG_VERSION="5ff90a1"
+PKG_VERSION="fe4a384"
 PKG_GIT_URL="https://github.com/NVIDIA/libglvnd"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-sysroot=$SYSROOT_PREFIX"
+#PKG_CONFIGURE_OPTS_TARGET="--with-sysroot=$SYSROOT_PREFIX"
