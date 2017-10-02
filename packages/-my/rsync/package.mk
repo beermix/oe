6fd197789b868f3c10ad70310e36ae28874ef1dc
AV <27rurk@gmail.com>
2017-10-02 04:09:49 +0300

    autocommit on change

--- packages/-my/rsync/package.mk
+++ packages/-my/rsync/package.mk
@@ -1,12 +1,8 @@
 PKG_NAME="rsync"
-PKG_VERSION="87bc224"
+PKG_VERSION="881addc"
 PKG_GIT_URL="git://git.samba.org/rsync.git"
 PKG_DEPENDS_TARGET="toolchain popt attr"
 PKG_SECTION="network/backup"
 PKG_AUTORECONF="no"
 
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-			      --without-gssapi \
-			      --disable-ipv6 \
-			      --disable-debug"
+PKG_CONFIGURE_OPTS_TARGET="--with-included-popt=no --disable-ipv6 --disable-debug"
