92ebdec1de346aa18e7e25ea1b86b612791d2c2d
AV <27rurk@gmail.com>
2017-10-04 03:37:46 +0300

    autocommit on change

---
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package - Copy (2).mk
@@ -0,0 +1,40 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="inputstream.adaptive"
+PKG_VERSION="f05370d"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_GIT_URL="https://github.com/peak3d/inputstream.adaptive"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="inputstream.adaptive"
+PKG_LONGDESC="inputstream.adaptive"
+
+PKG_IS_ADDON="yes"
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -R $ROOT/$PKG_BUILD/.install_pkg/usr/share/$MEDIACENTER/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
+
+  ADDONSO=$(xmlstarlet sel -t -v "/addon/extension/@library_linux" $ADDON_BUILD/$PKG_ADDON_ID/addon.xml)
+  cp -L $ROOT/$PKG_BUILD/.install_pkg/usr/lib/$MEDIACENTER/addons/$PKG_NAME/$ADDONSO $ADDON_BUILD/$PKG_ADDON_ID/
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib/
+  cp -P $ROOT/$PKG_BUILD/.$TARGET_NAME/wv/libssd_wv.so $ADDON_BUILD/$PKG_ADDON_ID/lib
+}
