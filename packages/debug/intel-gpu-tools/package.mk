c611fc3e278bed6d76a1aa2b790b164c8b539861
AV <27rurk@gmail.com>
2017-10-06 20:51:19 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="09f8cb1"
+PKG_VERSION="d8954f0"
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
 PKG_GIT_URL="git://anongit.freedesktop.org/xorg/app/intel-gpu-tools"
 PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo dri2proto swig:host"
