9e5ffa2b8eedfd7f554609389167450aedfc58b2
AV <27rurk@gmail.com>
2017-10-06 22:31:43 +0300

    autocommit on change

--- packages/graphics/openjpeg/package.mk
+++ packages/graphics/openjpeg/package.mk
@@ -1,7 +1,9 @@
 PKG_NAME="openjpeg"
-PKG_VERSION="2.2.0"
+PKG_VERSION="2.3.0"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain zlib libpng"
+PKG_DEPENDS_TARGET="toolchain zlib xz libpng libjpeg-turbo libxml2"
 PKG_SECTION="my"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF"
