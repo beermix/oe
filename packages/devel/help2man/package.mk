41b2945d2c5dc1cfe6c1734c412d1288f4b258fb
AV <27rurk@gmail.com>
2017-10-06 20:55:57 +0300

    autocommit on change

--- packages/devel/help2man/package.mk
+++ packages/devel/help2man/package.mk
@@ -1,13 +1,9 @@
 PKG_NAME="help2man"
-PKG_VERSION="1.47.4"
+PKG_VERSION="1.47.5"
 PKG_SITE="https://github.com/westes/flex/releases"
 PKG_URL="https://ftp.gnu.org/gnu/help2man/help2man-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_SECTION="toolchain/devel"
-PKG_SHORTDESC="flex: Fast lexical analyzer generator"
-PKG_LONGDESC="flex is a tool for generating programs that perform pattern-matching on text."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
