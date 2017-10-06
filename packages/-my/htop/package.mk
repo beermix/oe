ba1ba4e8223e00af86f10252a9363a7e5546c35c
AV <27rurk@gmail.com>
2017-10-06 22:48:58 +0300

    autocommit on change

--- packages/-my/htop/package.mk
+++ packages/-my/htop/package.mk
@@ -1,13 +1,13 @@
 PKG_NAME="htop"
 PKG_VERSION="e3f65c8"
 PKG_GIT_URL="https://github.com/hishamhm/htop"
-PKG_DEPENDS_TARGET="toolchain ncurses libpciaccess libxml2 udevil hwloc"
+PKG_DEPENDS_TARGET="toolchain ncurses libpciaccess libxml2 udevil"
 PKG_SECTION="debug/tools"
 PKG_AUTORECONF="yes"
 
-pre_configure_target() {
-  export LIBS="$LIBS -lm -ludev -lltdl -lpthread -ldl -lhwloc"
-}
+#pre_configure_target() {
+#  export LIBS="$LIBS -lm -ludev -lltdl -lpthread -ldl -lhwloc"
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/htop \
 			      --datarootdir=/storage/.config/htop \
@@ -16,4 +16,4 @@ PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/htop \
                            --enable-unicode \
                            --enable-linux-affinity \
                            --enable-setuid \
-                           --enable-hwloc"
+                           --disable-hwloc"
