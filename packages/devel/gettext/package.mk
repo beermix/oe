46aeae2f019d1addfbee37a8b2cc4bdd4974291f
AV <27rurk@gmail.com>
2017-10-07 06:08:26 -0400

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -22,17 +22,22 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host"
+PKG_DEPENDS_HOST="ccache:host automake:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="gettext: A program internationalization library and tools"
 PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_SCRIPT="gettext-tools/configure"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
+PKG_CONFIGURE_OPTS_HOST="EMACS="no" \
+			    am_cv_lib_iconv=no \
+			    am_cv_func_iconv=no \
+			    ac_cv_header_iconv_h=no \
+			    --enable-static \
+			    --disable-shared \
                          --disable-rpath \
                          --with-gnu-ld \
                          --disable-java \
@@ -43,4 +48,4 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
                          --without-emacs \
                          --disable-openmp \
                          --without-cvs \
-                         --without-git"
+                         --without-libxml2-prefix"
