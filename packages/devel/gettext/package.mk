2cdb0c75ce3e6a9e71fec7e0f7c9373f42201c2d
AV <27rurk@gmail.com>
2017-10-07 12:23:57 -0400

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -43,6 +43,7 @@ PKG_CONFIGURE_OPTS_HOST="EMACS="no" \
                          --disable-java \
                          --disable-curses \
                          --with-included-libxml \
+                         --with-included-glib \
                          --disable-native-java \
                          --disable-csharp \
                          --without-emacs \
