48d99c7aefb50a5026c42179cc7f4ca1ca23ea02
AV <27rurk@gmail.com>
2017-10-07 15:59:33 -0400

    autocommit on change

--- packages/network/nghttp2/package.mk
+++ packages/network/nghttp2/package.mk
@@ -2,11 +2,13 @@ PKG_NAME="nghttp2"
 PKG_VERSION="1.26.0"
 PKG_SITE="https://github.com/nghttp2/nghttp2/releases/"
 PKG_URL="https://github.com/nghttp2/nghttp2/releases/download/v$PKG_VERSION/nghttp2-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libev"
-PKG_USE_CMAKE="no"
+PKG_DEPENDS_TARGET="toolchain libevent"
+PKG_USE_CMAKE="yes"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-spdylay=no \
-			      --disable-examples \
-			      --disable-python-bindings \
-			      --enable-lib-only"
+PKG_CMAKE_OPTS_TARGET="-DENABLE_LIB_ONLY=1 \
+			  -DENABLE_EXAMPLES=0 \
+			  -DENABLE_PYTHON_BINDINGS=0 \
+			  -DENABLE_THREADS=1 \
+			  -DWITH_SPDYLAY=0 \
+			  -DCMAKE_BUILD_TYPE=Release"
