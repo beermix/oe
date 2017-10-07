d6480558906edb3c3d5864565aae1806c39f6d3b
AV <27rurk@gmail.com>
2017-10-07 10:00:42 -0400

    autocommit on change

--- packages/-my/libtorrent-rasterbar/package.mk
+++ packages/-my/libtorrent-rasterbar/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="libtorrent-rasterbar"
-PKG_VERSION="1.1.4"
+PKG_VERSION="cdd9f91"
 PKG_SITE="https://github.com/arvidn/libtorrent/tree/RC_1_1"
 PKG_GIT_URL="https://github.com/arvidn/libtorrent"
-PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_4/libtorrent-rasterbar-1.1.4.tar.gz"
+#PKG_URL="https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_4/libtorrent-rasterbar-1.1.4.tar.gz"
 PKG_DEPENDS_TARGET="toolchain boost openssl"
 PKG_LONGDESC="An efficient feature complete C++ bittorrent implementation"
 PKG_USE_CMAKE="no"
@@ -13,21 +13,21 @@ PKG_AUTORECONF="no"
 #  touch $ROOT/$PKG_BUILD/build-aux/config.rpath
 #}
 
-#pre_configure_target() {
-  #cd $ROOT/$PKG_BUILD
-#  CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
-#  CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O3|"`
-#  ./autotool.sh
+pre_configure_target() {
+  cd $ROOT/$PKG_BUILD
+#  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
+#  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O3|"`
+  ./autotool.sh
   #export LDFLAGS="$LDFLAGS -lpthread -ldl  -lutil -lm"
  # sed -i 's/$PKG_CONFIG openssl --libs-only-/$PKG_CONFIG openssl --static --libs-only-/' configure
 #  cp -PR ../* .
-#}
+}
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-python-binding \
                            --with-boost-libdir=$SYSROOT_PREFIX/usr/lib \
                            --with-boost-python=$SYSROOT_PREFIX/usr/lib \
-                           --with-libiconv \
-                           --enable-deprecated-functions \
+                           --without-libiconv \
+                           --disable-deprecated-functions \
                            --disable-shared \
                            --with-pic \
                            --with-gnu-ld \
