9541f3a69adc88d0ff0db162a8fbd193c98d674a
AV <27rurk@gmail.com>
2017-10-06 23:18:26 -0400

    autocommit on change

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -32,7 +32,7 @@ PKG_LONGDESC="Ccache is a compiler cache. It speeds up re-compilation of C/C++ c
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--without-bundled-zlib"
+PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
 
 post_makeinstall_host() {
 # setup ccache
