e577d06653cfffda2e8fb660ffb3eab7d6584d8d
AV <27rurk@gmail.com>
2017-10-07 19:12:31 -0400

    autocommit on change

--- packages/devel/pkgconf/package.mk
+++ packages/devel/pkgconf/package.mk
@@ -1,14 +1,17 @@
 PKG_NAME="pkgconf"
-PKG_VERSION="1.3.8"
+PKG_VERSION="1.3.9"
 PKG_URL="https://distfiles.dereferenced.org/pkgconf/pkgconf-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="ccache:host gettext:host"
+PKG_DEPENDS_HOST="ccache:host gettext:host autoconf:host automake:host"
 PKG_SECTION="toolchain/devel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_HOST="--disable-shared"
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared"
 
 post_makeinstall_host() {
   ln -sfv pkgconf $ROOT/$TOOLCHAIN/bin/pkg-config
+}
+
+pre_configure_host() {
+  export CFLAGS="$CFLAGS -Wall"
 }
