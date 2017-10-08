1da7fa1f41526712bf3c2927b2f0702440fa2498
AV <27rurk@gmail.com>
2017-10-08 05:41:11 -0400

    autocommit on change

---
+++ packages/devel/mkinitcpio/package.mk
@@ -0,0 +1,10 @@
+PKG_NAME="mkinitcpio"
+PKG_VERSION="master"
+PKG_GIT_URL="git://git.archlinux.org/mkinitcpio.git"
+PKG_DEPENDS_TARGET=""
+PKG_DEPENDS_HOST=""
+PKG_SECTION="dev"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
