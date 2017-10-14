595043d98cf1cfa5df36d1ea51ef93bc367798d3
AV <27rurk@gmail.com>
2017-10-13 21:31:56 -0400

    autocommit on change

--- packages/lang/go1.4/package.mk
+++ packages/lang/go1.4/package.mk
@@ -17,8 +17,9 @@
 ################################################################################
 
 PKG_NAME="go1.4"
-PKG_VERSION="a3310f9"
-PKG_GIT_URL="https://github.com/golang/go"
+PKG_VERSION="4d5426a"
+PKG_URL="https://github.com/golang/go/archive/${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="go-${PKG_VERSION}*"
 PKG_DEPENDS_HOST="toolchain"
 PKG_SECTION="system"
 PKG_SHORTDESC="Go is an open source programming language that makes it easy to build simple, reliable, and efficient software."
@@ -38,20 +39,20 @@ PKG_AUTORECONF="no"
 
 configure_host() {
   export GOOS=linux
-  export GOROOT_FINAL=$ROOT/$TOOLCHAIN/lib/golang-1.4
+  export GOROOT_FINAL=$TOOLCHAIN/lib/golang-1.4
   export GOARCH=amd64
   export CGO_ENABLED=0
-  export PKG_CONFIG="$ROOT/$TOOLCHAIN/bin/pkg-config"
+  export PKG_CONFIG="$TOOLCHAIN/bin/pkg-config"
   export CC_FOR_TARGET="$CC"
   export CXX_FOR_TARGET="$CXX"
 }
 
 make_host() {
-  cd $ROOT/$PKG_BUILD/src
+  cd $PKG_BUILD/src
   bash make.bash --no-banner
 }
 
 makeinstall_host() {
-  mkdir -p $ROOT/$TOOLCHAIN/lib/golang-1.4
-  cp -av $ROOT/$PKG_BUILD/* $ROOT/$TOOLCHAIN/lib/golang-1.4/
+  mkdir -p $TOOLCHAIN/lib/golang-1.4
+  cp -av $PKG_BUILD/* $TOOLCHAIN/lib/golang-1.4/
 }
