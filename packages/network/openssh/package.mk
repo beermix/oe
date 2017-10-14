785c5f431a20ad2d932cd1f3b4225099c5ca1aa5
AV <27rurk@gmail.com>
2017-10-14 01:13:00 -0400

    autocommit on change

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -67,7 +67,6 @@ post_makeinstall_target() {
   sed -e "s|^#PermitRootLogin.*|PermitRootLogin yes|g" \
       -e "s|^#StrictModes.*|StrictModes no|g" \
       -e "s|^#X11Forwarding.*|X11Forwarding yes|g" \
-      -e "s|^#UsePrivilegeSeparation.*|UsePrivilegeSeparation no|g" \
       -i $INSTALL/etc/ssh/sshd_config
 
   echo "PubkeyAcceptedKeyTypes +ssh-dss" >> $INSTALL/etc/ssh/sshd_config
