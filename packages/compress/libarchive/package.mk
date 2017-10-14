9a1eb379f648e8908d493a9eb225bff90b7f7e57
AV <27rurk@gmail.com>
2017-10-13 21:50:15 -0400

    autocommit on change

--- packages/compress/libarchive/package.mk
+++ packages/compress/libarchive/package.mk
@@ -6,7 +6,7 @@ PKG_DEPENDS_HOST="openssl:host zlib:host lzo:host xz:host bzip2:host lz4:host"
 PKG_SECTION="compress"
 PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTING=OFF \
 			  -DENABLE_CAT_SHARED=OFF \
