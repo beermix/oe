fd048c5e7505f6fee85214c87931db4bad8772d7
AV <27rurk@gmail.com>
2017-10-14 02:58:58 -0400

    autocommit on change

--- packages/lang/clang/package.mk
+++ packages/lang/clang/package.mk
@@ -1,9 +1,12 @@
+# Made by github.com/escalade
+
 PKG_NAME="clang"
-PKG_VERSION="4.0.1"
+PKG_VERSION="5.0.0"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
-PKG_URL="https://dl.dropboxusercontent.com/s/64stoxn39ggfb5c/clang-4.0.1.tar.xz"
+PKG_URL="http://releases.llvm.org/$PKG_VERSION/cfe-$PKG_VERSION.src.tar.xz"
+PKG_SOURCE_DIR="cfe-$PKG_VERSION.src"
 PKG_DEPENDS_TARGET="toolchain llvm:host libxml2"
 PKG_SECTION="lang"
 PKG_SHORTDESC="C language family frontend for LLVM"
@@ -16,4 +19,5 @@ pre_configure_target() {
   strip_gold
 }
 
-#PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
