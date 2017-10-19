1d21845e210334f064c716d332e13e6550713f94
AV <27rurk@gmail.com>
2017-10-19 14:55:38 -0400

    autocommit on change

---
+++ packages/graphics/jpeg/package.mk
@@ -0,0 +1,23 @@
+PKG_NAME="jpeg"
+PKG_VERSION="9b"
+PKG_URL="http://www.ijg.org/files/jpegsrc.v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="graphics"
+PKG_AUTORECONF="yes"
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -fPIC"
+}
+
+pre_configure_host() {
+  export CFLAGS="$CFLAGS -fPIC"
+}
+
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static --with-gnu-ld"
+
+PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
+
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
