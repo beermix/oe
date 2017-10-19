6d1750475f879e76777928b8555623578693f6fb
AV <27rurk@gmail.com>
2017-10-19 14:11:20 -0400

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -48,7 +48,7 @@ PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --enable-plugins \
                          --enable-ld=default \
                          --enable-lto \
-                         --disable-nls \
+                         --enable-nls \
                          --enable-poison-system-directories \
                          MAKEINFO=true LDFLAGS=-s"
 
