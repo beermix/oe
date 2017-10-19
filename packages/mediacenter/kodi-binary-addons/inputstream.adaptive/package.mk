862793d22796e27c9f9a714b971e4039f53a55e1
AV <27rurk@gmail.com>
2017-10-19 06:50:51 -0400

    autocommit on change

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -28,6 +28,19 @@ PKG_LONGDESC="inputstream.adaptive"
 
 PKG_IS_ADDON="yes"
 
+export CCACHE_DISABLE=1
+
+post_makeinstall_target() {
+  mkdir -p wv && cd wv
+    cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DDECRYPTERPATH=special://home/cdm \
+        $ROOT/$PKG_BUILD/wvdecrypter
+    make
+
+  cp -P $ROOT/$PKG_BUILD/.$TARGET_NAME/wv/libssd_wv.so $INSTALL/usr/lib
+}
+
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
   cp -R $PKG_BUILD/.install_pkg/usr/share/$MEDIACENTER/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
