17b28a5dd67751263be74177038bfd8be1f9ab06
AV <27rurk@gmail.com>
2017-10-19 09:47:28 -0400

    autocommit on change

--- packages/-my/wine/package.mk
+++ packages/-my/wine/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="wine"
-PKG_VERSION="2.18"
-PKG_URL="https://fossies.org/linux/misc/wine-$PKG_VERSION.tar.xz"
+PKG_VERSION="2.19"
+PKG_URL="https://dl.winehq.org/wine/source/2.x/wine-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain glib gmp x11 mesa xorg-server pcre libXcursor freetype fontconfig libjpeg-turbo libpng tiff libdrm glu libexif expat harfbuzz libxcb libXcursor libXrender libX11 x11 libXext libXtst cairo unclutter xdotool  libXScrnSaver libexif libXcomposite libXcursor libXtst libpcap gtk2 gst-plugins-base wine:host"
 PKG_DEPENDS_HOST="freetype:host"
 PKG_SECTION="tools"
