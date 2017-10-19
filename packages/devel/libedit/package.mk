0685fcf78e41ecea0eb4c375e5466debb04d6e47
AV <27rurk@gmail.com>
2017-10-19 14:05:04 -0400

    autocommit on change

--- packages/devel/libedit/package.mk
+++ packages/devel/libedit/package.mk
@@ -2,12 +2,11 @@ PKG_NAME="libedit"
 PKG_VERSION="20170329-3.1"
 PKG_URL="http://thrysoee.dk/editline/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
-
 PKG_AUTORECONF="no"
 
-pre_configure_target() {
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
-}
+#pre_configure_target() {
+#  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-widec --disable-shared --with-pic"
 
