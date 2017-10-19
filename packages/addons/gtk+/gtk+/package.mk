ebd6d8ed66405388b50905bb0154a884b43ca8a3
AV <27rurk@gmail.com>
2017-10-19 07:59:38 -0400

    autocommit on change

--- packages/addons/gtk+/gtk+/package.mk
+++ packages/addons/gtk+/gtk+/package.mk
@@ -1,16 +1,13 @@
 PKG_NAME="gtk+"
-PKG_VERSION="3.92.1"
-#PKG_VERSION="3.22.24"
+PKG_VERSION="3.22.24"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://ftp.acc.umu.se/pub/gnome/sources/gtk+/3.22/?C=M;O=D"
-PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-#PKG_URL="http://ftp.gnome.org/pub/gnome/sources/gtk+/3.22/gtk+-$PKG_VERSION.tar.xz"
+PKG_URL="http://ftp.gnome.org/pub/gnome/sources/gtk+/3.22/gtk+-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain atk libX11 libXrandr libXi glib pango cairo gdk-pixbuf at-spi2-atk glib:host"
 PKG_SECTION="x11/toolkits"
 PKG_SHORTDESC="gtk+: The Gimp ToolKit (GTK)"
 PKG_LONGDESC="This is GTK+. GTK+, which stands for the Gimp ToolKit, is a library for creating graphical user interfaces for the X Window System. It is designed to be small, efficient, and flexible. GTK+ is written in C with a very object-oriented approach."
-
 
 PKG_AUTORECONF="no"
 
--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -14,7 +14,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################ unrar
+################################################################################ unrar coreutils psmisc procps-ng
 
 PKG_NAME="busybox"
 PKG_VERSION="1.27.2"
@@ -24,7 +24,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://www.busybox.net"
 PKG_URL="http://busybox.net/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs zip unzip zlib pciutils usbutils parted procps-ng gptfdisk psmisc findutils grep gawk coreutils bash less tar"
+PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs zip unzip zlib pciutils usbutils parted gptfdisk findutils grep gawk bash less tar"
 PKG_DEPENDS_INIT="toolchain"
 PKG_PRIORITY="required"
 PKG_SECTION="system"
