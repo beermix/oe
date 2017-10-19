a70892fdb936696f4b41cff1b4c29de1bd5a948d
AV <27rurk@gmail.com>
2017-10-19 18:37:18 -0400

    autocommit on change

---
+++ packages/mediacenter/skin.xonfluence/package.mk
@@ -0,0 +1,38 @@
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
+PKG_NAME="skin.xonfluence"
+PKG_VERSION="177119d"
+PKG_GIT_URL="https://github.com/Helly1206/skin.xonfluence"
+PKG_DEPENDS_TARGET="toolchain kodi"
+PKG_SECTION="mediacenter"
+PKG_SHORTDESC="kodi-theme-AeonNox: Kodi Mediacenter theme"
+PKG_LONGDESC="kodi-theme-AeonNox: Kodi Mediacenter theme."
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  TexturePacker -input media/ \
+                -output Textures.xbt \
+                -dupecheck \
+}
+
+makeinstall_target() {
+  rm -f resources/screenshot-*.jpg
+  mkdir -p $INSTALL/usr/config/kodi.skins/skin.xonfluence
+    cp -R */ $INSTALL/usr/config/kodi.skins/skin.xonfluence
+    cp *.txt $INSTALL/usr/config/kodi.skins/skin.xonfluence
+    cp *.xml $INSTALL/usr/config/kodi.skins/skin.xonfluence
+      rm -rf $INSTALL/usr/config/kodi.skins/skin.xonfluence/media
+
+  mkdir -p $INSTALL/usr/config/kodi.skins/skin.xonfluence/media
+    cp Textures.xbt $INSTALL/usr/config/kodi.skins/skin.xonfluence/media
+    for theme in themes/*; do
+      cp $(basename $theme).xbt $INSTALL/usr/config/kodi.skins/skin.xonfluence/media
+    done
+
+  mkdir -p $INSTALL/usr/share/kodi/addons
+  ln -sf /storage/.config/kodi.skins/skin.xonfluence $INSTALL/usr/share/kodi/addons/skin.xonfluence
+}
