bc876aff32290cf4e1152beb896aa9ad0af9dd5d
AV <27rurk@gmail.com>
2017-10-20 14:28:47 -0400

    autocommit on change

--- packages/multimedia/freshplayerplugin/package.mk
+++ packages/multimedia/freshplayerplugin/package.mk
@@ -2,11 +2,10 @@ PKG_NAME="freshplayerplugin"
 PKG_VERSION="fb3c00d"
 PKG_URL="https://github.com/i-rinat/freshplayerplugin/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ragel:host alsa xrandr libXrender libXcursor libdrm libevent freetype openssl icu libva libvdpau cairo pango"
-PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 
 PKG_USE_CMAKE="yes"
-PKG_AUTORECONF="no"
+PKG_USE_NINJA="no"
 
 PKG_CMAKE_OPTS_TARGET="-DWITH_LIBV4L2=0 \
 			  -DWITH_JACK=0 \
