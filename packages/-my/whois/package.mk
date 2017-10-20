68cba1fd42ccf97dbdaaf714301ab4eff6c6057e
AV <27rurk@gmail.com>
2017-10-19 22:04:36 -0400

    autocommit on change

--- packages/-my/whois/package.mk
+++ packages/-my/whois/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="whois"
-PKG_VERSION="5.2.16"
+PKG_VERSION="5.2.18"
 PKG_SITE="http://ftp.debian.org/"
 PKG_URL="http://ftp.debian.org/debian/pool/main/w/whois/${PKG_NAME}_${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libidn2"
