fbc44ceff7d99fd8e0fb0eb1388ff2d9596153ee
AV <27rurk@gmail.com>
2017-10-20 04:24:53 -0400

    autocommit on change

--- packages/-my/gawk/package.mk
+++ packages/-my/gawk/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="gawk"
-PKG_VERSION="4.1.4"
+PKG_VERSION="4.2.0"
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline ncurses"
 PKG_SECTION="my"
