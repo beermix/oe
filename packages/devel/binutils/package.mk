c2a61d3056e0b0bdb9068ef4d9532dc0b084779b
AV <27rurk@gmail.com>
2017-10-20 03:24:34 -0400

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -27,13 +27,7 @@ PKG_DEPENDS_HOST="ccache:host bison:host flex:host libelf:host bc:host linux:hos
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="binutils: A GNU collection of binary utilities"
 PKG_LONGDESC="The GNU binutils are utilities of use when dealing with object files. the packages includes ld - the GNU linker, as - the GNU assembler, addr2line - converts addresses into filenames and line numbers, ar - a utility for creating, modifying and extracting from archives, c++filt - filter to demangle encoded C++ symbols, gprof - displays profiling information, nlmconv - converts object code into an NLM, nm - lists symbols from object files, objcopy - Copys and translates object files, objdump - displays information from object files, ranlib - generates an index to the contents of an archive, readelf - displays information from any ELF format object file, size - lists the section sizes of an object or archive file, strings - lists printable strings from files, strip - discards symbols as well as windres - a compiler for Windows resource files."
-
-
 PKG_AUTORECONF="no"
-
-#post_unpack() {
-#  sed -i "/ac_cpp=/s/\$CPPFLAGS/\$CPPFLAGS -O2/" $PKG_BUILD/libiberty/configure
-#}
 
 PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --with-sysroot=$SYSROOT_PREFIX \
@@ -81,5 +75,4 @@ makeinstall_target() {
   cp binutils/ar $INSTALL/usr/bin
 }
 
-#PKG_CONFIGURE_OPTS_TARGET="MAKEINFO=true --disable-shared --disable-multilib --without-ppl --without-cloog"
 PKG_CONFIGURE_OPTS_TARGET="--enable-shared=no"
