7d72c4270c83e14a886a7e624f4520aaceb7a1a5
AV <27rurk@gmail.com>
2017-10-20 13:35:33 -0400

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -7,5 +7,8 @@ PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess ca
 PKG_SECTION="tools"
 PKG_SHORTDESC="x11-utils"
 PKG_AUTORECONF="yes"
+PKG_USE_MESON="no"
 
-PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no --disable-tests --without-libunwind --disable-shared --enable-static --with-gnu-ld"
+PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no --disable-tests --without-libunwind --disable-shared"
+
+PKG_MESON_OPTS_TARGET=""
