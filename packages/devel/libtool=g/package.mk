df29744a02cf7e7aa9436c40fb1183dd2ecd20e8
AV <27rurk@gmail.com>
2017-10-21 17:35:34 +0000

    autocommit on change

--- packages/devel/libtool=g/package.mk
+++ packages/devel/libtool=g/package.mk
@@ -17,17 +17,23 @@
 ################################################################################
 
 PKG_NAME="libtool"
-PKG_VERSION="2.4.6"
-PKG_SHA256="e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3"
+PKG_VERSION="6ca5e22"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.gnu.org/software/libtool/"
-PKG_URL="http://ftpmirror.gnu.org/libtool/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host"
+#PKG_GIT_URL="https://git.savannah.gnu.org/git/libtool.git"
+PKG_URL="https://dl.dropboxusercontent.com/s/bnkfpojtqnjwm3y/libtool-6ca5e22.tar.xz"
+PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host help2man:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="devel"
 PKG_SHORTDESC="libtool: Generic library support script"
 PKG_LONGDESC="This is GNU Libtool, a generic library support script. Libtool hides the complexity of using shared libraries behind a consistent, portable interface."
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
+
+post_unpack() {
+  cd $PKG_BUILD
+  ./bootstrap --no-warnings --force --skip-git
+}
+
 
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
