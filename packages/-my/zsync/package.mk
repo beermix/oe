1a8e7ebc7c79e8772e2234629e69253a9b2f8834
AV <27rurk@gmail.com>
2017-10-21 06:03:55 -0400

    autocommit on change

---
+++ packages/-my/zsync/package.mk
@@ -0,0 +1,9 @@
+PKG_NAME="zsync"
+PKG_VERSION="0.6.2"
+PKG_SITE="http://ftp.debian.org/"
+PKG_URL="http://zsync.moria.org.uk/download/zsync-0.6.2.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="system"
+
+PKG_AUTORECONF="no"
+
