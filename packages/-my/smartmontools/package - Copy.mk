676ed12970104168752518576d7808a1cf897112
AV <27rurk@gmail.com>
2017-10-22 17:46:55 +0000

    autocommit on change

---
+++ packages/-my/smartmontools/package - Copy.mk
@@ -0,0 +1,19 @@
+PKG_NAME="smartmontools"
+PKG_VERSION="caa75b6"
+PKG_URL="https://github.com/mirror/smartmontools/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain ncurses"
+PKG_SECTION="my"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/smartmontools \
+			      --datarootdir=/storage/.config/smartmontools/share \
+			      --disable-silent-rules"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/storage
+#  rm -rf $INSTALL/usr/lib
+}
+
+#post_install() {
+#  enable_service smartd.service
+#}
