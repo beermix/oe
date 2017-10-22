235d357df5c1e86840a3860d99939709b38e7c37
AV <27rurk@gmail.com>
2017-10-22 17:41:33 +0000

    autocommit on change

--- packages/python/devel/cryptography/package.mk
+++ packages/python/devel/cryptography/package.mk
@@ -22,11 +22,8 @@ PKG_SHA256="adbb7ddb9623616ea2c0354b7ce0506eee529b7f2d0e3894e513de5b8dce4ef8"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cryptography.io/"
 PKG_URL="https://github.com/pyca/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET=""
-PKG_LONGDESC="A package designed to expose cryptographic primitives and recipes to Python developers"
-
-PKG_IS_PYTHON="yes"
 PKG_PYTHON_DEPENDS_TARGET="asn1crypto cffi enum34 idna ipaddress six"
+PKG_LONGDESC="A package designed to expose cryptographic primitives and recipes to Python developers"
 
 pre_configure_target() {
   export CC="$CC -pthread"
