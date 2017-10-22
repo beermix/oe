301bd680484d426c078f7ccecc00c899c07dc13c
AV <27rurk@gmail.com>
2017-10-22 13:43:54 +0000

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -45,7 +45,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
                            --enable-stack-protector=strong \
                            --with-binutils=$BUILD/toolchain/bin \
                            --with-headers=$SYSROOT_PREFIX/usr/include \
-                           --enable-kernel=3.0.0 \
+                           --enable-kernel=3.2.0 \
                            --without-cvs \
                            --without-gd \
                            --enable-obsolete-rpc \
