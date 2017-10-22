e1493fab126c5503e9e954f4e4959cb927db41e6
AV <27rurk@gmail.com>
2017-10-22 22:36:45 +0000

    autocommit on change

--- packages/-my/git/package.mk
+++ packages/-my/git/package.mk
@@ -3,27 +3,20 @@ PKG_VERSION="2.14.2"
 PKG_URL="https://www.kernel.org/pub/software/scm/git/git-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain curl pcre expat zlib openssl"
 PKG_SECTION="devel"
-
 PKG_AUTORECONF="yes"
 
 pre_configure_target() {
   cd $PKG_BUILD
-  
-#  export CC="$CC"
-#  export CFLAGS="$CFLAGS"
-#  export CXXFLAGS="$CXXFLAGS"
-#  export CPPFLAGS="$CPPFLAGS"
-#  export LDFLAGS="$LDFLAGS"
-  
+ 
 #  export NO_EXPAT="YesPlease"
 #  export NO_ICONV="YesPlease"
 #  export NO_PYTHON="YesPlease"
-
-  export NO_MKSTEMPS="YesPlease"
-  export NO_GETTEXT="YesPlease"
 #  export NO_UNIX_SOCKETS="YesPlease"
+#  export NO_GETTEXT="YesPlease"
+#  export NO_MKSTEMPS="YesPlease"
+#  export NO_PERL="YesPlease"
+
   export NO_NSEC="YesPlease"
-  export NO_PERL="YesPlease"
   export NO_TCLTK="YesPlease"
   export NO_INSTALL_HARDLINKS="yes"
 }
