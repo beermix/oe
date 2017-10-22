95699f6eeeca05eea1bd72c27fed6e15fdac6035
AV <27rurk@gmail.com>
2017-10-22 21:13:47 +0000

    autocommit on change

--- packages/network/libnl/package.mk
+++ packages/network/libnl/package.mk
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="libnl"
-PKG_VERSION="3.3.0"
+PKG_VERSION="3.4.0"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
-PKG_SITE="http://people.suug.ch/~tgr/libnl/"
+PKG_SITE="https://github.com/thom311/libnl/"
 PKG_URL="https://github.com/thom311/$PKG_NAME/releases/download/${PKG_NAME}${PKG_VERSION//./_}/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="network"
