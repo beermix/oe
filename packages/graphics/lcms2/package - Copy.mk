cb4e5dff561fb5b099d1c55250882454a0799e55
AV <27rurk@gmail.com>
2017-10-22 21:53:53 +0000

    autocommit on change

--- packages/graphics/lcms2/package - Copy.mk
+++ packages/graphics/lcms2/package - Copy.mk
@@ -17,13 +17,12 @@
 ################################################################################
 
 PKG_NAME="lcms2"
-PKG_VERSION="lcms2.9rc1"
+PKG_VERSION="2.8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.littlecms.com"
-PKG_URL="https://github.com/mm2/Little-CMS/archive/${PKG_VERSION}.tar.gz"
-PKG_SOURCE_DIR="Little-CMS-$PKG_VERSION*"
+PKG_URL="http://downloads.sourceforge.net/sourceforge/lcms/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib libjpeg-turbo tiff"
 PKG_SECTION="devel"
 PKG_SHORTDESC="lcms2: small-footprint color management engine"
