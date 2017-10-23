d11a16739e6564e4b75d0755ecde80165c8d0156
AV <27rurk@gmail.com>
2017-10-23 00:48:09 +0000

    autocommit on change

--- packages/-my/libunwind/package.mk
+++ packages/-my/libunwind/package.mk
@@ -1,9 +1,7 @@
 PKG_NAME="libunwind"
-PKG_VERSION="1.2"
+PKG_VERSION="1.2.1"
 PKG_URL="http://download.savannah.gnu.org/releases/libunwind/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-
-
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
