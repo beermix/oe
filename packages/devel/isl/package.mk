137c6548d6f66edad074e4ebbb41389c4760b5fe
AV <27rurk@gmail.com>
2017-10-23 04:44:32 +0000

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -6,4 +6,11 @@ PKG_DEPENDS_TARGET="toolchain gmp"
 PKG_SECTION="devel"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --with-gmp=$TOOLCHAIN"
+PKG_CONFIGURE_OPTS_HOST="--enable-static \
+			    --disable-shared \
+			    --with-int=gmp \
+			    --with-gmp=system \
+			    --with-gmp-prefix=$TOOLCHAIN \
+			    --with-gcc-arch=$TARGET_ARCH \
+			    --with-clang=no \
+			    --with-pic"
