8682358255f51911b60f46f5715c7e5d8f00d53c
AV <27rurk@gmail.com>
2017-10-23 03:17:04 +0000

    autocommit on change

--- packages/-my/ps3remote/package.mk
+++ packages/-my/ps3remote/package.mk
@@ -5,19 +5,12 @@ PKG_DEPENDS_TARGET="toolchain systemd"
 PKG_SECTION="my"
 PKG_AUTORECONF="no"
 
-make_target() {
-  make \
-  	CC="$CC" \
-  	CFLAGS="$CFLAGS -Wall -DDEBUG=0" \
-  	LDFLAGS="$LDFLAGS" \
-  	RANLIB="$RANLIB" \
-  	-j1
-}
+MAKEFLAGS=-j1
 
 post_make_target() {
   mkdir -p $INSTALL/usr/bin/
   mkdir -p $INSTALL_DEV/usr/bin/
-  $STRIP $PKG_BUILD/ps3remote
+  debug_strip $PKG_BUILD/ps3remote
   cp $PKG_BUILD/ps3remote $INSTALL/usr/bin/
 }
 
