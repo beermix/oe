95899c64ebf55d6458adaa21b01840687c6f15c4
AV <27rurk@gmail.com>
2017-10-31 17:20:55 +0000

    autocommit on change

--- packages/devel/swig/package.mk
+++ packages/devel/swig/package.mk
@@ -22,21 +22,16 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.swig.org"
 PKG_URL="$SOURCEFORGE_SRC/swig/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host"
+PKG_DEPENDS_HOST="ccache:host pcre:host"
 PKG_SECTION="devel"
 PKG_SHORTDESC="SWIG: a software development tool that connects programs written in C and C++ with a variety of high-level programming languages."
 PKG_LONGDESC="SWIG is a software development tool that connects programs written in C and C++ with a variety of high-level programming languages."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--with-pcre-prefix=$TOOLCHAIN \
-                         --with-boost=no \
-                         --without-pcre \
+PKG_CONFIGURE_OPTS_HOST="--with-boost=no \
                          --without-x \
                          --without-tcl \
-                         --without-python \
-                         --without-python3 \
                          --without-perl5 \
                          --without-octave \
                          --without-java \
@@ -50,9 +45,7 @@ PKG_CONFIGURE_OPTS_HOST="--with-pcre-prefix=$TOOLCHAIN \
                          --without-pike \
                          --without-chicken \
                          --without-csharp \
-                         --without-lua \
                          --without-allegrocl \
                          --without-clisp \
                          --without-r \
-                         --without-go \
                          --without-d"
