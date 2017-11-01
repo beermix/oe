dc8942ad72d22cdf9454ffb5e65010ff0b119a9a
AV <27rurk@gmail.com>
2017-11-01 02:32:56 +0000

    autocommit on change

--- packages/-my/chromium-launcher/package.mk
+++ packages/-my/chromium-launcher/package.mk
@@ -1,9 +1,8 @@
 PKG_NAME="chromium-launcher"
 PKG_VERSION="de48b31"
-PKG_SITE="http://ftp.debian.org/"
-PKG_GIT_URL="https://github.com/foutrelis/chromium-launcher"
+PKG_SITE="https://github.com/foutrelis/chromium-launcher"
+PKG_URL="https://github.com/foutrelis/chromium-launcher/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain json-glib"
 PKG_SECTION="system"
-
 PKG_AUTORECONF="no"
 
