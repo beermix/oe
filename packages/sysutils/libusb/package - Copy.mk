68225f66f66e7f50050c2d1bb9d14e4b67efdbcb
AV <27rurk@gmail.com>
2017-11-03 13:02:14 +0300

    autocommit on change

--- packages/sysutils/libusb/package - Copy.mk
+++ packages/sysutils/libusb/package - Copy.mk
@@ -17,18 +17,24 @@
 ################################################################################
 
 PKG_NAME="libusb"
-PKG_VERSION="b4c9b42"
+PKG_VERSION="1.0.21"
+PKG_SHA256="7dce9cce9a81194b7065ee912bcd55eeffebab694ea403ffb91b67db66b1824b"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1"
 PKG_SITE="http://libusb.info/"
-PKG_URL="https://github.com/libusb/libusb/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="$SOURCEFORGE_SRC/libusb/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain systemd"
 PKG_SECTION="system"
 PKG_SHORTDESC="libusb: OS independent USB device access"
 PKG_LONGDESC="The libusb project's aim is to create a Library for use by user level applications to USB devices regardless of OS."
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-log --disable-debug-log --enable-udev --disable-examples-build"
+PKG_CONFIGURE_OPTS_TARGET="--enable-shared \
+            --enable-static \
+            --disable-log \
+            --disable-debug-log \
+            --enable-udev \
+            --disable-examples-build"
 
 pre_configure_target () {
   #libusb sometimes fails to build if building paralell
