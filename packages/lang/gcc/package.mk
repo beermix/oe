df933daf7c27d44622b5de8c01afa528c253f592
AV <27rurk@gmail.com>
2017-11-07 18:19:50 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -56,6 +56,7 @@ GCC_COMMON_CONFIGURE_OPTS="--target=$TARGET_NAME \
                            --without-cuda-driver \
                            --disable-vtable-verify \
                            --disable-libsanitizer \
+                           --disable-werror \
                            --enable-default-pie \
                            --enable-poison-system-directories \
                            --with-tune=generic -v"
