7a34c1b444705f623e3ea402c3fd498e6c7e4276
AV <27rurk@gmail.com>
2017-11-08 02:31:20 +0300

    autocommit on change

--- packages/graphics/libdrm/package.mk
+++ packages/graphics/libdrm/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://dri.freedesktop.org"
 PKG_URL="http://dri.freedesktop.org/libdrm/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain libpciaccess"
+PKG_DEPENDS_TARGET="toolchain libpthread-stubs libpciaccess"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libdrm: Userspace interface to kernel DRM services"
 PKG_LONGDESC="The userspace interface library to kernel DRM services."
@@ -31,18 +31,21 @@ PKG_AUTORECONF="yes"
 
 get_graphicdrivers
 
-DRM_CONFIG="--disable-intel --disable-radeon --disable-amdgpu"
-DRM_CONFIG="$DRM_CONFIG --disable-nouveau --disable-vmwgfx --disable-vc4"
+DRM_CONFIG="--disable-libkms --disable-intel --disable-radeon --disable-amdgpu"
+DRM_CONFIG="$DRM_CONFIG --disable-nouveau --disable-vmwgfx"
 
 for drv in $GRAPHIC_DRIVERS; do
   [ "$drv" = "i915" -o "$drv" = "i965" ] && \
+    DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-libkms/enable-libkms/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-intel/enable-intel/'`
 
   [ "$drv" = "r200" -o "$drv" = "r300" -o "$drv" = "r600" -o "$drv" = "radeonsi" ] && \
+    DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-libkms/enable-libkms/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-radeon/enable-radeon/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-amdgpu/enable-amdgpu/'`
 
   [ "$drv" = "vmware" ] && \
+    DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-libkms/enable-libkms/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-vmwgfx/enable-vmwgfx/'`
 
   [ "$drv" = "vc4" ] && \
@@ -51,11 +54,7 @@ done
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-udev \
                            --enable-largefile \
-                           --with-kernel-source=$(kernel_path) \
-                           --disable-libkms \
                            $DRM_CONFIG \
-                           --disable-nouveau \
-                           --disable-freedreno \
                            --disable-install-test-programs \
                            --disable-cairo-tests \
                            --disable-manpages \
