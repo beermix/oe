2ca3ed00dd6ae8d3b1b943427110b5155a0632fd
AV <27rurk@gmail.com>
2017-11-07 22:33:53 +0300

    autocommit on change

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="flex"
 PKG_VERSION="2.6.4"
-PKG_SITE="http://flex.sourceforge.net/"
+PKG_SITE="https://github.com/westes/flex/releases"
 PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_SECTION="toolchain/devel"
