a9946c9dbb26148ba2ab72c0b0209a2c01d25321
AV <27rurk@gmail.com>
2017-11-07 18:17:48 +0300

    autocommit on change

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 PKG_NAME="configtools"
-PKG_VERSION="ddf1db9"
+PKG_VERSION="64198f1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
