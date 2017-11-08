c81bbd75300f3d926479418d44a3e3fc620f5b1a
AV <27rurk@gmail.com>
2017-11-08 03:58:58 +0300

    autocommit on change

--- packages/-my/zapret/package.mk
+++ packages/-my/zapret/package.mk
@@ -1,9 +1,8 @@
 PKG_NAME="zapret"
-PKG_VERSION="03eb41f"
-PKG_GIT_URL="https://github.com/bol-van/zapret"
+PKG_VERSION="4668258"
+PKG_URL="https://github.com/bol-van/zapret/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux systemd libnfnetlink libnetfilter_queue libnetfilter_conntrack ipset"
 PKG_SECTION="my"
-
 PKG_AUTORECONF="no"
 
 make_target() {
--- packages/databases/mariadb/package.mk
+++ packages/databases/mariadb/package.mk
@@ -123,6 +123,7 @@ configure_host() {
         -DWITH_SYSTEMD=no \
         -DWITH_LIBWRAP=OFF \
         -DWITH_WSREP=OFF \
+        -DSECURITY_HARDENED=0 \
         ..
 }
 
@@ -162,6 +163,7 @@ configure_target() {
         -DWITH_ZLIB=bundled \
         -DWITH_SYSTEMD=no \
         -DWITH_LIBWRAP=OFF \
+        -DSECURITY_HARDENED=0 \
         -DWITH_SSL=$SYSROOT_PREFIX/usr \
         $MARIADB_OPTS \
         ..
