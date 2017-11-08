dc4ba7b213384eed8a3fd62fa795809b14d4c1a1
AV <27rurk@gmail.com>
2017-11-08 05:46:17 +0300

    autocommit on change

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -18,7 +18,6 @@ PKG_CONFIGURE_OPTS_SHARED="--openssldir=/etc/ssl \
                            enable-unit-test \
                            no-ssl2 \
                            no-ssl3 \
-                           no-heartbeats \
                            enable-ec_nistp_64_gcc_128"
 
 pre_configure_host() {
