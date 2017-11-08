7886f65a34268727112b66d7572d29c83fdd4c9e
AV <27rurk@gmail.com>
2017-11-08 04:44:59 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="glibc"
-PKG_VERSION="glibc-2.26.9000"
+PKG_VERSION="3683b42"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/bminor/glibc/tree/release/2.26/master"
