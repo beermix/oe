81620290c8dd0da67e76f1498228e4026b4f2d9d
AV <27rurk@gmail.com>
2017-11-10 14:30:55 +0300

    autocommit on change

--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -34,5 +34,7 @@ export CC=$LOCAL_CC
 PKG_CONFIGURE_OPTS_HOST="--without-guile"
 
 post_makeinstall_host() {
-  ln -sf make $TOOLCHAIN/bin/gmake
+#  ln -sf make $TOOLCHAIN/bin/gmake
+  ln -sfr /usr/bin/make $TOOLCHAIN/bin/gmake
+  ln -sfr /usr/bin/make $TOOLCHAIN/bin/make
 }
