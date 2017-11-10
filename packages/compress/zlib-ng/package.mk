7f8049953e03e183414e44bcb0ebb0556820ec39
AV <27rurk@gmail.com>
2017-11-10 11:49:58 +0300

    autocommit on change

--- packages/compress/zlib-ng/package.mk
+++ packages/compress/zlib-ng/package.mk
@@ -1,11 +1,37 @@
 PKG_NAME="zlib-ng"
 PKG_VERSION="e07a52d"
 PKG_URL="https://github.com/Dead2/zlib-ng/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="cmake:host"
+PKG_DEPENDS_TARGET=""
 PKG_DEPENDS_HOST=""
 PKG_SECTION="compress"
 PKG_AUTORECONF="no"
+PKG_USE_CMAKE="no"
 PKG_USE_NINJA="no"
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_HOST="-DWITH_NATIVE_INSTRUCTIONS=ON -DZLIB_COMPAT=ON -DWITH_GZFILEOP=ON"
+
+pre_configure_host() {
+  mkdir -p $PKG_BUILD/.$HOST_NAME
+  cp -r -i $PKG_BUILD/* $PKG_BUILD/.$HOST_NAME/
+}
+
+configure_host() {
+  cd $PKG_BUILD/.$HOST_NAME
+  ./configure --prefix=/
+}
+
+pre_configure_target() {
+  mkdir -p $PKG_BUILD/.$TARGET_NAME
+  cp -r -i $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME/
+}
+
+configure_target() {
+  cd $PKG_BUILD/.$TARGET_NAME
+  ./configure --prefix=/usr --zlib-compat --64
+}
+
+configure_host() {
+  cd $PKG_BUILD/.$HOST_NAME
+  ./configure --prefix=/usr --libdir=$TOOLCHAIN/lib --zlib-compat --64
+}
