20c014c00a527ec8a42c67befcbd7a6ef53a75b5
AV <27rurk@gmail.com>
2017-12-13 23:27:04 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -47,8 +47,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
     ;;
   *)
-    PKG_VERSION="4.11.12"
-    PKG_SHA256="2e37d4e18094f66b8bd632cac0a3c08a39a14a1ada4a1c78a5e336c4d3036192"
+    PKG_VERSION="4.14.5"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
