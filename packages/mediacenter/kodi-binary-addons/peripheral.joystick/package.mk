343f016cf7ebda49ac642905987dca057046183b
AV <27rurk@gmail.com>
2017-12-14 07:49:25 +0300

    autocommit on change

--- packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="peripheral.joystick"
-PKG_VERSION="b464260"
+PKG_VERSION="0acb777"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -30,7 +30,7 @@ PKG_LONGDESC="peripheral.joystick provides joystick support and button mapping"
 
 
 PKG_IS_ADDON="yes"
-PKG_USE_NINJA="no"
+PKG_TOOLCHAIN="cmake-make"
 PKG_ADDON_TYPE="kodi.peripheral"
 
 addon() {
