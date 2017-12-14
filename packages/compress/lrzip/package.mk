c793642bb866ee0855d71c14cdbb5a2db3bce23d
AV <27rurk@gmail.com>
2017-12-14 08:34:08 +0300

    autocommit on change

--- packages/compress/lrzip/package.mk
+++ packages/compress/lrzip/package.mk
@@ -6,7 +6,6 @@ PKG_DEPENDS_HOST="zlib:host lzo:host xz:host bzip2:host"
 PKG_SECTION="compress"
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-largefile --disable-doc --enable-static-bin"
-
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-largefile --disable-doc"
 PKG_CONFIGURE_OPTS_HOST="--enable-largefile --disable-doc --with-zlib=$TOOLCHAIN"
 
