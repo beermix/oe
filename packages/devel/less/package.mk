a5ec116f579ef6990fc419035ea2d911f41578ac
AV <27rurk@gmail.com>
2017-12-14 07:37:11 +0300

    autocommit on change

--- packages/devel/less/package.mk
+++ packages/devel/less/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="less"
-PKG_VERSION="527"
+PKG_VERSION="529"
 PKG_URL="http://www.greenwoodsoftware.com/less/less-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain readline pcre"
 PKG_SECTION="my"
 PKG_TOOLCHAIN="autotools"
 
-#PKG_CONFIGURE_OPTS_TARGET="--with-regex=pcre"
+PKG_CONFIGURE_OPTS_TARGET="--with-regex=pcre"
