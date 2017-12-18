9eb54a86829eae9f8d3a126113034637c79697fc
AV <27rurk@gmail.com>
2017-12-18 19:19:32 +0300

    autocommit on change

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://ccache.samba.org/"
 PKG_URL="https://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_HOST="make:host"
+PKG_DEPENDS_HOST="make:host zlib:host"
 PKG_SECTION="devel"
 PKG_SHORTDESC="ccache: A fast compiler cache"
 PKG_LONGDESC="Ccache is a compiler cache. It speeds up re-compilation of C/C++ code by caching previous compiles and detecting when the same compile is being done again."
@@ -31,7 +31,7 @@ PKG_LONGDESC="Ccache is a compiler cache. It speeds up re-compilation of C/C++ c
 export CC=$LOCAL_CC
 export CXX=$LOCAL_CXX
 
-PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
+PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib=no"
 
 post_makeinstall_host() {
 # setup ccache
