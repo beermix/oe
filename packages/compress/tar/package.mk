99f46b05d25e1727ec310cbecef3cfa4b9204ba4
AV <27rurk@gmail.com>
2017-12-18 17:32:59 +0300

    autocommit on change

--- packages/compress/tar/package.mk
+++ packages/compress/tar/package.mk
@@ -1,7 +1,6 @@
 PKG_NAME="tar"
-PKG_VERSION="1.29"
+PKG_VERSION="1.30"
 PKG_URL="http://mirrors.kernel.org/gnu/tar/tar-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain attr acl"
-
 
 PKG_CONFIGURE_OPTS_TARGET="--without-selinux"
