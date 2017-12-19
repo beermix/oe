5a240a28c4991b8cc4a93b74b8bca3592c6282c0
AV <27rurk@gmail.com>
2017-12-19 10:25:04 +0300

    autocommit on change

--- packages/databases/gdbm/package.mk
+++ packages/databases/gdbm/package.mk
@@ -3,7 +3,7 @@ PKG_VERSION="1.13"
 PKG_URL="http://mirror.switch.ch/ftp/mirror/gnu/gdbm/gdbm-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="my"
-
+PKG_TOOLCHAIN="configure"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-libgdbm-compat --disable-shared --with-pic"
 PKG_CONFIGURE_OPTS_HOST="--enable-libgdbm-compat"
