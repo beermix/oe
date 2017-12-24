d0b91796d7957070c8b24cfd32fe9fa0e2a31806
AV <27rurk@gmail.com>
2017-12-24 04:53:36 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -177,7 +177,7 @@ if [ "$GLIBC_LOCALES" = yes ]; then
   mkdir -p $INSTALL/etc/profile.d
   I18NPATH=../localedata 
   localedef -i ../localedata/locales/en_US -f ../localedata/charmaps/UTF-8 en_US.UTF-8 --prefix=$INSTALL
-  localedef -i ../localedata/locales/en_US -f ../localedata/charmaps/LC_CTYPE=ru_RU.UTF-8 --prefix=$INSTALL
+  localedef -i ../localedata/locales/en_US -f ../localedata/charmaps/UTF-8 ru_ru.utf-8 --prefix=$INSTALL
   echo "export LANG=en_US.UTF-8" > $INSTALL/etc/profile.d/01-locale.conf
 fi
 
