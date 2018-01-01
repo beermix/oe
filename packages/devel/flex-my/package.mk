b054e6cbdc94f770c16220acf823b1bba8d0ab1c
AV <27rurk@gmail.com>
2018-01-01 08:18:57 +0300

    autocommit on change

--- packages/devel/flex-my/package.mk
+++ packages/devel/flex-my/package.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="flex"
 PKG_VERSION="2.6.4"
-PKG_SITE="http://flex.sourceforge.net/"
+PKG_SITE="https://github.com/westes/flex/releases"
 PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host m4:host"
 PKG_SECTION="toolchain/devel"
@@ -26,7 +26,9 @@ PKG_SHORTDESC="flex: Fast lexical analyzer generator"
 PKG_LONGDESC="flex is a tool for generating programs that perform pattern-matching on text."
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="ac_cv_path_M4=$TOOLCHAIN/bin/m4 --disable-doc --disable-shared"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_reallocarray=no --disable-program --disable-doc"
 
 post_makeinstall_host() {
   cat > $TOOLCHAIN/bin/lex << "EOF"
@@ -36,3 +38,4 @@ EOF
 
   chmod -v 755 $TOOLCHAIN/bin/lex
 }
+
