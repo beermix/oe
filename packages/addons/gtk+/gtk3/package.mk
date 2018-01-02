6bbe4155763f1464e28e9d574eabbb2ad5c968eb
AV <27rurk@gmail.com>
2018-01-02 05:09:53 +0300

    autocommit on change

--- packages/addons/gtk+/gtk3/package.mk
+++ packages/addons/gtk+/gtk3/package.mk
@@ -1,18 +1,15 @@
 PKG_NAME="gtk3"
-PKG_VERSION="3.92.1"
+PKG_VERSION="3.93.0"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.gtk.org/"
-PKG_URL="http://ftp.gnome.org/pub/gnome/sources/gtk+/3.92/gtk+-$PKG_VERSION.tar.xz"
+PKG_URL="http://ftp.gnome.org/pub/gnome/sources/gtk+/3.93/gtk+-$PKG_VERSION.tar.xz"
 PKG_SOURCE_DIR="gtk+-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain atk libX11 libXrandr libXi glib:host pango cairo gdk-pixbuf at-spi2-atk"
 PKG_SECTION="x11/toolkits"
 PKG_SHORTDESC="gtk+: The Gimp ToolKit (GTK)"
 PKG_LONGDESC="This is GTK+. GTK+, which stands for the Gimp ToolKit, is a library for creating graphical user interfaces for the X Window System. It is designed to be small, efficient, and flexible. GTK+ is written in C with a very object-oriented approach."
-
-
-PKG_USE_MESON="yes"
-PKG_USE_NINJA="no"
+PKG_TOOLCHAIN="meson"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_path_GLIB_GENMARSHAL=$TOOLCHAIN/bin/glib-genmarshal \
                            --disable-glibtest \
@@ -52,8 +49,7 @@ post_makeinstall_target() {
 
 pre_configure_target() {
   export LC_ALL=en_US.UTF-8
-
-  # meson needs a host compiler and it's detected through the environment. meh.
+  
   export CC="$HOST_CC"
   export CXX="$HOST_CXX"
 }
