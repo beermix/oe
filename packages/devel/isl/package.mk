67dd9fcd288d8b0bb06eeaf62249128737201f4b
AV <27rurk@gmail.com>
2018-01-09 00:29:16 +0300

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -1,8 +1,5 @@
 PKG_NAME="isl"
 PKG_VERSION="0.16.1"
 PKG_URL="http://isl.gforge.inria.fr/isl-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="libtool:host intltool:host gmp:host"
 PKG_SECTION="devel"
 PKG_TOOLCHAIN="manual"
-
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --with-gmp-prefix=$TOOLCHAIN --with-pic"
