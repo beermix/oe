0a12da4fcc953fba72549c78da1defea2bdbc070
AV <27rurk@gmail.com>
2018-01-12 20:42:30 +0300

    autocommit on change

--- packages/-my/Pcap_DNSProxy/package.mk
+++ packages/-my/Pcap_DNSProxy/package.mk
@@ -3,3 +3,5 @@ PKG_VERSION="39ad239"
 PKG_URL="https://github.com/chengr28/Pcap_DNSProxy/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl libpcap libsodium"
 PKG_TOOLCHAIN="cmake-make"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DENABLE_LIBSODIUM=1 -DENABLE_PCAP=1 -DENABLE_TLS=1"
