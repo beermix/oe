22b752de337d9838312d01423d54f2895cf882d9
AV <27rurk@gmail.com>
2018-01-18 15:49:29 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -17,12 +17,11 @@
 ################################################################################
 
 PKG_NAME="gcc"
-PKG_VERSION="d56301c"
+PKG_VERSION="7.2.1-20180116"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/gcc-mirror/gcc/tree/gcc-7-branch"
-#PKG_URL="ftp://gcc.gnu.org/pub/gcc/snapshots/$PKG_VERSION/gcc-$PKG_VERSION.tar.xz"
-PKG_URL="https://github.com/gcc-mirror/gcc/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://sources.archlinux.org/other/gcc"
+PKG_URL="https://sources.archlinux.org/other/gcc/gcc-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_BOOTSTRAP="ccache:host autoconf:host binutils:host gmp:host mpfr:host zlib:host mpc:host"
 PKG_DEPENDS_TARGET="gcc:host"
 PKG_DEPENDS_HOST="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host zlib:host glibc"
