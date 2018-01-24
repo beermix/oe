a03d946c4cfcb65088ff6f2ae05218085377c6a5
AV <27rurk@gmail.com>
2018-01-24 15:54:01 +0300

    autocommit on change

--- packages/sysutils/tz/package.mk
+++ packages/sysutils/tz/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="tz"
-PKG_VERSION="2017c"
-PKG_SHA256="83d1c6a5910e827db88528a1a1f6f7b4ad9fe98b6076895749bf49660b02cc68"
+PKG_VERSION="2018c"
+PKG_SHA256="9c653d7b7b127d81f7db07920d1c3f7a0a7f1e5bc042d7907e01427593954e15"
 PKG_ARCH="any"
 PKG_LICENSE="Public Domain"
 PKG_SITE="http://www.iana.org/time-zones"
