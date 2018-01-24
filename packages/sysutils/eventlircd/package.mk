206d9534c9441e873618a215303c954b822fed53
AV <27rurk@gmail.com>
2018-01-24 04:30:31 +0300

    autocommit on change

--- packages/sysutils/eventlircd/package.mk
+++ packages/sysutils/eventlircd/package.mk
@@ -17,20 +17,20 @@
 ################################################################################
 
 PKG_NAME="eventlircd"
-PKG_VERSION="fb51489"
+PKG_VERSION="3b753e9"
+PKG_SHA256="4eca52d0570fa568b3296a2c9bc2af252423e25c1a67654bd79680fc5a93092a"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/OpenELEC/eventlircd"
-PKG_URL="https://github.com/OpenELEC/eventlircd/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/LibreELEC/eventlircd"
+PKG_URL="https://github.com/LibreELEC/eventlircd/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}*"
-PKG_DEPENDS_TARGET="toolchain systemd"
+PKG_DEPENDS_TARGET="toolchain systemd lirc"
 PKG_SECTION="system/remote"
 PKG_SHORTDESC="eventlircd:The eventlircd daemon provides various functions for LIRC devices"
 PKG_LONGDESC="The eventlircd daemon provides four functions for LIRC devices"
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-udev-dir=/usr/lib/udev \
-                           --with-lircd-socket=/run/lirc/lircd"
+PKG_CONFIGURE_OPTS_TARGET="--with-udev-dir=/usr/lib/udev"
 
 post_makeinstall_target() {
 # install our own evmap files and udev rules
