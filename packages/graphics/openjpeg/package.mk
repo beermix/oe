a59a61380b74d6f420a48f30d27e6471e1e26c8a
AV <27rurk@gmail.com>
2018-01-24 05:50:58 +0300

    autocommit on change

--- packages/graphics/openjpeg/package.mk
+++ packages/graphics/openjpeg/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="openjpeg"
 PKG_VERSION="2.3.0"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain zlib xz libxml2 libpng libjpeg-turbo tiff lcms2"
+PKG_DEPENDS_TARGET="toolchain zlib xz libxml2 libpng libjpeg-turbo tiff"
 PKG_SECTION="graphics"
 PKG_TOOLCHAIN="cmake-make"
 
