c9405034cde3e9ac1b0ae8e9d77f327400988e8c
AV <27rurk@gmail.com>
2018-02-01 22:46:14 +0300

    autocommit on change

--- packages/-my/shadowsocks-libev/package.mk
+++ packages/-my/shadowsocks-libev/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="shadowsocks-libev"
-PKG_VERSION="3.1.0"
+PKG_VERSION="3.1.1"
 PKG_URL="https://github.com/shadowsocks/shadowsocks-libev/releases/download/v$PKG_VERSION/shadowsocks-libev-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain pcre libsodium libev mbedtls c-ares"
 PKG_SECTION="my"
