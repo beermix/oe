f8b05b275f14369ca988c5b5bfff114bc00ba5ef
AV <27rurk@gmail.com>
2018-02-01 21:29:13 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="chromium"
-PKG_VERSION="63.0.3239.108"
-PKG_SHA256="47d80798194da78bdd519b7ce012425b13cf89d6eb287e22a34342a245c31a2b"
+PKG_VERSION="64.0.3282.119"
+#PKG_SHA256="47d80798194da78bdd519b7ce012425b13cf89d6eb287e22a34342a245c31a2b"
 PKG_REV="108"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Mixed"
