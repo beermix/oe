b0213f12a1a3a08f8ab698c0423094b7d6ef4adc
AV <27rurk@gmail.com>
2018-02-05 22:09:28 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -58,6 +58,7 @@ GCC_COMMON_CONFIGURE_OPTS="--target=$TARGET_NAME \
                            --disable-libssp \
                            --without-cuda-driver \
                            --disable-libquadmath \
+                           --disable-libsanitizer \
                            --with-tune=haswell \
                            --with-arch=westmere"
 
@@ -72,7 +73,6 @@ PKG_CONFIGURE_OPTS_BOOTSTRAP="$GCC_COMMON_CONFIGURE_OPTS \
                               --disable-libgomp \
                               --disable-libada \
                               --disable-libatomic \
-                              --disable-libsanitizer \
                               --without-headers \
                               --with-newlib \
                               --disable-decimal-float \
