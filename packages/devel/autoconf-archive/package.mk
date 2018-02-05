b30b260b6e5f7856deba2c4b268e863bd8e5ecda
AV <27rurk@gmail.com>
2018-02-05 23:50:04 +0300

    autocommit on change

--- packages/devel/autoconf-archive/package.mk
+++ packages/devel/autoconf-archive/package.mk
@@ -18,6 +18,7 @@
 
 PKG_NAME="autoconf-archive"
 PKG_VERSION="2017.09.28"
+PKG_SHA256="5c9fb5845b38b28982a3ef12836f76b35f46799ef4a2e46b48e2bd3c6182fa01"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/autoconf-archive/"
