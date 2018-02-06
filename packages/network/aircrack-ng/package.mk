be8f59539dfe1b6517e938b5734f7b585405c3b4
AV <27rurk@gmail.com>
2018-02-06 04:12:44 +0300

    autocommit on change

--- packages/network/aircrack-ng/package.mk
+++ packages/network/aircrack-ng/package.mk
@@ -2,6 +2,7 @@ prPKG_NAME="aircrack-ng"
 PKG_VERSION="1.2-rc4"
 PKG_URL="https://github.com/aircrack-ng/aircrack-ng/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl libdnet libnl libpcap openssl pcre usbutils iw"
+PKG_TOOLCHAIN="manual"
 
 pre_configure_target() {
   strip_lto
@@ -13,3 +14,7 @@ pre_configure_target() {
 PKG_MAKE_OPTS_TARGET="prefix=/usr libnl=false sqlite=true unstable=false pcre=true ext_scripts=true"
 
 PKG_MAKEINSTALL_OPTS_TARGET="$PKG_MAKE_OPTS_TARGET"
+
+make_target() {
+  make
+}
