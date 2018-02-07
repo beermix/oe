113a78426967dda2eb134aff13605d5eeefba7bc
AV <27rurk@gmail.com>
2018-02-07 14:41:02 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -196,7 +196,7 @@ make_target() {
       NO_SDT=1 \
       LDFLAGS="-ldw -ldwfl -lebl -lelf -ldl -lz" \
       EXTRA_PERFLIBS="-lebl" \
-        make
+        make DEBUG=1 LIBUNWIND_DIR=$SYSROOT_PREFIX/usr
       mkdir -p $INSTALL/usr/bin
         cp perf $INSTALL/usr/bin
     )
