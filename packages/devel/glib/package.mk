da428ebb6a96f3daad5d252438d34e44f81546ac
AV <27rurk@gmail.com>
2018-02-09 03:21:55 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -30,7 +30,7 @@ PKG_SHORTDESC="glib: C support library"
 PKG_LONGDESC="GLib is a library which includes support routines for C such as lists, trees, hashes, memory allocation, and many other things."
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --with-pcre=internal --with-pic --enable-libmount=no --disable-gtk-doc --disable-gtk-doc-html --disable-man --disable-selinux --with-threads=posix --with-python=python"
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --with-pcre=internal --with-pic --enable-libmount=no --with-python=python"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_snprintf_c99=yes \
                            ac_cv_func_vsnprintf_c99=yes \
