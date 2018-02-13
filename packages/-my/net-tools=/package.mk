3401cb935de9a5fac9a5c48d9b96bfa5499d1b18
AV <27rurk@gmail.com>
2018-02-13 07:35:07 +0300

    autocommit on change

--- packages/-my/net-tools=/package.mk
+++
@@ -1,7 +0,0 @@
-PKG_NAME="net-tools"
-PKG_VERSION="479bb4a"
-PKG_URL="https://dl.dropboxusercontent.com/s/tr6vat2hzkplp5i/net-tools-479bb4a.tar.xz"
-PKG_DEPENDS_TARGET="toolchain acl attr libcap readline openssl"
-PKG_SECTION="my"
-
-
