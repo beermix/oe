9fa8dbd25cd71f1382be6d14b592cc83edb7eea7
AV <27rurk@gmail.com>
2018-03-14 22:59:59 +0300

    autocommit on change

--- packages/-my/zsh/package.mk
+++ packages/-my/zsh/package.mk
@@ -3,10 +3,10 @@ PKG_VERSION="5.4.2"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses pcre readline libcap"
 PKG_SECTION="my"
+PKG_TOOLCHAIN="autotools"
 
 pre_configure_target() {
   export LIBS="$LIBS -lncursesw -ltinfo"
-#  export CPPFLAGS=`echo $CPPFLAGS | sed -e "s|-D_FORTIFY_SOURCE=2||g"`
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-multibyte \
--- packages/multimedia/freshplayerplugin/package.mk
+++ packages/multimedia/freshplayerplugin/package.mk
@@ -1,9 +1,10 @@
 PKG_NAME="freshplayerplugin"
-PKG_VERSION="1f48001"
+PKG_VERSION="ce233f6"
 PKG_URL="https://github.com/i-rinat/freshplayerplugin/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ragel:host alsa xrandr libXrender libXcursor libdrm libevent freetype openssl icu libva libvdpau cairo pango"
 PKG_SECTION="multimedia"
 PKG_TOOLCHAIN="cmake-make"
+# https://github.com/i-rinat/freshplayerplugin
 
 PKG_CMAKE_OPTS_TARGET="-DWITH_LIBV4L2=0 \
 			  -DWITH_JACK=0 \
