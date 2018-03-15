0fffeff15a83b47f390dfcf8d3bf4d355b45a61c
AV <27rurk@gmail.com>
2018-03-15 08:31:36 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -75,7 +75,7 @@ PKG_CONFIGURE_OPTS_BOOTSTRAP="$GCC_COMMON_CONFIGURE_OPTS \
                               $GCC_OPTS"
 
 PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
-                         --enable-languages=c,c++ \
+                         --enable-languages=c,c++,lto \
                          --enable-__cxa_atexit \
                          --enable-decimal-float \
                          --enable-tls \
