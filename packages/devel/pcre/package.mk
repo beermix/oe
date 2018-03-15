f08d226d173c793216cbed601f74afe323021f6e
AV <27rurk@gmail.com>
2018-03-15 20:12:21 +0300

    autocommit on change

--- packages/devel/pcre/package.mk
+++ packages/devel/pcre/package.mk
@@ -42,7 +42,3 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
   sed -e "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/$PKG_NAME-config
 }
-
-#post_makeinstall_host() {
-#  rm $TOOLCHAIN/bin/$PKG_NAME-config
-#}
