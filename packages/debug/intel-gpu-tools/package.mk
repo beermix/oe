28253648634b05cf7194b9f7f0944753e9a8ce1f
AV <27rurk@gmail.com>
2018-03-17 10:42:18 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -1,9 +1,17 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="1.21"
+PKG_VERSION="1.22"
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
 PKG_URL="https://www.x.org/releases/individual/app/intel-gpu-tools-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo dri2proto swig:host libunwind"
+PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo swig:host libunwind"
 PKG_SECTION="tools"
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no --disable-tests --disable-shared --disable-git-hash --disable-debug --disable-vc4 --disable-amdgpu --disable-nouveau --enable-intel"
+PKG_CONFIGURE_OPTS_TARGET="enable_gtk_doc=no \
+			      --disable-tests \
+			      --disable-shared \
+			      --disable-git-hash \
+			      --disable-debug \
+			      --disable-vc4 \
+			      --disable-amdgpu \
+			      --disable-nouveau \
+			      --enable-intel"
