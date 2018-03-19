8773af19cfa09cb4b2b22b0989719d7451118ea9
AV <27rurk@gmail.com>
2018-03-19 21:02:23 +0300

    autocommit on change

--- packages/devel/slang/package.mk
+++ packages/devel/slang/package.mk
@@ -30,7 +30,14 @@ PKG_SHORTDESC="slang: multi-platform programmer's library designed to allow a de
 PKG_LONGDESC="S-Lang is a multi-platform programmer's library designed to allow a developer to create robust multi-platform software. It provides facilities required by interactive applications such as display/screen management, keyboard input, keymaps, and so on. The most exciting feature of the library is the slang interpreter that may be easily embedded into a program to make it extensible. While the emphasis has always been on the embedded nature of the interpreter, it may also be used in a stand-alone fashion through the use of slsh, which is part of the S-Lang distribution."
 PKG_BUILD_FLAGS="-parallel"
 
-PKG_CONFIGURE_OPTS_TARGET="--without-onig"
+PKG_CONFIGURE_OPTS_TARGET="--without-iconv \
+                           --without-onig \
+                           --without-pcre \
+                           --with-png \
+                           --without-z \
+                           --without-x"
+
+PKG_MAKE_OPTS_TARGET="-C src static"
 
 pre_configure_target() {
  # slang fails to build in subdirs
@@ -46,5 +53,5 @@ pre_configure_host() {
 
 makeinstall_target() {
   make DESTDIR="$SYSROOT_PREFIX" -C src install-static
-  make DESTDIR="$SYSROOT_PREFIX" -C src pkg-config
+  make DESTDIR="$SYSROOT_PREFIX" -C pkg-config
 }
