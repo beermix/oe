a5c11e6ce4eb841a9c87f78f93982593d3e7c7ca
AV <27rurk@gmail.com>
2018-03-19 22:36:02 +0300

    autocommit on change

--- packages/audio/libcdio/package.mk
+++ packages/audio/libcdio/package.mk
@@ -27,13 +27,10 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="audio"
 PKG_SHORTDESC="libcdio: A CD-ROM reading and control library"
 PKG_LONGDESC="This library is to encapsulate CD-ROM reading and control. Applications wishing to be oblivious of the OS- and device-dependant properties of a CD-ROM can use this library. Some support for disk image types like BIN/CUE and NRG is available, so applications that use this library also have the ability to read disc images as though they were CD's."
-PKG_BUILD_FLAGS="+pic"
 
 # package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="--enable-cxx \
                            --disable-cpp-progs \
-                           --disable-shared \
-                           --enable-static \
                            --enable-joliet \
                            --disable-rpath \
                            --enable-rock \
