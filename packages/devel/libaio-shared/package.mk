f5328dda8d80ce8619382ac4b5f2a1f100ba1872
AV <27rurk@gmail.com>
2018-03-20 08:38:43 +0300

    autocommit on change

--- packages/devel/libaio-shared/package.mk
+++ packages/devel/libaio-shared/package.mk
@@ -30,8 +30,6 @@ PKG_SHORTDESC="libaio: Kernel Asynchronous I/O (AIO) Support for Linux"
 PKG_LONGDESC="AIO enables even a single application thread to overlap I/O operations with other processing, by providing an interface for submitting one or more I/O requests in one system call (io_submit()) without waiting for completion, and a separate interface (io_getevents()) to reap completed I/O operations associated with a given completion group."
 
 
-
-
 #make_target() {
 #  make -C src
 #}
