7cba460abbd2c499dc66f2170b03e8b5b06082c8
AV <27rurk@gmail.com>
2018-03-20 10:46:15 +0300

    autocommit on change

--- packages/network/samba/package.mk
+++ packages/network/samba/package.mk
@@ -152,11 +152,11 @@ post_make_target() {
 
 makeinstall_target() {
   if [ "$SAMBA_SERVER" = "yes" ]; then
-    mkdir -p $INSTALL/usr/sbin
-      cp bin/samba_multicall $INSTALL/usr/sbin
-      ln -sf samba_multicall $INSTALL/usr/sbin/smbd
-      ln -sf samba_multicall $INSTALL/usr/sbin/nmbd
-      ln -sf samba_multicall $INSTALL/usr/sbin/smbpasswd
+    mkdir -p $INSTALL/usr/bin
+      cp bin/samba_multicall $INSTALL/usr/bin
+      ln -sf samba_multicall $INSTALL/usr/bin/smbd
+      ln -sf samba_multicall $INSTALL/usr/bin/nmbd
+      ln -sf samba_multicall $INSTALL/usr/bin/smbpasswd
 
     mkdir -p $INSTALL/etc/samba
       cp ../codepages/lowcase.dat $INSTALL/etc/samba
