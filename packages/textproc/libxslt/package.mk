3a43a5dcdf441874d8bbfe4671e2089cada3a59c
AV <27rurk@gmail.com>
2018-03-21 02:25:56 +0300

    autocommit on change

--- packages/textproc/libxslt/package.mk
+++ packages/textproc/libxslt/package.mk
@@ -23,26 +23,22 @@ PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org/xslt/"
 PKG_URL="ftp://xmlsoft.org/libxml2/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="libxml2:host"
-PKG_DEPENDS_TARGET="toolchain libxml2"
+PKG_DEPENDS_TARGET="toolchain libxml2 libgcrypt"
 PKG_SECTION="textproc"
 PKG_SHORTDESC="libxslt"
 PKG_LONGDESC="libxslt"
 
 PKG_CONFIGURE_OPTS_HOST="  ac_cv_header_ansidecl_h=no \
                            ac_cv_header_xlocale_h=no \
-                           --enable-static \
-                           --enable-shared \
                            --without-python \
                            --with-libxml-prefix=$TOOLCHAIN \
                            --without-crypto"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_header_ansidecl_h=no \
                            ac_cv_header_xlocale_h=no \
-                           --enable-static \
-                           --enable-shared \
                            --without-python \
                            --with-libxml-prefix=$SYSROOT_PREFIX/usr \
-                           --without-crypto"
+                           --with-crypto=$SYSROOT_PREFIX/usr"
 
 post_makeinstall_target() {
   $SED "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" $SYSROOT_PREFIX/usr/bin/xslt-config
