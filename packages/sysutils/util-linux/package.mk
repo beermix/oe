3a1f29ee62ef0d8ae760fbeeec5ff0b10435b221
AV <27rurk@gmail.com>
2018-03-23 20:43:17 +0300

    autocommit on change

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.32"
-PKG_SHA256="6c7397abc764e32e8159c2e96042874a190303e77adceb4ac5bd502a272a4734"
+PKG_VERSION="2.31.1"
+PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v${PKG_VERSION%-*}/$PKG_NAME-$PKG_VERSION.tar.xz"
