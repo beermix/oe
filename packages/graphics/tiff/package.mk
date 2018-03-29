176e5e0b70243245214136a223420e91cd77b26b
AV <27rurk@gmail.com>
2018-03-30 02:36:36 +0300

    autocommit on change

--- packages/graphics/tiff/package.mk
+++ packages/graphics/tiff/package.mk
@@ -17,9 +17,9 @@
 ################################################################################
 
 PKG_NAME="tiff"
-#PKG_VERSION="4.0.9"
-PKG_VERSION="3.9.7"
-PKG_SHA256="f5d64dd4ce61c55f5e9f6dc3920fbe5a41e02c2e607da7117a35eb5c320cef6a"
+PKG_VERSION="4.0.9"
+#PKG_VERSION="3.9.7"
+#PKG_SHA256="f5d64dd4ce61c55f5e9f6dc3920fbe5a41e02c2e607da7117a35eb5c320cef6a"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://download.osgeo.org/libtiff/?C=M;O=D"
@@ -28,6 +28,7 @@ PKG_DEPENDS_TARGET="toolchain libjpeg-turbo zlib"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libtiff: A library for reading and writing TIFF files"
 PKG_LONGDESC="libtiff is a library for reading and writing data files encoded with the Tag Image File format, Revision 6.0 (or revision 5.0 or revision 4.0). This file format is suit- able for archiving multi-color and monochromatic image data."
+PKG_TOOLCHAIN="configure"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-shared \
