53ef448a93605fdc034d8216b4273115136cbfc6
AV <27rurk@gmail.com>
2018-03-29 23:05:55 +0300

    autocommit on change

--- packages/graphics/libdrm/package.mk
+++ packages/graphics/libdrm/package.mk
@@ -31,37 +31,36 @@ PKG_TOOLCHAIN="autotools"
 
 get_graphicdrivers
 
-DRM_CONFIG="--disable-intel --disable-radeon --disable-amdgpu"
-DRM_CONFIG="$DRM_CONFIG --disable-nouveau --disable-vmwgfx --disable-vc4"
+DRM_CONFIG="--disable-libkms --disable-intel --disable-radeon --disable-amdgpu"
+DRM_CONFIG="$DRM_CONFIG --disable-nouveau --disable-vmwgfx"
 
 for drv in $GRAPHIC_DRIVERS; do
   [ "$drv" = "i915" -o "$drv" = "i965" ] && \
+    DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-libkms/enable-libkms/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-intel/enable-intel/'`
 
   [ "$drv" = "r200" -o "$drv" = "r300" -o "$drv" = "r600" -o "$drv" = "radeonsi" ] && \
+    DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-libkms/enable-libkms/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-radeon/enable-radeon/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-amdgpu/enable-amdgpu/'`
 
   [ "$drv" = "vmware" ] && \
+    DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-libkms/enable-libkms/'` && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-vmwgfx/enable-vmwgfx/'`
 
   [ "$drv" = "vc4" ] && \
     DRM_CONFIG=`echo $DRM_CONFIG | sed -e 's/disable-vc4/enable-vc4/'`
 done
 
-pre_configure_target() {
-  export LIBS="-lrt -lc -latomic"
-  export CFLAGS="$CFLAGS -std=gnu99"
-}
+#pre_configure_target() {
+#  export LIBS="-lrt -lc -latomic"
+#  export CFLAGS="$CFLAGS -std=gnu99"
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-udev \
                            --enable-largefile \
-                           --disable-silent-rules \
                            --with-kernel-source=$(kernel_path) \
-                           --disable-libkms \
                            $DRM_CONFIG \
-                           --disable-nouveau \
-                           --disable-freedreno \
                            --disable-install-test-programs \
                            --disable-cairo-tests \
                            --disable-manpages \
