d2c014a14a67a416883c9b7092e4c60d36b397dc
AV <27rurk@gmail.com>
2018-03-30 12:06:06 +0300

    autocommit on change

--- packages/devel/libcap-ng/package.mk
+++ packages/devel/libcap-ng/package.mk
@@ -1,9 +1,9 @@
 PKG_NAME="libcap-ng"
-PKG_VERSION="0.7.8"
+PKG_VERSION="0.7.9"
 PKG_URL="http://people.redhat.com/sgrubb/libcap-ng/libcap-ng-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="devel"
 PKG_TOOLCHAIN="configure"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_swig_found=no --disable-shared --enable-static --with-pic --with-python=no"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_swig_found=no --with-python=no"
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
