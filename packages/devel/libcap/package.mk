f31e5071bbf622a6e3685a477332249c4958a853
AV <27rurk@gmail.com>
2018-03-31 00:07:16 +0300

    autocommit on change

--- packages/devel/libcap/package.mk
+++ packages/devel/libcap/package.mk
@@ -42,7 +42,7 @@ make_host() {
   make CC=$CC \
        AR=$AR \
        RANLIB=$RANLIB \
-       CFLAGS="$HOST_CFLAGS" \
+       CFLAGS="$HOST_CFLAGS -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" \
        BUILD_CFLAGS="$HOST_CFLAGS -I$PKG_BUILD/libcap/include" \
        PAM_CAP=no \
        lib=/lib \
@@ -55,7 +55,7 @@ make_target() {
   make CC=$CC \
        AR=$AR \
        RANLIB=$RANLIB \
-       CFLAGS="$TARGET_CFLAGS" \
+       CFLAGS="$TARGET_CFLAGS -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" \
        BUILD_CC=$HOST_CC \
        BUILD_CFLAGS="$HOST_CFLAGS -I$PKG_BUILD/libcap/include" \
        PAM_CAP=no \
