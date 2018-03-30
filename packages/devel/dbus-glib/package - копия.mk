fa57e97e8c310972216c745240c0fb8dc030c191
AV <27rurk@gmail.com>
2018-03-30 20:11:13 +0300

    autocommit on change

--- packages/devel/dbus-glib/package - копия.mk
+++ packages/devel/dbus-glib/package - копия.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="dbus-glib"
 PKG_VERSION="0.110"
-PKG_SHA256="7ce4760cf66c69148f6bd6c92feaabb8812dee30846b24cd0f7395c436d7e825"
+PKG_SHA256="DA98F25C0871C49A59EAFF2C4DE8FF2A63C7CC90"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://freedesktop.org/wiki/Software/dbus"
@@ -28,8 +28,14 @@ PKG_SECTION="devel"
 PKG_SHORTDESC="dbus-glib: A message bus system"
 PKG_LONGDESC="D-BUS is a message bus, used for sending messages between applications. Conceptually, it fits somewhere in between raw sockets and CORBA in terms of complexity."
 PKG_TOOLCHAIN="autotools"
+#PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-tests"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_have_abstract_sockets=yes \
+                           ac_cv_func_posix_getpwnam_r=yes \
+                           have_abstract_sockets=yes \
+                           --disable-tests \
+                           --disable-bash-completion \
+                           --enable-asserts=no"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/dbus-binding-tool
