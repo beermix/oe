39a1baa29e46de8e86e78181013f2f57d93159a1
AV <27rurk@gmail.com>
2018-04-02 19:05:03 +0300

    autocommit on change

--- packages/x11/xserver/xorg-server/package.mk
+++ packages/x11/xserver/xorg-server/package.mk
@@ -139,7 +139,7 @@ pre_configure_target() {
   LDFLAGS=`echo $LDFLAGS | sed -e "s|-O3|-O2|" -e "s|-Ofast|-O2|"`
   
   CFLAGS=`echo $CFLAGS | sed -e "s|-fno-plt||"`
-  LDFLAGS=`echo $LDFLAGS | sed -e "s|,-z,relro,-z,now||"`
+  LDFLAGS=`echo $LDFLAGS | sed -e "s|,-z,now||"`
 }
 
 post_makeinstall_target() {
