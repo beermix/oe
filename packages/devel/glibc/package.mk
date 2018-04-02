71f4d4ff1f47ce8e55426539ed49c5d3de01df65
AV <27rurk@gmail.com>
2018-04-03 01:24:50 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -114,9 +114,7 @@ pre_configure_target() {
 
   unset LD_LIBRARY_PATH
 
-  export CFLAGS="-O2 -march=westmere -g -m64 -Wl,-z,max-page-size=0x1000"
-  unset LDFLAGS
-  export LDFLAGS="-Wl,-z,max-page-size=0x1000"
+  export CFLAGS="-O2 -march=westmere -g -m64"
 
   export BUILD_CC=$HOST_CC
   export OBJDUMP_FOR_HOST=objdump
