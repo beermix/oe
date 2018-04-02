744e8accb2488c4c531e9a0bc172d38906e1a94b
AV <27rurk@gmail.com>
2018-04-02 23:27:47 +0300

    autocommit on change

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="dbus"
-PKG_VERSION="1.11.16"
-PKG_SHA256="7cf993e97df62c73b939b77dcd920e8883d8e866f9ced1a9b5c715eb28e4b031"
+PKG_VERSION="1.13.2"
+#PKG_SHA256="7cf993e97df62c73b939b77dcd920e8883d8e866f9ced1a9b5c715eb28e4b031"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://dbus.freedesktop.org/releases/dbus/?C=M;O=D"
@@ -27,6 +27,7 @@ PKG_DEPENDS_TARGET="toolchain expat systemd"
 PKG_SECTION="system"
 PKG_SHORTDESC="dbus: simple interprocess messaging system"
 PKG_LONGDESC="D-Bus is a message bus, used for sending messages between applications. This package contains the D-Bus daemon and related utilities and the dbus shared library."
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="export ac_cv_have_abstract_sockets=yes \
                            --with-sysroot=$SYSROOT_PREFIX \
