b9563d7f9fe8c2c13ce03d856ece8ee0b6d23911
AV <27rurk@gmail.com>
2018-04-02 19:54:46 +0300

    autocommit on change

--- packages/devel/pcre/package.mk
+++ packages/devel/pcre/package.mk
@@ -23,16 +23,15 @@ PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/svn2github/pcre/"
 PKG_URL="http://ftp.csx.cam.ac.uk/pub/software/programming/pcre/$PKG_NAME-$PKG_VERSION.tar.bz2"
 #PKG_URL="https://github.com/svn2github/pcre/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain pcre:host"
 PKG_SECTION="devel"
 PKG_SHORTDESC="pcre: Perl Compatible Regulat Expressions"
 PKG_LONGDESC="The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5. PCRE has its own native API, as well as a set of wrapper functions that correspond to the POSIX regular expression API. The PCRE library is free, even for building commercial software."
 PKG_TOOLCHAIN="configure"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-static --enable-jit --enable-utf --enable-unicode-properties --enable-pcre16 --enable-pcre32"
+PKG_CONFIGURE_OPTS_TARGET="--disable-static --enable-jit --enable-utf --enable-unicode-properties --enable-pcre16"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --with-pic --enable-jit --enable-utf --enable-unicode-properties --enable-pcre16 --enable-pcre32"
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --with-pic --enable-jit --enable-utf --enable-unicode-properties --enable-pcre16"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
