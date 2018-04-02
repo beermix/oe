f5a8b1eae020edbf8d6ef18f3331688407c96662
AV <27rurk@gmail.com>
2018-04-02 20:22:33 +0300

    autocommit on change

--- packages/-my/tig/package.mk
+++ packages/-my/tig/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="tig"
-PKG_VERSION="0f340c0"
+PKG_VERSION="f5d6b15"
 PKG_URL="https://github.com/jonas/tig/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses readline"
 PKG_SECTION="tools"
