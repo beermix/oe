2e335cc650f1ab6276a232da823dc7d9d65ac47b
AV <27rurk@gmail.com>
2018-04-03 00:26:53 +0300

    autocommit on change

--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -14,7 +14,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
- ################################################################################  libXcursor libSM libXtst
+ ################################################################################  libXcursor libSM libXtst libXvMC
 
 PKG_NAME="xf86-video-intel"
 PKG_VERSION="af36a4a"
@@ -24,7 +24,7 @@ PKG_SITE="https://cgit.freedesktop.org/xorg/driver/xf86-video-intel/log/"
 PKG_URL="https://cgit.freedesktop.org/xorg/driver/xf86-video-intel/snapshot/$PKG_VERSION.tar.xz"
 PKG_SOURCE_DIR="$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain libXcomposite libXxf86vm libXdamage util-macros systemd xorg-server libXtst"
-PKG_DEPENDS_TARGET="toolchain libXcomposite libXxf86vm libXdamage util-macros systemd xorg-server xcb-util libXvMC libXtst libXcursor libXScrnSaver"
+PKG_DEPENDS_TARGET="toolchain libXcomposite libXxf86vm libXdamage util-macros systemd xorg-server xcb-util libXtst libXcursor libXScrnSaver"
 PKG_SECTION="x11/driver"
 PKG_SHORTDESC="xf86-video-intel: The Xorg driver for Intel video chips"
 PKG_LONGDESC="The Xorg driver for Intel i810, i815, 830M, 845G, 852GM, 855GM, 865G, 915G, 915GM and 965G video chips."
