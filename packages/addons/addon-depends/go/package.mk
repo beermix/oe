b56d185aa3e67e10abd4630193e0946f16e8593e
AV <27rurk@gmail.com>
2018-04-02 20:46:45 +0300

    autocommit on change

--- packages/addons/addon-depends/go/package.mk
+++ packages/addons/addon-depends/go/package.mk
@@ -40,11 +40,11 @@ PKG_TOOLCHAIN="manual"
 configure_host() {
   export GOOS=linux
   export GOROOT_FINAL=$TOOLCHAIN/lib/golang
-  if [ -x /usr/lib/go/bin/go ]; then
-    export GOROOT_BOOTSTRAP=/usr/lib/go
-  else
-    export GOROOT_BOOTSTRAP=/usr/lib/golang
-  fi
+#  if [ -x /usr/lib/go/bin/go ]; then
+#    export GOROOT_BOOTSTRAP=/usr/lib/go
+#  else
+#    export GOROOT_BOOTSTRAP=/usr/lib/golang
+#  fi
   export GOARCH=amd64
 }
 
