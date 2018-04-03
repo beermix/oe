bdf3a14a65d36d2864d8d21e8f486302ab232628
AV <27rurk@gmail.com>
2018-04-03 14:13:02 +0300

    autocommit on change

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="setuptools"
-PKG_VERSION="39.0.1"
+PKG_VERSION="38.5.1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://github.com/pypa/setuptools/releases"
