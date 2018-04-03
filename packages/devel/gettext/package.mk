f7718da3c8fb69d7266fc579832781cb4e80ddfa
AV <27rurk@gmail.com>
2018-04-03 04:39:56 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -41,9 +41,6 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
                          --disable-java \
                          --disable-curses \
                          --with-included-libxml \
-                         --with-included-glib \
-                         --with-included-libcroco \
-                         --with-included-libunistring \
                          --disable-native-java \
                          --disable-csharp \
                          --without-emacs"
