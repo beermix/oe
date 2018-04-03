a1ea089f608811e7d2a322f9b42c0400f6ad57bd
AV <27rurk@gmail.com>
2018-04-04 00:58:38 +0300

    autocommit on change

--- packages/x11/lib/libXtst/package.mk
+++ packages/x11/lib/libXtst/package.mk
@@ -27,5 +27,6 @@ PKG_DEPENDS_TARGET="toolchain util-macros libXext libXi libX11"
 PKG_SECTION="x11/lib"
 PKG_SHORTDESC="libxtst: The Xtst Library"
 PKG_LONGDESC="The Xtst Library"
+PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-gnu-ld --without-xmlto"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-gnu-ld --without-xmlto"
