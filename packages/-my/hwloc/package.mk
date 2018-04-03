b6195cb46e8ace9c63fba84a22b6d835148a9ed8
AV <27rurk@gmail.com>
2018-04-03 12:16:46 +0300

    autocommit on change

--- packages/-my/hwloc/package.mk
+++ packages/-my/hwloc/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="hwloc"
-PKG_VERSION="2.0.0"
-PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_VERSION="1.11.10"
+PKG_URL="https://www.open-mpi.org/software/hwloc/v1.11/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain zlib libevent libpciaccess"
 PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+pic"
@@ -10,7 +10,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --disable-nvml \
 			      --enable-plugins \
 			      --enable-pci"
-			      
+
 post_makeinstall_target() {
   rm -rf $INSTALL
 }
