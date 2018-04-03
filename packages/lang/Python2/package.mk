d8c81b57b11ec69a60d1a95484ef8b7151fa27ed
AV <27rurk@gmail.com>
2018-04-03 12:07:27 +0300

    autocommit on change

--- packages/lang/Python2/package.mk
+++ packages/lang/Python2/package.mk
@@ -38,6 +38,7 @@ PKG_PY_DISABLED_MODULES="_tkinter nis gdbm bsddb ossaudiodev"
 PKG_CONFIGURE_OPTS_HOST="--cache-file=config.cache \
                          --without-cxx-main \
                          --with-threads \
+                         --with-computed-gotos \
                          --enable-unicode=ucs4"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
