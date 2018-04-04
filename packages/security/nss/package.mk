0ca443501e6784e1e440fb92aa1402055a1e268a
AV <27rurk@gmail.com>
2018-04-04 04:53:58 +0300

    autocommit on change

--- packages/security/nss/package.mk
+++ packages/security/nss/package.mk
@@ -44,9 +44,6 @@ make_host() {
      NSPR_INCLUDE_DIR=$TOOLCHAIN/include/nspr \
      USE_SYSTEM_ZLIB=1 ZLIB_LIBS="-lz -L$TOOLCHAIN/lib" \
      SKIP_SHLIBSIGN=1 \
-     NSS_ENABLE_WERROR=0 \
-     FREEBL_NO_DEPEND=1 \
-     NSS_ENABLE_TLS_1_3=1 \
      NSS_TESTS="dummy" \
      CC=$CC LDFLAGS="$LDFLAGS -L$TOOLCHAIN/lib" \
      V=1
@@ -73,13 +70,11 @@ make_target() {
   rm -rf $PKG_BUILD/dist
 
   make BUILD_OPT=1 $TARGET_USE_64 \
+     NSS_USE_SYSTEM_SQLITE=1 \
      NSPR_INCLUDE_DIR=$SYSROOT_PREFIX/usr/include/nspr \
      NSS_USE_SYSTEM_SQLITE=1 \
      USE_SYSTEM_ZLIB=1 ZLIB_LIBS=-lz \
      SKIP_SHLIBSIGN=1 \
-     NSS_ENABLE_WERROR=0 \
-     FREEBL_NO_DEPEND=1 \
-     NSS_ENABLE_TLS_1_3=1 \
      OS_TEST=$TARGET_ARCH \
      NSS_TESTS="dummy" \
      NSINSTALL=$TOOLCHAIN/bin/nsinstall \
