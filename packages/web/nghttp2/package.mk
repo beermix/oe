3d2ba27218496aae8c00e295122a36dca0970d4d
AV <27rurk@gmail.com>
2018-04-10 18:37:51 +0300

    autocommit on change

--- packages/web/nghttp2/package.mk
+++ packages/web/nghttp2/package.mk
@@ -2,7 +2,7 @@ PKG_NAME="nghttp2"
 PKG_VERSION="1.31.0"
 PKG_SITE="https://github.com/nghttp2/nghttp2/releases/"
 PKG_URL="https://github.com/nghttp2/nghttp2/releases/download/v$PKG_VERSION/nghttp2-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain libevent libev libuv"
 PKG_TOOLCHAIN="cmake-make"
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_LIB_ONLY=1 \
