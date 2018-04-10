32b3169ff91227273fb3fca013c78c5faa425bf2
AV <27rurk@gmail.com>
2018-04-10 21:51:38 +0300

    autocommit on change

--- packages/-my/wine/package.mk
+++ packages/-my/wine/package.mk
@@ -41,51 +41,8 @@ PKG_CONFIGURE_OPTS_TARGET="--with-wine-tools=$PKG_BUILD/.$HOST_NAME \
 			      --with-zlib"
 
 PKG_CONFIGURE_OPTS_HOST="--disable-tests \
-	--disable-win16 \
-	--without-alsa \
-	--without-capi \
-	--without-cms \
-	--without-coreaudio \
-	--without-cups \
-	--without-curses \
-	--without-dbus \
-	--without-fontconfig \
-	--without-gphoto \
-	--without-glu \
-	--without-gnutls \
-	--without-gsm \
-	--without-gstreamer \
-	--without-hal \
-	--without-jpeg \
-	--without-ldap \
-	--without-mpg123 \
-	--without-netapi \
-	--without-openal \
-	--without-opencl \
-	--without-opengl \
-	--without-osmesa \
-	--without-oss \
-	--without-pcap \
-	--without-pulse \
-	--without-png \
-	--without-sane \
-	--without-tiff \
-	--without-v4l \
-	--without-x \
-	--without-xcomposite \
-	--without-xcursor \
-	--without-xinerama \
-	--without-xinput \
-	--without-xinput2 \
-	--without-xml \
-	--without-xrandr \
-	--without-xrender \
-	--without-xshape \
-	--without-xshm \
-	--without-xslt \
-	--without-xxf86vm \
-	--without-zlib \
-	--enable-win64"
+			    --disable-win16 \
+			    --enable-win64"
 
 make_host() {
     cd $PKG_BUILD/.$HOST_NAME
