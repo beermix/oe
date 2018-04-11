2c3fc6a4c6c081f141b9e9ed3ae08ba83ea0cd56
AV <27rurk@gmail.com>
2018-04-11 14:35:02 +0300

    autocommit on change

--- packages/-my/lnav/package.mk
+++ packages/-my/lnav/package.mk
@@ -1,7 +1,8 @@
 PKG_NAME="lnav"
 PKG_VERSION="8091591"
 PKG_URL="https://github.com/tstack/lnav/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="toolchain sqlite:host pcre:host ncurses:host readline:host"
+PKG_DEPENDS_TARGET="toolchain sqlite pcre ncurses readline"
+PKG_DEPENDS_HOST="sqlite:host pcre:host ncurses:host readline:host"
 PKG_SECTION="tools"
 PKG_TOOLCHAIN="configure"
 #PKG_TOOLCHAIN="autotools"
@@ -16,4 +17,6 @@ pre_configure_host() {
   sh autogen.sh
 }
 
-PKG_CONFIGURE_OPTS_HOST="--disable-silent-rules --disable-shared --enable-static --without-ncursesw"
+PKG_CONFIGURE_OPTS_HOST="--disable-silent-rules --disable-shared --enable-static"
+
+PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
