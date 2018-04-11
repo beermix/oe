329a9e473265ce7064c766a7d859507586b3c016
AV <27rurk@gmail.com>
2018-04-11 16:43:12 +0300

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -3,6 +3,13 @@ PKG_VERSION="0.18"
 PKG_URL="http://isl.gforge.inria.fr/isl-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="libtool:host intltool:host gmp:host"
 PKG_SECTION="devel"
-PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --with-gmp-prefix=$TOOLCHAIN --disable-silent-rules"
+pre_configure_host() {
+  unset CFLAGS
+  unset CXXFLAGS
+}
+
+PKG_CONFIGURE_OPTS_HOST="--enable-static \
+			    --disable-shared \
+			    --with-gmp-prefix=$TOOLCHAIN \
+			    --disable-silent-rules"
