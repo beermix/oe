d5bf0c81ef495d63862de60df90a522f21900243
AV <27rurk@gmail.com>
2018-04-12 06:21:34 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="glibc"
-PKG_VERSION="15a05fa"
+PKG_VERSION="27b89b5"
 #PKG_VERSION="3cab0b7"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
