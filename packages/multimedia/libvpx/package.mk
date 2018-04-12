b0b34b26239752c17ad169271872f9e7de6047f0
AV <27rurk@gmail.com>
2018-04-12 18:36:59 +0300

    autocommit on change

--- packages/multimedia/libvpx/package.mk
+++ packages/multimedia/libvpx/package.mk
@@ -1,69 +1,45 @@
 ################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2016-present Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
 
 PKG_NAME="libvpx"
-PKG_VERSION="1.7.0"
-PKG_SHA256="1fec931eb5c94279ad219a5b6e0202358e94a93a90cfb1603578c326abfc1238"
+PKG_VERSION="768b018"
+#PKG_SHA256="1fec931eb5c94279ad219a5b6e0202358e94a93a90cfb1603578c326abfc1238"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
-PKG_SITE="https://www.webmproject.org"
-PKG_URL="https://github.com/webmproject/libvpx/archive/v${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/webmproject/libvpx/releases"
+PKG_URL="https://github.com/webmproject/libvpx/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain yasm:host"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="WebM VP8/VP9 Codec SDK"
 PKG_LONGDESC="The WebM Project is dedicated to developing a high-quality, open video format for the web that's freely available to everyone."
-PKG_BUILD_FLAGS="-lto -gold"
+PKG_BUILD_FLAGS="-lto -gold -hardening"
 
 configure_target() {
 
-  case $ARCH in
-    aarch64)
-      PKG_TARGET_NAME_LIBVPX="arm64-linux-gcc"
-      ;;
-    arm)
-      PKG_TARGET_NAME_LIBVPX="armv7-linux-gcc"
-      ;;
-    x86_64)
-      PKG_TARGET_NAME_LIBVPX="x86_64-linux-gcc"
-      ;;
-  esac
+# CFLAGS=`echo $CFLAGS | sed -e "s|-D_FORTIFY_SOURCE=2||"`
+# CPPFLAGS=`echo $CPPFLAGS | sed -e "s|-D_FORTIFY_SOURCE=2||"`
 
-  CFLAGS=`echo $CFLAGS | sed -e "s|-D_FORTIFY_SOURCE=2||"`
-  CPPFLAGS=`echo $CPPFLAGS | sed -e "s|-D_FORTIFY_SOURCE=2||"`
-
-  $PKG_CONFIGURE_SCRIPT --prefix=/usr \
-                        --extra-cflags="$CFLAGS" \
-                        --extra-cxxflags="$CXXFLAGS" \
+  $PKG_CONFIGURE_SCRIPT --prefix="/usr" \
+                        --target="x86_64-linux-gcc" \
+                        --cpu="$TARGET_CPU" \
                         --as=yasm \
-                        --target=$PKG_TARGET_NAME_LIBVPX \
-                        --disable-docs \
-                        --enable-runtime-cpu-detect \
-                        --disable-examples \
-                        --enable-shared \
                         --enable-pic \
-                        --disable-install-docs \
-                        --disable-install-srcs \
                         --enable-vp8 \
-                        --enable-postproc \
                         --enable-vp9 \
+                        --enable-postproc \
+                        --enable-vp9-postproc \
+                        --enable-vp9-temporal-denoising \
+                        --enable-libyuv \
+                        --disable-encode-perf-tests \
+                        --disable-decode-perf-tests \
+                        --disable-examples \
+                        --disable-debug-libs \
+                        --disable-docs \
+                        --disable-install-docs \
+                        --enable-runtime-cpu-detect \
+                        --disable-shared \
+                        --disable-install-srcs \
                         --enable-vp9-highbitdepth \
                         --enable-experimental \
                         --enable-spatial-svc \
                         --disable-unit-tests
- 
 }
