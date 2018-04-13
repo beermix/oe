e0ad00812011a8e5e1347e8adbfdbe552e0cc020
AV <27rurk@gmail.com>
2018-04-13 03:22:25 +0300

    autocommit on change

--- packages/-my/mc/package.mk
+++ packages/-my/mc/package.mk
@@ -1,21 +1,25 @@
 PKG_NAME="mc"
 PKG_VERSION="4.8.20"
 PKG_URL="http://ftp.midnight-commander.org/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libtool:host gettext:host e2fsprogs util-linux glib pcre fuse libssh2 libevent expat slang"
+PKG_DEPENDS_TARGET="toolchain libtool:host gettext:host e2fsprogs util-linux glib pcre fuse libssh2 libevent expat"
 PKG_SECTION="tools"
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/home/user/.bin/mcc \
-                           --datadir=/home/user/.bin/mcc \
-                           --libdir=/home/user/.bin/mcc \
-                           --libexecdir=/home/user/.bin/mcc \
-                           --sharedstatedir=/home/user/.bin/mcc \
-                           --localstatedir=/home/user/.bin/mcc \
-                           --includedir=/home/user/.bin/mcc \
-                           --oldincludedir=/home/user/.bin/mcc \
-                           --datarootdir=/home/user/.bin/mcc \
-                           --infodir=/home/user/.bin/mcc \     
-                           --localedir=/home/user/.bin/mcc \
+pre_configure_target() {
+  export LIBS="$LIBS -lssh2 -lncursesw -ltinfo"
+}
+
+PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/mc \
+                           --datadir=/storage/.config/mc \
+                           --libdir=/storage/.config/mc \
+                           --libexecdir=/storage/.config/mc \
+                           --sharedstatedir=/storage/.config/mc \
+                           --localstatedir=/storage/.config/mc \
+                           --includedir=/storage/.config/mc \
+                           --oldincludedir=/storage/.config/mc \
+                           --datarootdir=/storage/.config/mc \
+                           --infodir=/storage/.config/mc \     
+                           --localedir=/storage/.config/mc \
                            --disable-doxygen-doc \
                            --disable-doxygen-rtf \
                            --disable-doxygen-xml \
@@ -28,12 +32,12 @@ PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/home/user/.bin/mcc \
                            --enable-background \
                            --enable-charset \
                            --without-gpm-mouse \
-                           --with-screen=slang \
+                           --with-screen=ncursesw \
                            --with-subshell=no \
                            --enable-vfs-sftp \
                            --enable-vfs-tar \
                            --enable-vfs-extfs \
                            --enable-vfs-cpio \
                            --disable-vfs-smb"
-                           
+
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
