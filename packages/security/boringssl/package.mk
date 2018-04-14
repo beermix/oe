877f1b979981578a943f9962b34b1848566f512d
AV <27rurk@gmail.com>
2018-04-15 02:11:35 +0300

    autocommit on change

--- packages/security/boringssl/package.mk
+++ packages/security/boringssl/package.mk
@@ -7,12 +7,7 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="security"
 PKG_SHORTDESC="openssl: a FREE version of the SSL/TLS protocol forked from openssl"
 PKG_LONGDESC="openssl is a FREE version of the SSL/TLS protocol forked from openssl"
-PKG_USE_CMAKE="yes"
-
-
-pre_configure_target() {
-  strip_lto
-}
++PKG_BUILD_FLAGS="-lto -gold -hardening"
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DFOUND_LIBRT=$SYSROOT_PREFIX/usr/lib/librt.so"
 
