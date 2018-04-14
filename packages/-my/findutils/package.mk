f08a98cb8d67ebd521d102f15c62e860d78f2413
AV <27rurk@gmail.com>
2018-04-15 02:53:42 +0300

    autocommit on change

--- packages/-my/findutils/package.mk
+++ packages/-my/findutils/package.mk
@@ -3,7 +3,7 @@ PKG_VERSION="4.6.0"
 PKG_URL="http://ftpmirror.gnu.org/findutils/findutils-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="sysutils"
-PKG_TOOLCHAIN="configure"
+#PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--without-selinux --disable-debug"
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
--- packages/-my/grep/package.mk
+++ packages/-my/grep/package.mk
@@ -3,6 +3,6 @@ PKG_VERSION="3.1"
 PKG_URL="http://ftp.gnu.org/gnu/grep/grep-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline pcre libcap"
 PKG_SECTION="sysutils"
-PKG_TOOLCHAIN="autotools"
+#PKG_TOOLCHAIN="autotools"
 
-#PKG_CONFIGURE_OPTS_TARGET="--disable-perl-regexp"
+PKG_CONFIGURE_OPTS_TARGET="--disable-perl-regexp"
