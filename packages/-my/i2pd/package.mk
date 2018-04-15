e0feb1fb4e45fd616955f5b4894394d4c1d68b4f
AV <27rurk@gmail.com>
2018-04-15 15:22:45 +0300

    autocommit on change

--- packages/-my/i2pd/package.mk
+++ packages/-my/i2pd/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="i2pd"
-PKG_VERSION="82534ee"
+PKG_VERSION="c175dc3"
 PKG_URL="https://github.com/PurpleI2P/i2pd/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain boost zlib miniupnpc openssl"
 PKG_SECTION="my"
@@ -7,9 +7,11 @@ PKG_TOOLCHAIN="make"
 			  
 make_target() {
   cd $PKG_BUILD
-  make USE_STATIC=no USE_AESNI=no USE_AVX=no USE_UPNP=no \
-  	CC="$CC" \
-  	CXX="$CXX -lpthread -fno-omit-frame-pointer -ffunction-sections -fdata-sections" \
+  unset CFLAGS
+  unset CXXFLAGS
+  make USE_STATIC=no USE_AESNI=no USE_AVX=no USE_UPNP=yes \
+  	CC="$CC -O2 -pipe" \
+  	CXX="$CXX -O2 -pipe -lpthread -fno-omit-frame-pointer -ffunction-sections -fdata-sections" \
   	LDFLAGS="$LDFLAGS -lpthread -lm -lrt -latomic -ldl -Wl,--gc-sections" \
   	RANLIB="$RANLIB" \
   	AR="$AR"
