77dfe1c160e419c4d2514e8c3a85299f661172d3
AV <27rurk@gmail.com>
2018-04-15 04:13:56 +0300

    autocommit on change

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -26,16 +26,15 @@ PKG_DEPENDS_HOST="ccache:host openssl:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="cmake: A cross-platform, open-source make system"
 PKG_LONGDESC="CMake is used to control the software compilation process using simple platform and compiler independent configuration files. CMake generates native makefiles and workspaces that can be used in the compiler environment of your choice. CMake is quite sophisticated: it is possible to support complex environments requiring system configuration, preprocessor generation, code generation, and template instantiation."
-PKG_TOOLCHAIN="manual"
-#PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="configure"
 
-#configure_host() {
-#  ../configure --prefix=$TOOLCHAIN \
-#               --no-qt-gui --no-system-libs \
-#               -- \
-#               -DCMAKE_C_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
-#               -DCMAKE_CXX_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
-#               -DCMAKE_EXE_LINKER_FLAGS="$HOST_LDFLAGS" \
-#               -DCMAKE_USE_OPENSSL=ON \
-#               -DBUILD_CursesDialog=0
-#}
+configure_host() {
+  ../configure --prefix=$TOOLCHAIN \
+               --no-qt-gui --no-system-libs \
+               -- \
+               -DCMAKE_C_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
+               -DCMAKE_CXX_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
+               -DCMAKE_EXE_LINKER_FLAGS="$HOST_LDFLAGS -s" \
+               -DCMAKE_USE_OPENSSL=ON \
+               -DBUILD_CursesDialog=0
+}
