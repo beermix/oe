5a795ee46c28e869c63885986994fb0e077a19c2
AV <27rurk@gmail.com>
2018-04-15 19:14:31 +0300

    autocommit on change

--- packages/-my/htop/package.mk
+++ packages/-my/htop/package.mk
@@ -1,14 +1,14 @@
 PKG_NAME="htop"
-PKG_VERSION="2.2.0"
+PKG_VERSION="04cc193"
 PKG_SITE="https://github.com/hishamhm/htop"
 PKG_URL="https://github.com/hishamhm/htop/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses libpciaccess libxml2"
 PKG_SECTION="debug/tools"
 PKG_TOOLCHAIN="autotools"
 
-pre_configure_target() {
-  export LIBS="$LIBS -lm -ludev -lltdl -lpthread -ldl"
-}
+#pre_configure_target() {
+#  export LIBS="$LIBS -lm -ludev -lltdl -lpthread -ldl"
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config/htop \
 			      --datarootdir=/storage/.config/htop \
