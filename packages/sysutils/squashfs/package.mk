5a0527fe4b9e36aa77768814aebf5ac48b9bf0f9
AV <27rurk@gmail.com>
2018-04-16 19:50:00 +0300

    autocommit on change

--- packages/sysutils/squashfs/package.mk
+++ packages/sysutils/squashfs/package.mk
@@ -33,7 +33,7 @@ PKG_TOOLCHAIN="manual"
 
 make_host() {
   make -C squashfs-tools mksquashfs \
-       XZ_SUPPORT=1 LZO_SUPPORT=1 ZSTD_SUPPORT=1 \
+       XZ_SUPPORT=1 LZO_SUPPORT=1 ZSTD_SUPPORT=1 ZSTD_SUPPORT=1 \
        XATTR_SUPPORT=0 XATTR_DEFAULT=0 \
        INCLUDEDIR="-I. -I$TOOLCHAIN/include"
 }
