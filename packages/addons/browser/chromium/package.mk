4879826cf73b9a8b5d4efa4d4d6c44f3a3ed9b37
AV <27rurk@gmail.com>
2018-04-23 20:48:40 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -195,8 +195,8 @@ addon() {
   cp -PL $(get_build_dir cairo)/.install_pkg/usr/lib/libcairo.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib
   cp -PL $(get_build_dir cairo)/.install_pkg/usr/lib/libcairo-gobject.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib
 
-  # icu
-  # cp -ri $(get_build_dir icu)/.install_pkg/usr/lib/libicu*.so.* $ADDON_BUILD/$PKG_ADDON_ID/lib
+  # libXtst
+  cp -ri $(get_build_dir libXtst)/.install_pkg/usr/lib/* $ADDON_BUILD/$PKG_ADDON_ID/lib
 
   # gtk
   cp -PL $(get_build_dir gtk+)/.install_pkg/usr/lib/libgdk-x11-2.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
