26bfd229ca199f62fa6bcb624c444eec02cce02e
AV <27rurk@gmail.com>
2018-04-24 06:01:54 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -44,7 +44,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
                            --with-__thread \
                            --with-binutils=$BUILD/toolchain/bin \
                            --with-headers=$SYSROOT_PREFIX/usr/include \
-                           --enable-kernel=4.4 \
+                           --enable-kernel=3.10 \
                            --without-cvs \
                            --without-gd \
                            --enable-obsolete-rpc \
--- packages/lang/yasm/package.mk
+++ packages/lang/yasm/package.mk
@@ -17,29 +17,23 @@
 ################################################################################
 
 PKG_NAME="yasm"
-PKG_VERSION="1.3.0"
-PKG_SHA256="3dce6601b495f5b3d45b59f7d2492a340ee7e84b5beca17e48f862502bd5603f"
-PKG_ARCH="any"
-PKG_LICENSE="BSD"
-PKG_SITE="http://www.tortall.net/projects/yasm/"
-PKG_URL="http://www.tortall.net/projects/yasm/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host re2c:host nasm:host"
+PKG_VERSION="e256985"
+PKG_SITE="https://github.com/yasm/yasm"
+PKG_URL="https://github.com/yasm/yasm/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="ccache:host re2c:host nasm:host gperf:host"
 PKG_SECTION="toolchain/lang"
 PKG_SHORTDESC="yasm: A complete rewrite of the NASM assembler"
 PKG_LONGDESC="Yasm is a complete rewrite of the NASM assembler under the new BSD License (some portions are under other licenses, see COPYING for details). It is designed from the ground up to allow for multiple assembler syntaxes to be supported (eg, NASM, TASM, GAS, etc.) in addition to multiple output object formats and even multiple instruction sets. Another primary module of the overall design is an optimizer module."
-PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_HOST="--disable-debug \
                          --disable-warnerror \
                          --disable-profiling \
                          --disable-gcov \
-                         --disable-python \
-                         --disable-python-bindings \
                          --enable-nls \
                          --disable-rpath \
                          --without-dmalloc \
                          --with-gnu-ld \
                          --without-libiconv-prefix \
-                         --without-libintl-prefix"
-
-PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
+                         --without-libintl-prefix \
+                         --disable-option-checking"
