e9eaf79c1c5cecd0f879c86e4a1e1ced579218de
AV <27rurk@gmail.com>
2018-04-27 08:39:30 +0300

    autocommit on change

--- packages/multimedia/libwebp/package.mk
+++ packages/multimedia/libwebp/package.mk
@@ -7,12 +7,9 @@ PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 PKG_USE_CMAKE="no"
 PKG_TOOLCHAIN="autotools"
+PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-swap-16bit-csp \
-			      --enable-experimental \
-			      --enable-libwebp-mux \
-			      --enable-libwebp-demux \
-			      --enable-libwebp-decoder"
+PKG_CONFIGURE_OPTS_TARGET="--disable-nls --disable-shared --enable-static"
 
 #post_makeinstall_target() {
 #  rm -rf $INSTALL
