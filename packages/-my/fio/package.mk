18155ad27f29365777de14239660b3303b8f8e1f
AV <27rurk@gmail.com>
2018-04-27 12:58:38 +0300

    autocommit on change

--- packages/-my/fio/package.mk
+++ packages/-my/fio/package.mk
@@ -1,15 +1,24 @@
 PKG_NAME="fio"
 PKG_VERSION="3.6"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET=""
+PKG_DEPENDS_TARGET="toolchain numactl libaio"
 PKG_SECTION="tools"
 PKG_TOOLCHAIN="manual"
 
 configure_target() {
   cd $PKG_BUILD
-  ./configure --prefix=/usr --enable-lex --esx --cpu=$TARGET_CPU --cc=$CC --build-static --disable-native
+  LIBS="-laio -lrt" ./configure --prefix=/usr --cpu=$TARGET_CPU --cc=$CC --disable-native --build-static --extra-cflags="$CFLAGS"
 }
 
 make_target() {
+ LDFLAGS=-s make
+}
+
+configure_host() {
+  cd $PKG_BUILD
+  LIBS="-laio -lrt" ./configure --build-static
+}
+
+make_host() {
  LDFLAGS=-s make
 }
