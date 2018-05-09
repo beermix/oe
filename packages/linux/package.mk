94d1f266961369f4ca291f5710fc2eaa49e466c9
AV <27rurk@gmail.com>
2018-05-09 12:30:09 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -59,7 +59,7 @@ case "$LINUX" in
     PKG_BUILD_POWER="no"
     ;;
   *)
-    PKG_VERSION="4.14.39"
+    PKG_VERSION="4.14.40"
     PKG_SHA256=""
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="4.14"
