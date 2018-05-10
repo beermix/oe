4ffad2b391995c564ae9118b75748dff59b22791
AV <27rurk@gmail.com>
2018-05-11 00:16:43 +0300

    autocommit on change

--- packages/security/mbedtls/package.mk
+++ packages/security/mbedtls/package.mk
@@ -1,18 +1,13 @@
 PKG_NAME="mbedtls"
-PKG_VERSION="2.6.0"
+PKG_VERSION="2.9.0"
 PKG_URL="https://tls.mbed.org/download/mbedtls-$PKG_VERSION-gpl.tgz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="security"
+PKG_TOOLCHAIN="cmake-make"
+PKG_BUILD_FLAGS="+pic"
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
 			  -DENABLE_PROGRAMS=0 \
 			  -DENABLE_TESTING=0 \
 			  -DUSE_SHARED_MBEDTLS_LIBRARY=0 \
 			  -DUSE_STATIC_MBEDTLS_LIBRARY=1"
-			  
-PKG_CMAKE_OPTS_HOST="$PKG_CMAKE_OPTS_TARGET"
-
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -fPIC"
-  export CXXFLAGS="$CXXFLAGS -fPIC"
-}
