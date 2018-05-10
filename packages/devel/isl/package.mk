e97dbc78eea7b5108c9477ca774b1cd756ffde06
AV <27rurk@gmail.com>
2018-05-10 14:34:39 +0300

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -3,6 +3,6 @@ PKG_VERSION="0.19"
 PKG_URL="http://isl.gforge.inria.fr/isl-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="libtool:host intltool:host gmp:host"
 PKG_SECTION="devel"
-#PKG_TOOLCHAIN="autotools"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_HOST="--with-gmp-prefix=$TOOLCHAIN --disable-silent-rules"
