9ea8281ef35e9647810a5dc6b51135351edfffb6
AV <27rurk@gmail.com>
2018-05-11 07:53:37 +0300

    autocommit on change

--- packages/debug/sysdig/package.mk
+++ packages/debug/sysdig/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="sysdig"
-PKG_VERSION="0.21.0"
+PKG_VERSION="e259ae0"
 PKG_URL="https://github.com/draios/sysdig/"
 PKG_URL="https://github.com/draios/sysdig/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain zlib ncurses openssl curl"
+PKG_DEPENDS_TARGET="toolchain elfutils"
 PKG_SECTION="debug"
 PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="-lto -gold -hardening"
@@ -13,4 +13,5 @@ PKG_CMAKE_OPTS_TARGET="-DBUILD_DRIVER=0 \
 			  -DUSE_BUNDLED_CURL=1 \
 			  -DUSE_BUNDLED_NCURSES=1 \
 			  -DUSE_BUNDLED_OPENSSL=1 \
-			  -DUSE_BUNDLED_ZLIB=1"
+			  -DUSE_BUNDLED_ZLIB=1 \
+			  -DDCMAKE_BUILD_TYPE=Release"
