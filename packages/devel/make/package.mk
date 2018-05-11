dd7901a991b52bc7d2b448451c483543f4e80430
AV <27rurk@gmail.com>
2018-05-11 03:40:50 +0300

    autocommit on change

--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -28,9 +28,9 @@ PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="make: GNU make utility to maintain groups of programs"
 PKG_LONGDESC="The 'make' utility automatically determines which pieces of a large program need to be recompiled, and issues commands to recompile them. This is GNU 'make', which was implemented by Richard Stallman and Roland McGrath. GNU 'make' conforms to section 6.2 of EEE Standard 1003.2-1992' (POSIX.2)."
 
-#post_unpack() {
-#  sed -i '211,217 d; 219,229 d; 232 d' $PKG_BUILD/glob/glob.c
-#}
+post_unpack() {
+  sed -i '211,217 d; 219,229 d; 232 d' $PKG_BUILD/glob/glob.c
+}
 
 export CC=$LOCAL_CC
 
@@ -39,10 +39,10 @@ PKG_CONFIGURE_OPTS_HOST="--without-guile"
 post_makeinstall_host() {
   ln -sf make $TOOLCHAIN/bin/gmake
 
-#  mkdir -p $TOOLCHAIN/share/aclocal/
+  mkdir -p $TOOLCHAIN/share/aclocal/
 
   cp -r -i $PKG_DIR/src/bin/* $TOOLCHAIN/bin/
-#  cp -r -i $PKG_DIR/src/m4/* $TOOLCHAIN/share/aclocal/
+  cp -r -i $PKG_DIR/src/m4/* $TOOLCHAIN/share/aclocal/
   
 #  mkdir -p $SYSROOT_PREFIX/share/aclocal/
 #  cp -r -i $PKG_DIR/src/m4/* $SYSROOT_PREFIX/share/aclocal/
