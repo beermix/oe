0c6141748d4be3bcbf4c444e6a3c1e09c5f0034a
AV <27rurk@gmail.com>
2018-05-17 20:53:07 +0300

    autocommit on change

--- packages/-my/nano/package.mk
+++ packages/-my/nano/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="nano"
-PKG_VERSION="2.9.6"
+PKG_VERSION="2.9.7"
 PKG_URL="https://www.nano-editor.org/dist/v2.9/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline file"
 PKG_SECTION="shell/texteditor"
