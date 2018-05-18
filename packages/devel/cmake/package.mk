68b12d31bc0a4379337aa038277e54da88cd23ba
AV <27rurk@gmail.com>
2018-05-18 10:36:45 +0300

    autocommit on change

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cmake"
-PKG_VERSION="3.11.1"
+PKG_VERSION="3.11.2"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/download/"
