c8f6dfc06cfd64da1cc7c1247919c4729fceec49
AV <27rurk@gmail.com>
2018-05-18 14:57:46 +0300

    autocommit on change

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -47,6 +47,10 @@ PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --enable-gold \
                          --enable-ld=default \
                          --enable-lto \
+                         --enable-relro \
+                         --enable-targets=x86_64-pep \
+                         --enable-threads \
+                         --with-pic \
                          --disable-sim \
                          --disable-gdb \
                          --disable-nls"
