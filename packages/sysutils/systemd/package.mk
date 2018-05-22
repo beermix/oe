1c2c6887332490c62775f8a24c6c0823f9535290
AV <27rurk@gmail.com>
2018-05-22 19:02:40 +0300

    autocommit on change

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -106,7 +106,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fno-schedule-insns -fno-schedule-insns2 -Wno-format-truncation"
-  export LC_ALL=en_US.UTF-8
+  export LC_ALL=ru_RU.UTF-8
 }
 
 post_makeinstall_target() {
