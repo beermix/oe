9fdcd677df2108a08ff3f77ff0ec1c59e910b31b
AV <27rurk@gmail.com>
2018-05-22 20:53:18 +0300

    autocommit on change

--- packages/-my/i2pd/package.mk
+++ packages/-my/i2pd/package.mk
@@ -1,22 +1,21 @@
 PKG_NAME="i2pd"
-PKG_VERSION="c175dc3"
-PKG_URL="https://github.com/PurpleI2P/i2pd/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain boost zlib miniupnpc openssl"
+PKG_VERSION="d289aa7"
+PKG_URL="https://github.com/PurpleI2P/i2pd/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain boost zlib openssl"
 PKG_SECTION="my"
-PKG_TOOLCHAIN="make"
-			  
-make_target() {
-  cd $PKG_BUILD
-  unset CFLAGS
-  unset CXXFLAGS
-  make USE_STATIC=no USE_AESNI=no USE_AVX=no USE_UPNP=yes \
-  	CC="$CC -O2 -pipe" \
-  	CXX="$CXX -O2 -pipe -lpthread -fno-omit-frame-pointer -ffunction-sections -fdata-sections" \
-  	LDFLAGS="$LDFLAGS -lpthread -lm -lrt -latomic -ldl -Wl,--gc-sections" \
-  	RANLIB="$RANLIB" \
-  	AR="$AR"
-}
+PKG_TOOLCHAIN="cmake-make"
 
-post_make_target() {
-  $STRIP $PKG_BUILD/i2pd
+PKG_CMAKE_SCRIPT="$PKG_BUILD/build/CMakeLists.txt"
+
+PKG_CMAKE_OPTS_TARGET="-DWITH_LIBRARY=OFF \
+			  -DWITH_PCH=OFF \
+			  -DWITH_MESHNET=OFF \
+			  -DWITH_STATIC=OFF \
+			  -DWITH_UPNP=OFF \
+			  -DWITH_HARDENING=OFF \
+			  -DWITH_AESNI=OFF"
+
+post_makeinstall_target() {
+ rm -rf $INSTALL/usr/src/
+ rm  $INSTALL/usr/LICENSE
 }
