642cb25d03ae7f30c8ecb0866eae43ff56c07ac3
AV <27rurk@gmail.com>
2018-05-23 13:14:04 +0300

    autocommit on change

--- packages/devel/boost/package - копия.mk
+++ packages/devel/boost/package - копия.mk
@@ -23,7 +23,7 @@ PKG_SITE="http://www.boost.org/"
 PKG_URL="https://dl.bintray.com/boostorg/release/1.67.0/source/boost_$PKG_VERSION.tar.gz"
 PKG_URL="https://fossies.org/linux/misc/boost_1_67_0.tar.xz"
 PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
-PKG_DEPENDS_TARGET="toolchain boost:host zlib bzip2"
+PKG_DEPENDS_TARGET="toolchain boost:host Python2 zlib bzip2 xz"
 PKG_LONGDESC="boost: Peer-reviewed STL style libraries for C++"
 PKG_TOOLCHAIN="manual"
 PKG_BUILD_FLAGS="+pic"
@@ -38,25 +38,36 @@ makeinstall_host() {
     cp bin.*/bjam $TOOLCHAIN/bin
 }
 
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/$PKG_PYTHON_VERSION"
+  export CXXFLAGS="$CXXFLAGS -I$SYSROOT_PREFIX/usr/include/$PKG_PYTHON_VERSION"
+}
+
 configure_target() {
   sh bootstrap.sh --prefix=/usr \
-                  --with-bjam=$TOOLCHAIN/bin/bjam 
+                  --with-bjam=$TOOLCHAIN/bin/bjam \
+                  --with-python=$TOOLCHAIN/bin/python \
+                  --with-python-root=$SYSROOT_PREFIX/usr
 
   echo "using gcc : `$CC -v 2>&1  | tail -n 1 |awk '{print $3}'` : $CC  : <compileflags>\"$CFLAGS\" <linkflags>\"$LDFLAGS\" ;" \
     > tools/build/src/user-config.jam
+  echo "using python : ${PKG_PYTHON_VERSION/#python} : $TOOLCHAIN : $SYSROOT_PREFIX/usr/include : $SYSROOT_PREFIX/usr/lib ;" \
+    >> tools/build/src/user-config.jam
 }
 
 makeinstall_target() {
   $TOOLCHAIN/bin/bjam -d2 --ignore-site-config \
                           --layout=system \
                           --prefix=$SYSROOT_PREFIX/usr \
-                          --toolset=gcc variant=release link=static runtime-link=static threading=multi \
+                          --toolset=gcc variant=release link=static runtime-link=static threading=multi address-model=64 \
+                          --build-type=minimal \
                           --with-chrono \
                           --with-date_time \
                           --with-filesystem \
                           --with-iostreams \
                           --with-program_options \
                           --with-exception \
+                          --with-python \
                           --with-atomic \
                           --with-random \
                           --with-regex -sICU_PATH="$SYSROOT_PREFIX/usr" \
