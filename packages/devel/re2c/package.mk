0209e7c100ad66f7e471d98474b16ec6a73ce3b7
AV <27rurk@gmail.com>
2018-06-06 20:03:16 +0300

    autocommit on change

--- packages/devel/re2c/package.mk
+++ packages/devel/re2c/package.mk
@@ -1,8 +1,14 @@
 PKG_NAME="re2c"
-PKG_VERSION="1.0.3"
+PKG_VERSION="0.16"
 PKG_URL="https://github.com/skvadrik/re2c/releases/download/$PKG_VERSION/re2c-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/skvadrik/re2c/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="bison:host"
 PKG_SECTION="devel"
+PKG_TOOLCHAIN="autotools"
+
+post_unpack() {
+  cp -r $PKG_BUILD/re2c/* $PKG_BUILD/
+}
 
 PKG_CONFIGURE_OPTS_HOST="--disable-docs"
 PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
