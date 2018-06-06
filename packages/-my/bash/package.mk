b9a7abc5b87e815fcbd2c179c175ec5a1cd1062f
AV <27rurk@gmail.com>
2018-06-06 11:54:54 +0300

    autocommit on change

--- packages/-my/bash/package.mk
+++ packages/-my/bash/package.mk
@@ -1,9 +1,9 @@
 PKG_NAME="bash"
-PKG_VERSION="4.4.18"
+PKG_VERSION="6444760"
 PKG_SITE="http://ftp.gnu.org/gnu/bash/?C=M;O=D"
 PKG_URL="http://ftp.gnu.org/gnu/bash/bash-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/bminor/bash/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses readline"
-#PKG_BUILD_FLAGS="-parallel"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-static \
 			      --enable-cond-command \
--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="readline"
-PKG_VERSION="6c32f81"
+PKG_VERSION="57ea398"
 PKG_SITE="http://www.gnu.org/software/readline/"
 PKG_URL="http://git.savannah.gnu.org/cgit/readline.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
