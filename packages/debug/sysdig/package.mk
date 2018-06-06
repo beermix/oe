d5dd1ef9db43b44efcd80bd8879ecfc0e0d44139
AV <27rurk@gmail.com>
2018-06-06 23:16:41 +0300

    autocommit on change

--- packages/debug/sysdig/package.mk
+++ packages/debug/sysdig/package.mk
@@ -1,12 +1,10 @@
 PKG_NAME="sysdig"
-PKG_VERSION="567c2e23"
+PKG_VERSION="0.21.0"
 PKG_URL="https://github.com/draios/sysdig/"
 PKG_URL="https://github.com/draios/sysdig/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain elfutils"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="debug"
 PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="-lto -gold -hardening"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_DRIVER=0 \
-			  -DBUILD_LIBSCAP_EXAMPLES=0 \
-			  -DENABLE_DKMS=0"
+#PKG_CMAKE_OPTS_TARGET="-DBUILD_DRIVER=0 -DBUILD_LIBSCAP_EXAMPLES=0 -DENABLE_DKMS=0"
