bf5c64658e84f9976ad1f308bb623012a5c7d0f5
AV <27rurk@gmail.com>
2018-06-10 05:23:32 +0300

    autocommit on change

--- packages/devel/re2c/package.mk
+++ packages/devel/re2c/package.mk
@@ -1,5 +1,6 @@
 PKG_NAME="re2c"
 PKG_VERSION="0.16"
+PKG_VERSION="1.03"
 PKG_URL="https://github.com/skvadrik/re2c/releases/download/$PKG_VERSION/re2c-$PKG_VERSION.tar.gz"
 PKG_URL="https://github.com/skvadrik/re2c/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="bison:host"
