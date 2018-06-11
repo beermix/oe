5100e6670c54bfd454eb3c1636ef8829fa639d39
AV <27rurk@gmail.com>
2018-06-11 20:48:24 +0300

    autocommit on change

--- packages/lang/clang/package.mk
+++ packages/lang/clang/package.mk
@@ -1,10 +1,10 @@
 PKG_NAME="clang"
-PKG_VERSION="b55ee77"
+PKG_VERSION="6.0.0"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
 PKG_URL="http://releases.llvm.org/$PKG_VERSION/cfe-$PKG_VERSION.src.tar.xz"
-PKG_URL="https://github.com/llvm-project/clang/archive/${PKG_VERSION}.tar.gz"
+#PKG_URL="https://github.com/llvm-project/clang/archive/${PKG_VERSION}.tar.gz"
 PKG_SOURCE_DIR="cfe-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain Python2:hpost clang:host llvm clang:host libxml2"
 PKG_DEPENDS_HOST="llvm:host libxml2:host"
@@ -12,31 +12,25 @@ PKG_SECTION="lang"
 PKG_SHORTDESC="C language family frontend for LLVM"
 PKG_TOOLCHAIN="cmake-make"
 
-cmake .. -G Ninja
+configure_host() {
+  mkdir build
+  cd build
+  cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release \
+  		      -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
+  		      -DBUILD_SHARED_LIBS=OFF \
+  		      -DLLVM_LINK_LLVM_DYLIB=OFF \
+  		      -DLLVM_ENABLE_RTTI=ON \
+  		      -DLLVM_BUILD_TESTS=ON \
+  		      -DLLVM_INCLUDE_DOCS=OFF \
+  		      -DLLVM_BUILD_DOCS=OFF \
+  		      -DLLVM_ENABLE_SPHINX=OFF \
+  		      -DSPHINX_WARNINGS_AS_ERRORS=OFF \
+  		      -DLLVM_CONFIG:FILEPATH=$TOOLCHAIN/bin/llvm-config \
+  		      -DLLVM_MAIN_SRC_DIR=$PKG_BUILD/../llvm-6.0.0 \
+			..
+			ninja
+}
 
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release \
-			-DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
-			-DPYTHON_EXECUTABLE=$TOOLCHAIN/bin/python2 \
-			-DBUILD_SHARED_LIBS=ON \
-			-DLLVM_LINK_LLVM_DYLIB=OFF \
-			-DLLVM_ENABLE_RTTI=ON \
-			-DLLVM_BUILD_TESTS=ON \
-			-DLLVM_INCLUDE_DOCS=OFF \
-			-DLLVM_BUILD_DOCS=OFF \
-			-DLLVM_ENABLE_SPHINX=ON \
-			-DSPHINX_WARNINGS_AS_ERRORS=OFF \
-			-DLLVM_CONFIG:FILEPATH=$TOOLCHAIN/bin/llvm-config \
-			-DLLVM_EXTERNAL_LIT=/usr/bin/lit \
-			-DLLVM_MAIN_SRC_DIR=$(get_build_dir llvm)"
-
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release \
-			-DCLANG_BUILD_TOOLS=ON \
-			-DCLANG_BUILD_EXAMPLES=OFF \
-			-DCLANG_INCLUDE_DOCS=OFF \
-			-DCLANG_INCLUDE_TESTS=OFF \
-			-DLLVM_CONFIG:FILEPATH=$TOOLCHAIN/bin/llvm-config \
-			-DLLVM_LINK_LLVM_DYLIB=ON \
-			-DLLVM_DYLIB_COMPONENTS=all"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF \
 			  -DCMAKE_BUILD_TYPE=Release \
