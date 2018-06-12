70387ffef3756509423d4c9d86f858544ed958dd
AV <27rurk@gmail.com>
2018-06-12 13:30:21 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -104,21 +104,21 @@ PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
                          $GCC_OPTS"
 
 pre_configure_host() {
-  export CFLAGS="-g -O2 -I$TOOLCHAIN/include"
-  export CXXFLAGS="-g -O2 -I$TOOLCHAIN/include"
-  export CFLAGS_FOR_TARGET="$TARGET_CFLAGS"
-  export CXXFLAGS_FOR_TARGET="$TARGET_CXXFLAGS"
+  # export CFLAGS="-g -O2 -I$TOOLCHAIN/include"
+  # export CXXFLAGS="-g -O2 -I$TOOLCHAIN/include"
+  # export CFLAGS_FOR_TARGET="$TARGET_CFLAGS"
+  # export CXXFLAGS_FOR_TARGET="$TARGET_CXXFLAGS"
   
   export CXXFLAGS="$CXXFLAGS -std=gnu++98"
   unset CPP
 }
 
-pre_configure_bootstrap() {
-  export CFLAGS="-g -O2 -I$TOOLCHAIN/include"
-  export CXXFLAGS="-g -O2 -I$TOOLCHAIN/include"
-  export CFLAGS_FOR_TARGET="$TARGET_CFLAGS"
-  export CXXFLAGS_FOR_TARGET="$TARGET_CXXFLAGS"
-}
+#pre_configure_bootstrap() {
+ # export CFLAGS="-g -O2 -I$TOOLCHAIN/include"
+ # export CXXFLAGS="-g -O2 -I$TOOLCHAIN/include"
+ # export CFLAGS_FOR_TARGET="$TARGET_CFLAGS"
+ # export CXXFLAGS_FOR_TARGET="$TARGET_CXXFLAGS"
+#}
 
 post_make_host() {
   # fix wrong link
