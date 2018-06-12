14baee9c4b2e0cd23f44be30bc7014cd9f56eba0
AV <27rurk@gmail.com>
2018-06-12 05:04:48 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -48,11 +48,11 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   zen)
-    PKG_VERSION="d82f518"
+    PKG_VERSION="a2b7ab4"
     PKG_SITE="https://github.com/zen-kernel/zen-kernel/branches/active"
     PKG_URL="https://github.com/zen-kernel/zen-kernel/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="zen-kernel-$PKG_VERSION*"
-    PKG_PATCH_DIRS="4.16"
+    PKG_PATCH_DIRS="4.17"
     PKG_BUILD_PERF="no"
     ;;
   *)
