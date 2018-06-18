65073f0d2e365242f225e2e0d1177bfc71342f3c
AV <27rurk@gmail.com>
2018-06-18 09:57:33 +0300

    autocommit on change

--- packages/-my/gawk/package.mk
+++ packages/-my/gawk/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="gawk"
 PKG_VERSION="4.2.0"
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="toolchain gmp:host mpfr:host"
-PKG_TOOLCHAIN="configure"
+PKG_DEPENDS_HOST="toolchain gmp:host mpfr:host readline"
+#PKG_TOOLCHAIN="configure"
 
-PKG_CONFIGURE_OPTS_TARGET="--without-selinux"
-PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
+#PKG_CONFIGURE_OPTS_TARGET="--without-selinux"
+#PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
