38356a500e896f893fbcd9fd9050135f617908cf
AV <27rurk@gmail.com>
2018-06-18 16:13:38 +0300

    autocommit on change

--- packages/addons/gtk+/harfbuzz/package.mk
+++ packages/addons/gtk+/harfbuzz/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="harfbuzz"
-PKG_VERSION="1.8.1"
+PKG_VERSION="1.7.7"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
