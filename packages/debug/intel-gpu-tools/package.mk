354b1db12e6ed5eb9b81be824f8f83b5531407d7
AV <27rurk@gmail.com>
2018-07-04 09:03:38 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -1,8 +1,11 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="1.22"
+PKG_VERSION="aeb3f4143572b81a907921ad9442858aafe1931e"
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
 PKG_URL="https://www.x.org/releases/individual/app/intel-gpu-tools-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo swig:host libunwind"
+PKG_URL="https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/snapshot/$PKG_VERSION.tar.xz"
+PKG_SOURCE_DIR="$PKG_VERSION"
+PKG_DEPENDS_TARGET="toolchain clang:host libxml llvm zlib"
+PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo swig:host libunwind libXv"
 PKG_SECTION="tools"
 #PKG_TOOLCHAIN="autotools"
 
