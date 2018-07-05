31e083d9619e4a1c4a4ec4606d4f5d47bf9e9e00
AV <27rurk@gmail.com>
2018-07-06 01:54:01 +0300

    autocommit on change

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -26,10 +26,6 @@ PKG_SHORTDESC="readline: The GNU Readline library provides a set of functions fo
 PKG_LONGDESC="The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in."
 PKG_BUILD_FLAGS="+pic:host +pic"
 
-#post_unpack() {
-#  sed -i 's|-Wl,-rpath,$(libdir) ||g' $PKG_BUILD/support/shobj-conf
-#}
-
 PKG_CONFIGURE_OPTS_TARGET="bash_cv_wcwidth_broken=no --with-curses --enable-static"
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
 
