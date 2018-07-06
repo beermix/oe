f396c634a1d763b39fe9670f75f186d85b1e1b84
AV <27rurk@gmail.com>
2018-07-06 08:14:33 +0300

    autocommit on change

--- packages/addons/chrome-depends/unclutter-xfixes/package.mk
+++ packages/addons/chrome-depends/unclutter-xfixes/package.mk
@@ -1,7 +1,6 @@
 ################################################################################
 #      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2018-present Team LibreELEC
-#      Copyright (C) 2017 Escalade
+#      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,29 +16,25 @@
 #  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="cups"
-PKG_VERSION="2.2.8"
-PKG_SHA256="8f87157960b9d80986f52989781d9de79235aa060e05008e4cf4c0a6ef6bca72"
+PKG_NAME="unclutter-xfixes"
+PKG_VERSION="d505b8e"
+PKG_SHA256=""
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="http://www.cups.org"
-PKG_URL="https://github.com/apple/cups/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain openssl zlib"
-PKG_LONGDESC="CUPS printing system"
-PKG_BUILD_FLAGS="+pic"
+PKG_LICENSE="Public Domain"
+PKG_SITE="https://sourceforge.net/projects/unclutter/"
+KG_URL="https://github.com/bminor/Airblader/unclutter-xfixes/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libX11"
+PKG_SECTION="x11"
+PKG_SOURCE_DIR="cfe-$PKG_VERSION*"
+PKG_SHORTDESC="Unclutter: Hide X11 Cursor"
+PKG_LONGDESC="Unclutter runs in the background of an X11 session and after a specified period of inactivity hides the cursor from display. When the cursor is moved its display is restored. Users may specify specific windows to be ignored by unclutter."
 
-pre_configure_target() {
-  cd ..
-  rm -rf .$TARGET_NAME
+make_target() {
+  rm -f Makefile
+  LDFLAGS="-lX11" $MAKE unclutter
 }
 
-PKG_CONFIGURE_OPTS_TARGET="--libdir=/usr/lib \
-                           --disable-gssapi \
-                           --disable-avahi \
-                           --disable-systemd \
-                           --disable-launchd \
-                           --disable-unit-tests"
-
 makeinstall_target() {
-  make BUILDROOT="$INSTALL/../.INSTALL_PKG"
+  mkdir -p .install_pkg/usr/bin
+  install -m 755 unclutter .install_pkg/usr/bin/
 }
