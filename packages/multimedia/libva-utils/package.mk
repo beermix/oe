124d37077aa3ec9f0131e23c44f081fc75e1d0ee
AV <27rurk@gmail.com>
2018-07-06 14:24:22 +0300

    autocommit on change

--- packages/multimedia/libva-utils/package.mk
+++ packages/multimedia/libva-utils/package.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="libva-utils"
 PKG_VERSION="1.8.3"
-#PKG_VERSION="2.1.0"
+PKG_VERSION="2.1.0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/intel/libva-utils/releases"
