6a762e4ff41353807e23ac3374fcf7e21f784076
AV <27rurk@gmail.com>
2018-07-06 12:23:57 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="aeb3f4143572b81a907921ad9442858aafe1931e"
-PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
+PKG_VERSION="8b3cc74c6911e9b2835fe6e160f84bae463a70ef"
+PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D" # https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/
 PKG_URL="https://www.x.org/releases/individual/app/intel-gpu-tools-$PKG_VERSION.tar.xz"
 PKG_URL="https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/snapshot/$PKG_VERSION.tar.xz"
 PKG_SOURCE_DIR="$PKG_VERSION"
@@ -12,10 +12,8 @@ PKG_TOOLCHAIN="autotools"
 PKG_CONFIGURE_OPTS_TARGET="--disable-tests \
 			      --disable-shared \
 			      --enable-static \
-			      --with-gnu-ld \
 			      --disable-git-hash \
 			      --disable-debug \
-			      --disable-vc4 \
 			      --disable-amdgpu \
 			      --disable-nouveau \
 			      --enable-intel"
