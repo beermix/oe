3d4095eb3525364ec95041c8236f1dbcd0fadc31
AV <27rurk@gmail.com>
2018-07-06 14:14:29 +0300

    autocommit on change

--- packages/network/ell/package.mk
+++ packages/network/ell/package.mk
@@ -17,28 +17,20 @@
 ################################################################################
 
 PKG_NAME="ell"
-PKG_VERSION="6123cf0"
+PKG_VERSION="726ca36"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/cgit/libs/ell/ell.git/about"
-PKG_GIT_URL="git://git.kernel.org/pub/scm/libs/ell/ell.git"
+PKG_URL="https://git.kernel.org/pub/scm/libs/ell/ell.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="network"
 PKG_SHORTDESC="Embedded Linux library"
 PKG_LONGDESC="Embedded Linux library"
+PKG_TOOLCHAIN="configure"
 
-
-
-
-configure_target() {
-  :
+pre_configure_target() {
+  cd $PKG_BUILD
+  ./bootstrap-configure --host=$TARGET_NAME --build=$HOST_NAME --prefix=/usr 
 }
 
-make_target() {
-  :
-}
-
-makeinstall_target() {
-  :
-}
