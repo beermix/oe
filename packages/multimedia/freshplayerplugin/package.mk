5ed4a301769389b3cd6e39d4dff23a38cee8fd8e
AV <27rurk@gmail.com>
2018-07-06 19:45:44 +0300

    autocommit on change

--- packages/multimedia/freshplayerplugin/package.mk
+++ packages/multimedia/freshplayerplugin/package.mk
@@ -5,9 +5,10 @@ PKG_URL="https://github.com/i-rinat/freshplayerplugin/archive/${PKG_VERSION}.tar
 PKG_DEPENDS_TARGET="toolchain ragel:host libva-vdpau-driver alsa xrandr libXrender libXcursor libdrm libevent freetype openssl icu libva cairo pango"
 PKG_SECTION="multimedia"
 PKG_TOOLCHAIN="cmake-make"
-PKG_BUILD_FLAGS="-hardening"
 
-PKG_CMAKE_OPTS_TARGET="-DWITH_LIBV4L2=0 \
+get_graphicdrivers
+
+PKG_CMAKE_OPTS_TARGET="-DWITH_LIBV4L2=1 \
 			  -DWITH_JACK=0 \
 			  -DWITH_PEPPERFLASH=1 \
 			  -DWITH_PULSEAUDIO=1 \
