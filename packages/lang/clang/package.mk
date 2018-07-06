f0e54f383c718629fdc84b38e76545298c529d19
AV <27rurk@gmail.com>
2018-07-06 08:22:32 +0300

    autocommit on change

--- packages/lang/clang/package.mk
+++ packages/lang/clang/package.mk
@@ -1,35 +1,48 @@
 PKG_NAME="clang"
-PKG_VERSION="01"
+PKG_VERSION="6.0.1"
+PKG_SHA256="7c243f1485bddfdfedada3cd402ff4792ea82362ff91fbdac2dae67c6026b667"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
-PKG_URL="http://192.168.1.200:8080/%2Fclang-01.tar.xz"
-#PKG_SOURCE_DIR="clang-$PKG_VERSION"
-PKG_DEPENDS_TARGET="toolchain lld:host clang:host"
-#PKG_DEPENDS_HOST="llvm:host"
+PKG_URL="http://releases.llvm.org/$PKG_VERSION/cfe-$PKG_VERSION.src.tar.xz"
+#PKG_URL="https://github.com/llvm-project/clang/archive/${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="cfe-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain clang:host libxml llvm zlib"
+PKG_DEPENDS_HOST="llvm:host"
+PKG_TOOLCHAIN="cmake-make"
+#PKG_TOOLCHAIN="manual"
 
 configure_host() {
      cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
     -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
-    -DPYTHON_EXECUTABLE=$TOOLCHAIN/bin/python \
-    -DLLVM_ENABLE_WARNINGS=OFF \
-    -DLLVM_TARGETS_TO_BUILD="X86" \
-    ../llvm
+    -DPYTHON_EXECUTABLE=$TOOLCHAIN/bin/python2 \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DLLVM_LINK_LLVM_DYLIB=OFF \
+    -DLLVM_ENABLE_RTTI=OFF \
+    -DLLVM_BUILD_TESTS=ON \
+    -DLLVM_INCLUDE_DOCS=OFF \
+    -DLLVM_BUILD_DOCS=OFF \
+    -DLLVM_ENABLE_SPHINX=OFF \
+    -DSPHINX_WARNINGS_AS_ERRORS=OFF \
+    -DCMAKE_CXX_FLAGS="${TARGET_CXXFLAGS} -Wno-uninitialized" \
+    ..
 }
 
-#PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_WARNINGS=OFF"
-
-#PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_WARNINGS=OFF"
-
-configure_host() {
+configure_target() {
      cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DPYTHON_EXECUTABLE=$TOOLCHAIN/bin/python \
-    -DLLVM_ENABLE_WARNINGS=OFF \
-    -DLLVM_TARGETS_TO_BUILD="X86" \
-    ../llvm
+     -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+     -DBUILD_SHARED_LIBS=OFF \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DCLANG_BUILD_TOOLS=ON \
+     -DCMAKE_CROSSCOMPILING=1 \
+     -DCLANG_BUILD_EXAMPLES=OFF \
+     -DCLANG_INCLUDE_DOCS=OFF \
+     -DCLANG_INCLUDE_TESTS=OFF \
+     -DLLVM_CONFIG:FILEPATH=$SYSROOT/usr/bin/llvm-config \
+     -DCLANG_TABLEGEN:FILEPATH=$TOOLCHAIN/usr/bin/clang-tblgen \
+     -DLLVM_TABLEGEN_EXE:FILEPATH=$TOOLCHAIN/bin/llvm-tblgen \
+     -DLLVM_LINK_LLVM_DYLIB=ON \
+     -DLLVM_DYLIB_COMPONENTS=all \
+     ..
 }
