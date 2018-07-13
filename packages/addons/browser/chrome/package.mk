a58eecd585b4149b1023b6078308fbc3ccfcee02
AV <27rurk@gmail.com>
2018-07-13 13:43:57 +0300

    autocommit on change

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -14,6 +14,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+# https://archive.archlinux.org/packages/p/pepper-flash/
 ################################################################################
 
 PKG_NAME="chrome"
