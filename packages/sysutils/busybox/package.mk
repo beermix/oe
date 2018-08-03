ce002f1516ed7eaa90cc7bdf9af82c84c54b666a
AV <27rurk@gmail.com>
2018-08-03 06:55:18 +0300

    autocommit on change

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -3,14 +3,14 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="busybox"
-PKG_VERSION="1.29.2"
-PKG_SHA256="67d2fa6e147a45875fe972de62d907ef866fe784c495c363bf34756c444a5d61"
+PKG_VERSION="1.28.1"
+PKG_SHA256="98fe1d3c311156c597cd5cfa7673bb377dc552b6fa20b5d3834579da3b13652e"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.busybox.net"
 PKG_URL="http://busybox.net/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain busybox:host dosfstools e2fsprogs zip unzip pciutils usbutils parted procps-ng gptfdisk libtirpc less bash gawk"
+PKG_DEPENDS_TARGET="toolchain busybox:host dosfstools e2fsprogs zip unzip unrar pciutils usbutils parted procps-ng gptfdisk libtirpc less bash"
 PKG_DEPENDS_INIT="toolchain libtirpc"
 PKG_SECTION="system"
 PKG_SHORTDESC="BusyBox: The Swiss Army Knife of Embedded Linux"
