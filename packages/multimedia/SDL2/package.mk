a7d9c7d8d0e3a87eed0aa1478ea574d369930634
AV <27rurk@gmail.com>
2018-08-03 11:59:59 +0300

    autocommit on change

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -1,54 +1,96 @@
-################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="SDL2"
-PKG_VERSION="2.0.7"
+PKG_VERSION="2.0.8"
+PKG_SHA256="edc77c57308661d576e843344d8638e025a7818bff73f8fbfab09c3c5fd092ec"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
-PKG_URL="https://www.libsdl.org/release/SDL2-$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="SDL-mirror-$PKG_VERSION*"
-PKG_DEPENDS_TARGET="toolchain cmake:host yasm:host alsa-lib systemd dbus"
+PKG_URL="https://www.libsdl.org/release/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain yasm:host alsa-lib systemd dbus"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="SDL2: A cross-platform Graphic API"
 PKG_LONGDESC="Simple DirectMedia Layer is a cross-platform multimedia library designed to provide fast access to the graphics framebuffer and audio device. It is used by MPEG playback software, emulators, and many popular games, including the award winning Linux port of 'Civilization: Call To Power.' Simple DirectMedia Layer supports Linux, Win32, BeOS, MacOS, Solaris, IRIX, and FreeBSD."
-PKG_TOOLCHAIN="configure"
-PKG_BUILD_FLAGS="-parallel"
 
-PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=OFF \
-		       -DDUMMYAUDIO=OFF \
-		       -DOSS=OFF \
-		       -DRPATH=OFF \
-		       -DVIDEO_DUMMY=OFF \
-		       -DDISKAUDIO=OFF"
+PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=ON \
+                       -DSDL_SHARED=OFF \
+                       -DLIBC=ON \
+                       -DGCC_ATOMICS=ON \
+                       -DASSEMBLY=ON \
+                       -DALTIVEC=OFF \
+                       -DOSS=OFF \
+                       -DALSA=ON \
+                       -DALSA_SHARED=ON \
+                       -DESD=OFF \
+                       -DESD_SHARED=OFF \
+                       -DARTS=OFF \
+                       -DARTS_SHARED=OFF \
+                       -DNAS=OFF \
+                       -DNAS_SHARED=ON \
+                       -DSNDIO=OFF \
+                       -DDISKAUDIO=OFF \
+                       -DDUMMYAUDIO=OFF \
+                       -DVIDEO_WAYLAND=OFF \
+                       -DVIDEO_WAYLAND_QT_TOUCH=ON \
+                       -DWAYLAND_SHARED=OFF \
+                       -DVIDEO_MIR=OFF \
+                       -DMIR_SHARED=OFF \
+                       -DVIDEO_COCOA=OFF \
+                       -DVIDEO_DIRECTFB=OFF \
+                       -DDIRECTFB_SHARED=OFF \
+                       -DFUSIONSOUND=OFF \
+                       -DFUSIONSOUND_SHARED=OFF \
+                       -DVIDEO_DUMMY=OFF \
+                       -DINPUT_TSLIB=OFF \
+                       -DPTHREADS=ON \
+                       -DPTHREADS_SEM=ON \
+                       -DDIRECTX=OFF \
+                       -DSDL_DLOPEN=ON \
+                       -DCLOCK_GETTIME=OFF \
+                       -DRPATH=OFF \
+                       -DRENDER_D3D=OFF"
+
 if [ "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libX11 libXrandr"
 
-  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET -DVIDEO_X11_XINERAMA=OFF -DVIDEO_X11_XVM=OFF"
+  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET \
+                         -DVIDEO_X11=ON \
+                         -DX11_SHARED=ON \
+                         -DVIDEO_X11_XCURSOR=OFF \
+                         -DVIDEO_X11_XINERAMA=OFF \
+                         -DVIDEO_X11_XINPUT=OFF \
+                         -DVIDEO_X11_XRANDR=ON \
+                         -DVIDEO_X11_XSCRNSAVER=OFF \
+                         -DVIDEO_X11_XSHAPE=OFF \
+                         -DVIDEO_X11_XVM=OFF"
+else
+  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET \
+                         -DVIDEO_X11=OFF"
 fi
 
 if [ ! "$OPENGL" = "no" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGL glu"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGL"
+
+  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET \
+                         -DVIDEO_OPENGL=ON \
+                         -DVIDEO_OPENGLES=OFF"
+else
+  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET \
+                         -DVIDEO_OPENGL=OFF \
+                         -DVIDEO_OPENGLES=ON"
 fi
 
-if [[ "$PROJECT" =~ "RPi" ]]; then
-  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET -DVIDEO_X11=OFF"
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-driver"
+if [ "$PULSEAUDIO_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET pulseaudio"
+
+  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET \
+                         -DPULSEAUDIO=ON \
+                         -DPULSEAUDIO_SHARED=ON"
+else
+  PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET \
+                         -DPULSEAUDIO=OFF \
+                         -DPULSEAUDIO_SHARED=OFF"
 fi
 
 post_makeinstall_target() {
