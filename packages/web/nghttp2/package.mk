24e65268baebdd0411d81ca0e39bd434c3d6cb54
AV <27rurk@gmail.com>
2018-08-04 00:11:52 +0300

    autocommit on change

--- packages/web/nghttp2/package.mk
+++ packages/web/nghttp2/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="nghttp2"
-PKG_VERSION="1.31.0"
+PKG_VERSION="1.32.0"
 PKG_SITE="https://github.com/nghttp2/nghttp2/releases/"
 PKG_URL="https://github.com/nghttp2/nghttp2/releases/download/v$PKG_VERSION/nghttp2-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libevent libev libuv"
