156a284c3ce81642f7a4226a1f35a0824ad111e3
AV <27rurk@gmail.com>
2018-08-04 20:59:58 +0300

    autocommit on change

--- packages/addons/chrome-depends/gtk3/package.mk
+++ packages/addons/chrome-depends/gtk3/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gtk3"
-PKG_VERSION="3.23.2"
-PKG_SHA256="65813efa5e0f681c522cc3fc8a966e142b2470caf31484b40d310466a2ca7ed3"
+PKG_VERSION="3.22.20"
+#sPKG_SHA256="65813efa5e0f681c522cc3fc8a966e142b2470caf31484b40d310466a2ca7ed3"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://ftp.acc.umu.se/pub/gnome/sources/gtk+/?C=M;O=D"
@@ -16,14 +16,15 @@ PKG_LONGDESC="The Gimp ToolKit (GTK) is a library for creating graphical user in
 PKG_CONFIGURE_OPTS_TARGET="--disable-cups \
                            --disable-debug \
                            --enable-explicit-deps=no \
-                           --disable-glibtest \
                            --disable-gtk-doc \
                            --disable-gtk-doc-html \
                            --disable-man \
                            --enable-modules \
-                           --disable-papi \
-                           --disable-xinerama \
-                           --enable-xkb"
+                           --enable-xkb \
+                           --disable-schemas-compile \
+                           --enable-x11-backend \
+                           --disable-broadway-backend \
+                           --disable-wayland-backend"
 
 pre_configure_target() {
   LIBS="$LIBS -lXcursor"
