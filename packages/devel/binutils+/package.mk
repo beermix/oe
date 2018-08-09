678d1beeace9e9b4bb78a02c4a4132ef465d22dd
AV <27rurk@gmail.com>
2018-08-10 01:22:46 +0300

    autocommit on change

--- packages/devel/binutils+/package.mk
+++ packages/devel/binutils+/package.mk
@@ -11,7 +11,7 @@ PKG_URL="http://ftpmirror.gnu.org/binutils/$PKG_NAME-$PKG_VERSION.tar.xz"
 #PKG_URL="https://github.com/bminor/binutils-gdb/archive/${PKG_VERSION}.tar.gz"
 PKG_SOURCE_DIR="$PKG_NAME-gdb-$PKG_VERSION*"
 PKG_DEPENDS_HOST="ccache:host bison:host flex:host elfutils:host linux:host"
-PKG_DEPENDS_TARGET="toolchain binutils:host"
+PKG_DEPENDS_TARGET="toolchain binutils:host zlib"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="binutils: A GNU collection of binary utilities"
 PKG_LONGDESC="The GNU binutils are utilities of use when dealing with object files. the packages includes ld - the GNU linker, as - the GNU assembler, addr2line - converts addresses into filenames and line numbers, ar - a utility for creating, modifying and extracting from archives, c++filt - filter to demangle encoded C++ symbols, gprof - displays profiling information, nlmconv - converts object code into an NLM, nm - lists symbols from object files, objcopy - Copys and translates object files, objdump - displays information from object files, ranlib - generates an index to the contents of an archive, readelf - displays information from any ELF format object file, size - lists the section sizes of an object or archive file, strings - lists printable strings from files, strip - discards symbols as well as windres - a compiler for Windows resource files."
@@ -32,7 +32,6 @@ PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --enable-version-specific-runtime-libs \
                          --enable-compressed-debug-sections=all \
                          --enable-targets=x86_64-linux \
-                         --enable-install-libiberty \
                          --enable-threads \
                          --disable-gdb \
                          --with-pic \
@@ -57,6 +56,7 @@ PKG_CONFIGURE_OPTS_TARGET="--target=$TARGET_NAME \
                          --disable-libssp \
                          --disable-plugins \
                          --disable-gold \
+                         --with-system-zlib \
                          --disable-ld \
                          --disable-lto \
                          --disable-nls \
