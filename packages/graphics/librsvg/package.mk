8b475bb765c6243af61a819cbe9dfdf59377d3bd
AV <27rurk@gmail.com>
2018-08-09 08:19:34 +0300

    autocommit on change

--- packages/graphics/librsvg/package.mk
+++ packages/graphics/librsvg/package.mk
@@ -1,8 +1,3 @@
-################################################################################
-#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
-################################################################################
-
 PKG_NAME="librsvg"
 PKG_VERSION="2.43.4"
 PKG_ARCH="any"
@@ -14,11 +9,3 @@ PKG_PRIORITY="optional"
 PKG_SECTION="xmedia/depends"
 PKG_SHORTDESC="librsvg is a free software SVG rendering library written as part of the GNOME project, intended to be lightweight and portable."
 PKG_LONGDESC="librsvg is a free software SVG rendering library written as part of the GNOME project, intended to be lightweight and portable."
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-introspection=no --disable-gtk-doc"
-
-pre_configure_target() {
-  export GDK_PIXBUF_QUERYLOADERS=$SYSROOT_PREFIX/usr/bin
-}
