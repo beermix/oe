15619ed498005ccd94fe0385330e3ccc1ac478d0
AV <27rurk@gmail.com>
2018-08-09 06:06:55 +0300

    autocommit on change

--- packages/-my/coreutils/package.mk
+++ packages/-my/coreutils/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="coreutils"
 PKG_VERSION="8.30"
 PKG_URL="http://ftpmirror.gnu.org/coreutils/coreutils-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain acl libcap gmp pcre readline openssl"
+PKG_DEPENDS_TARGET="acl libcap gmp pcre readline openssl"
 PKG_TOOLCHAIN="configure"
-
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -O3 -fdata-sections -ffat-lto-objects -ffunction-sections -flto=4 -fno-semantic-interposition "
-  export CXXFLAGS="$CXXFLAGS -O3 -fdata-sections -ffat-lto-objects -ffunction-sections -flto=4 -fno-semantic-interposition "
-}
 
 PKG_CONFIGURE_OPTS_TARGET="PERL=missing \
 			      MAKEINFO=missing \
