5e1c1c6709bf3dde6cfd17fbb73220314dcdfc7d
AV <27rurk@gmail.com>
2018-08-09 12:24:47 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv) elfutils
 
 PKG_NAME="glib"
 PKG_VERSION="2.57.2"
@@ -9,7 +9,7 @@ PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://ftp.gnome.org/pub/gnome/sources/glib/?C=M;O=D"
 PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/${PKG_VERSION%.*}/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain zlib libffi pcre elfutils Python2:host util-linux"
+PKG_DEPENDS_TARGET="toolchain zlib libffi pcre Python2:host util-linux"
 PKG_DEPENDS_HOST="libffi:host"
 PKG_SECTION="devel"
 PKG_SHORTDESC="glib: C support library"
@@ -31,9 +31,9 @@ PKG_MESON_OPTS_TARGET="-Dselinux=false \
 			  -Dman=false \
 			  -Dgtk_doc=false"
 
-pre_configure_target() {
-  LDFLAGS="$LDFLAGS -lz"
-}
+#pre_configure_target() {
+#  LDFLAGS="$LDFLAGS -lz"
+#}
 
 post_makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
