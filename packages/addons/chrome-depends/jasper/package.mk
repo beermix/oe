b507cbffa81671028f04068f252348093a495548
AV <27rurk@gmail.com>
2018-08-09 08:45:59 +0300

    autocommit on change

--- packages/addons/chrome-depends/jasper/package.mk
+++ packages/addons/chrome-depends/jasper/package.mk
@@ -25,9 +25,10 @@ PKG_SECTION="graphics"
 PKG_SHORTDESC="jasper: JPEG-2000 Part-1 standard (i.e., ISO/IEC 15444-1) implementation"
 PKG_LONGDESC="This distribution contains the public release of the an open-source implementation of the ISO/IEC 15444-1 also known as JPEG-2000 standard for image compression."
 PKG_TOOLCHAIN="autotools"
-#PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --with-pic"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
+			      --enable-static \
+			      --with-pic"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
