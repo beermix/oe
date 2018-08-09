74278b66c2f29fa831717cefd8147618d091e54b
AV <27rurk@gmail.com>
2018-08-09 06:04:18 +0300

    autocommit on change

--- packages/-my/coreutils/package.mk
+++ packages/-my/coreutils/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="coreutils"
-PKG_VERSION="8.29"
+PKG_VERSION="8.30"
 PKG_URL="http://ftpmirror.gnu.org/coreutils/coreutils-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain acl libcap gmp pcre readline openssl"
 PKG_TOOLCHAIN="configure"
