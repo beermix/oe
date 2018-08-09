21d7a7f18a32c3c0650356d73ee4b580c6b9771e
AV <27rurk@gmail.com>
2018-08-09 06:18:21 +0300

    autocommit on change

--- packages/devel/pkgconf/package.mk
+++ packages/devel/pkgconf/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="pkgconf"
 PKG_VERSION="1.5.3"
 PKG_URL="https://github.com/pkgconf/pkgconf/archive/pkgconf-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host gettext:host autoconf:host automake:host"
+PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host"
 PKG_SOURCE_DIR="pkgconf-pkgconf-$PKG_VERSION*"
 PKG_SECTION="toolchain/devel"
 PKG_IS_ADDON="no"
