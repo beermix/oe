628f9e4292369248bb18610a3e041cb1e80ee9b7
AV <27rurk@gmail.com>
2018-08-10 13:50:02 +0300

    autocommit on change

--- packages/lang/clang/package.mk
+++ packages/lang/clang/package.mk
@@ -6,24 +6,28 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
 PKG_URL="http://releases.llvm.org/$PKG_VERSION/cfe-$PKG_VERSION.src.tar.xz"
 PKG_SOURCE_DIR="cfe-$PKG_VERSION*"
-#PKG_DEPENDS_TARGET="clang:host"
-#PKG_DEPENDS_HOST="llvm:host"
+PKG_DEPENDS_HOST="llvm:host"
 
 configure_host() {
   cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
     -DPYTHON_EXECUTABLE="$TOOLCHAIN/bin/python2" \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DLLVM_LINK_LLVM_DYLIB=OFF \
-    -DLLVM_ENABLE_RTTI=ON \
-    -DLLVM_BUILD_TESTS=OFF \
-    -DLLVM_INCLUDE_DOCS=OFF \
-    -DLLVM_EXTERNAL_LIT=/usr/bin/lit \
-    -DLLVM_MAIN_SRC_DIR="$(get_build_dir llvm)" \
+    -DLLVM_LINK_LLVM_DYLIB=ON \
+    -DLLVM_DYLIB_COMPONENTS=all \
+    -DCLANG_BUILD_EXAMPLES=OFF \
+    -DCLANG_INCLUDE_DOCS=OFF \
+    -DCLANG_INCLUDE_TESTS=OFF \
+    -DCLANG_BUILD_TOOLS=ON \
+    -DLLVM_CONFIG:FILEPATH="$TOOLCHAIN/bin/llvm-config" \
     ..
 }
-
+	
 make_host() {
-  ninja
+  ninja -j${CONCURRENCY_MAKE_LEVEL}
+}
+
+post_makeinstall_host() {
+  ln -sf clang $TOOLCHAIN/bin/x86_64-libreelec-linux-gnu-clang
+  ln -sf clang $TOOLCHAIN/bin/x86_64-libreelec-linux-gnu-clang++
 }
