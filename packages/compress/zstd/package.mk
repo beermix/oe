1d5be3b4e45cae9025d2a7a9d941876f05f18450
AV <27rurk@gmail.com>
2018-08-16 09:40:55 +0300

    autocommit on change

--- packages/compress/zstd/package.mk
+++ packages/compress/zstd/package.mk
@@ -1,21 +1,3 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2017-present Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
-
 PKG_NAME="zstd"
 PKG_VERSION="1.3.5"
 PKG_SHA256="d6e1559e4cdb7c4226767d4ddc990bff5f9aab77085ff0d0490c828b025e2eea"
@@ -32,6 +14,6 @@ PKG_BUILD_FLAGS="+pic:host +pic"
 
 PKG_CMAKE_SCRIPT="$PKG_BUILD/build/cmake/CMakeLists.txt"
 
-PKG_CMAKE_OPTS_HOST="-DZSTD_MULTITHREAD_SUPPORT=0 -DZSTD_BUILD_SHARED=0"
+PKG_CMAKE_OPTS_HOST="-DZSTD_MULTITHREAD_SUPPORT=1 -DZSTD_BUILD_SHARED=0"
 
 PKG_CMAKE_OPTS_TARGET="-DZSTD_MULTITHREAD_SUPPORT=1 -DZSTD_BUILD_PROGRAMS=0 -DZSTD_BUILD_SHARED=0"
