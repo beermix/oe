8ce93a4c8947f9c4fe85bceae0979d0f820a268d
AV <27rurk@gmail.com>
2018-08-16 07:34:41 +0300

    autocommit on change

--- packages/lang/clang/package.mk
+++ packages/lang/clang/package.mk
@@ -1,14 +1,14 @@
 PKG_NAME="clang"
-PKG_VERSION="547357a"
+PKG_VERSION="bdb1c36"
 #PKG_SHA256="7c243f1485bddfdfedada3cd402ff4792ea82362ff91fbdac2dae67c6026b667"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
-PKG_SITE="http://llvm.org/"
+PKG_SITE="https://github.com/llvm-mirror/clang/tree/release_70"
 PKG_URL="http://releases.llvm.org/$PKG_VERSION/cfe-$PKG_VERSION.src.tar.xz"
 PKG_URL="https://github.com/llvm-mirror/clang/archive/${PKG_VERSION}.tar.gz"
 PKG_SOURCE_DIR="cfe-$PKG_VERSION*"
 PKG_SOURCE_DIR="clang-$PKG_VERSION*"
-PKG_DEPENDS_HOST="llvm:host"
+PKG_DEPENDS_HOST="llvm:host lld:host"
 
 configure_host() {
   cmake .. -G Ninja \
