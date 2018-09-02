a6b505f7122785165ea04cd375bfaba4d1dac4ca
AV <27rurk@gmail.com>
2018-09-03 02:42:31 +0300

    autocommit on change

--- packages/addons/chrome-depends/gtk3/package.mk
+++ packages/addons/chrome-depends/gtk3/package.mk
@@ -3,8 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gtk3"
-PKG_VERSION="3.22.30"
-PKG_SHA256="a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567"
+#PKG_VERSION="3.22.30"
+#PKG_SHA256="a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567"
+PKG_VERSION="3.23.3"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://ftp.acc.umu.se/pub/gnome/sources/gtk+/?C=M;O=D"
