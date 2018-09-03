c434eed2441d9a44d9aa258b06425a11aea9ef21
AV <27rurk@gmail.com>
2018-09-03 13:32:26 +0300

    autocommit on change

--- packages/graphics/cairo/package.mk
+++ packages/graphics/cairo/package.mk
@@ -13,7 +13,7 @@ PKG_SECTION="graphics"
 PKG_SHORTDESC="cairo: Multi-platform 2D graphics library"
 PKG_LONGDESC="Cairo is a vector graphics library with cross-device output support. Currently supported output targets include the X Window System and in-memory image buffers. PostScript and PDF file output is planned. Cairo is designed to produce identical output on all output media while taking advantage of display hardware acceleration when available."
 PKG_TOOLCHAIN="configure" # ToDo
-PKG_TOOLCHAIN="autotools"
+#PKG_TOOLCHAIN="autotools"
 
 if [ "$OPENGL" != "no" ]; then
   PKG_DEPENDS_TARGET+=" $OPENGL"
