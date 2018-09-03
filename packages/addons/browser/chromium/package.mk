719140eb4da14bfe0b554745064e88820d0de748
AV <27rurk@gmail.com>
2018-09-03 03:13:55 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -1,29 +1,26 @@
-#############################################################################################################################
-#  beautifulsoup4:host html5lib:host re2 snappy
+########################################################################################################################  beautifulsoup4:host html5lib:host re2 snappy
 #  
 #  https://chromereleases.googleblog.com/
 #  http://svnweb.mageia.org/packages/cauldron/chromium-browser-stable/current
 #  http://omahaproxy.appspot.com/
-#  https://www.chromestatus.com/
+#  https://www.chromestatus.com/ chromium68:host
 #  https://bazaar.launchpad.net/~chromium-team/chromium-browser/xenial-stable/files/head:/debian?sort=date
-#############################################################################################################################
-
-PKG_NAME="chromium"
-PKG_VERSION="64.0.3282.186"
-PKG_SHA256="5fd0218759231ac00cc729235823592f6fd1e4a00ff64780a5fed7ab210f1860"
-PKG_REV="186"
+#  https://src.fedoraproject.org/rpms/chromium/commits/master
+#######################################################################################################################
+PKG_NAME="chromium68"
+PKG_VERSION="68.0.3440.106"
+PKG_SHA256="7021040635a0a0d47f699bdb22e3ef5c91482e4f51b428d1de3016da95f0e698"
+PKG_REV="400"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Mixed"
 PKG_URL="https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$PKG_VERSION.tar.xz"
-PKG_URL="https://gsdview.appspot.com/chromium-browser-official/chromium-$PKG_VERSION.tar.xz"
-#PKG_DEPENDS_HOST="toolchain ninja:host Python2:host"
-#PKG_DEPENDS_TARGET="pciutils systemd dbus libXtst libXcomposite libXcursor unclutter alsa-lib yasm nss libXScrnSaver libexif libpng atk xdotool libdrm libjpeg-turbo freetype libxslt harfbuzz gtk3 libxss chromium:host"
-PKG_DEPENDS_TARGET="chromium:host"
+PKG_SOURCE_DIR="chromium-$PKG_VERSION*"
+PKG_DEPENDS_HOST="toolchain ninja:host Python2:host"
+PKG_DEPENDS_TARGET="pciutils systemd dbus libXtst libXcomposite libXcursor unclutter alsa-lib bzip2 yasm nss libXScrnSaver libexif libpng atk xdotool libdrm libjpeg-turbo freetype libxslt harfbuzz gtk+ re2 snappy libxss chromium68:host"
 PKG_SECTION="browser"
-PKG_SHORTDESC="Chromium Browser: the open-source web browser from Google"
-PKG_LONGDESC="Chromium Browser ($PKG_VERSION): the open-source web browser from Google"
 PKG_TOOLCHAIN="manual"
 PKG_BUILD_FLAGS="-lto -hardening"
+GOLD_SUPPORT="yes"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Chromium"
@@ -31,26 +28,39 @@ PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_PROVIDES="executable"
 
 post_patch() {
-  cd $(get_build_dir chromium)
+  cd $(get_build_dir chromium68)
 
   # Use Python 2
   find . -name '*.py' -exec sed -i -r "s|/usr/bin/python$|$TOOLCHAIN/bin/python|g" {} +
-
   # set correct widevine
   sed -i -e 's/@WIDEVINE_VERSION@/Pinkie Pie/' ./third_party/widevine/cdm/stub/widevine_cdm_version.h
 }
 
 make_host() {
-  # export CCACHE_SLOPPINESS=file_macro
-  ./tools/gn/bootstrap/bootstrap.py --no-rebuild --no-clean
+  export CCACHE_SLOPPINESS=time_macros
+  ./tools/gn/bootstrap/bootstrap.py -s --no-rebuild --no-clean
 }
 
 make_target() {
-  # export CCACHE_SLOPPINESS=file_macro
+  export CCACHE_SLOPPINESS=time_macros
 
   local _google_api_key=AIzaSyAQ6L9vt9cnN4nM0weaa6Y38K4eyPvtKgI
   local _google_default_client_id=740889307901-4bkm4e0udppnp1lradko85qsbnmkfq3b.apps.googleusercontent.com
   local _google_default_client_secret=9TJlhL661hvShQub4cWhANXa
+
+  sed -i 's/OFFICIAL_BUILD/GOOGLE_CHROME_BUILD/' \
+    ./tools/generate_shim_headers/generate_shim_headers.py
+
+  sed -i \
+    -e '/"-Wno-ignored-pragma-optimize"/d' \
+    ./build/config/compiler/BUILD.gn
+
+#  sed -i '1s|python$|&2|' ./third_party/dom_distiller_js/protoc_plugins/*.py.
+
+#  export CC="ccache x86_64-libreelec-linux-gnu-clang"
+#  export CXX="ccache x86_64-libreelec-linux-gnu-clang++"
+#  export AR=ar
+#  export NM=nm
 
   local _flags=(
     "host_toolchain=\"//build/toolchain/linux:x64_host\""
@@ -64,34 +74,30 @@ make_target() {
     'remove_webcore_debug_symbols=true'
     'ffmpeg_branding="Chrome"'
     'proprietary_codecs=true'
+    'is_component_build=false'
     'link_pulseaudio=true'
     'linux_use_bundled_binutils=false'
     'use_allocator="none"'
     'use_cups=false'
     'use_custom_libcxx=false'
-    'use_gconf=false'
     'use_gnome_keyring=false'
     'use_gold=false'
-    'use_gtk3=true'
+    'use_gtk3=false'
+    'use_dbus=true'
     'use_kerberos=false'
     'use_pulseaudio=false'
+    'linux_link_libudev=true'
+    'use_dbus=true'
+    'use_cups=false'
+    'use_system_libjpeg=true'
+    'use_system_libpng=false'
+    'use_system_libdrm=true'
+    'use_system_harfbuzz=true'
+    'use_system_freetype=true'
     'use_sysroot=true'
     'use_vaapi=true'
-    'use_dbus=true'
-    'use_gio=true'
-    'use_libpci=true'
-    'use_udev=true'
-    'use_system_zlib=true'
-    'use_system_freetype=true'
-    'use_system_libdrm=true'
-    'use_system_libpng=false'
-    'use_system_harfbuzz=true'
-    'linux_link_libudev=true'
-    'use_system_libjpeg=true'
-    'icu_use_data_file=true'
-    'enable_remoting=false'
-    'use_v8_context_snapshot=false'
     'enable_vulkan=false'
+    'use_v8_context_snapshot=false'
     "target_sysroot=\"${SYSROOT_PREFIX}\""
     'enable_hangout_services_extension=true'
     'enable_widevine=true'
@@ -104,28 +110,64 @@ make_target() {
     "google_default_client_secret=\"${_google_default_client_secret}\""
   )
 
+# Possible replacements are listed in build/linux/unbundle/replace_gn_files.py
+# Keys are the names in the above script; values are the dependencies in Arch
+declare -gA _system_libs=(
+  [fontconfig]=fontconfig
+  [freetype]=freetype2
+  [harfbuzz-ng]=harfbuzz
+  [icu]=icu
+  [libdrm]=
+  [libjpeg]=libjpeg
+  [libxml]=libxml2
+  [libxslt]=libxslt
+  [re2]=re2
+  [snappy]=snappy
+  [yasm]=
+  [zlib]=minizip
+)
+_unwanted_bundled_libs=(
+  ${!_system_libs[@]}
+  ${_system_libs[libjpeg]+libjpeg_turbo}
+)
+
+  local _lib
+  for _lib in ${_unwanted_bundled_libs[@]}; do
+    find "third_party/$_lib" -type f \
+      \! -path "third_party/$_lib/chromium/*" \
+      \! -path "third_party/$_lib/google/*" \
+      \! -path 'third_party/yasm/run_yasm.py' \
+      \! -regex '.*\.\(gn\|gni\|isolate\)' \
+      -delete
+  done
+
+  ./build/linux/unbundle/replace_gn_files.py --system-libraries "${!_system_libs[@]}"
+
   ./third_party/libaddressinput/chromium/tools/update-strings.py
 
   ./out/Release/gn gen out/Release --args="${_flags[*]}" --script-executable=$TOOLCHAIN/bin/python
 
-  ninja -j${CONCURRENCY_MAKE_LEVEL} $NINJA_OPTS -C out/Release chrome chrome_sandbox widevinecdmadapter
+  ninja -j${CONCURRENCY_MAKE_LEVEL} $NINJA_OPTS -C out/Release chrome chrome_sandbox
 }
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
   cp -P  $PKG_BUILD/out/Release/chrome $ADDON_BUILD/$PKG_ADDON_ID/bin/chromium.bin
   cp -P  $PKG_BUILD/out/Release/chrome_sandbox $ADDON_BUILD/$PKG_ADDON_ID/bin/chrome-sandbox
-  cp -ri  $PKG_BUILD/out/Release/{*.pak,*.dat,*.bin,libwidevinecdmadapter.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -ri  $PKG_BUILD/out/Release/{*.pak,*.bin} $ADDON_BUILD/$PKG_ADDON_ID/bin
   cp -PR $PKG_BUILD/out/Release/locales $ADDON_BUILD/$PKG_ADDON_ID/bin/
   cp -PR $PKG_BUILD/out/Release/gen/content/content_resources.pak $ADDON_BUILD/$PKG_ADDON_ID/bin/
 
   # config
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config \
-           $ADDON_BUILD/$PKG_ADDON_ID/gdk-pixbuf-modules \
-           $ADDON_BUILD/$PKG_ADDON_ID/lib
-
-  # config
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config
   cp -P $PKG_DIR/config/* $ADDON_BUILD/$PKG_ADDON_ID/config
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
+
+  # pango
+  cp -PL $(get_build_dir pango)/.install_pkg/usr/lib/libpangocairo-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -PL $(get_build_dir pango)/.install_pkg/usr/lib/libpango-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -PL $(get_build_dir pango)/.install_pkg/usr/lib/libpangoft2-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
 
   # atk
   cp -PL $(get_build_dir atk)/.install_pkg/usr/lib/libatk-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
@@ -134,28 +176,20 @@ addon() {
   cp -PL $(get_build_dir cairo)/.install_pkg/usr/lib/libcairo-gobject.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib
   cp -PL $(get_build_dir cairo)/.install_pkg/usr/lib/libcairo.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib
 
+  # gtk
+  cp -PL $(get_build_dir gtk+)/.install_pkg/usr/lib/libgdk-x11-2.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -PL $(get_build_dir gtk+)/.install_pkg/usr/lib/libgtk-x11-2.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
+
+  # harfbuzz
+  cp -PL $(get_build_dir harfbuzz)/.install_pkg/usr/lib/libharfbuzz.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -PL $(get_build_dir harfbuzz)/.install_pkg/usr/lib/libharfbuzz-icu.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
+
   # gdk-pixbuf
   cp -PL $(get_build_dir gdk-pixbuf)/.install_pkg/usr/lib/libgdk_pixbuf-2.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
 
-  # gdk-pixbuf modules
+  # pixbuf loaders
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/gdk-pixbuf-modules
   cp -PL $(get_build_dir gdk-pixbuf)/.install_pkg/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders/* $ADDON_BUILD/$PKG_ADDON_ID/gdk-pixbuf-modules
-
-  # gtk+
-  # cp -ri $(get_build_dir gtk+)/.install_pkg/usr/lib/* $ADDON_BUILD/$PKG_ADDON_ID/lib
-
-  # gtk3
-  cp -PL $(get_build_dir gtk3)/.install_pkg/usr/lib/libgtk-3.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
-  cp -PL $(get_build_dir gtk3)/.install_pkg/usr/lib/libgdk-3.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
-
-  # harfbuzz
-  cp -PL $(get_build_dir harfbuzz)/.install_pkg/usr/lib/libharfbuzz.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
-  cp -PL $(get_build_dir harfbuzz)/.install_pkg/usr/lib/libharfbuzz-icu.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
-
-  # libatk-bridge
-  cp -PL $(get_build_dir at-spi2-atk)/.install_pkg/usr/lib/libatk-bridge-2.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib 
-
-  # libatspi
-  cp -PL $(get_build_dir at-spi2-core)/.install_pkg/usr/lib/libatspi.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib 
 
   # libXcursor
   cp -PL $(get_build_dir libXcursor)/.install_pkg/usr/lib/libXcursor.so.1 $ADDON_BUILD/$PKG_ADDON_ID/lib 
@@ -166,11 +200,6 @@ addon() {
   # libXtst
   cp -PL $(get_build_dir libXtst)/.install_pkg/usr/lib/libXtst.so.6 $ADDON_BUILD/$PKG_ADDON_ID/lib
 
-  # pango
-  cp -PL $(get_build_dir pango)/.install_pkg/usr/lib/libpangocairo-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
-  cp -PL $(get_build_dir pango)/.install_pkg/usr/lib/libpango-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
-  cp -PL $(get_build_dir pango)/.install_pkg/usr/lib/libpangoft2-1.0.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
-
   # unclutter
   cp -P $(get_build_dir unclutter)/.install_pkg/usr/bin/unclutter $ADDON_BUILD/$PKG_ADDON_ID/bin
 
@@ -179,7 +208,4 @@ addon() {
 
   # libXft
   cp -PL $(get_build_dir libXft)/.install_pkg/usr/lib/libXft.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib
-
-  # libxkbcommon
-  # cp -P $(get_build_dir libxkbcommon)/libxkbcommon.so.0 $ADDON_BUILD/$PKG_ADDON_ID/lib
 }
--- packages/addons/browser/chromium/source/default.py
+++ packages/addons/browser/chromium/source/default.py
@@ -1,21 +1,6 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2018-present Team LibreELEC
-#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 import os
 import sys
@@ -33,53 +18,6 @@ __path__  = os.path.join(__addon__.getAddonInfo('path'), 'bin') + '/'
 
 pauseXBMC = __addon__.getSetting("PAUSE_XBMC")
 
-# widevine and flash stuff
-def install_flash():
-  __url__   = 'https://fpdownload.adobe.com/pub/flashplayer/pdc/24.0.0.186/flash_player_ppapi_linux.x86_64.tar.gz'
-  __file__  = __url__.split('/')[-1]
-  __tmp__   = '/tmp/pepperflash/'
-  __lib__ = 'libpepflashplayer.so'
-  try:
-    if not os.path.isdir(__tmp__):
-      os.mkdir(__tmp__)
-    if not os.path.exists(__tmp__ + __file__):
-      oe.download_file(__url__, __tmp__ + __file__)
-    if not os.path.exists(__tmp__ + __file__):
-      oe.notify('Chromium', 'Could not download file')
-    else:
-      oe.notify('Chromium', 'Extracting libpepflashplayer.so')
-    if not os.path.isdir(__tmp__ + __file__):
-      oe.execute('tar zxf ' + __tmp__ + __file__ + ' -C ' + __tmp__ + '')
-    if not os.path.isdir(__path__ + 'PepperFlash'):
-      os.mkdir(__path__ + 'PepperFlash')
-    oe.copy_file(__tmp__ + __lib__, __path__ + 'PepperFlash/' + __lib__)
-    oe.notify('Chromium', 'Installation of libpepflashplayer.so succeeded')
-  except Exception, e:
-    oe.notify('Chromium', 'Installation of libpepflashplayer.so failed')
-
-def install_widevine():
-  __url__   = 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
-  __file__  = __url__.split('/')[-1]
-  __tar__   = 'data.tar.xz'
-  __tmp__   = '/tmp/widevine/'
-  __lib__   = 'opt/google/chrome/libwidevinecdm.so'
-  try:
-    if not os.path.isdir(__tmp__):
-      os.mkdir(__tmp__)
-    if not os.path.exists(__tmp__ + __file__):
-      oe.download_file(__url__, __tmp__ + __file__)
-    if not os.path.exists(__tmp__ + __file__):
-      oe.notify('Chromium', 'Could not download file')
-    else:
-      oe.notify('Chromium', 'Extracting libwidevinecdm.so')
-    if not os.path.isdir(__tmp__ + __tar__):
-      oe.execute('cd ' + __tmp__ + ' && ar -x ' + __file__)
-    oe.execute('tar xf ' + __tmp__ + __tar__ + ' -C ' + __tmp__ + ' ./' + __lib__)
-    oe.copy_file(__tmp__ + __lib__, __path__ + __lib__.split('/')[-1])
-    oe.notify('Chromium', 'Installation of libwidevinecdm.so succeeded')
-  except Exception, e:
-    oe.notify('Chromium', 'Installation of libwidevinecdm.so failed')
-
 def pauseXbmc():
   if pauseXBMC == "true":
     xbmc.executebuiltin("PlayerControl(Stop)")
@@ -92,21 +30,57 @@ def resumeXbmc():
     xbmc.enableNavSounds(True)
 
 def startChromium(args):
-  try:
-    new_env = os.environ.copy()
-    new_env['VAAPI_MODE'] = __addon__.getSetting('VAAPI_MODE')
-    new_env['WINDOW_MODE'] = __addon__.getSetting('WINDOW_MODE')
-    new_env['RASTER_MODE'] = __addon__.getSetting('RASTER_MODE')
+  oe.execute('chmod +x ' + __path__ + 'chromium')
+  oe.execute('chmod +x ' + __path__ + 'chromium.bin')
+  oe.execute('chmod 4755 ' + __path__ + 'chrome-sandbox')
 
-    new_env['ALSA_DEVICE'] = ''
+  try:
+    window_mode = {
+      'maximized': '--start-maximized',
+      'kiosk': '--kiosk',
+      'none': '',
+    }
+
+    raster_mode = {
+      'default': '',
+      'off': '--disable-accelerated-2d-canvas --disable-gpu-compositing',
+      'force': '--enable-gpu-rasterization --enable-accelerated-2d-canvas --ignore-gpu-blacklist',
+    }
+
+    new_env = os.environ.copy()
+    vaapi_mode = __addon__.getSetting('VAAPI_MODE')
+    gpu_accel_mode = ''
+    if vaapi_mode == 'intel':
+      new_env['LIBVA_DRIVERS_PATH'] = '/usr/lib/va'
+      new_env['LIBVA_DRIVER_NAME'] = 'i965'
+    elif vaapi_mode == 'amd':
+      new_env['LIBVA_DRIVERS_PATH'] = os.path.join(__addon__.getAddonInfo('path'), 'lib')
+      new_env['LIBVA_DRIVER_NAME'] = 'vdpau'
+    elif vaapi_mode == 'nvidia':
+      new_env['LIBVA_DRIVERS_PATH'] = os.path.join(__addon__.getAddonInfo('path'), 'lib')
+      new_env['LIBVA_DRIVER_NAME'] = 'vdpau'
+      gpu_accel_mode = '--allow-no-sandbox-job --disable-gpu-sandbox'
+    else:
+      new_env['LIBGL_ALWAYS_SOFTWARE'] = '1'
+
+    flash_plugin = ''
+    if os.path.exists(__path__ + 'PepperFlash/libpepflashplayer.so'):
+      flash_plugin = '--ppapi-flash-path=' + __path__ + 'PepperFlash/libpepflashplayer.so'
+
     if __addon__.getSetting('USE_CUST_AUDIODEVICE') == 'true':
       alsa_device = __addon__.getSetting('CUST_AUDIODEVICE_STR')
     else:
       alsa_device = getAudioDevice()
+    alsa_param = ''
     if not alsa_device == None and not alsa_device == '':
-      new_env['ALSA_DEVICE'] = alsa_device
+      alsa_param = '--alsa-output-device=' + alsa_device
 
-    chrome_params = args + ' ' + \
+    chrome_params = window_mode.get(__addon__.getSetting('WINDOW_MODE')) + ' ' + \
+                    raster_mode.get(__addon__.getSetting('RASTER_MODE')) + ' ' + \
+                    flash_plugin + ' ' + \
+                    gpu_accel_mode + ' ' + \
+                    alsa_param + ' ' + \
+                    args + ' ' + \
                     __addon__.getSetting('HOMEPAGE')
     subprocess.call(__path__ + 'chromium ' + chrome_params, shell=True, env=new_env)
   except Exception, e:
