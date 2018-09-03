cad7540e5bfc203b884bbf7ee795997ab5e7a410
AV <27rurk@gmail.com>
2018-09-03 12:12:35 +0300

    autocommit on change

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -92,7 +92,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
 
 pre_configure_target() {
   # export CFLAGS="$CFLAGS -fno-schedule-insns -fno-schedule-insns2 -Wno-format-truncation"
-  export CFLAGS="$CFLAGS -g -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -Wformat -Wformat-security -Wno-error   -Wl,-z,max-page-size=0x1000 -m64"
+  export CFLAGS="$CFLAGS -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=32 -Wformat -Wformat-security -Wno-error   -Wl,-z,max-page-size=0x1000 -m64"
   export LC_ALL=en_US.UTF-8
 }
 
