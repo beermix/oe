bd903c00eeed8924fc1d1dd600d02eda456f1dda
AV <27rurk@gmail.com>
2018-09-06 02:44:04 +0300

    autocommit on change

--- packages/-my/grep/package.mk
+++ packages/-my/grep/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="grep"
 PKG_VERSION="3.1"
 PKG_URL="http://ftp.gnu.org/gnu/grep/grep-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain readline pcre libcap"
+PKG_DEPENDS_TARGET="toolchain readline pcre"
 PKG_SECTION="sysutils"
 #PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-perl-regexp"
+PKG_CONFIGURE_OPTS_TARGET="--enable-perl-regexp"
