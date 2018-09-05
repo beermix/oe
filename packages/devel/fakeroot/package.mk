a69bdaa3904a02d7bea4b294c0d164d4bba6d3fb
AV <27rurk@gmail.com>
2018-09-05 23:17:47 +0300

    autocommit on change

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -17,4 +17,4 @@ PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="fakeroot: provides a fake root environment by means of LD_PRELOAD and SYSV IPC (or TCP) trickery."
 PKG_LONGDESC="fakeroot provides a fake root environment by means of LD_PRELOAD and SYSV IPC (or TCP) trickery."
 
-PKG_CONFIGURE_OPTS_HOST="--with-ipc=sysv"
+PKG_CONFIGURE_OPTS_HOST="--with-ipc=sysv --with-gnu-ld"
