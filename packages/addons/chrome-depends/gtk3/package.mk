4c9e664ecdffe486c1b6fbb322da16db1d5b0fba
AV <27rurk@gmail.com>
2018-09-05 20:01:53 +0300

    autocommit on change

--- packages/addons/chrome-depends/gtk3/package.mk
+++ packages/addons/chrome-depends/gtk3/package.mk
@@ -3,9 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gtk3"
-#PKG_VERSION="3.22.30"
-#PKG_SHA256="a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567"
 PKG_VERSION="3.24.0"
+PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://ftp.acc.umu.se/pub/gnome/sources/gtk+/?C=M;O=D"
@@ -13,33 +12,25 @@ PKG_URL="https://ftp.gnome.org/pub/gnome/sources/gtk+/${PKG_VERSION:0:4}/gtk+-$P
 PKG_SOURCE_DIR="gtk+-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain glib:host at-spi2-atk atk cairo gdk-pixbuf glib libX11 libXi libXrandr libepoxy pango"
 PKG_LONGDESC="The Gimp ToolKit (GTK) is a library for creating graphical user interfaces for the X Window System."
-PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-x \
-                           --x-includes=$SYSROOT/usr/include/X11 \
-                           --x-libraries=$SYSROOT/usr/lib \
-                           --with-gdktarget=x11 \
-                           --enable-modules \
-                           --enable-explicit-deps=no \
+PKG_CONFIGURE_OPTS_TARGET="--disable-cups \
                            --disable-debug \
-                           --enable-shm \
-                           --disable-cups \
-                           --disable-schemas-compile \
-                           --enable-x11-backend \
-                           --enable-xkb \
+                           --enable-explicit-deps=no \
+                           --disable-glibtest \
+                           --disable-gtk-doc \
                            --disable-gtk-doc-html \
-                           --with-xinput \
-                           --enable-silent-rules"
+                           --disable-man \
+                           --enable-modules \
+                           --disable-papi \
+                           --disable-xinerama \
+                           --enable-xkb"
 
-#pre_configure_target() {
-#  LIBS="$LIBS -lXcursor"
-#}
-
-#make_target() {
-#  make SRC_SUBDIRS="gdk gtk modules"
-#  $MAKEINSTALL SRC_SUBDIRS="gdk gtk modules"
-#}
+pre_configure_target() {
+  LIBS="$LIBS -lXcursor"
+#  export PKG_CONFIG_PATH="$(get_build_dir pango)/.$TARGET_NAME"
+#  export CFLAGS="$CFLAGS -I$(get_build_dir pango) -L$(get_build_dir pango)/.$TARGET_NAME/pango"
+}
 
 #makeinstall_target() {
-#  make install DESTDIR=$INSTALL SRC_SUBDIRS="gdk gtk modules"
+#  :
 #}
