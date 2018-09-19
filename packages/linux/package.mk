e6e41cf3231221b3810c4a4068fa295aa397492a
AV <27rurk@gmail.com>
2018-09-20 00:17:48 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -47,9 +47,9 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   *)
-    PKG_VERSION="4.18.8"
+    PKG_VERSION="4.19-rc4"
     PKG_SHA256=""
-    PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
+    PKG_URL="https://git.kernel.org/torvalds/t/linux-4.19-rc4.tar.gz"
     PKG_PATCH_DIRS="default"
     PKG_BUILD_PERF="no"
     ;;
