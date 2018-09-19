93911e19b2e93aed7afb1dcc09e6205198db74a8
AV <27rurk@gmail.com>
2018-09-19 21:37:17 +0300

    autocommit on change

--- packages/graphics/libjpeg-turbo/package - копия.mk
+++ packages/graphics/libjpeg-turbo/package - копия.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="libjpeg-turbo"
-PKG_VERSION="2.0.0"
-PKG_SHA256="778876105d0d316203c928fd2a0374c8c01f755d0a00b12a1c8934aeccff8868"
+PKG_VERSION="1.5.3"
+PKG_SHA256="b24890e2bb46e12e72a79f7e965f409f4e16466d00e1dd15d93d73ee6b592523"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libjpeg-turbo.virtualgl.org/"
@@ -13,34 +13,19 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libjpeg-turbo: a high-speed version of libjpeg for x86 and x86-64 processors which uses SIMD instructions (MMX, SSE2, etc.) to accelerate baseline JPEG compression and decompression."
 PKG_LONGDESC="libjpeg-turbo is a high-speed version of libjpeg for x86 and x86-64 processors which uses SIMD instructions (MMX, SSE2, etc.) to accelerate baseline JPEG compression and decompression. libjpeg-turbo is generally 2-4x as fast as the unmodified version of libjpeg, all else being equal."
-PKG_TOOLCHAIN="cmake-make"
-PKG_BUILD_FLAGS="+pic:host"
+PKG_TOOLCHAIN="configure"
+PKG_BUILD_FLAGS="+pic +pic:host"
 
-configure_host() {
-  cmake -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
-  	 -DWITH_JPEG8=ON \
-  	 -DENABLE_STATIC=ON \
-        -DENABLE_SHARED=OFF \
-        -DWITH_SIMD=ON \
-  	 ..
-}
+PKG_CONFIGURE_OPTS_HOST="--enable-static \
+                         --disable-shared \
+                         --with-jpeg8 \
+                         --without-simd"
 
-configure_target() {
-  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        -DWITH_JPEG8=ON \
-        -DENABLE_STATIC=ON \
-        -DENABLE_SHARED=ON \
-        -DWITH_SIMD=ON \
-        ..
-}
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --enable-shared --with-jpeg8"
 
-post_makeinstall_host() {
-  mv -f $TOOLCHAIN/lib64/pkgconfig/* $TOOLCHAIN/lib/pkgconfig/
-  rm -rf $TOOLCHAIN/lib64/pkgconfig
-  mv -f $TOOLCHAIN/lib64/* $TOOLCHAIN/lib/
-  rm -rf $TOOLCHAIN/lib64
-}
+if ! target_has_feature "(neon|sse)"; then
+  PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_TARGET --without-simd"
+fi
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
