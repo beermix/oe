18858615c62cb33cfc32a0267185fb7d5868e596
AV <27rurk@gmail.com>
2018-09-19 15:01:30 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -3,12 +3,13 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv) elfutils
 
 PKG_NAME="glib"
-PKG_VERSION="2.58.0"
-PKG_SHA256="c0f4ce0730b4f95c47b711613b5406a887c2ee13ea6d25930d72a4fa7fdb77f6"
+PKG_VERSION="2a7d4d2"
+#PKG_SHA256="c0f4ce0730b4f95c47b711613b5406a887c2ee13ea6d25930d72a4fa7fdb77f6"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://ftp.gnome.org/pub/gnome/sources/glib/?C=M;O=D"
 PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/${PKG_VERSION%.*}/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://github.com/GNOME/glib/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib libffi pcre Python2:host util-linux"
 PKG_DEPENDS_HOST="libffi:host"
 PKG_SECTION="devel"
