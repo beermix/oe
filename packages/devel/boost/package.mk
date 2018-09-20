162a30fef924f6844e29ca80ebbfe69ab2816946
AV <27rurk@gmail.com>
2018-09-21 00:57:34 +0300

    autocommit on change

--- packages/devel/boost/package.mk
+++ packages/devel/boost/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="boost"
-PKG_VERSION="1_66_0"
-PKG_SHA256="5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad75819225ca9"
+PKG_VERSION="1_68_0"
+PKG_SHA256=""
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.boost.org/"
-PKG_URL="$SOURCEFORGE_SRC/boost/boost/1.66.0/${PKG_NAME}_${PKG_VERSION}.tar.bz2"
+PKG_URL="$SOURCEFORGE_SRC/boost/boost/1.68.0/${PKG_NAME}_${PKG_VERSION}.tar.bz2"
 PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain boost:host Python2 zlib bzip2"
 PKG_LONGDESC="boost: Peer-reviewed STL style libraries for C++"
