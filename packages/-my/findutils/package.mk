a2f47d4c7bafcbea85a9542e394de42413cbaab5
AV <27rurk@gmail.com>
2018-09-20 03:55:38 +0300

    autocommit on change

--- packages/-my/findutils/package.mk
+++ packages/-my/findutils/package.mk
@@ -5,5 +5,10 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="sysutils"
 #PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--without-selinux --disable-debug"
+PKG_CONFIGURE_OPTS_TARGET="gl_cv_func_stdin=yes \
+			      ac_cv_func_working_mktime=yes \
+			      gl_cv_func_wcwidth_works=yes \
+			      --without-selinux \
+			      --disable-debug"
+
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
