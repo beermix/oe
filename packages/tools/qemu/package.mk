6588f83e639f922e4a0a2e35ac7e2f37fef1c6a6
AV <27rurk@gmail.com>
2018-09-20 21:11:57 +0300

    autocommit on change

--- packages/tools/qemu/package.mk
+++ packages/tools/qemu/package.mk
@@ -3,29 +3,27 @@
 
 PKG_NAME="qemu"
 PKG_VERSION="2.12.0"
-#PKG_SHA256="33583800e0006cd00b78226b85be5a27c8e3b156bed2e60e83ecbeb7b9b8364f"
+PKG_SHA256="e69301f361ff65bf5dabd8a19196aeaa5613c1b5ae1678f0823bdf50e7d5c6fc"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://wiki.qemu.org"
 PKG_URL="https://download.qemu.org/qemu-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="toolchain glib:host pixman:host Python2:host zlib:host libgcrypt"
-PKG_SECTION="tools"
-PKG_SHORTDESC="QEMU is a generic and open source machine emulator and virtualizer."
+PKG_DEPENDS_HOST="toolchain glib:host pixman:host Python2:host zlib:host"
 PKG_LONGDESC="QEMU is a generic and open source machine emulator and virtualizer."
 
-HOST_CONFIGURE_OPTS="--prefix=$TOOLCHAIN \
-  --bindir=$TOOLCHAIN/bin \
-  --sbindir=$TOOLCHAIN/sbin \
-  --sysconfdir=$TOOLCHAIN/etc \
-  --libexecdir=$TOOLCHAIN/lib \
-  --localstatedir=$TOOLCHAIN/var \
+HOST_CONFIGURE_OPTS="--bindir=$TOOLCHAIN/bin \
   --extra-cflags=-I$TOOLCHAIN/include \
   --extra-ldflags=-L$TOOLCHAIN/lib \
+  --libexecdir=$TOOLCHAIN/lib \
+  --localstatedir=$TOOLCHAIN/var \
+  --prefix=$TOOLCHAIN \
+  --sbindir=$TOOLCHAIN/sbin \
   --static \
-  --disable-vnc \
-  --disable-werror \
+  --sysconfdir=$TOOLCHAIN/etc \
   --disable-blobs \
+  --disable-docs \
+  --disable-gcrypt \
   --disable-system \
   --disable-user \
-  --disable-docs \
-  --disable-gcrypt"
+  --disable-vnc \
+  --disable-werror"
