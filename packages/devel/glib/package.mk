db98fc6dd5599a8a22d8b7b34e76c20effb261b0
AV <27rurk@gmail.com>
2018-09-20 05:19:43 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv) elfutils
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glib"
 PKG_VERSION="2a7d4d2"
@@ -10,7 +10,7 @@ PKG_LICENSE="LGPL"
 PKG_SITE="http://ftp.gnome.org/pub/gnome/sources/glib/?C=M;O=D" # https://github.com/GNOME/glib/tree/glib-2-58
 PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/${PKG_VERSION%.*}/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://github.com/GNOME/glib/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain zlib libffi pcre Python2:host util-linux"
+PKG_DEPENDS_TARGET="toolchain zlib libffi elfutils pcre Python2:host util-linux"
 PKG_DEPENDS_HOST="libffi:host"
 PKG_SECTION="devel"
 PKG_SHORTDESC="glib: C support library"
@@ -19,16 +19,19 @@ PKG_LONGDESC="GLib is a library which includes support routines for C such as li
 PKG_MESON_OPTS_HOST="-Dselinux=false \
 			-Dlibmount=false \
 			-Dman=false \
+			-Dgtk_doc=false \
+			-Dinternal_pcre=true \
 			-Ddefault_library=static \
 			-Dgtk_doc=false"
 
 PKG_MESON_OPTS_TARGET="-Dselinux=false \
-			  -Dman=false \
 			  -Dlibmount=false \
+			  -Dman=false \
+			  -Dgtk_doc=false \
+			  -Dinternal_pcre=false \
 			  -Dbsymbolic_functions=true \
 			  -Dforce_posix_threads=true \
-			  -Ddefault_library=shared \
-			  -Dgtk_doc=false"
+			  -Ddefault_library=shared"
 
 post_makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
