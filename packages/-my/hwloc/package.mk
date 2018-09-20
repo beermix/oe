977062e1ceefcbdc7befe1fb7c85ef312e95dffa
AV <27rurk@gmail.com>
2018-09-20 08:53:30 +0300

    autocommit on change

--- packages/-my/hwloc/package.mk
+++ packages/-my/hwloc/package.mk
@@ -1,6 +1,7 @@
 PKG_NAME="hwloc"
-PKG_VERSION="1.11.10"
+PKG_VERSION="2.0.1"
 PKG_URL="https://www.open-mpi.org/software/hwloc/v1.11/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib libpciaccess libtool"
 PKG_TOOLCHAIN="configure"
 
