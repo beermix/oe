ca56ceb1c43e14f689f7eb7d98b2e3ff5baa92e1
AV <27rurk@gmail.com>
2018-09-21 02:55:32 +0300

    autocommit on change

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -203,7 +203,6 @@ PKG_CMAKE_OPTS_TARGET="-DNATIVEPREFIX=$TOOLCHAIN \
                        -DWITH_TEXTUREPACKER=$TOOLCHAIN/bin/TexturePacker \
                        -DDEPENDS_PATH=$PKG_BUILD/depends \
                        -DPYTHON_EXECUTABLE=$TOOLCHAIN/bin/$PKG_PYTHON_VERSION \
-                       -DPYTHON_INCLUDE_DIRS=$SYSROOT_PREFIX/usr/include/$PKG_PYTHON_VERSION \
                        -DGIT_VERSION=$PKG_VERSION \
                        -DENABLE_INTERNAL_FFMPEG=OFF \
                        -DFFMPEG_INCLUDE_DIRS=$SYSROOT_PREFIX/usr \
@@ -252,10 +251,11 @@ PKG_CMAKE_OPTS_TARGET="-DNATIVEPREFIX=$TOOLCHAIN \
                        $KODI_BLURAY \
                        $KODI_PLAYER"
 
-#pre_configure_target() {
+pre_configure_target() {
 # kodi should never be built with lto
-#  export LIBS="$LIBS -lz -lterminfo"
-#}
+
+  export LIBS="$LIBS -lz -ltinfo"
+}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/kodi
