8114c69a3e9ab995eabca14ecf09d10575a2fdf8
AV <27rurk@gmail.com>
2018-09-20 12:11:15 +0300

    autocommit on change

--- packages/-my/libevent/package.mk
+++ packages/-my/libevent/package.mk
@@ -1,16 +1,20 @@
 PKG_NAME="libevent"
-PKG_VERSION="90ae4c5"
-PKG_SITE="https://github.com/libevent/libevent/releases/"
-#PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION/libevent-$PKG_VERSION.tar.gz"
-PKG_URL="https://github.com/libevent/libevent/archive/${PKG_VERSION}.tar.gz"
+PKG_VERSION="2.1.8-stable"
+PKG_SHA256="316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d"
+PKG_LICENSE="BSD"
+PKG_SITE="http://libevent.org/"
+PKG_URL="https://github.com/libevent/$PKG_NAME/archive/release-$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="$PKG_NAME-release-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
-PKG_DEPENDS_HOST="openssl:host zlib:host"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress --disable-shared --enable-openssl --with-pic"
-PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
+PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress \
+                           --disable-samples \
+                           --disable-shared \
+                           --enable-static \
+                           --enable-openssl"
 
 post_makeinstall_target() {
-  rm -fr $INSTALL
+  rm -fr "$INSTALL"
 }
