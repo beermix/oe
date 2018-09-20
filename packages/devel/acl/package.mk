7735778108d9f84454b14076b35de629a5ed2922
AV <27rurk@gmail.com>
2018-09-20 03:40:51 +0300

    autocommit on change

--- packages/devel/acl/package.mk
+++ packages/devel/acl/package.mk
@@ -1,6 +1,7 @@
 PKG_NAME="acl"
 PKG_VERSION="2.2.53"
-PKG_URL="http://git.savannah.nongnu.org/cgit/acl.git/snapshot/acl-$PKG_VERSION.tar.gz"
+PKG_SHA256="06be9865c6f418d851ff4494e12406568353b891ffe1f596b34693c387af26c7"
+PKG_URL="https://mirrors.up.pt/pub/nongnu/acl/acl-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain attr"
 PKG_DEPENDS_HOST="toolchain attr:host"
 PKG_SECTION="tools"
