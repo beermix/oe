ad72ca61ddd16d5743f1e68172e8eb6b7676a037
AV <27rurk@gmail.com>
2018-09-20 22:19:50 +0300

    autocommit on change

--- packages/compress/libarchive/package.mk
+++ packages/compress/libarchive/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 PKG_NAME="libarchive"
-PKG_VERSION="3.3.2"
-PKG_SHA256="ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce"
+PKG_VERSION="3.3.3"
+PKG_SHA256="ba7eb1781c9fbbae178c4c6bad1c6eb08edab9a1496c64833d1715d022b30e2e"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libarchive/libarchive/releases"
