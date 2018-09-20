24772751a83dd6a180a7572f12bc9e9adb7fd0f7
AV <27rurk@gmail.com>
2018-09-20 22:45:50 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -9,14 +9,15 @@ PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/libexpat/libexpat/releases"
 PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_2_2_6/expat-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
-#PKG_DEPENDS_HOST="cmake:host"
+PKG_DEPENDS_HOST="cmake:host"
 PKG_SECTION="textproc"
 PKG_SHORTDESC="expat: XML parser library"
 PKG_LONGDESC="Expat is an XML parser library written in C. It is a stream-oriented parser in which an application registers handlers for things the parser might find in the XML document (like start tags). An introductory article on using Expat is available on xml.com."
 PKG_TOOLCHAIN="configure"
-#PKG_TOOLCHAIN="cmake-make"
+PKG_TOOLCHAIN="cmake-make"
 
 PKG_CMAKE_OPTS_TARGET="-BUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
+
 PKG_CMAKE_OPTS_HOST="$PKG_CMAKE_OPTS_TARGET"
 
 post_makeinstall_target() {
