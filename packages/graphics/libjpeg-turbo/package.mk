cebbedf804b5ae5fed499270fc4e5b301cf1252d
AV <27rurk@gmail.com>
2018-09-21 07:39:56 +0300

    autocommit on change

--- packages/graphics/libjpeg-turbo/package.mk
+++ packages/graphics/libjpeg-turbo/package.mk
@@ -8,8 +8,8 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libjpeg-turbo.virtualgl.org/"
 PKG_URL="$SOURCEFORGE_SRC/libjpeg-turbo/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
-#PKG_DEPENDS_HOST="toolchain"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_HOST="nasm:host"
+PKG_DEPENDS_TARGET="toolchain nasm:host"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libjpeg-turbo: a high-speed version of libjpeg for x86 and x86-64 processors which uses SIMD instructions (MMX, SSE2, etc.) to accelerate baseline JPEG compression and decompression."
 PKG_LONGDESC="libjpeg-turbo is a high-speed version of libjpeg for x86 and x86-64 processors which uses SIMD instructions (MMX, SSE2, etc.) to accelerate baseline JPEG compression and decompression. libjpeg-turbo is generally 2-4x as fast as the unmodified version of libjpeg, all else being equal."
@@ -19,25 +19,13 @@ PKG_BUILD_FLAGS="+pic:host"
 configure_host() {
   cmake -DCMAKE_INSTALL_PREFIX:PATH=$TOOLCHAIN \
   	 -DWITH_JPEG8=ON \
-  	 -DENABLE_STATIC=ON \
         -DENABLE_SHARED=OFF \
         -DWITH_SIMD=OFF \
-        -DWITH_JAVA=0 \
         -DCMAKE_INSTALL_LIBDIR:PATH=lib \
   	 ..
 }
 
-configure_target() {
-  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX:PATH=/usr \
-        -DWITH_JPEG8=ON \
-        -DENABLE_STATIC=OFF \
-        -DENABLE_SHARED=ON \
-        -DWITH_SIMD=ON \
-        -DWITH_JAVA=0 \
-        -DCMAKE_BUILD_TYPE=Release \
-        ..
-}
+PKG_CMAKE_OPTS_TARGET="-DWITH_JPEG8=ON -DWITH_SIMD=ON"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
