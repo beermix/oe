f9c8c58be1c7ec2795f827dd965cc8f5d35adf33
AV <27rurk@gmail.com>
2018-09-21 21:51:29 +0300

    autocommit on change

--- packages/web/nghttp2/package.mk
+++ packages/web/nghttp2/package.mk
@@ -5,9 +5,13 @@ PKG_URL="https://github.com/nghttp2/nghttp2/releases/download/v$PKG_VERSION/nght
 PKG_DEPENDS_TARGET="toolchain libevent libev libuv"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="cmake-make"
-PKG_TOOLCHAIN="autotools"
+#PKG_TOOLCHAIN="autotools"
 
-PKG_CMAKE_OPTS_TARGET="-DENABLE_LIB_ONLY=1 -DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_TARGET="-DENABLE_LIB_ONLY=1 \
+			  -DENABLE_EXAMPLES=0 \
+			  -DENABLE_PYTHON_BINDINGS=0 \
+			  -DENABLE_THREADS=1 \
+			  -DCMAKE_BUILD_TYPE=Release"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-examples \
 			      --disable-python-bindings \
