ed2e29580403c77cbc5ee26d183dcd3ecff89f63
AV <27rurk@gmail.com>
2018-09-21 12:41:35 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -25,8 +25,8 @@ PKG_TOOLCHAIN="manual"
 
 make_host() {
   # CXX=/usr/bin/clang++ $TOOLCHAIN/bin/python2 ./configure.py --bootstrap --verbose.
-  # CXX=$TOOLCHAIN/bin/clang++ $TOOLCHAIN/bin/python2 ./configure.py --bootstrap --verbose
-  $TOOLCHAIN/bin/python2 ./configure.py --bootstrap
+  CXX=/bin/clang++ $TOOLCHAIN/bin/python2 ./configure.py --bootstrap --verbose
+  # $TOOLCHAIN/bin/python2 ./configure.py --bootstrap
   # emacs -Q --batch -f batch-byte-compile misc/ninja-mode.el
 }
 
