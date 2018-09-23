d0397a2eb222506f975621a25985dfcf7c5beee8
AV <27rurk@gmail.com>
2018-09-23 23:26:18 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -39,6 +39,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
                            --disable-nscd \
                            --enable-lock-elision \
                            --without-selinux \
+                           --disable-werror \
                            --disable-timezone-tools"
 
 # busybox:init needs it
