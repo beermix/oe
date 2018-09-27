9462ee6383aebcbeee1f2ff2a70b90bf38f0bffe
AV <27rurk@gmail.com>
2018-09-27 22:44:15 +0300

    autocommit on change

--- packages/devel/gettext=o/package.mk
+++ packages/devel/gettext=o/package.mk
@@ -8,25 +8,20 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host autotools:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host m4:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="gettext: A program internationalization library and tools"
 PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
-PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_SCRIPT="gettext-tools/configure"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-rpath \
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
+                         --disable-rpath \
                          --with-gnu-ld \
                          --disable-java \
                          --disable-curses \
-                         --disable-nls \
+                         --with-included-libxml \
                          --disable-native-java \
                          --disable-csharp \
-                         --without-emacs \
-                         --disable-libasprintf \
-                         --disable-acl \
-                         --disable-openmp \
-                         --disable-csharp \
-                         --disable-relocatable \
-                         --without-included-gettext"
+                         --without-emacs"
--- packages/virtual/toolchain/package.mk
+++ packages/virtual/toolchain/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv) | gettext:host
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="toolchain"
 PKG_VERSION=""
@@ -7,7 +7,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="configtools:host make:host xz:host sed:host pkg-config:host autoconf:host automake:host intltool:host libtool:host autoconf-archive:host bison:host flex:host mpc:host cmake:host xmlstarlet:host yasm:host ninja:host meson:host bc:host nasm:host elfutils:host squashfs:host binutils:host gcc:host"
+PKG_DEPENDS_TARGET="configtools:host make:host xz:host sed:host pkg-config:host autoconf:host automake:host intltool:host libtool:host autoconf-archive:host bison:host flex:host mpc:host cmake:host xmlstarlet:host yasm:host ninja:host meson:host bc:host nasm:host elfutils:host squashfs:host binutils:host gcc:host gettext:host"
 PKG_SECTION="virtual"
 PKG_SHORTDESC="toolchain: LibreELEC.tv' toolchain"
 PKG_LONGDESC="a crosscompiling toolchain to compile all packages"
