a2a1672663ce98c4e6a4f1f692d14a1c3e60faeb
AV <27rurk@gmail.com>
2018-09-27 22:05:49 +0300

    autocommit on change

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv) unrar
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="busybox"
 PKG_VERSION="1.29.3"
@@ -10,7 +10,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://busybox.net/downloads/?C=M;O=D"
 PKG_URL="http://busybox.net/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs zip unzip pciutils usbutils parted procps-ng gptfdisk less bash grep findutils libtool libtirpc libxml2"
+PKG_DEPENDS_TARGET="toolchain busybox:host hdparm dosfstools e2fsprogs zip unzip unrar pciutils usbutils parted procps-ng gptfdisk less bash grep findutils libtool libtirpc"
 PKG_DEPENDS_INIT="toolchain libtirpc"
 PKG_SECTION="system"
 PKG_SHORTDESC="BusyBox: The Swiss Army Knife of Embedded Linux"
