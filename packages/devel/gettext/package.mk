978e0386fe412956b1b9bd3cd14c1594646fbc63
AV <27rurk@gmail.com>
2018-09-28 13:23:14 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv) | --with-included-gettext=no
 
 PKG_NAME="gettext"
 PKG_VERSION="0.19.8.1"
@@ -29,6 +29,6 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
                          --disable-openmp \
                          --disable-relocatable \
                          --enable-nls \
-                         --with-included-gettext \
                          --with-pic \
                          --with-included-libxml=no"
+                         
