680491f1462f76bd8df34322901ea33218d96502
AV <27rurk@gmail.com>
2018-09-28 18:35:50 +0300

    autocommit on change

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -18,7 +18,7 @@ PKG_LONGDESC="zlib is a general purpose data compression library. All the code i
 PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="+pic:host +pic"
 
-#PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
 
 #PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
 
