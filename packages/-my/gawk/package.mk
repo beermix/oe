ea61c6d73303417ec3689adf6ab97ec8a28fc5ea
AV <27rurk@gmail.com>
2018-09-29 04:19:29 +0300

    autocommit on change

--- packages/-my/gawk/package.mk
+++ packages/-my/gawk/package.mk
@@ -1,8 +1,9 @@
 PKG_NAME="gawk"
 PKG_VERSION="4.1.4"
+PKG_VERSION="4.2.1"
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="gmp:host mpfr:host"
-PKG_DEPENDS_TARGET="gmp:host mpfr:host readline"
+PKG_DEPENDS_TARGET="readline"
 PKG_TOOLCHAIN="configure"
 
 PKG_CONFIGURE_OPTS_TARGET="--without-libsigsegv"
