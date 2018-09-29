f5b077eef5a8f503a7153ca27f4314df8d6e34ed
AV <27rurk@gmail.com>
2018-09-30 02:49:27 +0300

    autocommit on change

--- packages/-my/patch/package.mk
+++ packages/-my/patch/package.mk
@@ -4,8 +4,6 @@ PKG_URL="http://ftpmirror.gnu.org/patch/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain attr"
 PKG_SECTION="devel"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_sys_long_file_names=yes ac_cv_path_ED=ed --enable-attr"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_sys_long_file_names=yes --enable-xattr --enable-attr"
 
-PKG_CONFIGURE_OPTS_HOST="ac_cv_sys_long_file_names=yes \
-			    --enable-xattr \
-			    --enable-attr"
+PKG_CONFIGURE_OPTS_HOST="ac_cv_sys_long_file_names=yes"
--- packages/sysutils/gawk/package.mk
+++ packages/sysutils/gawk/package.mk
@@ -3,8 +3,8 @@ PKG_NAME="gawk"
 PKG_VERSION="4.2.1"
 PKG_SHA256="d1119785e746d46a8209d28b2de404a57f983aa48670f4e225531d3bdc175551"
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain readline libsigsegv"
-PKG_DEPENDS_HOST="gmp:host mpfr:host libsigsegv:host"
+PKG_DEPENDS_TARGET="toolchain readline mpfr libsigsegv"
+PKG_DEPENDS_HOST="mpfr:host libsigsegv:host"
 
 post_makeinstall_target() {
   ln -sfv gawk $INSTALL/usr/bin/awk
