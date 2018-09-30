43c99e7ca12c34c282461fcbcf660eb0f73eadd8
AV <27rurk@gmail.com>
2018-09-30 03:38:26 +0300

    autocommit on change

--- packages/sysutils/gawk/package.mk
+++ packages/sysutils/gawk/package.mk
@@ -1,15 +1,15 @@
 PKG_NAME="gawk"
-#PKG_VERSION="4.1.4"
-PKG_VERSION="7f684e83cdfa0647d8e197271fe318d2c185d291"
+PKG_VERSION="4.1.4"
+#PKG_VERSION="7f684e83cdfa0647d8e197271fe318d2c185d291"
 # http://git.savannah.gnu.org/cgit/gawk.git/log/?h=gawk-4.2-stable
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
-PKG_URL="http://git.savannah.gnu.org/cgit/gawk.git/snapshot/gawk-$PKG_VERSION.tar.gz"
+#PKG_URL="http://git.savannah.gnu.org/cgit/gawk.git/snapshot/gawk-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain readline mpfr libsigsegv"
 PKG_DEPENDS_HOST="mpfr:host libsigsegv:host"
 PKG_TOOLCHAIN="configure"
-PKG_TOOLCHAIN="autotools"
+#PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--without-libsigsegv"
+#PKG_CONFIGURE_OPTS_TARGET="--libdir=/usr/lib/awk"
 
 post_makeinstall_target() {
   ln -sf gawk $INSTALL/usr/bin/awk
