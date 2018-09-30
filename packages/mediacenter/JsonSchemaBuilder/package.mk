b5b23b5cfe97c0d5ef96d672a7a7c8e3580c93bf
AV <27rurk@gmail.com>
2018-09-30 13:03:42 +0300

    autocommit on change

--- packages/mediacenter/JsonSchemaBuilder/package.mk
+++ packages/mediacenter/JsonSchemaBuilder/package.mk
@@ -26,9 +26,7 @@ PKG_NEED_UNPACK="$(get_pkg_directory $MEDIACENTER)"
 PKG_SECTION="mediacenter"
 PKG_SHORTDESC="kodi-platform:"
 PKG_LONGDESC="kodi-platform:"
-
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_TOOLCHAIN="cmake-make"
 
 PKG_CMAKE_SCRIPT="$(get_build_dir $MEDIACENTER)/tools/depends/native/JsonSchemaBuilder/CMakeLists.txt"
 
