0b1310f4f1c50dc1e29cd2499474ab46673fb621
AV <27rurk@gmail.com>
2018-09-30 11:49:07 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -8,7 +8,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host m4:host autotools:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host m4:host automake:host libxml2:host"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="gettext: A program internationalization library and tools"
 PKG_LONGDESC="This is the GNU gettext package. It is interesting for authors or maintainers of other packages or programs which they want to see internationalized. As one step the handling of messages in different languages should be implemented. For this task GNU gettext provides the needed tools and library functions."
@@ -28,9 +28,4 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
                          --disable-curses \
                          --disable-native-java \
                          --disable-csharp \
-                         --without-emacs \
-                         --disable-libasprintf \
-                         --disable-acl \
-                         --disable-openmp \
-                         --disable-relocatable \
-                         --with-included-libxml=no"
+                         --without-emacs"
