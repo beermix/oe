c6cf9e9099f46f5e1e70cd8970c0ad6d90fcd3b9
AV <27rurk@gmail.com>
2018-09-30 03:08:13 +0300

    autocommit on change

--- packages/sysutils/gawk/package.mk
+++ packages/sysutils/gawk/package.mk
@@ -5,6 +5,9 @@ PKG_SHA256="d1119785e746d46a8209d28b2de404a57f983aa48670f4e225531d3bdc175551"
 PKG_URL="http://ftpmirror.gnu.org/gawk/gawk-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline mpfr libsigsegv"
 PKG_DEPENDS_HOST="mpfr:host libsigsegv:host"
+PKG_TOOLCHAIN="configure"
+PKG_LOCALE_INSTALL="yes"
+PKG_INCLUDE_INSTALL="yes"
 
 post_makeinstall_target() {
   ln -sf gawk $INSTALL/usr/bin/awk
