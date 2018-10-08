8cf621983910daf43b78541cff2fba3d1fa99868
AV <27rurk@gmail.com>
2018-10-08 22:22:09 +0300

    autocommit on change

--- packages/-my/smartmontools/package.mk
+++ packages/-my/smartmontools/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="smartmontools"
-PKG_VERSION="011076b"
+PKG_VERSION="d6f4efe"
 PKG_URL="https://github.com/mirror/smartmontools/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses pcre libcap-ng"
 PKG_SECTION="my"
