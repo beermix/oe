dfb49baf15b9fe252707cf37f1ee0b6ac8d1c17a
AV <27rurk@gmail.com>
2018-10-09 02:00:22 +0300

    autocommit on change

--- packages/devel/pcre/package.mk
+++ packages/devel/pcre/package.mk
@@ -14,6 +14,7 @@ PKG_SECTION="devel"
 PKG_SHORTDESC="pcre: Perl Compatible Regulat Expressions"
 PKG_LONGDESC="The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5. PCRE has its own native API, as well as a set of wrapper functions that correspond to the POSIX regular expression API. The PCRE library is free, even for building commercial software."
 PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-utf8 \
 			      --enable-pcre8 \
