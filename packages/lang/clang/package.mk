367fd00ca40df331b5841bdd6959074443fcc083
AV <27rurk@gmail.com>
2018-10-09 04:00:38 +0300

    autocommit on change

--- packages/lang/clang/package.mk
+++ packages/lang/clang/package.mk
@@ -9,28 +9,27 @@ PKG_URL="http://releases.llvm.org/$PKG_VERSION/cfe-$PKG_VERSION.src.tar.xz"
 PKG_SOURCE_DIR="cfe-$PKG_VERSION*"
 #PKG_SOURCE_DIR="clang-$PKG_VERSION*"
 PKG_DEPENDS_HOST="llvm:host lld:host"
-#PKG_TOOLCHAIN="cmake-make"
+PKG_TOOLCHAIN="cmake-make"
 
-#configure_host() {
-#    cmake \
-#    -DCMAKE_BUILD_TYPE=Release \
-#    -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
-#    -DPYTHON_EXECUTABLE="$TOOLCHAIN/bin/python2" \
-#    -DLLVM_LINK_LLVM_DYLIB=ON \
-#    -DLLVM_DYLIB_COMPONENTS=all \
-#    -DCLANG_BUILD_EXAMPLES=OFF \
-#    -DCLANG_INCLUDE_DOCS=OFF \
-#    -DCLANG_INCLUDE_TESTS=OFF \
-#    -DCLANG_BUILD_TOOLS=ON \
-#    -DLLVM_CONFIG="$TOOLCHAIN/bin/llvm-config" \
-#    ..
-#}
+configure_host() {
+    cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN \
+    -DPYTHON_EXECUTABLE="$TOOLCHAIN/bin/python2" \
+    -DLLVM_LINK_LLVM_DYLIB=ON \
+    -DLLVM_DYLIB_COMPONENTS=all \
+    -DCLANG_BUILD_EXAMPLES=OFF \
+    -DCLANG_INCLUDE_DOCS=OFF \
+    -DCLANG_INCLUDE_TESTS=OFF \
+    -DCLANG_BUILD_TOOLS=ON \
+    ..
+}
 
 #make_host() {
 #  ninja -j${CONCURRENCY_MAKE_LEVEL}
-#}
+#}	
 
 post_makeinstall_host() {
-  ln -sf clang $TOOLCHAIN/bin/x86_64-libreelec-linux-gnu-clang
-  ln -sf clang $TOOLCHAIN/bin/x86_64-libreelec-linux-gnu-clang++
+  ln -sf $TOOLCHAIN/bin/clang $TOOLCHAIN/bin/x86_64-libreelec-linux-gnu-clang
+  ln -sf $TOOLCHAIN/bin/clang $TOOLCHAIN/bin/x86_64-libreelec-linux-gnu-clang++
 }
