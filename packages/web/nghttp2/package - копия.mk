56a258a4d30501a107237aa4a99f84804b9f10b7
AV <27rurk@gmail.com>
2018-10-09 12:43:43 +0300

    autocommit on change

--- packages/web/nghttp2/package - копия.mk
+++ packages/web/nghttp2/package - копия.mk
@@ -1,10 +1,16 @@
 PKG_NAME="nghttp2"
 PKG_VERSION="1.34.0"
-PKG_SHA256="ecb0c013141495e24bd6deca022b5a92097a7848a0c17c4e5af1243a97fa622e"
 PKG_SITE="https://github.com/nghttp2/nghttp2/releases/"
 PKG_URL="https://github.com/nghttp2/nghttp2/releases/download/v$PKG_VERSION/nghttp2-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libevent libev libuv"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="cmake-make"
 
-PKG_CMAKE_OPTS_TARGET="-DENABLE_LIB_ONLY=1"
+PKG_CMAKE_OPTS_TARGET="-DENABLE_LIB_ONLY=1 \
+			  -DENABLE_EXAMPLES=0 \
+			  -DENABLE_PYTHON_BINDINGS=0 \
+			  -DCMAKE_BUILD_TYPE=Release"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-examples \
+			      --disable-python-bindings \
+			      --enable-lib-only"
