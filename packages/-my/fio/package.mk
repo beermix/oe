93b928b499bc66f0bac6ca0e2e930a435fd78693
AV <27rurk@gmail.com>
2018-10-09 17:14:47 +0300

    autocommit on change

--- packages/-my/fio/package.mk
+++ packages/-my/fio/package.mk
@@ -1,13 +1,14 @@
 PKG_NAME="fio"
 PKG_VERSION="3.11"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain numactl libaio acl"
+#PKG_DEPENDS_TARGET="toolchain numactl libaio acl"
 PKG_SECTION="tools"
 PKG_TOOLCHAIN="manual"
 
 configure_target() {
   cd $PKG_BUILD
-  LIBS="-laio -lrt" ./configure --prefix=/usr --cpu=$TARGET_CPU --cc=$CC --disable-native --build-static --extra-cflags="$CFLAGS"
+  ./configure --prefix=/usr --cpu=$TARGET_CPU --cc=$CC --disable-native --build-static --extra-cflags="$CFLAGS"
+  #LIBS="-laio -lrt" ./configure --prefix=/usr --cpu=$TARGET_CPU --cc=$CC --disable-native --build-static --extra-cflags="$CFLAGS"
 }
 
 make_target() {
