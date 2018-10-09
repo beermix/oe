eefa9415b16ae3d33fd9f8f0da135a67812f01e3
AV <27rurk@gmail.com>
2018-10-09 08:00:58 +0300

    autocommit on change

--- packages/multimedia/intel-media-driver/package.mk
+++ packages/multimedia/intel-media-driver/package.mk
@@ -15,4 +15,4 @@ PKG_SHORTDESC="intel-media-driver: VA-API user mode driver for Intel GEN Graphic
 PKG_LONGDESC="intel-media-driver: VA-API user mode driver for Intel GEN Graphics family"
 PKG_TOOLCHAIN="cmake-make"
 
-PKG_CMAKE_OPTS_TARGET="-DINSTALL_DRIVER_SYSCONF=OFF -Wno-dev"
+PKG_CMAKE_OPTS_TARGET="-DINSTALL_DRIVER_SYSCONF=1 -Wno-dev"
