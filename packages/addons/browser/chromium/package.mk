d26c0c1b58c2c2650940350db7b8a03d35096902
AV <27rurk@gmail.com>
2018-10-09 08:26:40 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -10,7 +10,7 @@
 PKG_NAME="chromium"
 PKG_VERSION="64.0.3282.167"
 PKG_SHA256="f3308b41f241d53a269ec0c73e8512f4ac58b7aafe16967a3dba5b99abc4c455"
-PKG_REV="332-gtk2-icu-std-wo_unwind-tables-icu_shared"
+PKG_REV="332-gtk2-icu-std-wo_unwind-tables-no_icu"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Mixed"
 PKG_URL="https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$PKG_VERSION.tar.xz"
@@ -37,12 +37,6 @@ post_patch() {
   # set correct widevine
   sed -i -e 's/@WIDEVINE_VERSION@/Pinkie Pie/' ./third_party/widevine/cdm/stub/widevine_cdm_version.h
 }
-
-#make_host() {
-#  export CCACHE_SLOPPINESS=time_macros
-  #./tools/gn/bootstrap/bootstrap.py --no-rebuild --no-clean
-  #./tools/gn/bootstrap/bootstrap.py -s --no-clean
-#}
 
 make_target() {
   export CCACHE_SLOPPINESS=time_macros
@@ -74,7 +68,7 @@ make_target() {
     'use_gnome_keyring=false'
     'use_gold=false'
     'use_gtk3=false'
-    'use_kerberos=false'
+    'use_kerberos=true'
     'use_pulseaudio=false'
     'use_sysroot=true'
     'use_vaapi=true'
@@ -97,14 +91,10 @@ make_target() {
     "google_default_client_secret=\"${_google_default_client_secret}\""
   )
 
-#     'enable_google_now=false'
-#    'is_desktop_linux=true'
-#    'enable_vr=false'
-#    'enable_wayland_server=false'
 # Possible replacements are listed in build/linux/unbundle/replace_gn_files.py | 'exclude_unwind_tables=true'
 # Keys are the names in the above script; values are the dependencies in Arch     'enable_remoting=false'  'enable_linux_installer=false''rtc_enable_protobuf=false'
 readonly -A _system_libs=(
-  [icu]=icu
+  #[icu]=icu
   [libdrm]=
   [libjpeg]=libjpeg
   [libxml]=libxml2
@@ -133,7 +123,6 @@ depends+=(${_system_libs[@]} freetype2 harfbuzz)
     find -type f -path "*third_party/$_lib/*" \
       \! -path "*third_party/$_lib/chromium/*" \
       \! -path "*third_party/$_lib/google/*" \
-      \! -path './base/third_party/icu/*' \
       \! -path './third_party/freetype/src/src/psnames/pstables.h' \
       \! -path './third_party/yasm/run_yasm.py' \
       \! -regex '.*\.\(gn\|gni\|isolate\)' \
@@ -158,8 +147,8 @@ addon() {
   cp -PR $PKG_BUILD/out/Release/locales $ADDON_BUILD/$PKG_ADDON_ID/bin/
   cp -PR $PKG_BUILD/out/Release/gen/content/content_resources.pak $ADDON_BUILD/$PKG_ADDON_ID/bin/
 
-  #cp -ri  $PKG_BUILD/out/Release/{*.pak,*.dat,*.bin,libwidevinecdmadapter.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -ri  $PKG_BUILD/out/Release/{*.pak,*.bin,libwidevinecdmadapter.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -ri  $PKG_BUILD/out/Release/{*.pak,*.dat,*.bin,libwidevinecdmadapter.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
+  #cp -ri  $PKG_BUILD/out/Release/{*.pak,*.bin,libwidevinecdmadapter.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
 
   # config
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config \
