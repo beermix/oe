08fdb5a3f66f08d954007b008b403ae6252b7b0b
AV <27rurk@gmail.com>
2018-10-09 13:08:21 +0300

    autocommit on change

--- packages/python/security/pycryptodome/package.mk
+++ packages/python/security/pycryptodome/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pycryptodome"
-PKG_VERSION="3.6.1"
-PKG_SHA256="15013007e393d0cc0e69f4329a47c4c8597b7f3d02c12c03f805405542f70c71"
+PKG_VERSION="3.6.6"
+PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.python.org/pypi/pycryptodome"
