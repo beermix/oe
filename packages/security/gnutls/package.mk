8070a7b90768d9418ccf5f217e3d6b193cdf324a
AV <27rurk@gmail.com>
2018-10-09 09:18:34 +0300

    autocommit on change

--- packages/security/gnutls/package.mk
+++ packages/security/gnutls/package.mk
@@ -6,6 +6,7 @@ PKG_SITE="https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-$PKG_VERSION.tar.xz"
 #PKG_DEPENDS_TARGET="toolchain openssl libidn2 nettle libgcrypt libtasn1 libunistring"
 PKG_DEPENDS_TARGET="toolchain openssl nettle gmp libtasn1"
+PKG_DEPENDS_TARGET="toolchain openssl nettle gmp"
 PKG_SECTION="escalade/depends"
 PKG_SHORTDESC="GnuTLS is a secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them."
 
@@ -22,4 +23,5 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-doc \
 			      --without-p11-kit \
 			      --with-included-unistring \
 			      --with-zlib \
+			      --with-included-libtasn1 \
 			      --with-nettle-mini"
--- packages/security/nettle/package.mk
+++ packages/security/nettle/package.mk
@@ -7,4 +7,7 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="escalade/depends"
 PKG_SHORTDESC="Nettle is a cryptographic library that is designed to fit easily in more or less any context: In crypto toolkits for object-oriented languages (C++, Python, Pike, ...), in applications like LSH or GNUPG, or even in kernel space."
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-documentation --disable-openssl --disable-arm-neon"
+PKG_CONFIGURE_OPTS_TARGET="--disable-documentation \
+			      --disable-openssl \
+			      --disable-arm-neon \
+			      --enable-mini-gmp"
