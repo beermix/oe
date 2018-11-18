3450c92e87846223e196b88594cf901b86366fce
AV <27rurk@gmail.com>
2018-11-18 23:16:48 +0300

    autocommit on change

--- packages/multimedia/libwebp/package.mk
+++ packages/multimedia/libwebp/package.mk
@@ -1,11 +1,8 @@
 PKG_NAME="libwebp"
-PKG_VERSION="1.0.0"
-PKG_URL="https://johnvansickle.com/ffmpeg/release-source/libwebp-0.4.4.tar.xz"
+PKG_VERSION="1.0.1"
 PKG_URL="https://github.com/webmproject/libwebp/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain intel-vaapi-driver libva"
 PKG_PRIORITY="optional"
-PKG_SECTION="multimedia"
-PKG_USE_CMAKE="no"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
