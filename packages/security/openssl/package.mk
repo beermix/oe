f66e7448c6d8b89c1705ef38029584a44ce70257
AV <27rurk@gmail.com>
2018-11-18 21:36:06 +0300

    autocommit on change

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -2,25 +2,41 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssl"
-PKG_VERSION="1.0.2p"
-PKG_SHA256="50a98e07b1a89eb8f6a99477f262df71c6fa7bef77df4dc83025a2845c827d00"
+PKG_VERSION="1.1.1"
+PKG_SHA256="2836875a0f89c03d0fdf483941512613a50cfb421d6fd94b9f41d7279d586a3d"
 PKG_LICENSE="BSD"
-PKG_SITE="https://www.openssl.org/source/"
+PKG_SITE="https://www.openssl.org"
 PKG_URL="https://www.openssl.org/source/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="yasm:host"
-PKG_DEPENDS_TARGET="toolchain yasm:host zlib"
-PKG_SHORTDESC="The Open Source toolkit for Secure Sockets Layer and Transport Layer Security"
-PKG_BUILD_FLAGS="+hardening"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="The Open Source toolkit for Secure Sockets Layer and Transport Layer Security"
+PKG_TOOLCHAIN="configure"
+PKG_BUILD_FLAGS="-parallel"
 
-PKG_CONFIGURE_OPTS_SHARED="--openssldir=/etc/ssl \
-                           --libdir=lib \
+PKG_CONFIGURE_OPTS_SHARED="--libdir=lib \
                            shared \
                            threads \
-                           enable-camellia \
-                           enable-mdc2 \
-                           enable-unit-test \
-                           no-ssl3-method \
-                           enable-ec_nistp_64_gcc_128"
+                           no-ec2m \
+                           no-md2 \
+                           no-rc5 \
+                           no-rfc3779
+                           no-sctp \
+                           no-ssl-trace \
+                           no-ssl3 \
+                           no-unit-test \
+                           no-weak-ssl-ciphers \
+                           no-zlib \
+                           no-zlib-dynamic \
+                           no-static-engine"
+
+PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN \
+                         --openssldir=$TOOLCHAIN/etc/ssl"
+PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
+                           --openssldir=/etc/ssl"
+
+post_unpack() {
+  find $PKG_BUILD/apps -type f | xargs -n 1 -t sed 's|./demoCA|/etc/ssl|' -i
+}
 
 pre_configure_host() {
   mkdir -p $PKG_BUILD/.$HOST_NAME
@@ -29,33 +45,39 @@ pre_configure_host() {
 
 configure_host() {
   cd $PKG_BUILD/.$HOST_NAME
-  ./Configure --prefix=/ $PKG_CONFIGURE_OPTS_SHARED no-zlib no-zlib-dynamic no-static-engine linux-x86_64
-  #  "-Wa,--noexecstack $CFLAGS $LDFLAGS -ffunction-sections -fdata-sections -Wl,--gc-sections"
+  ./Configure $PKG_CONFIGURE_OPTS_HOST $PKG_CONFIGURE_OPTS_SHARED linux-x86_64 $CFLAGS $LDFLAGS
 }
 
 makeinstall_host() {
-  make INSTALL_PREFIX=$TOOLCHAIN install_sw -j1
+  make install_sw
 }
 
 pre_configure_target() {
   mkdir -p $PKG_BUILD/.$TARGET_NAME
   cp -a $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME/
-  
-#  sed -i -e '/^"linux-x86_64"/ s/-m64 -DL_ENDIAN -O3 -Wall//' $PKG_BUILD/.$TARGET_NAME/Configure
-#  CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|"`
-#  CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O3|"`
+
+  case $TARGET_ARCH in
+    x86_64)
+      OPENSSL_TARGET=linux-x86_64
+      PLATFORM_FLAGS=enable-ec_nistp_64_gcc_128
+      ;;
+    arm)
+      OPENSSL_TARGET=linux-armv4
+      ;;
+    aarch64)
+      OPENSSL_TARGET=linux-aarch64
+      ;;
+  esac
 }
 
 configure_target() {
   cd $PKG_BUILD/.$TARGET_NAME
-  ./Configure --prefix=/usr $PKG_CONFIGURE_OPTS_SHARED no-static-engine linux-x86_64 $CFLAGS $CPPFLAGS $LDFLAGS
+  ./Configure $PKG_CONFIGURE_OPTS_TARGET $PKG_CONFIGURE_OPTS_SHARED $PLATFORM_FLAGS $OPENSSL_TARGET $CFLAGS $LDFLAGS
 }
 
 makeinstall_target() {
-  make INSTALL_PREFIX=$INSTALL install_sw -j1
-  make INSTALL_PREFIX=$SYSROOT_PREFIX install_sw -j1
-  chmod 755 $INSTALL/usr/lib/*.so*
-  chmod 755 $INSTALL/usr/lib/engines/*.so
+  make DESTDIR=$INSTALL install_sw
+  make DESTDIR=$SYSROOT_PREFIX install_sw
 }
 
 post_makeinstall_target() {
@@ -64,29 +86,25 @@ post_makeinstall_target() {
 
   debug_strip $INSTALL/usr/bin/openssl
 
-  # create new cert: ./mkcerts.sh
   # cert from https://curl.haxx.se/docs/caextract.html
-
   mkdir -p $INSTALL/etc/ssl
-  # perl $PKG_DIR/cert/mk-ca-bundle.pl
-  # cp $PKG_BUILD/.$TARGET_NAME/ca-bundle.crt $INSTALL/etc/ssl/cert.pem
-  cp $PKG_DIR/scripts/openssl-config $INSTALL/usr/bin
-  cp $PKG_DIR/cert/cacert.pem $INSTALL/etc/ssl/cert.pem
-  cp $PKG_DIR/cert/cacert.pem $INSTALL/etc/ssl/cacert.pem
+    cp $PKG_DIR/cert/cacert.pem $INSTALL/etc/ssl/cacert.pem.system
+
+  # give user the chance to include their own CA
+  mkdir -p $INSTALL/usr/bin
+    cp $PKG_DIR/scripts/openssl-config $INSTALL/usr/bin
+    ln -sf /run/libreelec/cacert.pem $INSTALL/etc/ssl/cacert.pem
+    ln -sf /run/libreelec/cacert.pem $INSTALL/etc/ssl/cert.pem
 
   # backwards comatibility
   mkdir -p $INSTALL/etc/pki/tls
-    ln -sf /etc/ssl/cert.pem $INSTALL/etc/pki/tls/cacert.pem
+    ln -sf /run/libreelec/cacert.pem $INSTALL/etc/pki/tls/cacert.pem
   mkdir -p $INSTALL/etc/pki/tls/certs
-    ln -sf /etc/ssl/cert.pem $INSTALL/etc/pki/tls/certs/ca-bundle.crt
+    ln -sf /run/libreelec/cacert.pem $INSTALL/etc/pki/tls/certs/ca-bundle.crt
   mkdir -p $INSTALL/usr/lib/ssl
-    ln -sf /etc/ssl/cert.pem $INSTALL/usr/lib/ssl/cert.pem
+    ln -sf /run/libreelec/cacert.pem $INSTALL/usr/lib/ssl/cert.pem
+}
 
-  # mkdir -p $INSTALL/etc/ssl/certs
-  # ln -sf /etc/ssl/cert.pem $INSTALL/etc/ssl/certs/ca-certificates.crt
-
-  # for VDR-LIVE
-  # mkdir -p $INSTALL/usr/config/ssl
-  # cp $PKG_DIR/config/openssl.cnf $INSTALL/usr/config/ssl
-  # ln -sf /storage/.config/ssl/openssl.cnf $INSTALL/etc/ssl/openssl.cnf
+post_install() {
+  enable_service openssl-config.service
 }
