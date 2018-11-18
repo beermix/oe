6afd3db0d4ac12b4d7da4ad88b3fb044ec51ddc0
AV <27rurk@gmail.com>
2018-11-18 18:39:27 +0300

    autocommit on change

--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -12,7 +12,7 @@ PKG_DEPENDS_TARGET="toolchain libXcomposite libXxf86vm libXdamage libdrm util-ma
 PKG_SECTION="x11/driver"
 PKG_LONGDESC="The Xorg driver for Intel i810, i815, 830M, 845G, 852GM, 855GM, 865G, 915G, 915GM and 965G."
 PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="-lto -gold"
+PKG_BUILD_FLAGS="-lto -gold -hardening"
 # xf86-video-intel is broken enough. dont link with LTO
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-backlight \
@@ -35,8 +35,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-backlight \
                            --disable-create2 \
                            --disable-async-swap \
                            --with-default-dri=3 \
-                           --with-xorg-module-dir=$XORG_PATH_MODULES \
-                           --disable-silent-rules"
+                           --with-xorg-module-dir=$XORG_PATH_MODULES"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -D_GNU_SOURCE"
