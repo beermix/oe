4553325df4f6f80bb032fe234115b87893996a42
AV <27rurk@gmail.com>
2018-11-18 23:20:23 +0300

    autocommit on change

--- packages/multimedia/libwebp/package.mk
+++ packages/multimedia/libwebp/package.mk
@@ -2,12 +2,13 @@ PKG_NAME="libwebp"
 PKG_VERSION="1.0.1"
 PKG_URL="https://github.com/webmproject/libwebp/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain intel-vaapi-driver libva"
-PKG_PRIORITY="optional"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
-			      --disable-shared \
+# Dependencies
+get_graphicdrivers
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
 			      --enable-swap-16bit-csp \
 			      --enable-experimental \
