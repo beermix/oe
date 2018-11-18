9507a48e1a94445c972d52b490e577e3c0365363
AV <27rurk@gmail.com>
2018-11-18 17:49:16 +0300

    autocommit on change

--- packages/-my/htop/package.mk
+++ packages/-my/htop/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="htop"
-PKG_VERSION="67e3689"
+PKG_VERSION="b7b4200"
 PKG_SITE="https://github.com/hishamhm/htop"
 PKG_URL="https://github.com/hishamhm/htop/archive/${PKG_VERSION}.tar.gz"
 #PKG_DEPENDS_TARGET="toolchain ncurses libpciaccess"
