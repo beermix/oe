8b6989dc6090a11da19411a1c2722f49a9d81391
AV <27rurk@gmail.com>
2018-11-19 01:15:45 +0300

    autocommit on change

--- packages/sysutils/findutils/package.mk
+++ packages/sysutils/findutils/package.mk
@@ -1,7 +1,12 @@
 PKG_NAME="findutils"
+PKG_SHA256="ded4c9f73731cd48fec3b6bdaccce896473b6d8e337e9612e16cf1431bb1169d"
 PKG_VERSION="4.6.0"
 PKG_URL="http://ftpmirror.gnu.org/findutils/findutils-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
+
+post_unpack() {
+  sed -i '/^SUBDIRS/s/locate//' $PKG_BUILD/Makefile.in
+}
 
 pre_configure_target() {
   CFLAGS=`echo $CFLAGS | sed -e "s|-fno-plt||"
