9f604d6dd5ffc4cb0b6f23a3469b6376d753e81d
AV <27rurk@gmail.com>
2018-11-18 19:11:15 +0300

    autocommit on change

--- packages/devel/acl/package.mk
+++ packages/devel/acl/package.mk
@@ -4,7 +4,6 @@ PKG_SHA256="06be9865c6f418d851ff4494e12406568353b891ffe1f596b34693c387af26c7"
 PKG_URL="https://mirrors.up.pt/pub/nongnu/acl/acl-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain attr"
 PKG_DEPENDS_HOST="toolchain attr:host"
-PKG_SECTION="tools"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
--- packages/devel/attr/package.mk
+++ packages/devel/attr/package.mk
@@ -5,14 +5,12 @@
 PKG_NAME="attr"
 PKG_VERSION="2.4.48"
 PKG_SHA256="5ead72b358ec709ed00bbf7a9eaef1654baad937c001c044fe8b74c57f5324e7"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE=""
 PKG_URL="https://mirrors.up.pt/pub/nongnu/attr/attr-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="accessibility"
 PKG_SHORTDESC="attr: Extended Attributes Of Filesystem Objects"
-PKG_LONGDESC="Extended attributes are name:value pairs associated permanently with files and directories, similar to the environment strings associated with a process. An attribute may be defined or undefined. If it is defined, its value may be empty or non-empty. Extended attributes are extensions to the normal attributes which are associated with all inodes in the system (i.e. the stat(2) data). They are often used to provide additional functionality to a filesystem - for example, additional security features such as Access Control Lists (ACLs) may be implemented using extended attributes."
 PKG_BUILD_FLAGS="+pic:host +pic"
 
 PKG_CONFIGURE_OPTS_TARGET="OPTIMIZER= \
