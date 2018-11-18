2f60329f80cce1a7f51c6c60cb54f66229dcf206
AV <27rurk@gmail.com>
2018-11-19 01:06:12 +0300

    autocommit on change

--- packages/x11/driver/xf86-input-libinput/package.mk
+++ packages/x11/driver/xf86-input-libinput/package.mk
@@ -2,16 +2,12 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="xf86-input-libinput"
-PKG_VERSION="0.28.0"
-PKG_SHA256="21994d065fc26e85d1c3fc87d8479b9c22699ed5a0119df98fbe0000e84630a1"
-PKG_ARCH="any"
+PKG_VERSION="0.28.1"
+PKG_SHA256="9ada448e076c0078a84b48e5298fa8ce317565f9b342b74c20429214a707d98b"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/libinput/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/driver/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libinput"
-PKG_SECTION="x11/driver"
-PKG_SHORTDESC="This is an X driver based on libinput."
-PKG_LONGDESC="This is an X driver based on libinput."
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-xorg-module-dir=$XORG_PATH_MODULES"
