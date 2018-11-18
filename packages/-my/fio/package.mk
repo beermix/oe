01ef351c6cf2d6d5b69897f251b43c8036f38d0a
AV <27rurk@gmail.com>
2018-11-19 00:15:22 +0300

    autocommit on change

--- packages/-my/fio/package.mk
+++ packages/-my/fio/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="fio"
-PKG_VERSION="3.11"
+PKG_VERSION="3.12"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 #PKG_DEPENDS_TARGET="toolchain numactl libaio acl"
 PKG_SECTION="tools"
