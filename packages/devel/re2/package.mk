0924a15fbb3868edbfcc0b7e698dacea00dcb510
AV <27rurk@gmail.com>
2018-11-19 08:46:24 +0300

    autocommit on change

--- packages/devel/re2/package.mk
+++ packages/devel/re2/package.mk
@@ -3,6 +3,5 @@ PKG_VERSION="2018-10-01"
 PKG_SITE="https://github.com/google/re2/releases"
 PKG_URL="https://github.com/google/re2/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-#PKG_BUILD_FLAGS="+pic +hardening"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1"
