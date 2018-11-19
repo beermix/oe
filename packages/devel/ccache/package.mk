29035d89e7632c861e3921f6bf8e16aed73b668e
AV <27rurk@gmail.com>
2018-11-19 12:56:48 +0300

    autocommit on change

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -4,21 +4,20 @@
 PKG_NAME="ccache"
 PKG_VERSION="3.5"
 PKG_SHA256="bdd44b72ae4506a2e2deef9fefb15c606a474bbca7658cd2be26105155eec012"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.samba.org/ftp/ccache/?C=M;O=D"
 PKG_URL="https://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="make:host"
-PKG_LONGDESC="ccache: A fast compiler cache"
+PKG_LONGDESC="A compiler cache to speed up re-compilation of C/C++ code by caching."
 
 export CC=$LOCAL_CC
 export CXX=$LOCAL_CXX
 
-export CFLAGS="-march=native -O2 -pipe -fstack-protector-strong -Wp,-D_FORTIFY_SOURCE=2 -fno-plt -Wall"
-export CXXFLAGS="-march=native -O2 -pipe -fstack-protector-strong -Wp,-D_FORTIFY_SOURCE=2 -fno-plt -Wall"
-export LDFLAGS="-march=native -Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -s"
+export CFLAGS="-march=native -O2 -fstack-protector-strong -Wp,-D_FORTIFY_SOURCE=2 -fno-plt -Wall"
+export CXXFLAGS="-march=native -O2 -fstack-protector-strong -Wp,-D_FORTIFY_SOURCE=2 -fno-plt -Wall"
+export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -s"
 
-PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib --disable-silent-rules"
+PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
 
 post_makeinstall_host() {
 # setup ccache
