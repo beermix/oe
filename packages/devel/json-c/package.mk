6bd0ae0ea3c9ad1c28b2561521da105c78e93208
AV <27rurk@gmail.com>
2018-11-19 08:50:53 +0300

    autocommit on change

--- packages/devel/json-c/package.mk
+++ packages/devel/json-c/package.mk
@@ -1,16 +1,13 @@
 PKG_NAME="json-c"
-PKG_VERSION="0.13.1"
-PKG_ARCH="any"
+PKG_VERSION="985c46f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/json-c/json-c/wiki"
 PKG_URL="https://github.com/json-c/json-c/archive/$PKG_VERSION.tar.gz"
-PKG_URL="https://s3.amazonaws.com/json-c_releases/releases/json-c-$PKG_VERSION.tar.gz"
+#PKG_URL="https://s3.amazonaws.com/json-c_releases/releases/json-c-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="accessibility"
-PKG_SHORTDESC="json-c"
-PKG_LONGDESC="JSON-C implements a reference counting object model that allows you to easily construct JSON objects in C, output them as JSON formatted strings and parse JSON formatted strings back into the C representation of JSON objects"
 PKG_TOOLCHAIN="autotools"
-PKG_TOOLCHAIN="configure"
+#PKG_TOOLCHAIN="configure"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_realloc_0_nonnull=yes \
                            ac_cv_func_malloc_0_nonnull=yes \
