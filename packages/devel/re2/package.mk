49b3f692b72148f4bb0fdb17a23afda188a0a763
AV <27rurk@gmail.com>
2018-11-19 19:57:27 +0300

    autocommit on change

--- packages/devel/re2/package.mk
+++ packages/devel/re2/package.mk
@@ -2,6 +2,18 @@ PKG_NAME="re2"
 PKG_VERSION="2018-10-01"
 PKG_SITE="https://github.com/google/re2/releases"
 PKG_URL="https://github.com/google/re2/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="manual"
+PKG_BUILD_FLAGS="+hardening"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 -DBUILD_TESTING=0 -DRE2_BUILD_TESTING=0 -DCMAKE_BUILD_TYPE=Release"
+
+pre_build_target() {
+  mkdir -p $PKG_BUILD/.$TARGET_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
+  export RE2_LDFLAGS="$RE2_LDFLAGS $LDFLAGS"
+}
+
+makeinstall_target() {
+   make prefix=/usr DESTDIR=$INSTALL install
+   make prefix=/usr DESTDIR=$SYSROOT_PREFIX install
+}
