32a168cef935c53c018959395214d5bcc326fb59
AV <27rurk@gmail.com>
2018-11-19 13:55:01 +0300

    autocommit on change

--- packages/debug/libunwind/package.mk
+++ packages/debug/libunwind/package.mk
@@ -4,14 +4,10 @@
 PKG_NAME="libunwind"
 PKG_VERSION="1.2.1"
 PKG_SHA256="3f3ecb90e28cbe53fba7a4a27ccce7aad188d3210bb1964a923a731a27a75acb"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nongnu.org/libunwind/"
 PKG_URL="http://download.savannah.nongnu.org/releases/libunwind/libunwind-${PKG_VERSION}.tar.gz"
-PKG_SECTION="debug"
-PKG_SHORTDESC="library to determine the call-chain of a program"
 PKG_LONGDESC="library to determine the call-chain of a program"
-PKG_BUILD_FLAGS="+pic"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
 			   --disable-shared"
