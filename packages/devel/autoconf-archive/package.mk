c94d4f3d56f6b6bed46da53c554e78bd0ecfb0bd
AV <27rurk@gmail.com>
2018-11-19 17:46:16 +0300

    autocommit on change

--- packages/devel/autoconf-archive/package.mk
+++ packages/devel/autoconf-archive/package.mk
@@ -4,13 +4,10 @@
 PKG_NAME="autoconf-archive"
 PKG_VERSION="2018.03.13"
 PKG_SHA256="6175f90d9fa64c4d939bdbb3e8511ae0ee2134863a2c7bf8d9733819efa6e159"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/autoconf-archive/"
 PKG_URL="http://ftpmirror.gnu.org/autoconf-archive/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
-PKG_SECTION="toolchain/devel"
-PKG_SHORTDESC="autoconf-archive: macros for autoconf"
 PKG_LONGDESC="autoconf-archive is an package of m4 macros"
 
 PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME --prefix=$TOOLCHAIN"
