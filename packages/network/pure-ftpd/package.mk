ef731f5761846965f07322e8aed53a0961b63161
AV <27rurk@gmail.com>
2018-11-19 10:57:06 +0300

    autocommit on change

--- packages/network/pure-ftpd/package.mk
+++ packages/network/pure-ftpd/package.mk
@@ -3,7 +3,7 @@ PKG_VERSION="1.0.47"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libcap"
 #PKG_DEPENDS_TARGET="toolchain libcap libevent libsodium"
-PKG_SECTION="network"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config \
 			      --datadir=/storage/.config \
@@ -16,8 +16,10 @@ PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config \
 			      --datarootdir=/storage/.config \
 			      --infodir=/storage/.config \
 			      --localedir=/storage/.config \
-			      --with-minimal \
-			      --enable-largefile"
+			      --enable-largefile \
+			      --without-tls \
+			      --without-pam \
+			      --with-minimal"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/storage
