d199fd008f44fedff0f673c7811cf5d99f9b4d1e
AV <27rurk@gmail.com>
2018-11-19 13:24:30 +0300

    autocommit on change

--- packages/audio/libcdio/package.mk
+++ packages/audio/libcdio/package.mk
@@ -7,7 +7,7 @@ PKG_NAME="libcdio"
 PKG_VERSION="2.0.0"
 PKG_SHA256="1b481b5da009bea31db875805665974e2fc568e2b2afa516f4036733657cf958"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://www.gnu.org/software/libcdio/"
+PKG_SITE="http://ftp.gnu.org.ua/gnu/libcdio/?C=M;O=D"
 PKG_URL="http://ftpmirror.gnu.org/libcdio/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 #PKG_BUILD_FLAGS="+pic"
@@ -27,8 +27,7 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-cxx \
                            --without-cd-read \
                            --without-iso-info \
                            --without-iso-read \
-                           --without-libiconv-prefix \
-                           --with-gnu-ld"
+                           --without-libiconv-prefix"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
