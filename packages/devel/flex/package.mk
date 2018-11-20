6045855ce729db324872f8e116edc02fa1d3db7e
AV <27rurk@gmail.com>
2018-11-20 10:08:48 +0300

    autocommit on change

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -2,16 +2,15 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="flex"
-PKG_VERSION="2.6.4"
-PKG_SHA256="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
+PKG_VERSION="2.6.1"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/westes/flex/releases"
+PKG_SITE="https://github.com/westes/flex/releases/"
 PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="ccache:host"
-PKG_LONGDESC="A tool for generating programs that perform pattern-matching on text."
+PKG_DEPENDS_HOST="ccache:host m4:host"
+PKG_LONGDESC="flex is a tool for generating programs that perform pattern-matching on text."
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld --disable-doc"
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
 
 post_makeinstall_host() {
   cat > $TOOLCHAIN/bin/lex << "EOF"
@@ -21,5 +20,3 @@ EOF
 
   chmod -v 755 $TOOLCHAIN/bin/lex
 }
-
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_path_M4=/usr/bin/m4 ac_cv_func_reallocarray=no --disable-program --disable-doc"
