81a25910068b07ea7c57923392e373f7fb34dd54
AV <27rurk@gmail.com>
2018-11-20 23:26:47 +0300

    autocommit on change

--- packages/compress/lzo/package.mk
+++ packages/compress/lzo/package.mk
@@ -7,9 +7,9 @@ PKG_SHA256="c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.oberhumer.com/opensource/lzo"
 PKG_URL="http://www.oberhumer.com/opensource/lzo/download/$PKG_NAME-$PKG_VERSION.tar.gz"
-#PKG_DEPENDS_HOST="cmake:host ninja:host"
+PKG_DEPENDS_HOST="cmake:host ninja:host"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_TOOLCHAIN="configure"
+#PKG_TOOLCHAIN="configure"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto -fno-semantic-interposition "
@@ -23,7 +23,5 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/libexec
 }
 
-
 PKG_CONFIGURE_OPTS_TARGET="--disable-static --enable-shared"
-
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
