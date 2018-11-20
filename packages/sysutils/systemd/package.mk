35ad5ac77930f798925bd72e90ec3eec0eb21ef2
AV <27rurk@gmail.com>
2018-11-21 02:51:12 +0300

    autocommit on change

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -90,6 +90,9 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fno-schedule-insns -fno-schedule-insns2 -Wno-format-truncation"
   export LC_ALL=en_US.UTF-8
+
+  export CFLAGS="$CFLAGS -fstack-protector-strong -mzero-caller-saved-regs=used"
+  export CXXFLAGS="$CXXFLAGS -fstack-protector-strong -mzero-caller-saved-regs=used"
 }
 
 post_makeinstall_target() {
@@ -151,8 +154,8 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/lib/systemd/system/*.target.wants/systemd-user-sessions.service
 
   # remove nspawn
-  rm -rf $INSTALL/usr/bin/systemd-nspawn
-  rm -rf $INSTALL/usr/lib/systemd/system/systemd-nspawn@.service
+  # rm -rf $INSTALL/usr/bin/systemd-nspawn
+  # rm -rf $INSTALL/usr/lib/systemd/system/systemd-nspawn@.service
 
   # remove genetators/catalog
   rm -rf $INSTALL/usr/lib/systemd/system-generators
