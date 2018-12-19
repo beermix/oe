36bc4b347826989533a32fbcdb1095a898b76771
AV <27rurk@gmail.com>
2018-12-19 22:03:13 +0300

    autocommit on change

--- packages/multimedia/intel-vaapi-driver/package.mk
+++ packages/multimedia/intel-vaapi-driver/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-vaapi-driver"
-PKG_VERSION="2.2.0"
-PKG_SHA256="13eb518bd168106a64d8e1c0f0a72e9b9937e6fd9a4c713a10f51e52508ea9b2"
+PKG_VERSION="2.3.0"
+PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/intel/intel-vaapi-driver/releases"
 PKG_URL="https://github.com/intel/intel-vaapi-driver/archive/$PKG_VERSION.tar.gz"
