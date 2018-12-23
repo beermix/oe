e82e307f2997d813fb5fcbbc92f865e0654196db
AV <27rurk@gmail.com>
2018-12-24 01:12:51 +0300

    autocommit on change

--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://www.gnu.org/software/make/"
 PKG_URL="http://ftpmirror.gnu.org/make/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_TOOLCHAIN="manual"
+PKG_DEPENDS_HOST=""
 
 export CC=$LOCAL_CC
 
@@ -18,12 +18,12 @@ export LDFLAGS="-Wl,-z,relro -Wl,-z,now -s"
 PKG_CONFIGURE_OPTS_HOST="--without-guile"
 
 post_makeinstall_host() {
- #ln -sf make $TOOLCHAIN/bin/gmake
- #strip $TOOLCHAIN/bin/make
+  ln -sf make $TOOLCHAIN/bin/gmake
+ strip $TOOLCHAIN/bin/make
 
- mkdir -p $TOOLCHAIN/bin
- ln -sf /bin/make $TOOLCHAIN/bin/gmake
- ln -sf /bin/make $TOOLCHAIN/bin/make
+ #mkdir -p $TOOLCHAIN/bin
+ #ln -sf /bin/make $TOOLCHAIN/bin/gmake
+ #ln -sf /bin/make $TOOLCHAIN/bin/make
 
  #cp -r $PKG_DIR/src/bin/help2man $TOOLCHAIN/bin/
 
@@ -31,7 +31,7 @@ post_makeinstall_host() {
 
 #  cp -r $PKG_DIR/src/bin/* $TOOLCHAIN/bin/
 #  cp -r -i $PKG_DIR/src/m4/* $TOOLCHAIN/share/aclocal/
-  
+
 #  mkdir -p $TOOLCHAIN/share/include
 #  cp -r -i $PKG_DIR/include/* $TOOLCHAIN/share/include
 }
