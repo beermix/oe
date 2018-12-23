1f298c8c96bb2f649984f54d7cff1beb3e8d19d6
AV <27rurk@gmail.com>
2018-12-23 17:36:13 +0300

    autocommit on change

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -8,15 +8,11 @@
 PKG_NAME="fakeroot"
 PKG_VERSION="1.23"
 PKG_SHA256="009cd6696a931562cf1c212bb57ca441a4a2d45cd32c3190a35c7ae98506f4f6"
-PKG_ARCH="any"
 PKG_LICENSE="GPL3"
 PKG_SITE="http://fakeroot.alioth.debian.org/"
 PKG_URL="http://ftp.debian.org/debian/pool/main/f/fakeroot/${PKG_NAME}_${PKG_VERSION}.orig.tar.xz"
 PKG_DEPENDS_HOST="ccache:host libcap:host"
 PKG_SECTION="toolchain/devel"
-PKG_SHORTDESC="fakeroot: provides a fake root environment by means of LD_PRELOAD and SYSV IPC (or TCP) trickery."
 PKG_LONGDESC="fakeroot provides a fake root environment by means of LD_PRELOAD and SYSV IPC (or TCP) trickery."
 
-PKG_CONFIGURE_OPTS_HOST="ac_cv_header_sys_capability_h=no \
-			    ac_cv_func_capset=no \
-			    --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="--with-gnu-ld"
