fe1c2c5eac5b4f5c18d63c17cf9e3360afd628b7
AV <27rurk@gmail.com>
2018-12-24 01:01:09 +0300

    autocommit on change

--- packages/debug/gdb/package.mk
+++ packages/debug/gdb/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="gdb"
-PKG_VERSION="8.2"
-PKG_SHA256="c3a441a29c7c89720b734e5a9c6289c0a06be7e0c76ef538f7bbcef389347c39"
+PKG_VERSION="8.2.1"
+PKG_SHA256="0a6a432907a03c5c8eaad3c3cffd50c00a40c3a5e3c4039440624bae703f2202"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/gdb/"
 PKG_URL="http://ftpmirror.gnu.org/gdb/$PKG_NAME-$PKG_VERSION.tar.xz"
