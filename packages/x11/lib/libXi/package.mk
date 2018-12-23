ed7b81e88ddf25330ad7acb6a7c7de6a06aa59c6
AV <27rurk@gmail.com>
2018-12-23 19:32:54 +0300

    autocommit on change

--- packages/x11/lib/libXi/package.mk
+++ packages/x11/lib/libXi/package.mk
@@ -5,19 +5,16 @@
 PKG_NAME="libXi"
 PKG_VERSION="1.7.9"
 PKG_SHA256="c2e6b8ff84f9448386c1b5510a5cf5a16d788f76db018194dacdc200180faf45"
-PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain util-macros libX11 libXfixes"
 PKG_SECTION="x11/lib"
-PKG_SHORTDESC="libxi: X11 Input extension library"
-PKG_LONGDESC="LibXi provides an X Window System client interface to the XINPUT extension to the X protocol."
 PKG_BUILD_FLAGS="+pic"
 
 pre_configure_target() {
-  export CFLAGS="$CFLAGS -Os -fdata-sections -ffunction-sections -fno-semantic-interposition "
-  export CXXFLAGS="$CXXFLAGS -Os -fdata-sections -ffunction-sections -fno-semantic-interposition "
+  export CFLAGS="$CFLAGS -Os -fdata-sections -ffunction-sections -fno-semantic-interposition"
+  export CXXFLAGS="$CXXFLAGS -Os -fdata-sections -ffunction-sections -fno-semantic-interposition"
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-malloc0returnsnull \
