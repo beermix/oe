530f11eb809b1fd6040abebdc3398e455741eb7f
AV <27rurk@gmail.com>
2018-12-24 00:15:07 +0300

    autocommit on change

--- packages/devel/re2/package.mk
+++ packages/devel/re2/package.mk
@@ -1,9 +1,9 @@
 PKG_NAME="re2"
-PKG_VERSION="2018-10-01"
+PKG_VERSION="2018-12-01"
 PKG_SITE="https://github.com/google/re2/releases"
 PKG_URL="https://github.com/google/re2/archive/$PKG_VERSION.tar.gz"
 PKG_TOOLCHAIN="manual"
-PKG_BUILD_FLAGS="+piv"
+PKG_BUILD_FLAGS="+pic"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 -DBUILD_TESTING=0 -DRE2_BUILD_TESTING=0 -DCMAKE_BUILD_TYPE=Release"
 
