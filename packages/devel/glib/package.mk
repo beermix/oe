0292069a2d387e86208c7370e4839ad355d6cfa1
AV <27rurk@gmail.com>
2018-12-24 23:40:37 +0300

    autocommit on change

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -4,8 +4,8 @@
 # https://github.com/GNOME/glib/tree/glib-2-58
 
 PKG_NAME="glib"
-PKG_VERSION="2.58.2"
-PKG_SHA256=""
+PKG_VERSION="2.59.0"
+PKG_SHA256="664a5dee7307384bb074955f8e5891c7cecece349bbcc8a8311890dc185b428e"
 #PKG_VERSION="17519e039f29b5ffd0aad5ed1661bc56eb1dfeaa"
 #PKG_SHA256="527cdb675eb1d86d057e4d6ed878eb0d0f1b7d6fa672333deab34dd6c2e7bd9c"
 PKG_LICENSE="LGPL"
@@ -15,7 +15,7 @@ PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/${PKG_VERSION%.*}/$PKG_NAME
 PKG_DEPENDS_TARGET="toolchain zlib libffi pcre Python2:host util-linux"
 PKG_DEPENDS_HOST="libffi:host"
 
-PKG_MESON_OPTS_HOST="-Dselinux=false \
+PKG_MESON_OPTS_HOST="-Dselinux=disabled \
 			-Dlibmount=false \
 			-Dman=false \
 			-Dgtk_doc=false \
@@ -24,7 +24,7 @@ PKG_MESON_OPTS_HOST="-Dselinux=false \
 			-Dforce_posix_threads=true \
 			-Ddefault_library=static"
 
-PKG_MESON_OPTS_TARGET="-Dselinux=false \
+PKG_MESON_OPTS_TARGET="-Dselinux=disabled \
 			  -Dlibmount=false \
 			  -Dman=false \
 			  -Dgtk_doc=false \
