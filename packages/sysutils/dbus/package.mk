06cec1ad3389e0d794564faf6ba6b9d7b885ecd3
AV <27rurk@gmail.com>
2018-12-25 08:48:09 +0300

    autocommit on change

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -2,9 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="dbus"
-#PKG_VERSION="1.12.12"
-#PKG_SHA256="9546f226011a1e5d9d77245fe5549ef25af4694053189d624d0d6ac127ecf5f8"
-PKG_VERSION="1.13.8"
+PKG_VERSION="1.12.12"
+PKG_SHA256="9546f226011a1e5d9d77245fe5549ef25af4694053189d624d0d6ac127ecf5f8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://dbus.freedesktop.org/releases/dbus/?C=M;O=D"
 PKG_URL="https://dbus.freedesktop.org/releases/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -34,10 +33,10 @@ PKG_CONFIGURE_OPTS_TARGET="export ac_cv_have_abstract_sockets=yes \
                            --runstatedir=/run \
                            --with-system-socket=/run/dbus/system_bus_socket"
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -fstack-protector-strong -mzero-caller-saved-regs=used "
-  export CXXFLAGS="$CXXFLAGS -fstack-protector-strong -mzero-caller-saved-regs=used "
-}
+#pre_configure_target() {
+#  export CFLAGS="$CFLAGS -fstack-protector-strong -mzero-caller-saved-regs=used "
+#  export CXXFLAGS="$CXXFLAGS -fstack-protector-strong -mzero-caller-saved-regs=used "
+#}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/etc/rc.d
