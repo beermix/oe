b590985e702fe40a4e4b4c5546bbd85dd92d39e5
AV <27rurk@gmail.com>
2018-12-25 06:55:19 +0300

    autocommit on change

--- packages/devel/json-c/package.mk
+++ packages/devel/json-c/package.mk
@@ -1,11 +1,10 @@
 PKG_NAME="json-c"
-PKG_VERSION="985c46f"
+PKG_VERSION="39c9fa0"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/json-c/json-c/wiki"
 PKG_URL="https://github.com/json-c/json-c/archive/$PKG_VERSION.tar.gz"
 #PKG_URL="https://s3.amazonaws.com/json-c_releases/releases/json-c-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="accessibility"
 PKG_TOOLCHAIN="autotools"
 #PKG_TOOLCHAIN="configure"
 
