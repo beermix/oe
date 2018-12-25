f9487bf77929dd3f7be78964a955775403cba626
AV <27rurk@gmail.com>
2018-12-25 14:45:51 +0300

    autocommit on change

--- packages/-my/mc/package.mk
+++ packages/-my/mc/package.mk
@@ -2,7 +2,6 @@ PKG_NAME="mc"
 PKG_VERSION="4.8.21"
 PKG_URL="http://ftp.midnight-commander.org/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libtool:host e2fsprogs util-linux pcre fuse libssh2"
-PKG_SECTION="tools"
 PKG_TOOLCHAIN="autotools"
 
 pre_configure_target() {
