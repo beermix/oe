517558de0e91cef66ee9af47e7750645e57e0a47
AV <27rurk@gmail.com>
2018-12-25 09:23:47 +0300

    autocommit on change

--- packages/multimedia/intel-vaapi-driver/package.mk
+++ packages/multimedia/intel-vaapi-driver/package.mk
@@ -10,14 +10,17 @@ PKG_URL="https://github.com/intel/intel-vaapi-driver/archive/$PKG_VERSION.tar.gz
 PKG_DEPENDS_TARGET="toolchain libva libdrm"
 
 post_unpack() {
-  sed -i '1s/python$/&2/' $PKG_BUILD/src/shaders/gpp.py
-  sed -i 's/2.2.0/2.2.0.0/' $PKG_BUILD/meson.build
+  # Only relevant if intel-gpu-tools is installed,
+  # since then the shaders will be recompiled
+  sed -i '1s/python$/&2/' intel-vaapi-driver/src/shaders/gpp.py
+  # Fix undefined variable in src/meson.build
+  sed -i 's/2.2.0/2.2.0.0/' intel-vaapi-driver/meson.build
 }
 
-pre_configure_target() {
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
-  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
-}
+#pre_configure_target() {
+#  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
+#  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
+#}
 
 PKG_MESON_OPTS_TARGET="-Dwith_x11=yes \
 			  -Dwith_wayland=no \
--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -10,10 +10,10 @@ PKG_SITE="https://github.com/intel/libva/releases"
 PKG_URL="https://github.com/intel/libva/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="libX11 libXext libXfixes libdrm"
 
-pre_configure_target() {
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
-  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
-}
+#pre_configure_target() {
+#  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
+#  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-O2 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math|"`
+#}
 
 PKG_MESON_OPTS_TARGET="-Ddocs=false \
 			  -Ddisable_drm=false \
