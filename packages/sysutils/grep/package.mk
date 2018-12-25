956b814c00296455c45079b046f05a69bf8dc45a
AV <27rurk@gmail.com>
2018-12-25 14:15:18 +0300

    autocommit on change

--- packages/sysutils/grep/package.mk
+++ packages/sysutils/grep/package.mk
@@ -3,7 +3,6 @@ PKG_VERSION="3.3"
 PKG_SHA256="b960541c499619efd6afe1fa795402e4733c8e11ebf9fafccc0bb4bccdc5b514"
 PKG_URL="http://ftp.gnu.org/gnu/grep/grep-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline pcre"
-PKG_SECTION="sysutils"
 #PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-perl-regexp"
+PKG_CONFIGURE_OPTS_TARGET="--without-included-regex"
