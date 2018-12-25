3dd36104c137e5bb6c1ad3da6a191a69ef3e236d
AV <27rurk@gmail.com>
2018-12-25 21:12:06 +0300

    autocommit on change

--- packages/network/libssh2/package.mk
+++ packages/network/libssh2/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="libssh2"
-PKG_VERSION="1.8.0"
-PKG_URL="https://www.libssh2.org/download/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_SITE="https://www.libssh2.org/download/?C=M;O=D"
+PKG_VERSION="31aea1e"
+PKG_URL="https://github.com/libssh2/libssh2/archive/$PKG_VERSION.tar.gz"
+#PKG_SITE="https://www.libssh2.org/download/?C=M;O=D"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
@@ -9,7 +9,6 @@ PKG_BUILD_FLAGS="+pic"
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
 			      --with-libz \
-			      --enable-silent-rules \
 			      --disable-examples-build \
 			      --disable-debug \
 			      --with-gnu-ld"
--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -11,7 +11,7 @@ PKG_URL="https://cgit.freedesktop.org/xorg/driver/xf86-video-intel/snapshot/$PKG
 PKG_DEPENDS_TARGET="toolchain libXcomposite libXxf86vm libXdamage libdrm util-macros systemd xorg-server"
 PKG_LONGDESC="The Xorg driver for Intel i810, i815, 830M, 845G, 852GM, 855GM, 865G, 915G, 915GM and 965G."
 PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="-lto -gold"
+PKG_BUILD_FLAGS="-lto -gold -hardening"
 # xf86-video-intel is broken enough. dont link with LTO
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-backlight \
@@ -23,12 +23,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-backlight \
                            --with-xorg-module-dir=$XORG_PATH_MODULES"
 
 pre_configure_target() {
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-fno-plt||g"`
-  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-fno-plt||g"`
-  export LDFLAGS=`echo $LDFLAGS | sed -e "s|,-z,now||g"`
-  export LDFLAGS=`echo $LDFLAGS | sed -e "s| -Wl,-z,now||g"`
-
-  #export CFLAGS="$CFLAGS -D_GNU_SOURCE"
+  export CFLAGS="$CFLAGS -D_GNU_SOURCE"
 }
 
 post_makeinstall_target() {
