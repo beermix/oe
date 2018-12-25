56d018ab55a5fe8ffa5f3b96d7849337e99f72a9
AV <27rurk@gmail.com>
2018-12-25 12:44:45 +0300

    autocommit on change

--- packages/virtual/toolchain/package.mk
+++ packages/virtual/toolchain/package.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv) |  tar:host patch:host gzip:host  bc:host
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv) |  tar:host patch:host gzip:host bc:host
 
 PKG_NAME="toolchain"
 PKG_VERSION=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="configtools:host make:host xz:host sed:host gettext:host pkg-config:host autoconf:host automake:host intltool:host libtool:host autoconf-archive:host bison:host flex:host mpc:host cmake:host xmlstarlet:host yasm:host ninja:host meson:host nasm:host elfutils:host squashfs:host nss:host swig:host binutils:host gcc:host"
+PKG_DEPENDS_TARGET="configtools:host make:host xz:host sed:host gettext:host pkg-config:host autoconf:host automake:host intltool:host libtool:host autoconf-archive:host bison:host flex:host mpc:host gzip:host bc:host cmake:host xmlstarlet:host yasm:host ninja:host meson:host nasm:host elfutils:host squashfs:host nss:host swig:host binutils:host gcc:host"
 PKG_SECTION="virtual"
 PKG_SHORTDESC="toolchain: LibreELEC.tv' toolchain"
