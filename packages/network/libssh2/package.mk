464d6afcb33bb17f041b6163df1c5efeecc4ed6c
AV <27rurk@gmail.com>
2018-12-25 21:16:38 +0300

    autocommit on change

--- packages/network/libssh2/package.mk
+++ packages/network/libssh2/package.mk
@@ -3,8 +3,8 @@ PKG_VERSION="31aea1e"
 PKG_URL="https://github.com/libssh2/libssh2/archive/$PKG_VERSION.tar.gz"
 #PKG_SITE="https://www.libssh2.org/download/?C=M;O=D"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
-PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="+pic"
+PKG_TOOLCHAIN="cmake-make"
+#PKG_BUILD_FLAGS="+pic"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --enable-static \
@@ -12,3 +12,9 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 			      --disable-examples-build \
 			      --disable-debug \
 			      --with-gnu-ld"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+			  -DBUILD_EXAMPLES=0 \
+			  -DBUILD_SHARED_LIBS=0 \
+			  -DBUILD_TESTING=0 \
+			  -DENABLE_ZLIB_COMPRESSION=1"
