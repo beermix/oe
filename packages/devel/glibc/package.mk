ebe1a4fb881745972b08f8dbeb14f4519fc6da46
AV <27rurk@gmail.com>
2018-12-25 19:51:54 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -29,7 +29,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
                            --with-__thread \
                            --with-binutils=$BUILD/toolchain/bin \
                            --with-headers=$SYSROOT_PREFIX/usr/include \
-                           --enable-kernel=4.20 \
+                           --enable-kernel=4.14 \
                            --enable-stack-protector=strong \
                            --enable-static-pie \
                            --without-cvs \
