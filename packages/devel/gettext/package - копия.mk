5af82a1fc3e3d41ee37d37d76f09c4d61368fc42
AV <27rurk@gmail.com>
2018-12-26 19:54:04 +0300

    autocommit on change

--- packages/devel/gettext/package - копия.mk
+++ packages/devel/gettext/package - копия.mk
@@ -7,17 +7,23 @@ PKG_SHA256="ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host bison:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host autotools:host bison:host libxml2:host"
 PKG_LONGDESC="A program internationalization library and tools."
+PKG_TOOLCHAIN="autotools"
+
+post_unpack() {
+  sed '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/examples$$//' $PKG_BUILD/gettext-tools/Makefile.in
+  sed '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/tests$$//' $PKG_BUILD/gettext-runtime/Makefile.in
+}
 
 PKG_CONFIGURE_SCRIPT="gettext-tools/configure"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
-                         --disable-rpath \
-                         --with-gnu-ld \
-                         --disable-java \
+PKG_CONFIGURE_OPTS_HOST="--disable-java \
                          --disable-curses \
-                         --without-included-libxml \
                          --disable-native-java \
                          --disable-csharp \
-                         --without-emacs"
+                         --without-emacs \
+                         --disable-libasprintf \
+                         --disable-acl \
+                         --disable-openmp \
+                         --disable-relocatable"
