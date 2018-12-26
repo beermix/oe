c45f23ec31406c87c3711a1e54f06d19f4c27a62
AV <27rurk@gmail.com>
2018-12-26 19:29:34 +0300

    autocommit on change

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -4,9 +4,9 @@
 PKG_NAME="ccache"
 PKG_SITE="https://www.samba.org/ftp/ccache/?C=M;O=D"
 PKG_VERSION="3.5"
-PKG_SHA256="bdd44b72ae4506a2e2deef9fefb15c606a474bbca7658cd2be26105155eec012"
-PKG_VERSION="3.3.6"
-PKG_SHA256="eac8d2a5055014bebae1434d9b7c66c25d64323800808c27a4534cee167e6bea"
+PKG_SHA256="3a4c81a05b6c98d4e671e00b15f09718da6afabfbeeace9030b067aee1395e66"
+#PKG_VERSION="3.3.6"
+#PKG_SHA256="eac8d2a5055014bebae1434d9b7c66c25d64323800808c27a4534cee167e6bea"
 PKG_LICENSE="GPL"
 PKG_URL="https://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST="make:host"
@@ -17,7 +17,7 @@ export CXX=$LOCAL_CXX
 
 export CFLAGS="-march=native -O2 -fstack-protector-strong"
 export CXXFLAGS="-march=native -O2 -fstack-protector-strong"
-export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -s"
+#export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -s"
 
 PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
 
