4d8108344b6572b91db054d1362c7909d750b4cd
AV <27rurk@gmail.com>
2018-12-26 12:20:47 +0300

    autocommit on change

--- packages/sysutils/procps-ng/package.mk
+++ packages/sysutils/procps-ng/package.mk
@@ -17,6 +17,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
                            ac_cv_func_realloc_0_nonnull=yes \
                            --disable-modern-top \
                            --enable-watch8bit \
+                           --with-systemd \
                            --disable-shared \
                            --disable-kill"
 
