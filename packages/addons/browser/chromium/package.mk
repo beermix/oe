5b179be17698e60f8d4bbfa3b0cff43effe79556
AV <27rurk@gmail.com>
2018-12-26 03:36:28 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -20,7 +20,7 @@ PKG_DEPENDS_TARGET="pciutils systemd dbus libXtst libXcomposite libXcursor unclu
 PKG_SECTION="browser"
 PKG_SHORTDESC="Chromium Browser: the open-source web browser from Google"
 PKG_TOOLCHAIN="manual"
-PKG_BUILD_FLAGS="-lto -gold -hardening"
+PKG_BUILD_FLAGS="-lto +gold -hardening"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Chromium"
