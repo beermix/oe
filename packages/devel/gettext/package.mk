51d7c88f2fb3c448dced244b54ffec7453a022a1
AV <27rurk@gmail.com>
2018-12-26 18:13:24 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -7,8 +7,9 @@ PKG_SHA256="ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host m4:host bison:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host autotools:host bison:host libxml2:host"
 PKG_LONGDESC="A program internationalization library and tools."
+PKG_TOOLCHAIN="autotools"
 
 post_unpack() {
   sed '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/examples$$//' $PKG_BUILD/gettext-tools/Makefile.in
