e9e1afb34b09bb40767a886191109c8dff108513
AV <27rurk@gmail.com>
2018-12-26 22:44:38 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -31,7 +31,6 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/bash \
                            --with-binutils=$BUILD/toolchain/bin \
                            --with-headers=$SYSROOT_PREFIX/usr/include \
                            --enable-kernel=4.19 \
-                           --enable-stack-protector=strong \
                            --without-cvs \
                            --without-gd \
                            --disable-build-nscd \
