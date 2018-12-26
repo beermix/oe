91286107e314c39a2defc56fab234927c676ced1
AV <27rurk@gmail.com>
2018-12-26 23:33:52 +0300

    autocommit on change

--- packages/devel/gperf/package.mk
+++ packages/devel/gperf/package.mk
@@ -3,7 +3,6 @@
 PKG_NAME="gperf"
 PKG_VERSION="3.1"
 #PKG_VERSION="3.0.4"
-PKG_REV="1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/gperf/"
 PKG_URL="https://ftp.gnu.org/pub/gnu/gperf/$PKG_NAME-$PKG_VERSION.tar.gz"
--- packages/lang/yasm/package.mk
+++ packages/lang/yasm/package.mk
@@ -2,22 +2,9 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="yasm"
-PKG_VERSION="80bc804"
+PKG_VERSION="e256985"
 PKG_SITE="https://github.com/yasm/yasm"
 PKG_URL="https://github.com/yasm/yasm/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="ccache:host re2c:host nasm:host"
+PKG_DEPENDS_HOST="ccache:host re2c:host nasm:host gperf:host"
 PKG_SHORTDESC="yasm: A complete rewrite of the NASM assembler"
 PKG_TOOLCHAIN="autotools"
-
-#CCACHE_DISABLE=true
-
-PKG_CONFIGURE_OPTS_HOST="--disable-debug \
-                         --disable-warnerror \
-                         --disable-profiling \
-                         --disable-gcov \
-                         --disable-python-bindings \
-                         --enable-nls \
-                         --disable-rpath \
-                         --without-dmalloc \
-                         --without-libiconv-prefix \
-                         --without-libintl-prefix"
