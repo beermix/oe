a5ac3de2862f283d4231440dd172afea81e480da
AV <27rurk@gmail.com>
2018-12-26 22:46:42 +0300

    autocommit on change

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="3.13.2"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/download/"
 PKG_URL="http://www.cmake.org/files/v${PKG_VERSION%.*}/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:hostt"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="A cross-platform, open-source make system."
 PKG_TOOLCHAIN="configure"
 
