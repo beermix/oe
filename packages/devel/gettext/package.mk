302233e0ad11d4cd7572cebd840399f56df25f5b
AV <27rurk@gmail.com>
2018-12-26 18:00:21 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -1,16 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv) | --with-included-gettext=no --enable-nls --with-pic \
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="gettext"
 PKG_VERSION="0.19.8.1"
 PKG_SHA256="ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="ccache:host m4:host autotools:host libxml2:host bison:host"
-PKG_SHORTDESC="gettext: A program internationalization library and tools"
-#PKG_TOOLCHAIN="autotools"
+PKG_DEPENDS_HOST="ccache:host libxml2:host"
+PKG_LONGDESC="A program internationalization library and tools."
 
 post_unpack() {
   sed '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/examples$$//' $PKG_BUILD/gettext-tools/Makefile.in
@@ -19,10 +18,7 @@ post_unpack() {
 
 PKG_CONFIGURE_SCRIPT="gettext-tools/configure"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
-                         --disable-rpath \
-                         --with-gnu-ld \
-                         --disable-java \
+PKG_CONFIGURE_OPTS_HOST="--disable-java \
                          --disable-curses \
                          --disable-native-java \
                          --disable-csharp \
