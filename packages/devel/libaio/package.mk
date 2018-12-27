a1ad0da6bc1b04d17d548e5d83fae8ba9e802697
AV <27rurk@gmail.com>
2018-12-27 09:24:09 +0300

    autocommit on change

--- packages/devel/libaio/package.mk
+++ packages/devel/libaio/package.mk
@@ -8,7 +8,28 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://lse.sourceforge.net/io/aio.html"
 PKG_URL="http://http.debian.net/debian/pool/main/liba/libaio/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Kernel Asynchronous I/O (AIO) Support for Linux."
-PKG_BUILD_FLAGS="-lto -gold -parallel +pic"
+PKG_BUILD_FLAGS="-lto -gold -parallel"
 
-#CFLAGS:=-nostdlib -nostartfiles -I. $(CFLAGS) $(CPPFLAGS)
+make_target() {
+  make -C src
+}
+
+makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/lib
+    cp -PR src/libaio.a $SYSROOT_PREFIX/usr/lib
+
+  mkdir -p $SYSROOT_PREFIX/usr/include
+    cp -PR src/libaio.h $SYSROOT_PREFIX/usr/include
+}
+
+make_host() {
+  make -C src
+}
+
+makeinstall_host() {
+  mkdir -p $TOOLCHAIN/lib
+    cp -PR src/libaio.a $TOOLCHAIN/lib
+
+  mkdir -p $TOOLCHAIN/include
+    cp -PR src/libaio.h $TOOLCHAIN/include
+}
