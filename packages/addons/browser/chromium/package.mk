3287f45f391b26b4a55020439506a116f5547da6
AV <27rurk@gmail.com>
2018-12-30 11:45:15 +0300

    autocommit on change

--- packages/addons/browser/chromium/package.mk
+++ packages/addons/browser/chromium/package.mk
@@ -10,7 +10,7 @@
 PKG_NAME="chromium"
 PKG_VERSION="64.0.3282.186"
 PKG_SHA256="5fd0218759231ac00cc729235823592f6fd1e4a00ff64780a5fed7ab210f1860"
-PKG_REV="451-static-png-xml2-glibc26-gcc7"
+PKG_REV="452-static-png-xml2-glibc26-gcc7"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Mixed"
 PKG_URL="https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$PKG_VERSION.tar.xz"
@@ -101,7 +101,6 @@ make_target() {
     'enable_hangout_services_extension=true'
     'enable_wayland_server=false'
     'enable_webrtc=true'
-    'is_component_ffmpeg=true'
     "google_api_key=\"${_google_api_key}\""
     "google_default_client_id=\"${_google_default_client_id}\""
     "google_default_client_secret=\"${_google_default_client_secret}\""
