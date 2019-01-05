16b6bec0349f609075f249b0e640e92ffd09ec54
AV <27rurk@gmail.com>
2019-01-05 20:47:45 +0300

    autocommit on change

--- packages/graphics/libdrm/package.mk
+++ packages/graphics/libdrm/package.mk
@@ -12,10 +12,10 @@ PKG_URL="http://dri.freedesktop.org/libdrm/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libpciaccess"
 PKG_TOOLCHAIN="meson"
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-  export CXXFLAGS="$CXXFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-}
+#pre_configure_target() {
+#  export CFLAGS="$CFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+#  export CXXFLAGS="$CXXFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+#}
 
 get_graphicdrivers
 
