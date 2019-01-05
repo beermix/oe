0de55a0e155ae030daa095b9463dd6d5578d71b9
AV <27rurk@gmail.com>
2019-01-05 21:34:31 +0300

    autocommit on change

--- packages/-my/htop/package.mk
+++ packages/-my/htop/package.mk
@@ -6,9 +6,9 @@ PKG_URL="https://github.com/hishamhm/htop/archive/${PKG_VERSION}.tar.gz"
 PKG_SECTION="debug/tools"
 PKG_TOOLCHAIN="autotools"
 
-pre_configure_target() {
-  export LIBS="$LIBS -lm  -ludev -lltdl -lpthread -ldl -lhwloc"
-}
+#pre_configure_target() {
+#  export LIBS="$LIBS -lm  -ludev -lltdl -lpthread -ldl -lhwloc"
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_file__proc_stat=yes \
                            ac_cv_file__proc_meminfo=yes \
@@ -19,5 +19,5 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_file__proc_stat=yes \
                            --disable-unicode \
                            --enable-linux-affinity \
                            --enable-setuid \
-                           --enable-hwloc \
+                           --enable-hwloc=no \
                            --enable-cgroup"
