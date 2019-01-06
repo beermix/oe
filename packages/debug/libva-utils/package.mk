17751bae9db00194621fdf848869b56805f805db
AV <27rurk@gmail.com>
2019-01-06 17:32:47 +0300

    autocommit on change

--- packages/debug/libva-utils/package.mk
+++ packages/debug/libva-utils/package.mk
@@ -5,8 +5,10 @@ PKG_NAME="libva-utils"
 PKG_VERSION="2.3.0"
 PKG_SHA256="f338497b867bbc9bf008e4892eaebda08955785dc7eb2005855bba5f1a20b037"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/intel/libva-utils/releases"
+PKG_SITE="https://github.com/01org/libva-utils"
 PKG_URL="https://github.com/intel/libva-utils/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Libva-utils is a collection of tests for VA-API (VIdeo Acceleration API)"
+PKG_TOOLCHAIN="autotools"
 
 if [ "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="toolchain libva libdrm libX11"
