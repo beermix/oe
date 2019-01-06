b0a0030bdf0e57b57b1837876c06498c3cfddd6e
AV <27rurk@gmail.com>
2019-01-06 05:46:02 +0300

    autocommit on change

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="dbus"
-PKG_VERSION="1.13.8"
-PKG_SHA256="82a89f64e1b55e459725186467770995f33cac5eb8a050b5d8cbeb338078c4f6"
+PKG_VERSION="1.12.12"
+PKG_SHA256="9546f226011a1e5d9d77245fe5549ef25af4694053189d624d0d6ac127ecf5f8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://dbus.freedesktop.org/releases/dbus/?C=M;O=D"
-PKG_URL="https://dbus.freedesktop.org/releases/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://dbus.freedesktop.org/releases/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain expat systemd"
 PKG_LONGDESC="D-Bus is a message bus, used for sending messages between applications."
 
