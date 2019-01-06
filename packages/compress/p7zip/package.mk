0e621511406de44763bfafd8a7e7c622321e6151
AV <27rurk@gmail.com>
2019-01-06 09:38:57 +0300

    autocommit on change

--- packages/compress/p7zip/package.mk
+++ packages/compress/p7zip/package.mk
@@ -6,11 +6,11 @@ PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_TOOLCHAIN="cmake-make"
 
-PKG_CMAKE_SCRIPT="$PKG_BUILD/CPP/7zip/CMAKE/CMakeLists.txt"
-
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
-
+configure_package() {
+  PKG_CMAKE_SCRIPT="$PKG_BUILD/CPP/7zip/CMAKE/CMakeLists.txt"
+  PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
+  PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
+}
 
 makeinstall_host() {
  : # nothing todo
