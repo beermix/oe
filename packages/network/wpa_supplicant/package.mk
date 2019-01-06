e314d1ce6dea31e0bd5c778aeaa408242bf06a64
AV <27rurk@gmail.com>
2019-01-06 06:12:29 +0300

    autocommit on change

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -16,7 +16,7 @@ PKG_MAKE_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 PKG_MAKEINSTALL_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 
 configure_target() {
-  LDFLAGS="$LDFLAGS -lpthread -lm"
+  export LIBS="$LIBS -lpthread -lrt -lm"
 
   cp $PKG_DIR/config/makefile.config wpa_supplicant/.config
 
