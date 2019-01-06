cd1b47cc83f397b1f5f20570b4404d14652f9752
AV <27rurk@gmail.com>
2019-01-06 04:21:16 +0300

    autocommit on change

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -5,9 +5,11 @@
 PKG_NAME="ccache"
 PKG_VERSION="3.5.1a"
 PKG_SHA256="98b6386cbac1fe43dde209ecb65591803dc7fcc16153b824d58dee5908691ed5"
+PKG_VERSION="3.5"
+PKG_SHA256="bdd44b72ae4506a2e2deef9fefb15c606a474bbca7658cd2be26105155eec012"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.samba.org/ftp/ccache/?C=M;O=D"
-PKG_URL="https://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="make:host zlib:host"
 PKG_LONGDESC="A compiler cache to speed up re-compilation of C/C++ code by caching."
 
