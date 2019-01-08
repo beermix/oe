015a15db1f9f248906961c824e2617b5ed7e5440
AV <27rurk@gmail.com>
2019-01-08 06:35:14 +0300

    autocommit on change

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -7,7 +7,7 @@ PKG_SITE="http://www.gnu.org/software/readline/"
 PKG_URL="http://git.savannah.gnu.org/cgit/readline.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
-
+	
 PKG_CONFIGURE_OPTS_TARGET="bash_cv_wcwidth_broken=no --with-curses"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
