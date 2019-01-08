49963afa3b957e2dee712741ced14a9658837042
AV <27rurk@gmail.com>
2019-01-08 05:35:42 +0300

    autocommit on change

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -3,13 +3,13 @@
 
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
-PKG_REV="153-glibc2.25-gcc8"
+PKG_REV="155-glibc28.900-gcc8"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.google.com/chrome"
 PKG_DEPENDS_TARGET="toolchain at-spi2-atk atk cairo \
                     cups gdk-pixbuf gtk3 harfbuzz \
-                    libXtst libXcursor libxss nss pango scrnsaverproto unclutter libnotify icu"
+                    libXtst libXcursor libxss nss pango scrnsaverproto unclutter libnotify icu re2 snappy"
 PKG_SECTION="browser"
 PKG_SHORTDESC="Google Chrome Browser"
 PKG_LONGDESC="Google Chrome Browser"
@@ -88,10 +88,10 @@ addon() {
   cp -PL $(get_build_dir libnotify)/.install_pkg/usr/lib/libnotify.so.* $ADDON_BUILD/$PKG_ADDON_ID/lib
 
   # re2
-  # cp -PL $(get_build_dir re2)/.install_pkg/usr/lib/libre2.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -PL $(get_build_dir re2)/.install_pkg/usr/lib/libre2.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
 
   # snappy
-  # cp -PL $(get_build_dir snappy)/.install_pkg/usr/lib/libsnappy.so.* $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -PL $(get_build_dir snappy)/.install_pkg/usr/lib/libsnappy.so.* $ADDON_BUILD/$PKG_ADDON_ID/lib
 
   # icu
   cp -PL $(get_build_dir icu)/.install_pkg/usr/lib/libicuuc.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
--- packages/compress/snappy/package.mk
+++ packages/compress/snappy/package.mk
@@ -4,15 +4,15 @@ PKG_LICENSE="BSD/GPLv2"
 PKG_SITE="https://github.com/google/snappy"
 PKG_URL="https://github.com/google/snappy/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_BUILD_FLAGS="+pic"
-PKG_TOOLCHAIN="cmake-make"
+#PKG_BUILD_FLAGS="+pic"
+#PKG_TOOLCHAIN="cmake-make"
 
 pre_configure_target() {
  export CXXFLAGS="$CXXFLAGS -DNDEBUG"
 }
 
 configure_package() {
-  PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0"
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release"
 }
 
 post_makeinstall_target() {
