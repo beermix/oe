f89fc0791ef0eb2be49f692ea9834793aa0e3529
AV <27rurk@gmail.com>
2019-01-21 01:02:22 +0300

    autocommit on change

--- packages/lang/yasm/package.mk
+++ packages/lang/yasm/package.mk
@@ -8,7 +8,7 @@ PKG_URL="https://github.com/yasm/yasm/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host gperf:host re2c:host nasm:host"
 PKG_SHORTDESC="yasm: A complete rewrite of the NASM assembler"
 PKG_TOOLCHAIN="autotools"
-PKG_TOOLCHAIN="cmake-make"
+#PKG_TOOLCHAIN="cmake-make"
 
 configure_package() {
   PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0"
