2f871dc3d47ec0ed595d7705b78ada83a5e3fa4d
AV <27rurk@gmail.com>
2019-01-20 18:37:44 +0300

    autocommit on change

--- packages/-my/atop/package.mk
+++ packages/-my/atop/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="atop"
-PKG_VERSION="4516bd3"
+PKG_VERSION="c3073ee"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://github.com/Atoptool/atop/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain systemd"
