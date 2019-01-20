281c0e3aca5fc508ec6915a73923af8c3b4cba78
AV <27rurk@gmail.com>
2019-01-20 23:09:46 +0300

    autocommit on change

--- packages/security/nettle/package.mk
+++ packages/security/nettle/package.mk
@@ -1,11 +1,9 @@
 PKG_NAME="nettle"
-PKG_VERSION="3.4"
-PKG_ARCH="any"
+PKG_VERSION="3.4.1"
+PKG_SHA256="f941cf1535cd5d1819be5ccae5babef01f6db611f9b5a777bae9c7604b8a92ad"
 PKG_SITE="https://ftp.gnu.org/gnu/nettle/?C=M;O=D"
 PKG_URL="https://ftp.gnu.org/gnu/nettle/nettle-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="escalade/depends"
-PKG_SHORTDESC="Nettle is a cryptographic library that is designed to fit easily in more or less any context: In crypto toolkits for object-oriented languages (C++, Python, Pike, ...), in applications like LSH or GNUPG, or even in kernel space."
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-documentation \
 			      --disable-openssl \
