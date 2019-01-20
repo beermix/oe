b25cef7b5b7f74905b3809fd2023b1a143619ddb
AV <27rurk@gmail.com>
2019-01-20 21:42:56 +0300

    autocommit on change

--- packages/devel/gconf=/package.mk
+++ packages/devel/gconf=/package.mk
@@ -1,24 +1,5 @@
-################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
-
 PKG_NAME="gconf"
 PKG_VERSION="3.2.6"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://eigen.tuxfamily.org/index.php?title=Main_Page"
 PKG_URL="https://dl.dropboxusercontent.com/s/a3nfo3i31adwhj2/gconf-3.2.6.tar.xz"
