6a7fe187d9120bc9a8d148bec09cab80d7e8fb21
AV <27rurk@gmail.com>
2019-01-20 19:03:31 +0300

    autocommit on change

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -3,10 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ccache"
-#PKG_VERSION="3.5.1a"
-#PKG_SHA256="98b6386cbac1fe43dde209ecb65591803dc7fcc16153b824d58dee5908691ed5"
-PKG_VERSION="3.5"
-PKG_SHA256="bdd44b72ae4506a2e2deef9fefb15c606a474bbca7658cd2be26105155eec012"
+PKG_VERSION="3.6"
+PKG_SHA256="a6b129576328fcefad00cb72035bc87bc98b6a76aec0f4b59bed76d67a399b1f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.samba.org/ftp/ccache/?C=M;O=D"
 PKG_URL="https://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.xz"
