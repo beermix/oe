2bc58640d2acd0be77f3fc284a6d91934355441c
AV <27rurk@gmail.com>
2019-01-21 15:17:06 +0300

    autocommit on change

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -196,7 +196,7 @@ configure_target() {
               --disable-neon \
               --enable-yasm \
               --disable-symver \
-              --disable-lto \
+              --enable-lto \
               --enable-indev=x11grab_xcb
 }
 
