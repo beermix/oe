aa0a727dff72ae763d427eb180be2aca96058bfc
AV <27rurk@gmail.com>
2019-01-21 03:28:24 +0300

    autocommit on change

--- packages/databases/db/package.mk
+++ packages/databases/db/package.mk
@@ -5,21 +5,22 @@ PKG_VERSION="4.8.30.NC"
 #PKG_VERSION="6.2.32.NC" 			      --with-mutex=POSIX/pthreads/library \
 PKG_URL="http://download.oracle.com/berkeley-db/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="databases"
+PKG_TOOLCHAIN="configure"
 
-PKG_CONFIGURE_SCRIPT="dist/configure"
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-			      --enable-static \
-			      --disable-java \
-			      --disable-tcl \
-			      --enable-compat185 \
-			      --disable-debug \
-			      --enable-cxx \
-			      --with-pic \
-			      --disable-atomicsupport"
+configure_package() {
+  PKG_CONFIGURE_SCRIPT="$PKG_BUILD/dist/configure"
+  PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
+  				 --enable-static \
+  				 --disable-java \
+  				 --disable-tcl \
+  				 --enable-compat185 \
+  				 --disable-debug \
+  				 --enable-cxx \
+  				 --with-pic \
+  				 --disable-atomicsupport"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
+}
 
 post_makeinstall_target() {
   rm -rf $INSTALL
