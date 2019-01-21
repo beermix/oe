5f38f72212e68f699d9fe19e3e8975fdf0e5841a
AV <27rurk@gmail.com>
2019-01-21 12:19:19 +0300

    autocommit on change

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -12,7 +12,7 @@ PKG_SOURCE_DIR="xbmc-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain JsonSchemaBuilder:host TexturePacker:host xmlstarlet:host Python2 zlib systemd pciutils lzo pcre swig:host libass curl fontconfig fribidi tinyxml libjpeg-turbo freetype libcdio taglib libxml2 libxslt rapidjson sqlite ffmpeg crossguid giflib libdvdnav"
 PKG_LONGDESC="A free and open source cross-platform media player."
 PKG_TOOLCHAIN="cmake-make"
-PKG_BUILD_FLAGS="-lto -gold -hardening"
+#PKG_BUILD_FLAGS="-lto -hardening"
 
 configure_package() {
   PKG_CMAKE_SCRIPT="$PKG_BUILD/project/cmake/CMakeLists.txt"
