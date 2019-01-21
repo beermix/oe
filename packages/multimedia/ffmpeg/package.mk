2d7591b027ba817962af9a426687b89678b7ff4f
AV <27rurk@gmail.com>
2019-01-21 15:29:42 +0300

    autocommit on change

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -73,10 +73,8 @@ pre_configure_target() {
   cd $PKG_BUILD
   rm -rf .$TARGET_NAME
 
-  #export CFLAGS="$CFLAGS --param=ssp-buffer-size=4 -fstack-protector -mzero-caller-saved-regs"
-  #export CXXFLAGS="$CXXFLAGS --param=ssp-buffer-size=4 -fstack-protector -mzero-caller-saved-regs"
-
-  # -falign-functions=32 -fno-math-errno -fno-semantic-interposition -fno-trapping-math 
+  export CFLAGS="$CFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+  export CXXFLAGS="$CXXFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
 
   if [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
     CFLAGS="-I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux $CFLAGS"
@@ -196,7 +194,7 @@ configure_target() {
               --disable-neon \
               --enable-yasm \
               --disable-symver \
-              --enable-lto \
+              --disable-lto \
               --enable-indev=x11grab_xcb
 }
 
