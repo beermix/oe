b93d05c2b40f37f5fc1fbbc90d5e5f0d8200bd2f
AV <27rurk@gmail.com>
2019-01-23 02:30:42 +0300

    autocommit on change

--- packages/compress/snappy/package.mk
+++ packages/compress/snappy/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="snappy"
-PKG_VERSION="ea660b5"
+PKG_VERSION="3f194ac"
 PKG_LICENSE="BSD/GPLv2"
 PKG_SITE="https://github.com/google/snappy"
 PKG_URL="https://github.com/google/snappy/archive/${PKG_VERSION}.tar.gz"
