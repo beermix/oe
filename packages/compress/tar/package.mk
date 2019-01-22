2a417981289284304f97d4a77c232ffe42042c45
AV <27rurk@gmail.com>
2019-01-22 21:31:03 +0300

    autocommit on change

--- packages/compress/tar/package.mk
+++ packages/compress/tar/package.mk
@@ -4,10 +4,10 @@ PKG_URL="http://mirrors.kernel.org/gnu/tar/tar-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 
 PKG_CONFIGURE_OPTS_TARGET="--without-selinux \
-			      --without-posix-acls \
-			      --without-selinux \
-			      --without-xattrs \
-			      --disable-acl \
-			      --disable-nls"
+			   --without-posix-acls \
+			   --without-selinux \
+			   --without-xattrs \
+			   --disable-acl \
+			   --disable-nls"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
