002dcb1b59953c82f1bba8a26fbac4179f5698aa
AV <27rurk@gmail.com>
2019-01-23 15:54:51 +0300

    autocommit on change

--- packages/addons/chrome-depends/pango/package.mk
+++ packages/addons/chrome-depends/pango/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pango"
-PKG_VERSION="1.42.4"
-#PKG_SHA256="d2c0c253a5328a0eccb00cdd66ce2c8713fabd2c9836000b6e22a8b06ba3ddd2"
+PKG_VERSION="1.43.0"
+PKG_SHA256="d2c0c253a5328a0eccb00cdd66ce2c8713fabd2c9836000b6e22a8b06ba3ddd2"
 PKG_LICENSE="GPL"
 PKG_SITE="https://ftp.gnome.org/pub/gnome/sources/pango/?C=M;O=D"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/pango/${PKG_VERSION:0:4}/pango-$PKG_VERSION.tar.xz"
@@ -12,5 +12,4 @@ PKG_DEPENDS_TARGET="toolchain cairo freetype fontconfig fribidi glib harfbuzz li
 PKG_DEPENDS_HOST="fribidi:host"
 PKG_TOOLCHAIN="meson"
 
-PKG_MESON_OPTS_TARGET="-Denable_docs=false \
-                       -Dgir=false"
+PKG_MESON_OPTS_TARGET="-Denable_docs=false -Dgir=false"
--- packages/compress/minizip/package.mk
+++ packages/compress/minizip/package.mk
@@ -2,16 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="minizip"
-PKG_VERSION="1.1"
-PKG_SHA256="5666b5ee3e85dfd2dd119970613c12e6267d31813f07d3ffa5d359fe272cb6d1"
+PKG_VERSION="2.8.2"
+PKG_SHA256=""
 PKG_LICENSE="zlib"
 PKG_SITE="https://github.com/nmoinvaz/minizip"
 PKG_URL="https://github.com/nmoinvaz/minizip/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="zlib"
+PKG_DEPENDS_TARGET="zlib bzip2 openssl"
 PKG_LONGDESC="Minizip zlib contribution fork with latest bug fixes"
 
-PKG_CMAKE_OPTS_TARGET="-DUSE_AES=OFF \
-                       -DBUILD_TEST=ON"
+PKG_CMAKE_OPTS_TARGET="-DUSE_BZIP2=ON -DUSE_OPENSSL=ON -DUSE_ZLIB=ON"
 
 makeinstall_target() {
   cp -v miniunz_exec $SYSROOT_PREFIX/usr/bin/miniunz
