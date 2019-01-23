716cd1d45ff8ae3b44b4a6bc6d3b8fc89359f622
AV <27rurk@gmail.com>
2019-01-23 03:54:41 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host m4:host automake:host bison:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host m4:host automake:host autoconf-archive:host bison:host libxml2:host"
 PKG_LONGDESC="A program internationalization library and tools."
 
 configure_package() {
