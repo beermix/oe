0e1c89b4d37d93fe5a541503105cf32b4b8e4563
AV <27rurk@gmail.com>
2019-01-26 00:35:52 +0300

    autocommit on change

--- packages/devel/flex=git/package.mk
+++ packages/devel/flex=git/package.mk
@@ -2,16 +2,16 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="flex"
-PKG_VERSION="2.6.4"
-PKG_SHA256="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
+PKG_VERSION="98018e3"
+PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/westes/flex/releases"
-PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="ccache:host m4:host"
+PKG_URL="https://github.com/westes/flex/archive/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="A tool for generating programs that perform pattern-matching on text."
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-doc --enable-shared --enable-static"
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
 
 post_makeinstall_host() {
   cat > $TOOLCHAIN/bin/lex << "EOF"
@@ -22,4 +22,3 @@ EOF
   chmod -v 755 $TOOLCHAIN/bin/lex
 }
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-program --disable-doc"
