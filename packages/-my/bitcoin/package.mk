9ca2e0a0f213cc37a56aa7cf978ff0bb4d441acc
AV <27rurk@gmail.com>
2019-01-25 16:16:58 +0300

    autocommit on change

--- packages/-my/bitcoin/package.mk
+++ packages/-my/bitcoin/package.mk
@@ -1,5 +1,6 @@
 PKG_NAME="bitcoin"
-PKG_VERSION="0.16.2"
+PKG_VERSION="0.17.0"
+PKG_SHA256="98ef39e851644bba1ac51edaa53c7c47c20a3980333def7bc642c8261a12785e"
 PKG_SITE="https://bitcoin.org/bin/"
 PKG_URL="https://bitcoin.org/bin/bitcoin-core-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
 #PKG_SOURCE_DIR="${PKG_NAME}-0.15.0"
