b9333b8ca11609638d1b960ce317691e2f821aa0
AV <27rurk@gmail.com>
2019-01-26 16:25:37 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -10,7 +10,6 @@ PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="ccache:host m4:host automake:host autoconf-archive:host bison:host libxml2:host"
 PKG_LONGDESC="A program internationalization library and tools."
-PKG_TOOLCHAIN="autotools"
 
 configure_package() {
   PKG_CONFIGURE_SCRIPT="${PKG_BUILD}/gettext-tools/configure"
