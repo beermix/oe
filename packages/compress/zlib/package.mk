b898acdb2821b04dea4188212373ed74e01dca80
AV <27rurk@gmail.com>
2019-01-27 22:37:03 +0300

    autocommit on change

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,16 +2,16 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="a346a9f"
-PKG_SHA256=""
+PKG_VERSION="1.2.11"
+PKG_SHA256="4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066"
+PKG_LICENSE="OSS"
 PKG_SITE="http://www.zlib.net"
 PKG_URL="http://zlib.net/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_URL="https://github.com/Dead2/zlib-ng/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="nss:host"
-PKG_DEPENDS_TARGET="toolchain nss"
+PKG_DEPENDS_HOST=""
+PKG_DEPENDS_TARGET="toolchain zlib:host"
 PKG_LONGDESC="A general purpose (ZIP) data compression library."
 PKG_TOOLCHAIN="configure"
-#PKG_BUILD_FLAGS="+pic:host +pic"
+#PKG_BUILD_FLAGS="+pic:host +pic +hardening"
 
 post_unpack() {
   mkdir -p $PKG_BUILD/.$HOST_NAME
@@ -22,8 +22,8 @@ post_unpack() {
 }
 
 pre_configure_target() {
-  export CFLAGS="$CFLAGS -O3"
-  export CXXFLAGS="$CXXFLAGS -O3"
+  export CFLAGS="$CFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+  export CXXFLAGS="$CXXFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
 }
 
 pre_configure_host() {
@@ -33,32 +33,32 @@ pre_configure_host() {
 
 configure_target() {
   cd $PKG_BUILD/.$TARGET_NAME
-  ./configure --prefix=/usr --zlib-compat --libdir=/usr/lib
+  ./configure --prefix=/usr --libdir=/usr/lib --static --shared
 }
 
 configure_host() {
   cd $PKG_BUILD/.$HOST_NAME
-  ./configure --prefix=$TOOLCHAIN --zlib-compat --libdir=$TOOLCHAIN/lib
+  ./configure --prefix=$TOOLCHAIN --libdir=$TOOLCHAIN/lib --static --shared
 }
 
-#post_configure_target() {
+post_configure_target() {
  ## configure minizip
-# (
-#  cd $PKG_BUILD/.$TARGET_NAME/contrib/minizip
-#  rm Makefile
-#  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:../../"
-#  do_autoreconf
-#  ./configure --host=$TARGET_NAME --build=$HOST_NAME $TARGET_CONFIGURE_OPTS --disable-static
-# )
-#}
+ (
+  cd $PKG_BUILD/.$TARGET_NAME/contrib/minizip
+  rm Makefile
+  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:../../"
+  do_autoreconf
+  ./configure --host=$TARGET_NAME --build=$HOST_NAME $TARGET_CONFIGURE_OPTS --disable-static
+ )
+}
 
-#post_make_target() {
+post_make_target() {
  # make minizip
-# make -C $PKG_BUILD/.$TARGET_NAME/contrib/minizip
-#}
+ make -C $PKG_BUILD/.$TARGET_NAME/contrib/minizip
+}
 
-#post_makeinstall_target() {
+post_makeinstall_target() {
  # Install minizip
-# make -C $PKG_BUILD/.$TARGET_NAME/contrib/minizip DESTDIR=$SYSROOT_PREFIX install
-# cp -PL $PKG_BUILD/.$TARGET_NAME/contrib/minizip/.libs/libminizip.so* $INSTALL/usr/lib/
-#}
+ make -C $PKG_BUILD/.$TARGET_NAME/contrib/minizip DESTDIR=$SYSROOT_PREFIX install
+ cp -PL $PKG_BUILD/.$TARGET_NAME/contrib/minizip/.libs/libminizip.so* $INSTALL/usr/lib/
+}
