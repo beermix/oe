7fc39b51fb4cb4a9aac7fb2b5ac28e345270cde2
AV <27rurk@gmail.com>
2019-01-28 00:38:03 +0300

    autocommit on change

--- packages/-my/diffutils/package.mk
+++ packages/-my/diffutils/package.mk
@@ -1,14 +1,13 @@
 PKG_NAME="diffutils"
-PKG_VERSION="3.6"
+PKG_VERSION="3.7"
 PKG_SITE="http://www.gnu.org/software/diffutils/"
 PKG_URL="ftp://ftp.gnu.org/gnu/diffutils/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SHORTDESC="GNU Diffutils"
 #PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
-			      --without-libsigsegv-prefix \
-			      --without-libiconv-prefix \
-			      --without-libintl-prefix"
+PKG_CONFIGURE_OPTS_TARGET="--without-libsigsegv-prefix \
+			   --without-libiconv-prefix \
+			    --without-libintl-prefix"
 
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
