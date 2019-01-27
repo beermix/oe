2c9dd26791720f5100f12a4b5f483080683dc2a0
AV <27rurk@gmail.com>
2019-01-28 00:58:35 +0300

    autocommit on change

--- packages/devel/ncurses/package.mk
+++ packages/devel/ncurses/package.mk
@@ -2,65 +2,80 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ncurses"
-PKG_VERSION="6.1-20181215"
-PKG_SHA256="08b07c3e792961f300829512c283d5fefc0b1c421a57b76922c3d13303ed677d"
-PKG_LICENSE="MIT"
-PKG_SITE="http://www.gnu.org/software/ncurses/"
-PKG_URL="http://invisible-mirror.net/archives/ncurses/current/ncurses-$PKG_VERSION.tgz"
-PKG_DEPENDS_TARGET="toolchain zlib ncurses:host"
-PKG_LONGDESC="A library is a free software emulation of curses in System V Release 4.0, and more."
-# causes some segmentation fault's (dialog) when compiled with gcc's link time optimization.
-PKG_BUILD_FLAGS="+pic"
+PKG_VERSION="6.0"
+PKG_SITE="http://invisible-mirror.net/archives/ncurses/current/?C=M;O=D"
+PKG_URL="http://invisible-mirror.net/archives/ncurses/ncurses-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain zlib"
+PKG_DEPENDS_HOST="zlib:host"
+PKG_SECTION="devel"
+PKG_SHORTDESC="ncurses: The ncurses (new curses) library"
+PKG_BUILD_FLAGS="-lto +pic:host +pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--without-ada \
-                           --without-cxx \
-                           --without-cxx-binding \
-                           --disable-db-install \
-                           --without-manpages \
-                           --without-progs \
-                           --without-tests \
-                           --without-shared \
-                           --with-normal \
-                           --without-debug \
-                           --without-profile \
-                           --without-termlib \
-                           --without-ticlib \
-                           --without-gpm \
-                           --without-dbmalloc \
-                           --without-dmalloc \
-                           --disable-rpath \
-                           --disable-database \
-                           --with-fallbacks=linux,screen,xterm,xterm-color \
-                           --with-termpath=/storage/.config/termcap \
-                           --disable-big-core \
-                           --enable-termcap \
-                           --enable-getcap \
-                           --disable-getcap-cache \
-                           --enable-symlinks \
-                           --disable-bsdpad \
-                           --without-rcs-ids \
-                           --enable-ext-funcs \
-                           --disable-const \
-                           --enable-no-padding \
-                           --disable-sigwinch \
-                           --enable-pc-files \
-                           --with-pkg-config-libdir=/usr/lib/pkgconfig \
-                           --disable-tcap-names \
-                           --without-develop \
-                           --disable-hard-tabs \
-                           --disable-xmc-glitch \
-                           --disable-hashmap \
-                           --disable-safe-sprintf \
-                           --disable-scroll-hints \
-                           --disable-widec \
-                           --disable-echo \
-                           --disable-warnings \
-                           --disable-home-terminfo \
-                           --disable-assertions"
+PKG_CONFIGURE_OPTS_TARGET="--enable-overwrite \
+			      --disable-termcap \
+			      --disable-warnings \
+			      --with-termlib=tinfo \
+			      --with-ticlib \
+			      --with-ncursesw \
+			      --disable-rpath \
+			      --without-ada \
+			      --without-tests \
+			      --without-debug \
+			      --without-manpages \
+			      --with-shared \
+			      --enable-static \
+			      --with-progs \
+			      --enable-pc-files \
+			      --enable-widec \
+			      --with-pkg-config-libdir=/usr/lib/pkgconfig \
+			      --with-build-cppflags=-D_GNU_SOURCE"
+			      
+PKG_CONFIGURE_OPTS_HOST="--enable-overwrite \
+			    --disable-termcap \
+			    --disable-warnings \
+			    --without-ncursesw \
+			    --disable-rpath \
+			    --without-ada \
+			    --without-tests \
+			    --without-debug \
+			    --without-manpages \
+			    --without-shared \
+			    --enable-static \
+			    --without-progs \
+			    --enable-pc-files \
+			    --enable-widec \
+			    --with-pkg-config-libdir=$TOOLCHAIN/lib/pkgconfig \
+			    --with-build-cppflags=-D_GNU_SOURCE"
 
 post_makeinstall_target() {
   cp misc/ncurses-config $TOOLCHAIN/bin
   chmod +x $TOOLCHAIN/bin/ncurses-config
-  sed -e "s:\(['=\" ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $TOOLCHAIN/bin/ncurses-config
-  rm -rf $INSTALL/usr/bin
+  sed "s:\(['=\" ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" $TOOLCHAIN/bin/ncurses-config
+
+  ln -sf ncursesw.pc $SYSROOT_PREFIX/usr/lib/pkgconfig/ncurses.pc
+  ln -sf formw.pc $SYSROOT_PREFIX/usr/lib/pkgconfig/form.pc
+  ln -sf menuw.pc $SYSROOT_PREFIX/usr/lib/pkgconfig/menu.pc
+  ln -sf panelw.pc $SYSROOT_PREFIX/usr/lib/pkgconfig/panel.pc
+  ln -sf tinfow.pc $SYSROOT_PREFIX/usr/lib/pkgconfig/tinfo.pc
+  
+  echo "INPUT(-lncursesw -ltinfo)" > $INSTALL/usr/lib/libncurses.so
+  echo "INPUT(-lncursesw -ltinfo)" > $SYSROOT_PREFIX/usr/lib/libncurses.so
+
+  echo "INPUT(-lncursesw -ltinfo)" > $INSTALL/usr/lib/libcursesw.so
+  echo "INPUT(-lncursesw -ltinfo)" > $SYSROOT_PREFIX/usr/lib/libcursesw.so
+  
+  ln -s libncurses.so $INSTALL/usr/lib/libcurses.so
+  ln -sf libncurses.so $SYSROOT_PREFIX/usr/lib/libcurses.so
+   
+  ln -sf libformw.so $SYSROOT_PREFIX/usr/lib/libform.so
+  ln -sf libmenuw.so $SYSROOT_PREFIX/usr/lib/libmenu.so
+  ln -sf libpanelw.so $SYSROOT_PREFIX/usr/lib/libpanel.so
+  ln -sf ticw.so $SYSROOT_PREFIX/usr/lib/tic.so
+  
+  ln -s libformw.so $INSTALL/usr/lib/libform.so
+  ln -s libmenuw.so $INSTALL/usr/lib/libmenu.so
+  ln -s libpanelw.so $INSTALL/usr/lib/libpanel.so
+  ln -s ticw.so $INSTALL/usr/lib/tic.so
+  
+  rm -rf $INSTALL/usr/bin/ncurses*-config
 }
