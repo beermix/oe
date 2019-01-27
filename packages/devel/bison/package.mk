6a0b5b5ae969b96c01722de4a3d592c305bb8255
AV <27rurk@gmail.com>
2019-01-27 20:57:44 +0300

    autocommit on change

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="bison"
-PKG_VERSION="3.2.4"
-PKG_SHA256="523d44419f4df68286503740c95c7b3400b748d7d8b797209195ee5d67f05634"
+PKG_VERSION="3.3"
+PKG_SHA256="162ea71d21e134c44942f4ebb74685e19c942dcf40a7120eba165ba5e2553bb9"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/bison/"
 PKG_URL="http://ftpmirror.gnu.org/bison/$PKG_NAME-$PKG_VERSION.tar.xz"
