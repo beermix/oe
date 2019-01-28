ad879f29f0dcb2611ba4884f5169e670bdc05d9b
AV <27rurk@gmail.com>
2019-01-28 04:48:13 +0300

    autocommit on change

--- packages/devel/re2/package.mk
+++ packages/devel/re2/package.mk
@@ -3,16 +3,19 @@ PKG_VERSION="2019-01-01"
 PKG_SITE="https://github.com/google/re2/releases"
 PKG_URL="https://github.com/google/re2/archive/$PKG_VERSION.tar.gz"
 PKG_TOOLCHAIN="manual"
+#PKG_TOOLCHAIN="cmake-make"
+PKG_BUILD_FLAGS="-lto -gold -hardening"
 #PKG_BUILD_FLAGS="+pic"
 
-#configure_package() {
-#  PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DRE2_BUILD_TESTING=0"
-#}
+configure_package() {
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 -DCMAKE_VERBOSE_MAKEFILE=ON"
+}
 
 pre_build_target() {
   mkdir -p $PKG_BUILD/.$TARGET_NAME
   cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
-  export RE2_LDFLAGS="$RE2_LDFLAGS $LDFLAGS"
+  export LDFLAGS="$LDFLAGS $RE2_LDFLAGS"
+  export CXXFLAGS="$CXXFLAGS $RE2_CXXFLAGS"
 }
 
 makeinstall_target() {
