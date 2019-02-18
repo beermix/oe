962d234e1b0fa458ae97b3ff56fb3849cc47b749
AV <27rurk@gmail.com>
2019-02-19 00:07:16 +0300

    autocommit on change

--- packages/graphics/giflib/package.mk
+++ packages/graphics/giflib/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="giflib"
-PKG_VERSION="5.1.4"
-PKG_SHA256="df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5"
+PKG_VERSION="5.1.6"
+PKG_SHA256="53968f6d39bb4b99f57cc29887a09a3ca500e33b69fb259edaf4f6a622c8b578"
 PKG_LICENSE="OSS"
 PKG_SITE="http://giflib.sourceforge.net/"
-PKG_URL="$SOURCEFORGE_SRC/giflib/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="$SOURCEFORGE_SRC/giflib/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="giflib: giflib service library"
