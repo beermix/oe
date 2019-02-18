fc761644cd0cfc3501be2eda3190e6c8cb89bc60
AV <27rurk@gmail.com>
2019-02-18 21:21:01 +0300

    autocommit on change

--- packages/-my/zsh/package.mk
+++ packages/-my/zsh/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="zsh"
-PKG_VERSION="5.7"
-PKG_SHA256="7807b290b361d9fa1e4c2dfafc78cb7e976e7015652e235889c6eff7468bd613"
+PKG_VERSION="5.7.1"
+PKG_SHA256="7260292c2c1d483b2d50febfa5055176bd512b32a8833b116177bf5f01e77ee8"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://sourceforge.net/projects/zsh/files/zsh/$PKG_VERSION/zsh-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses pcre readline libcap"
