6d4b990016ed0210e828e45b0b118c1303d50145
AV <27rurk@gmail.com>
2019-04-26 10:39:00 +0300

    autocommit on change

--- packages/print/freetype/package.mk
+++ packages/print/freetype/package.mk
@@ -8,11 +8,10 @@ PKG_SHA256="fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freetype.org"
 PKG_URL="http://download.savannah.gnu.org/releases/freetype/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain zlib bzip2 libpng"
-PKG_DEPENDS_HOST="zlib:host zlib:host bzip2:host libpng:host"
+PKG_DEPENDS_HOST="gcc:host"
+PKG_DEPENDS_TARGET="toolchain zlib libpng"
 PKG_LONGDESC="The FreeType engine is a free and portable TrueType font rendering engine."
 PKG_TOOLCHAIN="configure"
-PKG_BUILD_FLAGS="+hardening"
 
 # package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="LIBPNG_CFLAGS=-I$SYSROOT_PREFIX/usr/include \
@@ -38,14 +37,8 @@ pre_configure_target() {
       unset LIBTOOL
       sh autogen.sh
     )
-
-# export CFLAGS="$CFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-# export CXXFLAGS="$CXXFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
 }
 
 post_makeinstall_target() {
-  sed -e "s:\(['=\" ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/freetype-config
-  ln -v -sf $SYSROOT_PREFIX/usr/include/freetype2 $SYSROOT_PREFIX/usr/include/freetype
-
-  rm -rf $INSTALL/usr/bin
+  cp -P "${PKG_BUILD}/.${TARGET_NAME}/freetype-config" "${SYSROOT_PREFIX}/usr/bin"
 }
--- packages/textproc/tinyxml2/package.mk
+++ packages/textproc/tinyxml2/package.mk
@@ -2,13 +2,17 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="tinyxml2"
-PKG_VERSION="7.0.1"
-PKG_SHA256="a381729e32b6c2916a23544c04f342682d38b3f6e6c0cad3c25e900c3a7ef1a6"
+PKG_VERSION="1.0.12"
+PKG_SHA256="53a4dd1b3aed4aa05b18782e303646669d2d3b2de3c1919fe21aea319b44de7f"
 PKG_LICENSE="zlib"
-PKG_URL="https://github.com/leethomason/tinyxml2/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="http://www.grinninglizard.com/tinyxml2/index.html"
+PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="TinyXML2 is a simple, small, C++ XML parser."
 PKG_BUILD_FLAGS="+pic"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=off -DBUILD_STATIC_LIBS=on"
 
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr
+}
