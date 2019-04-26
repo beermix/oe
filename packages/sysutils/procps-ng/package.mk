b9d82d1287e039e881d991465c57bfbadb94e592
AV <27rurk@gmail.com>
2019-04-26 05:09:23 +0300

    autocommit on change

--- packages/sysutils/procps-ng/package.mk
+++ packages/sysutils/procps-ng/package.mk
@@ -5,22 +5,27 @@ PKG_SITE="https://gitlab.com/procps-ng/procps"
 PKG_URL="$SOURCEFORGE_SRC/$PKG_NAME/Production/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
-#PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
 pre_configure_target() {
- sed 's:<ncursesw/:<:g' -i $PKG_BUILD/watch.c
- cd $PKG_BUILD
+  sed 's:<ncursesw/:<:g' -i $PKG_BUILD/watch.c
+  cd $PKG_BUILD
 }
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
                            ac_cv_func_realloc_0_nonnull=yes \
                            --disable-modern-top \
                            --enable-watch8bit \
-                           --with-systemd \
                            --disable-shared \
                            --disable-kill"
 
-post_makeinstall_target() {
-  rm -rf $INSTALL/usr/sbin
+PKG_MAKE_OPTS_TARGET="top/top proc/libprocps.la proc/libprocps.pc"
+
+PKG_MAKEINSTALL_OPTS_TARGET="install-libLTLIBRARIES install-pkgconfigDATA"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+    cp -P $PKG_BUILD/.$TARGET_NAME/top/top $INSTALL/usr/bin
+
+  make DESTDIR=$SYSROOT_PREFIX -j1 $PKG_MAKEINSTALL_OPTS_TARGET
 }
