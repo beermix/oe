626705dcb3f52698dc0ef247e0b2ce7941f8156b
AV <27rurk@gmail.com>
2019-04-26 15:22:37 +0300

    autocommit on change

--- packages/network/acestream/package - копия.mk
+++ packages/network/acestream/package - копия.mk
@@ -4,19 +4,19 @@
 ################################################################################
 
 PKG_NAME="acestream"
-PKG_VERSION="3.1.35_ubuntu_18.04_x86_64"
-PKG_SHA256="c20f427e635238857eb2c2a8402e3d90690c252f7f187d295fbc038f2a2be001"
+PKG_VERSION="3.1.16_ubuntu_16.04_x86_64"
 PKG_SITE="http://wiki.acestream.org/wiki/index.php/Download"
-#PKG_URL="http://dl.acestream.org/linux/acestream_3.1.16_ubuntu_16.04_x86_64.tar.gz"
+PKG_URL="http://dl.acestream.org/linux/acestream_3.1.16_ubuntu_16.04_x86_64.tar.gz"
 #PKG_URL="https://dl.dropboxusercontent.com/s/9cf62xv4dieb3qd/acestream-3.1.16_ubuntu_16.04_x86_64.tar.xz"
 PKG_URL="http://acestream.org/downloads/linux-beta/acestream_3.1.35_ubuntu_18.04_x86_64.tar.gz"
 PKG_DEPENDS_TARGET="toolchain M2Crypto apsw setuptools"
-#PKG_SOURCE_DIR="acestream-3.1.35_ubuntu_18.04_x86_64"
+PKG_SOURCE_DIR="${PKG_NAME}_$PKG_VERSION*"
+PKG_SHORTDESC="This is an innovative media platform of a new generation, which will take you to a new high-quality level of multimedia space on the Internet."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
    mkdir -p $INSTALL/opt
-   cp -pr $PKG_BUILD/ $INSTALL/opt/
+    cp -pr $PKG_BUILD/* $INSTALL/opt
 }
 
 post_install() {
