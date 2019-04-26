75aaeba52c6d21ac0c7eb33857cb756e5607fa34
AV <27rurk@gmail.com>
2019-04-26 16:00:46 +0300

    autocommit on change

--- packages/devel/libelf-compat/package.mk
+++ packages/devel/libelf-compat/package.mk
@@ -1,14 +1,8 @@
 PKG_NAME="libelf-compat"
 PKG_VERSION="328643b"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_GIT_URL="https://github.com/sabotage-linux/libelf-compat"
 PKG_DEPENDS_HOST="ccache:host"
-PKG_PRIORITY="optional"
-PKG_SECTION="toolchain/devel"
-
-
-
 
 make_host() {
   make prefix=$TOOLCHAIN install
