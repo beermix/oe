82b54c85e100f4ec3fa1ebb3dbd3b0be5e9c7cdc
AV <27rurk@gmail.com>
2019-04-26 05:15:28 +0300

    autocommit on change

--- packages/compress/zstd/package.mk
+++ packages/compress/zstd/package.mk
@@ -1,19 +1,16 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="zstd"
 PKG_VERSION="1.4.0"
-PKG_SHA256="63be339137d2b683c6d19a9e34f4fb684790e864fee13c7dd40e197a64c705c1"
+PKG_SHA256="7f323f0e0c18488748f3d9b2d4353f00e904ea2ccd0372ea04d7f77aa1156557"
 PKG_LICENSE="BSD/GPLv2"
-PKG_SITE="https://github.com/facebook/zstd/releases"
-PKG_URL="https://github.com/facebook/zstd/archive/v${PKG_VERSION}.tar.gz"
-PKG_SOURCE_DIR=$PKG_NAME-$PKG_VERSION
+PKG_SITE="http://www.zstd.net"
+PKG_URL="https://github.com/facebook/zstd/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="gcc:host ninja:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A fast real-time compression algorithm."
 
 configure_package() {
   PKG_CMAKE_SCRIPT="$PKG_BUILD/build/cmake/CMakeLists.txt"
-  PKG_CMAKE_OPTS_HOST="-DZSTD_BUILD_SHARED=0"
-  PKG_CMAKE_OPTS_TARGET="-DZSTD_BUILD_SHARED=0"
 }
