37b729808f8f9d60eae714f1e0803fa247cd2bd3
AV <27rurk@gmail.com>
2019-04-28 18:31:57 +0300

    autocommit on change

--- packages/network/libidn2/package.mk
+++
@@ -1,9 +0,0 @@
-PKG_NAME="libidn2"
-PKG_VERSION="2.0.4"
-PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="network"
-PKG_BUILD_FLAGS="+pic"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
-
--- packages/virtual/libc/package.mk
+++ packages/virtual/libc/package.mk
@@ -9,7 +9,7 @@ PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain glibc tz"
 PKG_DEPENDS_INIT="toolchain glibc:init"
 PKG_SECTION="virtual"
-PKG_LONGDESC="Meta package for installing various tools and libs needed for libc"
+PKG_LONGDESC="Meta package for installing various tools and libs needed for libc libidn2"
 
 if [ "$BOOTLOADER" = "bcm2835-bootloader" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET arm-mem"
