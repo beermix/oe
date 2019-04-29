d833867b65715ee0af95f97ff730d2c85cd32ccd
AV <27rurk@gmail.com>
2019-04-29 09:01:52 +0300

    autocommit on change

--- packages/debug/xorg-intel-gpu-tools/package.mk
+++ packages/debug/xorg-intel-gpu-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xorg-intel-gpu-tools"
-PKG_VERSION="504367d33b787de2ba8e007a5b620cfd6f0b3074"
-PKG_SHA256="3e118ce6ab58b506de88ab158e9e630b3db9ab75b14cd0c4a8019548d2d0c320"
+PKG_VERSION="caed251"
+PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_DEPENDS_TARGET="toolchain cairo procps-ng elfutils"
 PKG_SITE="https://github.com/freedesktop/xorg-intel-gpu-tools"
@@ -11,7 +11,7 @@ PKG_URL="https://github.com/freedesktop/xorg-intel-gpu-tools/archive/$PKG_VERSIO
 PKG_LONGDESC="Test suite and tools for DRM/KMS drivers"
 PKG_TOOLCHAIN="meson"
 
-PKG_MESON_OPTS_TARGET="-Dbuild_overlay=false \
+PKG_MESON_OPTS_TARGET="-Dbuild_overlay=true \
                        -Dbuild_man=false \
                        -Dwith_valgrind=false \
                        -Dbuild_audio=false \
