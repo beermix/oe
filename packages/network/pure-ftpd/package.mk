3933b18156a916ff7e4477b3fe2fe080b97efbc0
AV <27rurk@gmail.com>
2019-04-29 09:10:24 +0300

    autocommit on change

--- packages/network/pure-ftpd/package.mk
+++ packages/network/pure-ftpd/package.mk
@@ -1,12 +1,11 @@
 PKG_NAME="pure-ftpd"
-PKG_VERSION="1.0.47"
+PKG_VERSION="1.0.49"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libcap"
+PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-largefile \
-			      --without-tls \
-			      --without-pam \
-			      --with-minimal"
+#PKG_CONFIGURE_OPTS_TARGET="--with-minimal"
+
 post_install () {
   enable_service ftpd.service
 }
