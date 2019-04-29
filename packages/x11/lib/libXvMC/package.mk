e77c421d8f5a522973e7a9500990efd6f9e2fcfa
AV <27rurk@gmail.com>
2019-04-29 05:48:30 +0300

    autocommit on change

--- packages/x11/lib/libXvMC/package.mk
+++ packages/x11/lib/libXvMC/package.mk
@@ -1,7 +1,6 @@
 PKG_NAME="libXvMC"
-PKG_VERSION="1.0.10"
+PKG_VERSION="1.0.11"
 PKG_URL="https://www.x.org/releases/individual/lib/libXvMC-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain glib libXv"
-PKG_SECTION="devel"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-malloc0returnsnull"
