09f98713de711fcbd70e5443f52e59cac3f62cdf
AV <27rurk@gmail.com>
2019-04-29 06:18:25 +0300

    autocommit on change

--- packages/network/ipset/package.mk
+++ packages/network/ipset/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="ipset"
-PKG_VERSION="6.38"
+PKG_VERSION="7.1"
 PKG_URL="http://ipset.netfilter.org/ipset-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain linux libmnl libnftnl"
 PKG_SECTION="tools"
