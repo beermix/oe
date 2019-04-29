a5792fe36bc145d68350cccf5167d8a1a444ad57
AV <27rurk@gmail.com>
2019-04-29 06:16:58 +0300

    autocommit on change

--- packages/network/bridge-utils/package.mk
+++ packages/network/bridge-utils/package.mk
@@ -2,7 +2,6 @@ PKG_NAME="bridge-utils"
 PKG_VERSION="1.6"
 PKG_URL="https://www.kernel.org/pub/linux/utils/net/bridge-utils/bridge-utils-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="devel"
 
 PKG_TOOLCHAIN="autotools"
 
--- packages/network/c-ares/package.mk
+++ packages/network/c-ares/package.mk
@@ -2,7 +2,6 @@ PKG_NAME="c-ares"
 PKG_VERSION="1.14.0"
 PKG_URL="https://c-ares.haxx.se/download/c-ares-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
-PKG_SECTION="network"
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-pic"
