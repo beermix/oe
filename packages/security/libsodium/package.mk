573f8466b9c4da321f7a26af4b098ecfc1b29f18
AV <27rurk@gmail.com>
2019-04-30 20:47:55 +0300

    autocommit on change

--- packages/security/libsodium/package.mk
+++ packages/security/libsodium/package.mk
@@ -1,9 +1,8 @@
 PKG_NAME="libsodium"
-PKG_VERSION="1.0.16"
+PKG_VERSION="1.0.17"
 PKG_SITE="https://github.com/jedisct1/libsodium/tree/stable"
 PKG_URL="https://github.com/jedisct1/libsodium/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl"
-PKG_SECTION="my"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
