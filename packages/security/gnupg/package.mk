ad14d016b87ab9c936d13133227bb3457f3139a9
AV <27rurk@gmail.com>
2019-05-01 08:12:00 +0300

    autocommit on change

--- packages/security/gnupg/package.mk
+++ packages/security/gnupg/package.mk
@@ -1,14 +1,13 @@
 PKG_NAME="gnupg"
 #PKG_VERSION="2.2.0"
-PKG_VERSION="1.4.22"
+PKG_VERSION="1.4.23"
 PKG_SITE="http://www.gnupg.org/"
 PKG_URL="ftp://ftp.gnupg.org/gcrypt/gnupg/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain zlib curl libassuan npth libksba libgpg-error libgcrypt"
+PKG_DEPENDS_TARGET="toolchain zlib curl npth libksba libgpg-error libgcrypt"
 PKG_SECTION="security"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-libgpg-error-prefix=$SYSROOT_PREFIX/usr \
 			      --with-libgcrypt-prefix=$SYSROOT_PREFIX/usr \
 			      --with-libassuan-prefix=$SYSROOT_PREFIX/usr \
-			      --with-ksba-prefix=$SYSROOT_PREFIX/usr \
 			      --with-npth-prefix=$SYSROOT_PREFIX/usr \
 			      --enable-silent-rules --disable-shared --with-pic"
--- packages/security/libassuan/package.mk
+++ packages/security/libassuan/package.mk
@@ -2,13 +2,10 @@ PKG_NAME="libassuan"
 PKG_VERSION="2.4.3"
 PKG_URL="ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libgpg-error"
-PKG_SECTION="my"
 
-
-
-pre_configure_target() {
-  CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
-  CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
-}
+#pre_configure_target() {
+#  CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
+#  CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --with-gnu-ld --with-pic"
