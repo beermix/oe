a052e1bf83c99c1900dd03bba87adae99088a03f
AV <27rurk@gmail.com>
2019-05-01 21:09:03 +0300

    autocommit on change

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="5c581b3dd6979b79cb3e3ab8e2e03b442e6ecb0d"
+PKG_VERSION="70da00ffd65e56c8c8cc6ecfcec462fb52ee5102"
 PKG_SITE="https://cgit.freedesktop.org/mesa/mesa/log"
 PKG_URL="https://github.com/mesa3d/mesa/archive/$PKG_VERSION.tar.gz"
 PKG_URL="https://cgit.freedesktop.org/mesa/mesa/snapshot/$PKG_VERSION.tar.xz"
