32a59965cb537171073acf74aa741569b64f1bd8
AV <27rurk@gmail.com>
2019-05-01 08:13:30 +0300

    autocommit on change

--- packages/compress/lrzip/package.mk
+++ packages/compress/lrzip/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="lrzip"
-PKG_VERSION="9819e86"
+PKG_VERSION="79f5051"
 PKG_URL="https://github.com/ckolivas/lrzip/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib lzo xz bzip2"
 PKG_DEPENDS_HOST="zlib:host lzo:host xz:host bzip2:host"
