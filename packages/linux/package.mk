5417cbfd274561f98d434a83eff2d898e2dda538
AV <27rurk@gmail.com>
2019-05-07 09:11:30 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -31,7 +31,7 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   zen)
-    PKG_VERSION="5.0.13-lqx1"
+    PKG_VERSION="5.1-zen1"
     PKG_SHA256=""
     PKG_URL="https://github.com/zen-kernel/zen-kernel/archive/v$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="zen-kernel-$PKG_VERSION*"
