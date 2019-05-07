b282c9b9295ce9a428923c1571fdd82d43592eb0
AV <27rurk@gmail.com>
2019-05-07 13:00:00 +0300

    autocommit on change

--- packages/network/libnetfilter_conntrack/package - копия.mk
+++
@@ -1,9 +0,0 @@
-PKG_NAME="libnetfilter_conntrack"
-PKG_VERSION="1.0.6"
-PKG_URL="ftp://ftp.netfilter.org/pub/libnetfilter_conntrack/libnetfilter_conntrack-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain libnfnetlink libnl" 
-PKG_SECTION="my"
-PKG_IS_ADDON="no"
-PKG_BUILD_FLAGS="+pic"
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
