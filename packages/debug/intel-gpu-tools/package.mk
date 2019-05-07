94ec9f1767796a869c126a76248e09e0edb90598
AV <27rurk@gmail.com>
2019-05-07 11:23:30 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -3,9 +3,9 @@ PKG_VERSION="f052e49a43cc9704ea5f240df15dd9d3dfed68ab"
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D" # https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/
 PKG_URL="https://www.x.org/releases/individual/app/intel-gpu-tools-$PKG_VERSION.tar.xz"
 PKG_URL="https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/snapshot/$PKG_VERSION.tar.xz"
-PKG_SOURCE_DIR="$PKG_VERSION"
+#PKG_SOURCE_DIR="$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain clang:host libxml llvm zlib"
-PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo swig:host libunwind libXv"
+PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo swig:host libunwind libunwind libXv"
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-tests \
@@ -15,4 +15,5 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-tests \
 			      --disable-debug \
 			      --disable-amdgpu \
 			      --disable-nouveau \
+			      --disable-libunwind \
 			      --enable-intel"
