6dec3141fa1dae13c435487338f21361327dedd6
AV <27rurk@gmail.com>
2019-05-10 19:08:31 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="5194991e786f1e98fe9682449a7630f03deea201c7f0d668eea28cd31a800dff"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host libxml2:host"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="A program internationalization library and tools."
 
 post_unpack() {
@@ -19,6 +19,7 @@ post_unpack() {
 PKG_CONFIGURE_OPTS_HOST="--disable-rpath \
 			    --disable-java \
 			    --disable-curses \
+			    --with-included-libxml \
 			    --with-included-libunistring \
 			    --disable-native-java \
 			    --disable-openmp \
