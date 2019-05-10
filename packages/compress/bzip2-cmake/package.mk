5513afd200ee9be23650280c665da8632f7e8950
AV <27rurk@gmail.com>
2019-05-10 18:54:36 +0300

    autocommit on change

--- packages/compress/bzip2-cmake/package.mk
+++ packages/compress/bzip2-cmake/package.mk
@@ -1,15 +1,13 @@
-PKG_NAME="bzip2"
+PKG_NAME="bzip2-cmake"
 #PKG_VERSION="0496eaa"
 PKG_VERSION="3ac9ab1"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 #URL="https://github.com/sspring/bzip2_cmake"
 #URL=="https://github.com/osrf/bzip2_cmake"
 PKG_URL="https://github.com/sspring/bzip2_cmake/archive/${PKG_VERSION}.tar.gz"
 #PKG_URL="https://github.com/osrf/bzip2_cmake/archive/${PKG_VERSION}.tar.gz"
-PKG_SOURCE_DIR="bzip2_cmake-$PKG_VERSION*"
+#PKG_SOURCE_DIR="bzip2_cmake-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain bzip2:host"
-PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="+pic:host +pic"
 
 post_makeinstall_target() {
