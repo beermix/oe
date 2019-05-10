f54c7c42cc911ce3f5f1cd36ff1c7235f7225390
AV <27rurk@gmail.com>
2019-05-10 16:53:11 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="ninja"
-PKG_VERSION="2e64645"
-PKG_SHA256=""
+PKG_VERSION="1db07ed"
+PKG_SHA256="5cef852927283ed5e2f059f10dc21ac7eac0537b366002d9f15e003dd83edba1"
 PKG_LICENSE="Apache"
 PKG_SITE="https://github.com/ninja-build/ninja"
 PKG_URL="https://github.com/ninja-build/ninja/archive/$PKG_VERSION.tar.gz"
