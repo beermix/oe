23e174a71170c3c3de10d6ea0303e45b671c3ca9
AV <27rurk@gmail.com>
2019-05-10 20:06:48 +0300

    autocommit on change

--- packages/addons/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/chrome-depends/at-spi2-core/package.mk
@@ -4,8 +4,8 @@
 
 PKG_NAME="at-spi2-core"
 PKG_VERSION="2.32.1"
-PKG_VERSION="2.28.0"
-PKG_SHA256="42a2487ab11ce43c288e73b2668ef8b1ab40a0e2b4f94e80fca04ad27b6f1c87"
+#PKG_VERSION="2.28.0"
+#PKG_SHA256="42a2487ab11ce43c288e73b2668ef8b1ab40a0e2b4f94e80fca04ad27b6f1c87"
 PKG_LICENSE="OSS"
 PKG_SITE="http://ftp.acc.umu.se/pub/gnome/sources/at-spi2-core/?C=M;O=D"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-$PKG_VERSION.tar.xz"
