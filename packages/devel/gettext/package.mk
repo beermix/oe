516a5e798eec8a07d5b8ec02a2499af84e033a97
AV <27rurk@gmail.com>
2019-05-11 03:02:48 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -3,26 +3,24 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gettext"
-PKG_VERSION="0.20"
-PKG_SHA256="5194991e786f1e98fe9682449a7630f03deea201c7f0d668eea28cd31a800dff"
+PKG_VERSION="0.19.8.1"
+PKG_SHA256="ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="A program internationalization library and tools."
 
-post_unpack() {
-  sed -i '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/examples$$//' $PKG_BUILD/gettext-tools/Makefile.in
-  sed -i '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/tests$$//' $PKG_BUILD/gettext-runtime/Makefile.in
-}
+configure_package() {
+  PKG_CONFIGURE_SCRIPT="${PKG_BUILD}/gettext-tools/configure"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-rpath \
-			    --disable-java \
-			    --disable-curses \
-			    --with-included-libxml \
-			    --with-included-libunistring \
-			    --disable-native-java \
-			    --disable-openmp \
-			    --disable-csharp \
-			    --disable-relocatable \
-			    --without-emacs"
+  PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
+                           --disable-rpath \
+                           --with-gnu-ld \
+                           --disable-java \
+                           --disable-curses \
+                           --with-included-libxml \
+                           --disable-native-java \
+                           --disable-csharp \
+                           --without-emacs"
+}
