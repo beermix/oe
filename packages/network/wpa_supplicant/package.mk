71aff6433aa4121c346cdce7ec0cfcd69dadf3e3
AV <27rurk@gmail.com>
2019-05-12 01:30:07 +0300

    autocommit on change

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="wpa_supplicant"
-PKG_VERSION="2.7"
-PKG_SHA256="76ea6b06b7a2ea8e6d9eb1a9166166f1656e6d48c7508914f592100c95c73074"
+PKG_VERSION="2.8"
+PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://w1.fi/releases/?C=M;O=D"
 PKG_URL="https://w1.fi/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -11,14 +11,14 @@ PKG_DEPENDS_TARGET="toolchain dbus libnl openssl"
 PKG_LONGDESC="A free software implementation of an IEEE 802.11i supplicant."
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="+lto-parallel"
-LTO_SUPPORT="yes"
-GOLD_SUPPORT="yes"
+#LTO_SUPPORT="yes"
+#GOLD_SUPPORT="yes"
 
 PKG_MAKE_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 PKG_MAKEINSTALL_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 
 configure_target() {
-  LDFLAGS="$LDFLAGS -lnl-3 -lm -lpthread"
+  export LIBS="$LIBS -lnl-3 -lm -lpthread"
 
   cp $PKG_DIR/config/makefile.config wpa_supplicant/.config
 
@@ -37,5 +37,5 @@ mkdir -p $INSTALL/usr/lib/systemd/system
 
 mkdir -p $INSTALL/usr/share/dbus-1/system-services
   cp wpa_supplicant/dbus/fi.w1.wpa_supplicant1.service $INSTALL/usr/share/dbus-1/system-services
-  cp wpa_supplicant/dbus/fi.epitest.hostap.WPASupplicant.service $INSTALL/usr/share/dbus-1/system-services
+  #cp wpa_supplicant/dbus/fi.epitest.hostap.WPASupplicant.service $INSTALL/usr/share/dbus-1/system-services
 }
