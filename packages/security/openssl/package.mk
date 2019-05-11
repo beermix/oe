9aee9cc9e44a3b9decc6f02211ab2affda45da43
AV <27rurk@gmail.com>
2019-05-11 04:38:19 +0300

    autocommit on change

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -17,9 +17,7 @@ PKG_BUILD_FLAGS="-parallel"
 PKG_CONFIGURE_OPTS_SHARED="--libdir=lib \
                            shared \
                            threads \
-                           no-ssl2 \
                            no-ssl3 \
-                           no-rc5 \
                            zlib-dynamic"
 
 PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN \
