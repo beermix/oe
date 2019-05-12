fa3030a737182c5703763c2aa05eba584a33b07c
AV <27rurk@gmail.com>
2019-05-12 20:46:19 +0300

    autocommit on change

--- packages/devel/gettextx/package.mk
+++ packages/devel/gettextx/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gettext"
-PKG_VERSION="0.20"
-PKG_SHA256="5194991e786f1e98fe9682449a7630f03deea201c7f0d668eea28cd31a800dff"
+PKG_VERSION="0.20.1"
+PKG_SHA256="66415634c6e8c3fa8b71362879ec7575e27da43da562c798a8a2f223e6e47f5c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -15,3 +15,13 @@ post_unpack() {
   sed -i '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/examples$$//' $PKG_BUILD/gettext-tools/Makefile.in
   sed -i '/^SUBDIRS/s/ doc //;/^SUBDIRS/s/tests$$//' $PKG_BUILD/gettext-runtime/Makefile.in
 }
+
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
+                           --disable-rpath \
+                           --with-gnu-ld \
+                           --disable-java \
+                           --disable-curses \
+                           --with-included-libxml \
+                           --disable-native-java \
+                           --disable-csharp \
+                           --without-emacs"
