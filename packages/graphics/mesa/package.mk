64253714154cd4ad85b6c585b1b717ddb0591cda
AV <27rurk@gmail.com>
2019-05-29 08:31:16 +0300

    autocommit on change

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -95,9 +95,9 @@ fi
 pre_configure_target() {
   if [ "$DISPLAYSERVER" = "x11" ]; then
     export LIBS="-lxcb-dri3 -lxcb-dri2 -lxcb-xfixes -lxcb-present -lxcb-sync -lxshmfence -lz"
-    export CFLAGS="$CFLAGS -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-    export CXXFLAGS="$CXXFLAGS -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-    export LDFLAGS=`echo $LDFLAGS | sed -e "s|-Wl,--as-needed||"`
+    #export CFLAGS="$CFLAGS -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+    #export CXXFLAGS="$CXXFLAGS -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+    #export LDFLAGS=`echo $LDFLAGS | sed -e "s|-Wl,--as-needed||"`
   fi
 }
 
