466161419cdfa9554a11978b72d721078a4972c1
AV <27rurk@gmail.com>
2019-06-12 16:12:54 +0300

    autocommit on change

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,40 +2,17 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="1.2.11.1_jtkv6.3"
-PKG_SHA256="897980569aa46d20a4ed9fd63e2e7ce9465e0dd4742a135603db397f168f7f01"
+PKG_VERSION="4230228b86594f9e3561e444421c4df95f3638c7"
+PKG_SHA256="14f9430514d99a6b6857accd439abb3425a230beab89fed85cc378bf0a19e533"
+PKG_VERSION="d1c35e4"
+PKG_SHA256=""
 PKG_LICENSE="OSS"
-PKG_SITE="https://github.com/jtkukunas/zlib/releases"
-PKG_URL="https://github.com/jtkukunas/zlib/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET=""
+PKG_SITE="https://github.com/zlib-ng/zlib-ng"
+PKG_URL="https://github.com/zlib-ng/zlib-ng/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST="cmake:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A general purpose (ZIP) data compression library."
-PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="cmake-make"
 
-post_unpack() {
-  mkdir -p $PKG_BUILD/.$HOST_NAME
-  cp -RP $PKG_BUILD/* $PKG_BUILD/.$HOST_NAME
-
-  mkdir -p $PKG_BUILD/.$TARGET_NAME
-  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
-}
-
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-  export CXXFLAGS="$CXXFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-}
-
-pre_configure_host() {
-  export CFLAGS="$CFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-  export CXXFLAGS="$CXXFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-}
-
-configure_target() {
-  cd $PKG_BUILD/.$TARGET_NAME
-  ./configure --prefix=/usr --libdir=/usr/lib --static --shared
-}
-
-configure_host() {
-  cd $PKG_BUILD/.$HOST_NAME
-  ./configure --prefix=$TOOLCHAIN --libdir=$TOOLCHAIN/lib --static --shared
-}
+PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release -DZLIB_COMPAT=ON -DWITH_NATIVE_INSTRUCTIONS=ON"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DZLIB_COMPAT=ON -DWITH_NATIVE_INSTRUCTIONS=ON"
---
+++ packages/compress/zlibz/package.mk
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+
+PKG_NAME="zlib"
+PKG_VERSION="1.2.11.1_jtkv6.3"
+PKG_SHA256="897980569aa46d20a4ed9fd63e2e7ce9465e0dd4742a135603db397f168f7f01"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/jtkukunas/zlib/releases"
+PKG_URL="https://github.com/jtkukunas/zlib/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_DEPENDS_TARGET=""
+PKG_LONGDESC="A general purpose (ZIP) data compression library."
+PKG_TOOLCHAIN="configure"
+
+post_unpack() {
+  mkdir -p $PKG_BUILD/.$HOST_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$HOST_NAME
+
+  mkdir -p $PKG_BUILD/.$TARGET_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
+}
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+  export CXXFLAGS="$CXXFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+}
+
+pre_configure_host() {
+  export CFLAGS="$CFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+  export CXXFLAGS="$CXXFLAGS -O3 -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
+}
+
+configure_target() {
+  cd $PKG_BUILD/.$TARGET_NAME
+  ./configure --prefix=/usr --libdir=/usr/lib --static --shared
+}
+
+configure_host() {
+  cd $PKG_BUILD/.$HOST_NAME
+  ./configure --prefix=$TOOLCHAIN --libdir=$TOOLCHAIN/lib --static --shared
+}
