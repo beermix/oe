d5028321350e2a7e012f08b5252e8bc2f75f0e5c
AV <27rurk@gmail.com>
2019-06-12 05:19:57 +0300

    autocommit on change

--- packages/devel/less/package.mk
+++ packages/devel/less/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="less"
-PKG_VERSION="550"
-PKG_SHA256="6a53639f08d7ed05b6e104b82c32193f79ac01a3eddb20e114e1c261948bd57b"
+PKG_VERSION="551"
+PKG_SHA256="ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d"
 PKG_URL="http://www.greenwoodsoftware.com/less/less-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain readline pcre"
 PKG_SECTION="my"
