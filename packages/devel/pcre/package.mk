0abebf9ac0d2bdbb4bce20f7b044e446782cbd34
AV <27rurk@gmail.com>
2019-06-14 03:32:01 +0300

    autocommit on change

--- packages/devel/pcre/package.mk
+++ packages/devel/pcre/package.mk
@@ -8,8 +8,8 @@ PKG_SHA256="91e762520003013834ac1adb4a938d53b22a216341c061b0cf05603b290faf6b"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/svn2github/pcre/"
 PKG_URL="https://ftp.pcre.org/pub/pcre/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_HOST="gcc:host cmake:host"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_HOST="gcc:host cmake:host zlib:host"
+PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="A set of functions that implement regular expression pattern matching."
 PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="+pic:host"
@@ -21,7 +21,6 @@ PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN \
 
 PKG_CMAKE_OPTS_HOST="-DBUILD_SHARED_LIBS=0 \
 			-DCMAKE_BUILD_TYPE=Release \
-			-DPCRE_BUILD_PCRE16=1 \
 			-DPCRE_SUPPORT_UNICODE_PROPERTIES=1 \
 			-DPCRE_SUPPORT_UTF=1 \
 			-DPCRE_SUPPORT_JIT=1"
