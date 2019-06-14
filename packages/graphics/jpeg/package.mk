42f686a09530f7c7fc36513a2500727d2a546867
AV <27rurk@gmail.com>
2019-06-14 06:56:32 +0300

    autocommit on change

--- packages/graphics/jpeg/package.mk
+++ packages/graphics/jpeg/package.mk
@@ -6,7 +6,6 @@ PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic:host +pic"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-shared --enable-static"
-
 PKG_CONFIGURE_OPTS_HOST="--enable-shared --enable-static"
 
 post_makeinstall_target() {
