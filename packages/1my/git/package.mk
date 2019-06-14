ccbd8cbc9a783d06c416419d411237cbcd7e1955
AV <27rurk@gmail.com>
2019-06-14 03:56:20 +0300

    autocommit on change

--- packages/1my/git/package.mk
+++ packages/1my/git/package.mk
@@ -1,16 +1,23 @@
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
 PKG_NAME="git"
-PKG_VERSION="2.22.0"
-PKG_URL="https://www.kernel.org/pub/software/scm/git/git-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain curl pcre expat zlib openssl"
-PKG_DEPENDS_HOST="zlib:host pcre:host expat:host"
+PKG_VERSION="2.9.5"
+PKG_SHA256="a4bea37ecb9a3fb5c0c8ef18c2f7eeaf8ccbcfec91f72f3bccfc6bf72a3e3902"
+PKG_LICENSE="GPL"
+PKG_SITE="http://git-scm.com/"
+PKG_URL="https://www.kernel.org/pub/software/scm/git/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain curl openssl"
+PKG_SHORTDESC="git: the fast distributed version control system"
 PKG_TOOLCHAIN="autotools"
 
-pre_configure_target() {
-  cd $PKG_BUILD
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
-  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
+pre_build_target() {
+  mkdir -p $PKG_BUILD/.$TARGET_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
 
-  export NO_EXPAT="YesPlease"
+  #export NO_EXPAT="YesPlease"
   export NO_ICONV="YesPlease"
   export NO_PYTHON="YesPlease"
   export NO_UNIX_SOCKETS="YesPlease"
@@ -20,27 +27,22 @@ pre_configure_target() {
   export NO_NSEC="YesPlease"
   export NO_TCLTK="YesPlease"
   export NO_INSTALL_HARDLINKS="yes"
+
+# export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
+# export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
 }
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_fread_reads_directories=yes ac_cv_snprintf_returns_bogus=yes"
-
-pre_configure_host() {
-  cd $PKG_BUILD
-
-  export NO_EXPAT="YesPlease"
-  export NO_ICONV="YesPlease"
-  export NO_PYTHON="YesPlease"
-  export NO_UNIX_SOCKETS="YesPlease"
-  export NO_GETTEXT="YesPlease"
-  export NO_MKSTEMPS="YesPlease"
-  export NO_PERL="YesPlease"
-  export NO_NSEC="YesPlease"
-  export NO_TCLTK="YesPlease"
-  export NO_INSTALL_HARDLINKS="yes"
+configure_target() {
+ac_cv_fread_reads_directories=no \
+ac_cv_snprintf_returns_bogus=no \
+  ./configure --host=$TARGET_NAME \
+              --build=$HOST_NAME \
+              --prefix=/usr \
+              --libexecdir=/usr/lib \
+              --with-gitconfig=/etc/gitconfig
 }
 
-PKG_CONFIGURE_OPTS_HOST="ac_cv_fread_reads_directories=yes ac_cv_snprintf_returns_bogus=yes"
-
-#make_target() {
-#  make SHELL='sh -x'
-#}
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/lib/x86_64-linux-gnu
+  rm -rf $INSTALL/usr/share/perl
+}
--- packages/devel/pcre2/package.mk
+++ packages/devel/pcre2/package.mk
@@ -1,13 +1,12 @@
 PKG_NAME="pcre2"
 PKG_VERSION="10.33"
 PKG_SHA256="bc7045250ce94d8022c121c1884970ae532f2cdc3295c03975cca8628e9febc6"
-PKG_SITE="http://ftp.csx.cam.ac.uk/pub/software/programming/pcre/?C=M;O=D"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+pic:host +pic"
-#PKG_TOOLCHAIN="autotools"
+PKG_TOOLCHAIN="cmake-make"
 
 #PKG_CONFIGURE_OPTS_TARGET="--enable-pcre2-8 --enable-pcre2-16 --disable-pcre2grep-callout"
 
@@ -21,7 +20,14 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
 
 PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN $PKG_CONFIGURE_OPTS_TARGET"
 
-post_makeinstall_target() {
-  rm -rf $INSTALL/usr/bin
-  sed -e "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/$PKG_NAME-config
-}
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
+			  -DCMAKE_BUILD_TYPE=Release \
+			  -DPCRE_SUPPORT_UNICODE_PROPERTIES=1 \
+			  -DPCRE_SUPPORT_UTF=1 \
+			  -DPCRE_SUPPORT_JIT=1 \
+			  -DCMAKE_VERBOSE_MAKEFILE=0"
+
+#post_makeinstall_target() {
+#  rm -rf $INSTALL/usr/bin
+#  sed -e "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/$PKG_NAME-config
+#}
