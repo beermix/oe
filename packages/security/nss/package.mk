fc59ed6148a4791339a96e0a940f1ce72afb0b91
AV <27rurk@gmail.com>
2019-06-14 03:27:42 +0300

    autocommit on change

--- packages/security/nss/package.mk
+++ packages/security/nss/package.mk
@@ -27,7 +27,6 @@ make_host() {
      USE_SYSTEM_ZLIB=1 ZLIB_LIBS="-lz -L$TOOLCHAIN/lib" \
      SKIP_SHLIBSIGN=1 \
      NSS_DISABLE_GTESTS=1 \
-     NSS_USE_SYSTEM_SQLITE=1 \
      NSS_ENABLE_ECC=1 \
      NSS_ENABLE_WERROR=0 \
      NSS_TESTS="dummy" \
