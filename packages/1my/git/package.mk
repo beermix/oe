28e6f469d204d6ea98c051146504eb403bd7c6cd
AV <27rurk@gmail.com>
2019-06-18 06:41:33 +0300

    autocommit on change

--- packages/1my/git/package.mk
+++ packages/1my/git/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="git"
-PKG_VERSION="2.22.0"
-PKG_SHA256="159e4b599f8af4612e70b666600a3139541f8bacc18124daf2cbe8d1b934f29f"
+PKG_VERSION="2.19.2"
+PKG_SHA256="fce9a3a3297db5f3756c4553a2fc1fec209ee08178f8491e76ff4ff8fe7b8be9"
 PKG_URL="https://www.kernel.org/pub/software/scm/git/git-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain curl pcre zlib openssl"
 PKG_DEPENDS_HOST="zlib:host pcre:host"
@@ -11,10 +11,10 @@ pre_configure_target() {
   #export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
   #export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
 
-  export CC="$TARGET_CC"
-  export CFLAGS="$TARGET_CFLAGS"
-  export CPPFLAGS="$TARGET_CPPFLAGS"
-  export LDFLAGS="$TARGET_LDFLAGS"
+  #export CC="$TARGET_CC"
+  #export CFLAGS="$TARGET_CFLAGS"
+  #export CPPFLAGS="$TARGET_CPPFLAGS"
+  #export LDFLAGS="$TARGET_LDFLAGS"
   export NO_EXPAT="YesPlease"
   export NO_MKSTEMPS="YesPlease"
   export NO_GETTEXT="YesPlease"
@@ -27,7 +27,7 @@ pre_configure_target() {
   export NO_INSTALL_HARDLINKS="yes"
 }
 
-#PKG_CONFIGURE_OPTS_TARGET="ac_cv_snprintf_returns_bogus=no ac_cv_fread_reads_directories=no"
+#PKG_CONFIGURE_OPTS_TARGET="ac_cv_snprintf_returns_bogus=no ac_cv_fread_reads_directories=no --with-curl=no"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/lib/x86_64-linux-gnu
--- packages/lang/perl/package.mk
+++ packages/lang/perl/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="perl"
-PKG_VERSION="5.29.10"
+PKG_VERSION="5.31.0"
 PKG_SITE="http://www.cpan.org/src/5.0/?C=M;O=D"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.gz"
 #PKG_URL="http://www.cpan.org/src/5.0/perl-${PKG_VERSION}.tar.xz"
