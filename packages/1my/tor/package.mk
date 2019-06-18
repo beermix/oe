ab946e7a55b77b0796429b5a3f07eef9d0f6f11e
AV <27rurk@gmail.com>
2019-06-19 00:26:33 +0300

    autocommit on change

--- packages/1my/tor/package.mk
+++ packages/1my/tor/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="tor"
-PKG_VERSION="0.3.2.9"
+PKG_VERSION="0.4.0.5"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib libevent libcap xz zstd"
 PKG_TOOLCHAIN="autotools"
