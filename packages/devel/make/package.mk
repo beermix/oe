02a55e542f25f4b9fc1d395fb38de5c9ae190e9c
AV <27rurk@gmail.com>
2019-06-18 07:40:32 +0300

    autocommit on change

--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -10,6 +10,7 @@ PKG_SITE="https://www.gnu.org/software/make/"
 PKG_URL="http://ftpmirror.gnu.org/make/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
 PKG_LONGDESC="Utility to maintain groups of programs."
+PKG_TOOLCHAIN="manual"
 
 pre_configure_host() {
   export CC=$LOCAL_CC
@@ -20,19 +21,20 @@ pre_configure_host() {
 PKG_CONFIGURE_OPTS_HOST="--without-guile"
 
 post_makeinstall_host() {
-  ln -sf make $TOOLCHAIN/bin/gmake
+  mkdir -p $TOOLCHAIN/bin
+  ln -sf /home/user/.bin/make $TOOLCHAIN/bin/gmake
+  ln -sf /home/user/.bin/make $TOOLCHAIN/bin/make
 
- mkdir -p $TOOLCHAIN/bin
  #ln -sf /bin/make $TOOLCHAIN/bin/gmake
  #ln -sf /bin/make $TOOLCHAIN/bin/make
 
- cp -r $PKG_DIR/src/bin/help2man $TOOLCHAIN/bin/
+ #cp -r $PKG_DIR/src/bin/help2man $TOOLCHAIN/bin/
 
-#  mkdir -p $TOOLCHAIN/share/aclocal
+ #mkdir -p $TOOLCHAIN/share/aclocal
 
-#  cp -r $PKG_DIR/src/bin/* $TOOLCHAIN/bin/
-#  cp -r -i $PKG_DIR/src/m4/* $TOOLCHAIN/share/aclocal/
+ #cp -r $PKG_DIR/src/bin/* $TOOLCHAIN/bin/
+ #cp -r -i $PKG_DIR/src/m4/* $TOOLCHAIN/share/aclocal/
 
-#  mkdir -p $TOOLCHAIN/share/include
-#  cp -r -i $PKG_DIR/include/* $TOOLCHAIN/share/include
+ #mkdir -p $TOOLCHAIN/share/include
+ #cp -r -i $PKG_DIR/include/* $TOOLCHAIN/share/include
 }
