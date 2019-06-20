0e3d03d16feba05c809f5e0b57a59eb3cc59250e
AV <27rurk@gmail.com>
2019-06-20 17:02:45 +0300

    autocommit on change

--- packages/network/libnl/package.mk
+++ packages/network/libnl/package.mk
@@ -3,13 +3,21 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnl"
-PKG_VERSION="3.4.0"
-PKG_SHA256="b7287637ae71c6db6f89e1422c995f0407ff2fe50cecd61a312b6a9b0921f5bf"
+#PKG_VERSION="3.4.0"
+#PKG_SHA256="b7287637ae71c6db6f89e1422c995f0407ff2fe50cecd61a312b6a9b0921f5bf"
+PKG_VERSION="65b3dd5"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/thom311/libnl/releases"
 PKG_URL="https://github.com/thom311/libnl/releases/download/libnl${PKG_VERSION//./_}/libnl-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/thom311/libnl/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A library for applications dealing with netlink socket."
+PKG_TOOLCHAIN="configure"
+
+pre_configure_target() {
+   cd $PKG_BUILD
+   ./autogen.sh
+}
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-shared \
