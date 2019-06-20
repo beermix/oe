375c8f70200ab9e15981345e2ebf62f8fde5a604
AV <27rurk@gmail.com>
2019-06-20 17:25:58 +0300

    autocommit on change

--- packages/network/libnl/package.mk
+++ packages/network/libnl/package.mk
@@ -1,24 +1,23 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv) --disable-debug
 
 PKG_NAME="libnl"
-#PKG_VERSION="3.4.0"
-#PKG_SHA256="b7287637ae71c6db6f89e1422c995f0407ff2fe50cecd61a312b6a9b0921f5bf"
 PKG_VERSION="65b3dd5"
+PKG_VERSION="3.4.0"
+PKG_SHA256="b7287637ae71c6db6f89e1422c995f0407ff2fe50cecd61a312b6a9b0921f5bf"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/thom311/libnl/releases"
 PKG_URL="https://github.com/thom311/libnl/releases/download/libnl${PKG_VERSION//./_}/libnl-$PKG_VERSION.tar.gz"
-PKG_URL="https://github.com/thom311/libnl/archive/$PKG_VERSION.tar.gz"
+#PKG_URL="https://github.com/thom311/libnl/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A library for applications dealing with netlink socket."
-PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="autotools"
 
-pre_configure_target() {
-  NOCONFIGURE=1 ./autogen.sh
-}
+#pre_configure_target() {
+#  NOCONFIGURE=1 ./autogen.sh
+#}
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-shared \
-                           --disable-cli \
-                           --disable-debug"
+                           --disable-cli"
