220dc638dc771eacb83cd7811052a88279d027ed
AV <27rurk@gmail.com>
2019-06-23 00:37:09 +0300

    autocommit on change

--- packages/network/iperf/package.mk
+++ packages/network/iperf/package.mk
@@ -3,7 +3,7 @@ PKG_VERSION="3.7"
 PKG_SHA256="d846040224317caf2f75c843d309a950a7db23f9b44b94688ccbe557d6d1710c"
 PKG_SITE="http://downloads.es.net/pub/iperf/?C=M;O=D"
 PKG_URL="http://downloads.es.net/pub/iperf/iperf-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain openssl"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --without-openssl"
