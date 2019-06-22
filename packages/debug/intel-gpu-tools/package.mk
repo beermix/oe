6690729bc7d7fa468eccdd8f67e52577bc4a4865
AV <27rurk@gmail.com>
2019-06-23 00:50:09 +0300

    autocommit on change

--- packages/debug/intel-gpu-tools/package.mk
+++ packages/debug/intel-gpu-tools/package.mk
@@ -1,9 +1,10 @@
 PKG_NAME="intel-gpu-tools"
-PKG_VERSION="c88ced79a7b71aec58f1d9c5c599ac2f431bcf7a"
+PKG_VERSION="1.24"
+PKG_SHA256=""
 PKG_SITE="https://www.x.org/releases/individual/app/?C=M;O=D"
 PKG_SITE="https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/log/"
 PKG_URL="https://www.x.org/releases/individual/app/intel-gpu-tools-$PKG_VERSION.tar.xz"
-PKG_URL="https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/snapshot/$PKG_VERSION.tar.xz"
+#PKG_URL="https://cgit.freedesktop.org/xorg/app/intel-gpu-tools/snapshot/$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain systemd procps-ng glib mesa libdrm libpciaccess cairo swig:host libunwind libXv elfutils peg:host"
 PKG_TOOLCHAIN="autotools"
 
--- packages/network/iperf/package.mk
+++ packages/network/iperf/package.mk
@@ -4,7 +4,10 @@ PKG_SHA256="d846040224317caf2f75c843d309a950a7db23f9b44b94688ccbe557d6d1710c"
 PKG_SITE="http://downloads.es.net/pub/iperf/?C=M;O=D"
 PKG_URL="http://downloads.es.net/pub/iperf/iperf-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_TOOLCHAIN="autotools"
+
+pre_configure_target() {
+  export CFLAGS=`echo $CFLAGS | sed -e "s|-fomit-frame-pointer||g"`
+}
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --without-openssl"
 
