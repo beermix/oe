f1f39a391d3304a09ad0262c05d36c5884e92678
AV <27rurk@gmail.com>
2019-06-22 22:15:44 +0300

    autocommit on change

--- packages/sysutils/patch=/package.mk
+++
@@ -1,8 +0,0 @@
-PKG_NAME="patch"
-PKG_VERSION="2.7.6"
-PKG_URL="http://ftpmirror.gnu.org/patch/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain attr"
-
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_sys_long_file_names=yes --enable-xattr --enable-attr"
-
-PKG_CONFIGURE_OPTS_HOST="ac_cv_sys_long_file_names=yes"
