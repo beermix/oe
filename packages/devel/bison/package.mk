8a56a170b923ce63421a39082377c2693999c980
AV <27rurk@gmail.com>
2019-06-25 00:38:08 +0300

    autocommit on change

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -5,8 +5,6 @@
 PKG_NAME="bison"
 PKG_VERSION="3.4.1"
 PKG_SHA256="27159ac5ebf736dffd5636fd2cd625767c9e437de65baa63cb0de83570bd820d"
-#PKG_VERSION="3.3.2"
-#PKG_SHA256="039ee45b61d95e5003e7e8376f9080001b4066ff357bde271b7faace53b9d804"
 PKG_LICENSE="GPL"
 PKG_SITE="https://ftp.gnu.org/gnu/bison/?C=M;O=D"
 PKG_URL="http://ftpmirror.gnu.org/bison/$PKG_NAME-$PKG_VERSION.tar.xz"
