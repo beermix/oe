1f174b22f05dbd8b221e99eb9b14e14769923527
AV <27rurk@gmail.com>
2019-06-25 10:54:17 +0300

    autocommit on change

--- packages/multimedia/media-driver/package.mk
+++ packages/multimedia/media-driver/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="media-driver"
-PKG_VERSION="82c0e6f7f6fd920218031a1d202b4ae06e3cf148"
-PKG_SHA256="6f689d174a7a9597bc1d4b368530f822f29c2a26200c44bead6d885754b0e4b8"
+PKG_VERSION="f6eca7f"
+PKG_SHA256="9d1a0472c407d0f2997d1ebd375926075677fea69c7745ed2ca03f53a8f0ba83"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
@@ -11,4 +11,4 @@ PKG_URL="https://github.com/intel/media-driver/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libva libdrm gmmlib"
 PKG_LONGDESC="media-driver: The Intel(R) Media Driver for VAAPI is a new VA-API (Video Acceleration API) user mode driver supporting hardware accelerated decoding, encoding, and video post processing for GEN based graphics hardware."
 
-PKG_CMAKE_OPTS_TARGET="-DENABLE_NONFREE_KERNELS=OFF -DBUILD_KERNELS=OFF"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DENABLE_NONFREE_KERNELS=OFF -DBUILD_KERNELS=OFF"
