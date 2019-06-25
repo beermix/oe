66240764053f494b990728dab8411af48b08cb20
AV <27rurk@gmail.com>
2019-06-25 12:28:44 +0300

    autocommit on change

--- packages/compress/p7zip/package.mk
+++ packages/compress/p7zip/package.mk
@@ -2,9 +2,8 @@ PKG_NAME="p7zip"
 PKG_VERSION="16.02"
 PKG_SITE="http://p7zip.sourceforge.net/"
 PKG_URL="http://downloads.sourceforge.net/project/p7zip/p7zip/${PKG_VERSION}/p7zip_${PKG_VERSION}_src_all.tar.bz2"
-PKG_SOURCE_DIR=${PKG_NAME}_${PKG_VERSION}
 PKG_DEPENDS_TARGET="toolchain zlib"
-PKG_TOOLCHAIN="cmake-make"
+#PKG_TOOLCHAIN="cmake-make"
 
 configure_package() {
   PKG_CMAKE_SCRIPT="$PKG_BUILD/CPP/7zip/CMAKE/CMakeLists.txt"
