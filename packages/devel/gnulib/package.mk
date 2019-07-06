fe579b8798432decc60720467bd2e83d73fed85d
AV <27rurk@gmail.com>
2019-07-06 07:36:59 +0300

    autocommit on change

--- packages/devel/gnulib/package.mk
+++ packages/devel/gnulib/package.mk
@@ -2,8 +2,9 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gnulib"
-PKG_VERSION="620857c4dccb8f0548cdb855f6b2f46ebea3919e"
-PKG_SHA256="4f7fa66538555a40b69f26dc56191178be75d80f20827aa3653516da5ce824bb"
+# Match version with GNULIB_REVISION in grub bootstrap.conf
+PKG_VERSION="d271f868a8df9bbec29049d01e056481b7a1a263"
+PKG_SHA256="4e23415ae2977ffca15e07419ceff3e9334d0369eafc9e7ae2578f8dd9a4839c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://savannah.gnu.org/git/?group=gnulib"
 PKG_URL="http://git.savannah.gnu.org/cgit/gnulib.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
