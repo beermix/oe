70cc05f8623a85135d0e6755948ca4f96f6ce96f
AV <27rurk@gmail.com>
2019-07-16 01:32:56 +0300

    autocommit on change

--- packages/x11/lib/libpciaccess/package.mk
+++ packages/x11/lib/libpciaccess/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libpciaccess"
-PKG_VERSION="0.14"
-PKG_SHA256="3df543e12afd41fea8eac817e48cbfde5aed8817b81670a4e9e493bb2f5bf2a4"
+PKG_VERSION="0.15"
+PKG_SHA256="a75643bb5cd02f6da8667d437b76492842dd56bc88e3dfb8410f5d535b99f5dc"
 PKG_LICENSE="OSS"
 PKG_SITE="http://freedesktop.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
