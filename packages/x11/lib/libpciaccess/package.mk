45d7205ddd736585beba6d321e695493c9d278fe
AV <27rurk@gmail.com>
2019-07-18 21:06:07 +0300

    autocommit on change

--- packages/x11/lib/libpciaccess/package.mk
+++ packages/x11/lib/libpciaccess/package.mk
@@ -4,7 +4,7 @@
 
 PKG_NAME="libpciaccess"
 PKG_VERSION="0.16"
-PKG_SHA256="214c9d0d884fdd7375ec8da8dcb91a8d3169f263294c9a90c575bf1938"
+PKG_SHA256="214c9d0d884fdd7375ec8da8dcb91a8d3169f263294c9a90c575bf1938b9f489"
 PKG_LICENSE="OSS"
 PKG_SITE="http://freedesktop.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
