ebe40c6e25da185c6de504d2110b5b3a4b2dc782
AV <27rurk@gmail.com>
2019-07-19 09:23:39 +0300

    autocommit on change

--- packages/security/openssl102/package.mk
+++ packages/security/openssl102/package.mk
@@ -64,7 +64,7 @@ pre_configure_target() {
 
 configure_target() {
   cd $PKG_BUILD/.$TARGET_NAME
-  ./Configure $PKG_CONFIGURE_OPTS_TARGET $PKG_CONFIGURE_OPTS_SHARED linux-x86_64 $CFLAGS $LDFLAGS
+  ./Configure $PKG_CONFIGURE_OPTS_TARGET $PKG_CONFIGURE_OPTS_SHARED linux-x86_64 -Wa,--noexecstack $CFLAGS $LDFLAGS
 }
 
 #make_target() {
