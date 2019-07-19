83b10e5d777bd772fb8f67d30e40c5de98441791
AV <27rurk@gmail.com>
2019-07-19 20:05:40 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -157,8 +157,7 @@ pre_make_target() {
   if [ "$TARGET_ARCH" = "x86_64" ]; then
     # copy some extra firmware to linux tree
     mkdir -p $PKG_BUILD/external-firmware
-      #cp -a $(get_build_dir kernel-firmware)/{e100,rtl_nic} $PKG_BUILD/external-firmware
-      cp -ar $(get_build_dir kernel-firmware)/iwlwifi-*.ucode $PKG_BUILD/external-firmware
+      #cp -a $(get_build_dir kernel-firmware)/{amdgpu,amd-ucode,i915,radeon,e100,rtl_nic} $PKG_BUILD/external-firmware
 
     cp -a $(get_build_dir intel-ucode)/intel-ucode $PKG_BUILD/external-firmware
 
