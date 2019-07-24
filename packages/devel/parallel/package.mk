4685855c73f3b3f326a106f9fad84b0875a00974
AV <27rurk@gmail.com>
2019-07-25 00:28:00 +0300

    autocommit on change

--- packages/devel/parallel/package.mk
+++ packages/devel/parallel/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="parallel"
-PKG_VERSION="20190122"
-PKG_SHA256="ae735f201a8ceeff2ace48ff779bda9d19846e629bcc02ea7c8768a42394190c"
+PKG_VERSION="20190722"
+PKG_SHA256="0ed0863184dbb396b4c030848e754b1ea76329c06ae9e43314bc0915eb6cbca7"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://www.gnu.org/software/parallel/"
+PKG_SITE="https://ftp.gnu.org/gnu/parallel/?C=M;O=D"
 PKG_URL="http://ftpmirror.gnu.org/parallel/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_HOST=""
 PKG_LONGDESC="GNU parallel is a shell tool for executing jobs in parallel using one or more computers."
