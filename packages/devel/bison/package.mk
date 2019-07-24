65cf529be0dc7a1e5a6e50b19ddbc109e28f2d63
AV <27rurk@gmail.com>
2019-07-25 01:41:34 +0300

    autocommit on change

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -11,6 +11,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://ftp.gnu.org/gnu/bison/?C=M;O=D"
 PKG_URL="http://ftpmirror.gnu.org/bison/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host m4:host flex:host"
+PKG_DEPENDS_HOST="ccache:host m4:host"
 PKG_LONGDESC="A general-purpose parser generator."
 
 PKG_CONFIGURE_OPTS_HOST="--disable-rpath --with-gnu-ld"
