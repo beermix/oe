bb404828f486aa7a9a444b1219991af6485df298
AV <27rurk@gmail.com>
2019-07-24 22:40:14 +0300

    autocommit on change

--- packages/compress/zstd/package.mk
+++ packages/compress/zstd/package.mk
@@ -5,14 +5,13 @@ PKG_NAME="zstd"
 PKG_VERSION="1.4.1"
 PKG_SHA256="535020867eb3af61fbadfcb402335af7f52dfe44f081ccfa201e961f01751aa0"
 PKG_LICENSE="BSD/GPLv2"
-PKG_SITE="https://github.com/facebook/zstd/releases/"
+PKG_SITE="http://www.zstd.net"
 PKG_URL="https://github.com/facebook/zstd/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="gcc:host ninja:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A fast real-time compression algorithm."
-PKG_TOOLCHAIN="cmake-make"
 
 configure_package() {
-  PKG_CMAKE_SCRIPT="$PKG_BUILD/build/cmake/CMakeLists.txt"
+  PKG_CMAKE_SCRIPT="${PKG_BUILD}/build/cmake/CMakeLists.txt"
   PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
 }
