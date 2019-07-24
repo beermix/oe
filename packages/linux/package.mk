24e7b7a4859cfe5cbc5c591f29eea79c9651c110
AV <27rurk@gmail.com>
2019-07-24 16:14:11 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -33,7 +33,7 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   git)
-    PKG_VERSION="master"
+    PKG_VERSION="ad5e427"
     PKG_SITE="https://github.com/torvalds/linux"
     PKG_URL="https://github.com/torvalds/linux/archive/$PKG_VERSION.tar.gz"
     PKG_PATCH_DIRS="default"
