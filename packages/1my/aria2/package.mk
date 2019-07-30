80cc7f2abce3b89ef9cda6192c553f023a182c23
AV <27rurk@gmail.com>
2019-07-30 12:11:09 +0300

    autocommit on change

--- packages/1my/aria2/package.mk
+++ packages/1my/aria2/package.mk
@@ -1,11 +1,12 @@
 PKG_NAME="aria2"
-PKG_VERSION="1.34.0"
-PKG_SHA256="3a44a802631606e138a9e172a3e9f5bcbaac43ce2895c1d8e2b46f30487e77a3"
+#PKG_VERSION="1.34.0"
+#PKG_SHA256="3a44a802631606e138a9e172a3e9f5bcbaac43ce2895c1d8e2b46f30487e77a3"
+PKG_VERSION="7f6578a"
 PKG_SITE="https://github.com/aria2/aria2/releases"
 PKG_URL="https://github.com/aria2/aria2/releases/download/release-$PKG_VERSION/aria2-$PKG_VERSION.tar.xz"
-#PKG_URL="https://github.com/aria2/aria2/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain expat pcre curl libev libxml2 libssh2 libuv"
-#PKG_TOOLCHAIN="autotools"
+PKG_URL="https://github.com/aria2/aria2/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain expat pcre curl libev libxml2 libuv c-ares"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --sysconfdir=/storage/.config \
@@ -13,14 +14,15 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --libdir=/storage/.config \
                            --libexecdir=/storage/.config \
                            --without-libnettle \
-                           --with-openssl \
+                           --without-openssl \
                            --disable-ipv6 \
                            --without-gnutls \
-                           --with-libssh2 \
+                           --without-libssh2 \
                            --with-ca-bundle=/etc/ssl/cacert.pem \
-                           --with-libexpat \
+                           --without-libexpat \
                            --with-zlib \
                            --with-libuv \
+                           --with-libcares \
                            --without-libgcrypt \
-                           --with-sqlite3 \
-                           --with-libxml2"
+                           --without-sqlite3 \
+                           --without-libxml2"
