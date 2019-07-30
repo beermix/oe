3ede26bc290a81a5eefe35efc3fee02e4a3b51a9
AV <27rurk@gmail.com>
2019-07-30 13:09:52 +0300

    autocommit on change

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -3,15 +3,15 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv) -I$PKG_BUILD/src/crypto
 
 PKG_NAME="wpa_supplicant"
-#PKG_VERSION="2.8"
-#PKG_SHA256="a689336a12a99151b9de5e25bfccadb88438f4f4438eb8db331cd94346fd3d96"
+PKG_VERSION="2.8"
+PKG_SHA256="a689336a12a99151b9de5e25bfccadb88438f4f4438eb8db331cd94346fd3d96"
 PKG_LICENSE="GPL"
 PKG_SITE="https://w1.fi/releases/?C=M;O=D"
 PKG_URL="https://w1.fi/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
 #PKG_VERSION="c2c6c01bb8b6fafc2074b46a53c4eab2c145ac6f"
 #PKG_SHA256="8647512bab9c47b8404ee085464ebdb01c4b718244354a3e5aecf46be0a4a676"
-PKG_VERSION="1e5ea68"
-PKG_URL="https://w1.fi/cgit/hostap/snapshot/hostap-$PKG_VERSION.tar.gz"
+#PKG_VERSION="1e5ea68"
+#PKG_URL="https://w1.fi/cgit/hostap/snapshot/hostap-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain dbus libnl-tiny openssl"
 PKG_DEPENDS_TARGET="toolchain dbus libnl openssl"
 PKG_LONGDESC="A free software implementation of an IEEE 802.11i supplicant."
@@ -28,7 +28,8 @@ configure_target() {
 #  export CFLAGS="$CFLAGS -D_GNU_SOURCE -DCONFIG_LIBNL20 -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
 
   LDFLAGS="$LDFLAGS -lpthread -lm"
-#  CFLAGS="$CFLAGS -D_GNU_SOURCE -DCONFIG_LIBNL20 -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
+  CFLAGS="$CFLAGS -D_GNU_SOURCE -DCONFIG_LIBNL20 -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
+  #CFLAGS="$CFLAGS -DCONFIG_LIBNL20 -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
 
 #  CFLAGS="$CFLAGS -ffunction-sections -fdata-sections"
 #  LDFLAGS="$LDFLAGS -Wl,--gc-sections"
