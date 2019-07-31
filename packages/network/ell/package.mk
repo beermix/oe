3528515fabf16336820ba6c8ecb0c85a0e691576
AV <27rurk@gmail.com>
2019-07-31 07:52:27 +0300

    autocommit on change

--- packages/network/ell/package.mk
+++ packages/network/ell/package.mk
@@ -1,25 +1,5 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
-
 PKG_NAME="ell"
 PKG_VERSION="726ca36"
-PKG_REV="1"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/cgit/libs/ell/ell.git/about"
 PKG_URL="https://git.kernel.org/pub/scm/libs/ell/ell.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -27,7 +7,6 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="network"
 PKG_SHORTDESC="Embedded Linux library"
 PKG_LONGDESC="Embedded Linux library"
-PKG_TOOLCHAIN="configure"
 
 pre_configure_target() {
   cd $PKG_BUILD
--- packages/network/iw/package.mk
+++ packages/network/iw/package.mk
@@ -3,7 +3,6 @@ PKG_VERSION="4.14"
 PKG_SITE="http://wireless.kernel.org/en/users/Documentation/iw"
 PKG_URL="https://www.kernel.org/pub/software/network/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libnl"
-PKG_SECTION="tools"
 
 make_target() {
   make PKG_CONFIG=$TOOLCHAIN/bin/pkg-config CC=$CC
