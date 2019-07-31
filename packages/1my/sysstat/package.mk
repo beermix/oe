8947461df06904ca6e990de7fde1517cbe230cb0
AV <27rurk@gmail.com>
2019-07-31 07:57:38 +0300

    autocommit on change

--- packages/1my/sysstat/package.mk
+++ packages/1my/sysstat/package.mk
@@ -3,10 +3,11 @@ PKG_VERSION="12.1.5"
 PKG_SHA256="a496936bb3f5093d780a50735f00e39b0b7f3a688eb89051f2ef5f86739522c5"
 PKG_URL="http://pagesperso-orange.fr/sebastien.godard/sysstat-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain util-linux ncurses lm_sensors"
+#PKG_TOOLCHAIN="autotools"
 
-#pre_configure_target() {
-#  cd $PKG_BUILD
-#}
+pre_configure_target() {
+  cd $PKG_BUILD
+}
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-documentation \
 			      --enable-yesterday \
