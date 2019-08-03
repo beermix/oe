ca3a2182eefc7fd4b08acde5c6830a4b48e4f0e0
AV <27rurk@gmail.com>
2019-08-04 01:18:19 +0300

    autocommit on change

--- packages/1my/tig/package.mk
+++ packages/1my/tig/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="tig"
-PKG_VERSION="faf1b86"
+PKG_VERSION="f7e0a25"
 PKG_URL="https://github.com/jonas/tig/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses readline"
 PKG_TOOLCHAIN="autotools"
@@ -7,7 +7,6 @@ PKG_TOOLCHAIN="autotools"
 pre_configure_target() {
   cd $PKG_BUILD
   rm -rf .$TARGET_NAME
-  MAKEFLAGS=-j1
 }
 
 PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.config \
