06e632da83c176b65b2f46a3c30b53213c0cd757
AV <27rurk@gmail.com>
2019-08-04 08:20:28 +0300

    autocommit on change

--- packages/databases/mariadb-connector-c/package.mk
+++ packages/databases/mariadb-connector-c/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mariadb-connector-c"
-PKG_VERSION="3.1.1"
-PKG_SHA256="63a0ecfa3c87ba0361aef3b2c849abe280f7371030e84f41ce0dc4466db85637"
+PKG_VERSION="3.1.3"
+PKG_SHA256="7afac2ef28aea7d71c95473c7a3da0f021a5ef2364480cd3e80eab83016341c2"
 PKG_LICENSE="LGPL"
-PKG_SITE="https://mariadb.org/"
+PKG_SITE="https://github.com/MariaDB/mariadb-connector-c/releases"
 PKG_URL="https://github.com/MariaDB/mariadb-connector-c/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
 PKG_LONGDESC="mariadb-connector: library to conntect to mariadb/mysql database server"
