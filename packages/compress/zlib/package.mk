069d70c584f9d9067769a8f67d03e787c37b08c9
AV <27rurk@gmail.com>
2019-08-04 04:58:47 +0300

    autocommit on change

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -1,52 +1,43 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# PKG_VERSION="af9ef2e"
+# $PKG_SHA256="625b1c4dfaeac5d470bebb55d00a49d1e8db2299750fd00548191b9994a39b72"
+# PKG_URL="https://github.com/cloudflare/zlib/archive/$PKG_VERSION.tar.gz"
 
 PKG_NAME="zlib"
-PKG_VERSION="f06c71f"
-PKG_SHA256="04864be3db5ca900c88a2ef4cc5192fb49b3b4021dbefd5221d7ca5c88b0993a"
-#PKG_VERSION="1.2.11.1_jtkv6.3"
-#PKG_SHA256="897980569aa46d20a4ed9fd63e2e7ce9465e0dd4742a135603db397f168f7f01"
-#PKG_VERSION="af9ef2e"
+PKG_VERSION="1.2.11.1_jtkv6.3"
+PKG_SHA256="897980569aa46d20a4ed9fd63e2e7ce9465e0dd4742a135603db397f168f7f01"
 PKG_LICENSE="OSS"
-PKG_SITE="https://github.com/zlib-ng/zlib-ng"
-PKG_URL="https://github.com/zlib-ng/zlib-ng/archive/$PKG_VERSION.tar.gz"
-#PKG_URL="https://github.com//cloudflare/zlib/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="cmake:host"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_SITE="https://github.com/jtkukunas/zlib/releases"
+PKG_URL="https://github.com/jtkukunas/zlib/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_DEPENDS_TARGET=""
 PKG_LONGDESC="A general purpose (ZIP) data compression library."
-PKG_TOOLCHAIN="cmake-make"
-PKG_BUILD_FLAGS="+lto"
+PKG_TOOLCHAIN="configure"
+PKG_BUILD_FLAGS="+lto +speed"
 
 LTO_SUPPORT="yes"
 GOLD_SUPPORT="yes"
 
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release -DZLIB_COMPAT=ON -DWITH_OPTIM=ON -DCMAKE_VERBOSE_MAKEFILE=0"
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DZLIB_COMPAT=ON -DWITH_OPTIM=ON -DCMAKE_VERBOSE_MAKEFILE=0"
+post_unpack() {
+  mkdir -p $PKG_BUILD/.$HOST_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$HOST_NAME
+
+  mkdir -p $PKG_BUILD/.$TARGET_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
+}
 
 #pre_configure_target() {
 #  export CFLAGS="$CFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
 #  export CXXFLAGS="$CXXFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
 #}
 
-#post_unpack() {
-#  mkdir -p $PKG_BUILD/.$HOST_NAME
-#  cp -RP $PKG_BUILD/* $PKG_BUILD/.$HOST_NAME
-#
-#  mkdir -p $PKG_BUILD/.$TARGET_NAME
-#  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
-#}
+configure_target() {
+  cd $PKG_BUILD/.$TARGET_NAME
+  ./configure --prefix=/usr --libdir=/usr/lib --static --shared
+}
 
-#configure_target() {
-#  cd $PKG_BUILD/.$TARGET_NAME
-#  ./configure --prefix=/usr --libdir=/usr/lib --static --shared
-#}
-
-#configure_host() {
-#  cd $PKG_BUILD/.$HOST_NAME
-#  ./configure --prefix=$TOOLCHAIN --libdir=$TOOLCHAIN/lib --static --shared
-#}
-
-#post_makeinstall_target() {
-#  cd $PKG_BUILD/.$TARGET_NAME
-#  make test all
-#}
+configure_host() {
+  cd $PKG_BUILD/.$HOST_NAME
+  ./configure --prefix=$TOOLCHAIN --libdir=$TOOLCHAIN/lib --static --shared
+}
