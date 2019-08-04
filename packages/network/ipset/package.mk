c85557b60a490c757d9241bd637bcdfc7b7f257b
AV <27rurk@gmail.com>
2019-08-04 07:10:48 +0300

    autocommit on change

--- packages/network/ipset/package.mk
+++ packages/network/ipset/package.mk
@@ -2,7 +2,6 @@ PKG_NAME="ipset"
 PKG_VERSION="7.1"
 PKG_URL="http://ipset.netfilter.org/ipset-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain linux libmnl libnftnl"
-PKG_SECTION="tools"
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
