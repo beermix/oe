b6507c38a92d04424e640858fc9fd0f87bd27083
AV <27rurk@gmail.com>
2019-08-04 08:38:07 +0300

    autocommit on change

--- packages/network/libnl-tiny/package.mk
+++ packages/network/libnl-tiny/package.mk
@@ -1,20 +1,14 @@
 PKG_NAME="libnl-tiny"
-PKG_VERSION="4225e93"
-PKG_SHA256="d2b57597b234c84b113e7640064c3273616c0bf086ad223d71f49aaea086ad59"
+#PKG_VERSION="4225e93"
+#PKG_SHA256="d2b57597b234c84b113e7640064c3273616c0bf086ad223d71f49aaea086ad59"
+PKG_VERSION="233b913681a29c7a18251f42895d1f81b6bbcbc0"
 PKG_SITE="https://github.com/sabotage-linux/libnl-tiny"
 PKG_URL="http://ftp.barfooze.de/pub/sabotage/tarballs/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://github.com/sabotage-linux/libnl-tiny/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 
-pre_make_target() {
+make_target() {
   LIBNLTINY_MAKE_OPTS="CC=$CC AR=$AR RANLIB=$RANLIB prefix=/usr SHAREDLIB="
   PKG_MAKE_OPTS_TARGET="$LIBNLTINY_MAKE_OPTS"
   PKG_MAKEINSTALL_OPTS_TARGET="$LIBNLTINY_MAKE_OPTS"
 }
-
-#CC="$CC" CFLAGS="$optcflags" prefix="$butch_prefix"
-
-
-#LIBNLTINY_MAKE_OPTS="CC=$CC AR=$AR RANLIB=$RANLIB CFLAGS=$CFLAGS prefix=/usr SHAREDLIB="
-#PKG_MAKE_OPTS_TARGET="$LIBNLTINY_MAKE_OPTS"
-#PKG_MAKEINSTALL_OPTS_TARGET="$LIBNLTINY_MAKE_OPTS"
