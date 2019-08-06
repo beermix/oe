318aaaef55c586c60b9a7a66ad201005c1260b38
AV <27rurk@gmail.com>
2019-08-06 14:18:29 +0300

    autocommit on change

--- packages/1my/libevent/package.mk
+++ packages/1my/libevent/package.mk
@@ -1,19 +1,18 @@
 PKG_NAME="libevent"
-PKG_VERSION="2.1.10-stable"
-PKG_SHA256="52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79"
+PKG_VERSION="2.1.11-stable"
+PKG_SHA256="229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
 PKG_SITE="https://github.com/libevent/libevent/releases"
 PKG_URL="https://github.com/libevent/$PKG_NAME/archive/release-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
-PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="+pic"
+#PKG_TOOLCHAIN="autotools"
+#PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress \
-                           --disable-samples \
-                           --disable-shared \
-                           --enable-static \
-                           --enable-openssl"
-
-post_makeinstall_target() {
-  rm -fr "$INSTALL"
-}
+PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_BENCHMARK=ON \
+			  -DEVENT__DISABLE_DEBUG_MODE=ON \
+			  -DEVENT__DISABLE_REGRESS=ON \
+			  -DEVENT__DISABLE_SAMPLES=ON \
+			  -DEVENT__DISABLE_TESTS=ON \
+			  -DEVENT__DISABLE_OPENSSL=OFF \
+			  -DBUILD_TESTING=OFF \
+			  -DEVENT__LIBRARY_TYPE=STATIC"
