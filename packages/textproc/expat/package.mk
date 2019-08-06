a7ddbe53430ea0a99f4ba596fbf2c94da3b31e94
AV <27rurk@gmail.com>
2019-08-06 22:20:39 +0300

    autocommit on change

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,15 +3,20 @@
 
 PKG_NAME="expat"
 PKG_VERSION="2.2.7"
-PKG_SHA256="cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18"
+PKG_SHA256="30e3f40acf9a8fdbd5c379bdcc8d1178a1d9af306de29fc8ece922bc4c57bef8"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/libexpat/libexpat/releases"
-PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Expat is an XML parser library written in C."
 PKG_TOOLCHAIN="cmake-make"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+			  -DBUILD_doc=OFF \
+			  -DBUILD_tools=OFF \
+			  -DBUILD_examples=OFF \
+			  -DBUILD_tests=OFF \
+			  -DBUILD_shared=ON"
 
 PKG_CMAKE_OPTS_HOST="$PKG_CMAKE_OPTS_TARGET"
 
