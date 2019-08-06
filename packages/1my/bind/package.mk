60cb8db695a6f1dd4a89e55b0c512b7a10f11954
AV <27rurk@gmail.com>
2019-08-07 02:17:52 +0300

    autocommit on change

--- packages/1my/bind/package.mk
+++ packages/1my/bind/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="bind"
-PKG_VERSION="9.14.2"
-PKG_SHA256="0e4027573726502ec038db3973a086c02508671723a4845e21da1769a5c27f0c"
+PKG_VERSION="9.15.2"
+PKG_SHA256="69108ea935e9272287c3863cd17395ebeac8a70945130fbfb3cce95cfcc3fc54"
 PKG_URL="https://fossies.org/linux/misc/dns/bind9/$PKG_VERSION/bind-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain sqlite libcap openssl"
 PKG_TOOLCHAIN="autotools"
