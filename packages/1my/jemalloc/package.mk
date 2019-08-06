58ebf76fb470db881139226e9644a0194c2c7d7c
AV <27rurk@gmail.com>
2019-08-06 21:43:24 +0300

    autocommit on change

---
+++ packages/1my/jemalloc/package.mk
@@ -0,0 +1,20 @@
+PKG_NAME="jemalloc"
+PKG_VERSION="5.2.1"
+PKG_SHA256="34330e5ce276099e2e8950d9335db5a875689a4c6a56751ef3b1d8c537f887f6"
+PKG_URL="https://github.com/jemalloc/jemalloc/releases/download/$PKG_VERSION/jemalloc-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-autogen"
+
+make_target() {
+  make build_lib_static
+}
+
+makeinstall_target() {
+  cp lib/libjemalloc.a $SYSROOT_PREFIX/usr/lib
+  cp lib/libjemalloc_pic.a $SYSROOT_PREFIX/usr/lib
+}
+post_makeinstall_target() {
+  rm -rf $SYSROOT_PREFIX/usr/lib/libjemalloc.so*
+  rm -rf $SYSROOT_PREFIX/usr/lib/libjemalloc.so
+}
--- packages/compress/zstd/package.mk
+++ packages/compress/zstd/package.mk
@@ -10,8 +10,14 @@ PKG_URL="https://github.com/facebook/zstd/releases/download/v${PKG_VERSION}/${PK
 PKG_DEPENDS_HOST="gcc:host ninja:host cmake:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A fast real-time compression algorithm."
-PKG_TOOLCHAIN="cmake-make"
 
 configure_package() {
   PKG_CMAKE_SCRIPT="${PKG_BUILD}/build/cmake/CMakeLists.txt"
+
+  PKG_CMAKE_OPTS_HOST="-DZSTD_BUILD_SHARED=0 -DZSTD_BUILD_STATIC=1"
+  PKG_CMAKE_OPTS_TARGET="-DZSTD_BUILD_SHARED=0 -DZSTD_BUILD_STATIC=1"
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL
 }
