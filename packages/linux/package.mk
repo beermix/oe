ef10927faaa61606aff020369ab65cdfa65033cd
AV <27rurk@gmail.com>
2019-08-06 23:27:03 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -156,9 +156,9 @@ pre_make_target() {
   if [ "$TARGET_ARCH" = "x86_64" ]; then
     # copy some extra firmware to linux tree
     mkdir -p $PKG_BUILD/external-firmware
-      #cp -a $(get_build_dir kernel-firmware)/rtl_nic $PKG_BUILD/external-firmware
+    #cp -a $(get_build_dir kernel-firmware)/rtl_nic $PKG_BUILD/external-firmware
 
-    cp -a $(get_build_dir intel-ucode)/intel-ucode $PKG_BUILD/external-firmware
+    #cp -a $(get_build_dir intel-ucode)/intel-ucode $PKG_BUILD/external-firmware
 
     FW_LIST="$(find $PKG_BUILD/external-firmware \( -type f -o -type l \) \( -iname '*.bin' -o -iname '*.fw' -o -path '*/intel-ucode/*' \) | sed 's|.*external-firmware/||' | sort | xargs)"
     sed -i "s|CONFIG_EXTRA_FIRMWARE=.*|CONFIG_EXTRA_FIRMWARE=\"${FW_LIST}\"|" $PKG_BUILD/.config
