69f601cd30281969130050e2798ada2b0e6bd86a
AV <27rurk@gmail.com>
2019-08-06 12:46:31 +0300

    autocommit on change

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="ethtool"
-PKG_VERSION="5.1"
-PKG_SHA256="65feac1fec6565290b7784b2efc925dee900a9b11202ce7c6c30a967c3da3387"
+PKG_VERSION="5.2"
+PKG_SHA256="665fd70841860d6cb974387e3ab97e0dde7745f95cb3ef35b98ef9aace137805"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org/pub/software/network/ethtool/"
 PKG_URL="http://www.kernel.org/pub/software/network/ethtool/$PKG_NAME-$PKG_VERSION.tar.xz"
