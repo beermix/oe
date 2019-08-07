fa539da5bd742f7de1fad2d9294d14ab1ff3f149
AV <27rurk@gmail.com>
2019-08-07 12:46:07 +0300

    autocommit on change

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -3,16 +3,16 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ffmpeg"
-#PKG_VERSION="3.1.11-Krypton-17.5"
-#PKG_SHA256=""
-PKG_VERSION="5fd65eb"
-PKG_SHA256="633476e6c64fe373e14d49337005bed8e79ab6d18a8fd75ffa2c0735f8bf3688"
+PKG_VERSION="3.1.11-Krypton-17.5"
+PKG_SHA256="7df8bce40765b39de5766ca9d08b5b9ac1f498c65c805c989461b907cee6b7c0"
+#PKG_VERSION="5fd65eb"
+#PKG_SHA256="633476e6c64fe373e14d49337005bed8e79ab6d18a8fd75ffa2c0735f8bf3688"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://github.com/FFmpeg/FFmpeg/tree/release/3.2"
 PKG_URL="https://ffmpeg.org/releases/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://github.com/FFmpeg/FFmpeg/archive/${PKG_VERSION}.tar.gz"
 #PKG_URL="https://ffmpeg.org/releases/$PKG_NAME-$PKG_VERSION.tar.xz"
-#PKG_URL="https://github.com/xbmc/FFmpeg/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/xbmc/FFmpeg/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib bzip2 gnutls speex"
 PKG_LONGDESC="FFmpeg is a complete, cross-platform solution to record, convert and stream audio and video."
 PKG_BUILD_FLAGS="-gold"
@@ -203,7 +203,7 @@ configure_target() {
               --disable-altivec \
               $PKG_FFMPEG_FPU \
               --disable-symver \
-              --enable-lto \
+              --disable-lto \
               --enable-indev=x11grab_xcb
 }
 
