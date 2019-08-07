def035a3affbbcd7dcbbb1c5d83868fb2522ced4
AV <27rurk@gmail.com>
2019-08-07 10:27:58 +0300

    autocommit on change

--- packages/debug/libva-utils/package.mk
+++ packages/debug/libva-utils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libva-utils"
-PKG_VERSION="2.4.1"
-PKG_SHA256="4ab6faa6592ffe2d4817450f5453f3974b47441650e8f49e1a12497ce2a794ac"
+PKG_VERSION="2.5.0"
+PKG_SHA256="d6c37257933731c7936775376388dfe6c9ebc460adff29f9ffe74cde2ebb3f52"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/intel/libva-utils/releases"
 PKG_URL="https://github.com/intel/libva-utils/archive/$PKG_VERSION.tar.gz"
