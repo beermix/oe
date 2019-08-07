d7674f57fac13c7a82eecba1a54a0e274a317474
AV <27rurk@gmail.com>
2019-08-07 08:41:11 +0300

    autocommit on change

--- packages/1my/libevent/package.mk
+++ packages/1my/libevent/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libevent"
-PKG_VERSION="2.1.10-stable"
-PKG_SHA256="52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79"
+PKG_VERSION="2.1.11-stable"
+PKG_SHA256="229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
 PKG_SITE="https://github.com/libevent/libevent/releases"
 PKG_URL="https://github.com/libevent/$PKG_NAME/archive/release-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
