269cd22587d7928ab9c619af65628adbda9b3a89
AV <27rurk@gmail.com>
2019-08-09 19:51:05 +0300

    autocommit on change

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,33 +2,16 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="74a3e05"
-PKG_SHA256="72e4f99869b6b34455c1f1bebd348e43865c948566dedf5fd2bfaed245a2a0b3"
+PKG_VERSION="1.2.11"
+PKG_SHA256="4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066"
 PKG_LICENSE="OSS"
-PKG_SITE="https://github.com/zlib-ng/zlib-ng"
-PKG_URL="https://github.com/zlib-ng/zlib-ng/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="http://www.zlib.net"
+PKG_URL="http://zlib.net/zlib-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="cmake:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A general purpose (ZIP) data compression library."
 PKG_TOOLCHAIN="cmake-make"
-#PKG_BUILD_FLAGS="+lto"
 
-#LTO_SUPPORT="yes"
-#GOLD_SUPPORT="yes"
-# -DWITH_OPTIM=ON 
+PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
 
-#pre_configure_target() {
-#  export CFLAGS="$CFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-#  export CXXFLAGS="$CXXFLAGS -falign-functions=32 -ffat-lto-objects -flto=4 -fno-math-errno -fno-semantic-interposition -fno-trapping-math"
-#}
-
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release -DZLIB_COMPAT=ON"
-
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
-			  -DZLIB_COMPAT=ON \
-			  -DWITH_OPTIM=ON \
-			  -DCMAKE_VERBOSE_MAKEFILE=ON"
-			  
-makeinstall_host() {
-  : # nop
-}
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release"
