70c8bda0537cb449b89274fc4844e8701a387b23
AV <27rurk@gmail.com>
2019-08-10 01:57:34 +0300

    autocommit on change

--- packages/addons/chrome-depends/libev/package.mk
+++ packages/addons/chrome-depends/libev/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libev"
-PKG_VERSION="4.25"
-#PKG_SHA256="973593d3479abdf657674a55afe5f78624b0e440614e2b8cb3a07f16d4d7f821"
+PKG_VERSION="4.27"
+PKG_SHA256="2d5526fc8da4f072dd5c73e18fbb1666f5ef8ed78b73bba12e195cfdd810344e"
 PKG_LICENSE="GPL"
 PKG_SITE="http://software.schmorp.de/pkg/libev.html"
 PKG_URL="http://dist.schmorp.de/libev/${PKG_NAME}-${PKG_VERSION}.tar.gz"
