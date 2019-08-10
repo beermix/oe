aa3df6bdf76d9f0b216aaef373a395cb68a97fa3
AV <27rurk@gmail.com>
2019-08-10 07:24:46 +0300

    autocommit on change

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,7 +29,7 @@ case "$LINUX" in
     PKG_SHA256=""
     PKG_URL="https://github.com/zen-kernel/zen-kernel/archive/v$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="zen-kernel-$PKG_VERSION*"
-    PKG_PATCH_DIRS="default"
+    PKG_PATCH_DIRS="zen"
     PKG_BUILD_PERF="no"
     ;;
   git)
