26a6d10e33ad6dfbab60a327864f8cb1f6abd1ac
AV <27rurk@gmail.com>
2019-08-10 11:12:57 +0300

    autocommit on change

--- packages/compress/bzip2/package.mk
+++ packages/compress/bzip2/package.mk
@@ -3,67 +3,30 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bzip2"
-PKG_VERSION="1.0.8"
-PKG_SHA256="ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
+PKG_VERSION="3eff3392"
+PKG_SHA256="e48fab35e462aa4f4045bfe8d2d3949edb55700d10b1034cd03f6cb52e132b5f"
 PKG_LICENSE="GPL"
-PKG_SITE="https://sourceware.org/bzip2/"
-PKG_URL="https://sourceware.org/pub/bzip2/bzip2-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="gcc:host"
+PKG_SITE="https://gitlab.com/federicomenaquintero/bzip2"
+PKG_URL="https://gitlab.com/federicomenaquintero/bzip2/-/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="gcc:host cmake:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A high-quality bzip2 data compressor."
-PKG_BUILD_FLAGS="+pic:host +pic +speed"
+PKG_TOOLCHAIN="cmake-make"
 
-#pre_configure_target() {
-#  export CFLAGS="$CFLAGS -Wall -fno-semantic-interposition -ffunction-sections"
-#  export CXXFLAGS="$CXXFLAGS -Wall -fno-semantic-interposition -ffunction-sections"
-#}
+PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release \
+			-DENABLE_STATIC_LIB=ON \
+			-DENABLE_SHARED_LIB=OFF \
+			-DENABLE_WERROR=OFF \
+			-DENABLE_EXAMPLES=OFF \
+			-DENABLE_DOCS=OFF \
+			-DENABLE_DEBUG=OFF \
+			-DENABLE_APP=ON"
 
-pre_build_host() {
-  mkdir -p $PKG_BUILD/.$HOST_NAME
-  cp -r $PKG_BUILD/* $PKG_BUILD/.$HOST_NAME
-}
-
-make_host() {
-  cd $PKG_BUILD/.$HOST_NAME
-  make -f Makefile-libbz2_so CC=$HOST_CC CFLAGS="$CFLAGS"
-}
-
-makeinstall_host() {
-  make install PREFIX=$TOOLCHAIN
-}
-
-pre_build_target() {
-  mkdir -p $PKG_BUILD/.$TARGET_NAME
-  cp -r $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
-}
-
-pre_make_target() {
-  cd $PKG_BUILD/.$TARGET_NAME
-  sed -e "s,ln -s (lib.*),ln -snf \$$1; ln -snf libbz2.so.$PKG_VERSION libbz2.so,g" -i Makefile-libbz2_so
-}
-
-make_target() {
-  make -f Makefile-libbz2_so CC=$CC CFLAGS="$CFLAGS"
-  make -f Makefile libbz2.a bzip2 bzip2recover CC="$CC" CFLAGS="$CFLAGS"
-}
-
-post_make_target() {
-  ln -snf libbz2.so.1.0 libbz2.so
-}
-
-makeinstall_target() {
-  mkdir -p $SYSROOT_PREFIX/usr/include
-    cp bzlib.h $SYSROOT_PREFIX/usr/include
-  mkdir -p $SYSROOT_PREFIX/usr/lib
-    cp -P libbz2.so* $SYSROOT_PREFIX/usr/lib
-    cp libbz2.a $SYSROOT_PREFIX/usr/lib
-
-  mkdir -p $INSTALL/usr/lib
-    cp -P libbz2.so* $INSTALL/usr/lib
-    
-  mkdir -p $INSTALL/usr/bin
-    cp -P bzdiff $INSTALL/usr/bin
-    cp -P bzgrep $INSTALL/usr/bin
-    cp -P bzip2 $INSTALL/usr/bin
-    cp -P bzmore $INSTALL/usr/bin
-}
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+			  -DENABLE_STATIC_LIB=ON \
+			  -DENABLE_SHARED_LIB=ON \
+			  -DENABLE_WERROR=OFF \
+			  -DENABLE_EXAMPLES=OFF \
+			  -DENABLE_DOCS=OFF \
+			  -DENABLE_DEBUG=OFF \
+			  -DENABLE_APP=ON"
