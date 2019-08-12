ed3f3d5e7c024209288e7d8696042663f69575eb
AV <27rurk@gmail.com>
2019-08-12 21:41:26 +0300

    autocommit on change

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="configtools"
-PKG_VERSION="706fbe5"
-PKG_SHA256="185511fd06ccbb892e14d6c593f68f0f1fd7c713b21a8d8bd3c7fd949e9c5979"
+PKG_VERSION="04b07fc"
+PKG_SHA256="dfe9d202c759d1eba2f3a3dd7db14b116d6a103c93958cea9c3137782761c9e6"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
 PKG_URL="http://git.savannah.gnu.org/cgit/config.git/snapshot/$PKG_VERSION.tar.xz"
--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -5,6 +5,7 @@
 PKG_NAME="isl"
 PKG_VERSION="0.21"
 PKG_SHA256="777058852a3db9500954361e294881214f6ecd4b594c00da5eee974cd6a54960"
+PKG_SITE="http://isl.gforge.inria.fr/?C=M;O=D"
 PKG_URL="http://isl.gforge.inria.fr/isl-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="libtool:host intltool:host gmp:host"
 
@@ -12,5 +13,4 @@ pre_configure_host() {
   unset CFLAGS
 }
 
-PKG_CONFIGURE_OPTS_HOST="--with-gmp-prefix=$TOOLCHAIN \
-			    --disable-shared --enable-static"
+PKG_CONFIGURE_OPTS_HOST="--with-gmp-prefix=$TOOLCHAIN --disable-shared --enable-static"
