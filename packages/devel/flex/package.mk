8295d7916605e6897b8422e32c477a95ed06f5d9
AV <27rurk@gmail.com>
2019-08-13 12:47:27 +0300

    autocommit on change

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -8,15 +8,22 @@ PKG_SHA256="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/westes/flex/releases"
 PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="ccache:host m4:host autotools:host"
+PKG_DEPENDS_HOST="ccache:host m4:host autotools:host gettext:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A tool for generating programs that perform pattern-matching on text."
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="ac_cv_path_M4=$BUILD/toolchain/bin/m4 \
+			    --enable-static \
+			    --disable-shared \
+			    --disable-rpath \
+			    --with-gnu-ld \
+			    --disable-doc"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_realloc_0_nonnull=yes \
-                           ac_cv_func_malloc_0_nonnull=yes"
+                           ac_cv_func_malloc_0_nonnull=yes \
+                           --disable-doc \
+                           --disable-program"
 
 post_makeinstall_host() {
   cat > $TOOLCHAIN/bin/lex << "EOF"
