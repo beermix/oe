d47860510b491358fabb7e5eb7854898c1aa4734
AV <27rurk@gmail.com>
2019-08-13 11:49:39 +0300

    autocommit on change

--- packages/lang/Python2/package.mk
+++ packages/lang/Python2/package.mk
@@ -16,7 +16,7 @@ PKG_DEPENDS_TARGET="toolchain sqlite expat zlib bzip2 openssl libffi Python2:hos
 PKG_LONGDESC="Python2 is an interpreted object-oriented programming language."
 
 PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="-parallel +lto-parallel +speed"
+PKG_BUILD_FLAGS="-parallel +speed"
 
 PKG_PY_DISABLED_MODULES="_tkinter nis gdbm bsddb ossaudiodev"
 
@@ -49,7 +49,8 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
                            --without-cxx-main \
                            --with-system-ffi \
                            --with-system-expat \
-                           --with-computed-gotos"
+                           --with-computed-gotos \
+                           --with-lto"
 post_patch() {
   # This is needed to make sure the Python build process doesn't try to
   # regenerate those files with the pgen program. Otherwise, it builds
@@ -81,8 +82,8 @@ post_makeinstall_host() {
 pre_configure_target() {
   export PYTHON_FOR_BUILD=$TOOLCHAIN/bin/python
 
-#  export CFLAGS="$CFLAGS -ffunction-sections -fno-semantic-interposition -fopt-info-vec"
-#  export CXXFLAGS="$CXXFLAGS -ffunction-sections -fno-semantic-interposition -fopt-info-vec"
+  export CFLAGS="$CFLAGS -ffunction-sections -fno-semantic-interposition -fopt-info-vec -flto"
+  export CXXFLAGS="$CXXFLAGS -ffunction-sections -fno-semantic-interposition -fopt-info-vec"
 }
 
 make_target() {
