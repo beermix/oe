afc62dca3133484d6c6fc6b48a8322c20fc35501
AV <27rurk@gmail.com>
2019-08-13 20:22:29 +0300

    autocommit on change

--- packages/addons/chrome-depends/pango/package.mk
+++ packages/addons/chrome-depends/pango/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pango"
-PKG_VERSION="1.44.3"
-PKG_SHA256=""
+PKG_VERSION="1.44.4"
+PKG_SHA256="08dc13188498c685c2f8221682db783f48b87ff7ff2766f14988bfe22a2d3a45"
 PKG_LICENSE="GPL"
 PKG_SITE="https://ftp.gnome.org/pub/gnome/sources/pango/?C=M;O=D"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/pango/${PKG_VERSION:0:4}/pango-$PKG_VERSION.tar.xz"
