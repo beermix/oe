a766f2c126af236c132c568be86ab1b8111122aa
AV <27rurk@gmail.com>
2019-08-13 08:01:32 +0300

    autocommit on change

--- packages/1my/git/package.mk
+++ packages/1my/git/package.mk
@@ -1,8 +1,7 @@
 PKG_NAME="git"
-PKG_VERSION="2.19.2"
-PKG_SHA256="fce9a3a3297db5f3756c4553a2fc1fec209ee08178f8491e76ff4ff8fe7b8be9"
+PKG_VERSION="2.22.1"
+PKG_SHA256="4ce1f93ab77847e9a9d565ee7c94ceadb80ea7d477fe68123f2329f8d4937824"
 PKG_URL="https://www.kernel.org/pub/software/scm/git/git-$PKG_VERSION.tar.xz"
-#PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain curl pcre zlib openssl"
 PKG_DEPENDS_HOST="zlib:host pcre:host"
 PKG_TOOLCHAIN="autotools"
@@ -12,10 +11,10 @@ pre_configure_target() {
   mkdir -p $PKG_BUILD/.$TARGET_NAME
   cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
 
-  export CC="$(TARGET_CC)"
-  export CFLAGS="$(TARGET_CFLAGS)"
-  export CPPFLAGS="$(TARGET_CPPFLAGS)"
-  export LDFLAGS="$(TARGET_LDFLAGS)"
+  export CC="$CC"
+  export CFLAGS="$CFLAGS"
+  export CPPFLAGS="$CPPFLAGS"
+  export LDFLAGS="$LDFLAGS"
   export NO_EXPAT="YesPlease"
   export NO_MKSTEMPS="YesPlease"
   export NO_GETTEXT="YesPlease"
