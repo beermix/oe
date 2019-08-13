313f7a951bc4f476ded6f4b2cc89711b3fa84563
AV <27rurk@gmail.com>
2019-08-13 11:37:23 +0300

    autocommit on change

--- packages/multimedia/intel-vaapi-driver/package.mk
+++ packages/multimedia/intel-vaapi-driver/package.mk
@@ -4,22 +4,16 @@
 
 PKG_NAME="intel-vaapi-driver"
 PKG_VERSION="e60af21"
-PKG_SHA256=""
+PKG_SHA256="ae2f89351975050a3f56f979496027a3486e575c74b2a415870e2c1e8a419fc7"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/intel/intel-vaapi-driver/releases"
 PKG_URL="https://github.com/intel/intel-vaapi-driver/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libva libdrm"
 PKG_LONGDESC="intel-vaapi-driver: VA-API user mode driver for Intel GEN Graphics family"
-PKG_TOOLCHAIN="autotools"
+PKG_TOOLCHAIN="meson"
 
-if [ "$DISPLAYSERVER" = "x11" ]; then
-  DISPLAYSERVER_LIBVA="--enable-x11 --disable-wayland"
-elif [ "$DISPLAYSERVER" = "weston" ]; then
-  DISPLAYSERVER_LIBVA="--disable-x11 --enable-wayland"
-else
-  DISPLAYSERVER_LIBVA="--disable-x11 --disable-wayland"
-fi
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-silent-rules \
-                           $DISPLAYSERVER_LIBVA"
+PKG_MESON_OPTS_TARGET="-Dwith_x11=yes \
+                       -Dwith_wayland=no \
+                       -Denable_hybrid_codec=true \
+                       -Denable_tests=false"
--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -10,20 +10,11 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/intel/libva/releases"
 PKG_URL="https://github.com/intel/libva/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="Libva is an implementation for VA-API (VIdeo Acceleration API)."
-PKG_TOOLCHAIN="autotools"
+PKG_TOOLCHAIN="meson"
 
-if [ "$DISPLAYSERVER" = "x11" ]; then
-  PKG_DEPENDS_TARGET="toolchain libX11 libXext libXfixes libdrm"
-  DISPLAYSERVER_LIBVA="--enable-x11 --disable-glx --disable-wayland"
-elif [ "$DISPLAYSERVER" = "weston" ]; then
-  DISPLAYSERVER_LIBVA="--disable-x11 --disable-glx --enable-wayland"
-  PKG_DEPENDS_TARGET="toolchain libdrm wayland"
-else
-  PKG_DEPENDS_TARGET="toolchain libdrm"
-  DISPLAYSERVER_LIBVA="--disable-x11 --disable-glx --disable-wayland"
-fi
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-silent-rules \
-                           --disable-docs \
-                           --enable-drm \
-                           $DISPLAYSERVER_LIBVA"
+PKG_MESON_OPTS_TARGET="-Ddisable_drm=false \
+                       -Dwith_x11=yes \
+                       -Dwith_glx=no \
+                       -Dwith_wayland=no \
+                       -Denable_va_messaging=true \
+                       -Denable_docs=false"
