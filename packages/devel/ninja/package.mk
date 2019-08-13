03ce27dcad368b49f3acebe79e88721e530e35d3
AV <27rurk@gmail.com>
2019-08-13 13:28:35 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -2,19 +2,15 @@
 # Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="ninja"
-#PKG_VERSION="1.9.0"
-#PKG_SHA256="5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9"
-PKG_VERSION="bd17f23"
+PKG_VERSION="1.9.0"
+PKG_SHA256="5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9"
+#PKG_VERSION="bd17f23"
 PKG_LICENSE="Apache"
 PKG_SITE="https://github.com/ninja-build/ninja"
 PKG_URL="https://github.com/ninja-build/ninja/archive/v$PKG_VERSION.tar.gz"
-PKG_URL="https://github.com/ninja-build/ninja/archive/$PKG_VERSION.tar.gz"
+#PKG_URL="https://github.com/ninja-build/ninja/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="Python2:host"
 PKG_TOOLCHAIN="manual"
-
-pre_configure_host() {
-  export LDFLAGS="$LDFLAGS -s"
-}
 
 make_host() {
   python2 configure.py --bootstrap
@@ -24,4 +20,5 @@ make_host() {
 
 makeinstall_host() {
   cp ninja $TOOLCHAIN/bin
+  strip $TOOLCHAIN/bin/ninja
 }
