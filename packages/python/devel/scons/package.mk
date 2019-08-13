edc8c874ac41795c7786de21da4c09cbcef6c4e1
AV <27rurk@gmail.com>
2019-08-13 04:37:59 +0300

    autocommit on change

--- packages/python/devel/scons/package.mk
+++ packages/python/devel/scons/package.mk
@@ -1,11 +1,8 @@
 PKG_NAME="scons"
 PKG_VERSION="2.5.1"
-PKG_ARCH="any"
-PKG_LICENSE="MIT"
 PKG_SITE="http://scons.org"
 PKG_URL="http://downloads.sourceforge.net/sourceforge/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="Python2:host"
-PKG_SECTION="python/devel"
 PKG_SHORTDESC="Extensible Python-based build utility"
 PKG_LONGDESC="Extensible Python-based build utility"
 
