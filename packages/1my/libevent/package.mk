59dfa327dac7674735badbee62f286bca114d627
AV <27rurk@gmail.com>
2019-08-13 13:53:53 +0300

    autocommit on change

--- packages/1my/libevent/package.mk
+++ packages/1my/libevent/package.mk
@@ -5,6 +5,7 @@ PKG_SITE="https://github.com/libevent/libevent/releases"
 PKG_URL="https://github.com/libevent/$PKG_NAME/archive/release-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_BENCHMARK=ON \
 			  -DEVENT__DISABLE_DEBUG_MODE=ON \
@@ -14,3 +15,10 @@ PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_BENCHMARK=ON \
 			  -DEVENT__DISABLE_OPENSSL=OFF \
 			  -DBUILD_TESTING=OFF \
 			  -DEVENT__LIBRARY_TYPE=STATIC"
+			  
+PKG_CONFIGURE_OPTS_TARGET="--disable-libevent-regress \
+			      --disable-debug-mode \
+			      --disable-silent-rules \
+			      --disable-shared \
+			      --with-pic"
+			  
