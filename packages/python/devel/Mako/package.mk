6ce8af4891f2e4b5f6485be591162d2a2f6f6aa5
AV <27rurk@gmail.com>
2019-08-13 04:36:31 +0300

    autocommit on change

--- packages/python/devel/Mako/package.mk
+++ packages/python/devel/Mako/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Mako"
-PKG_VERSION="1.0.14"
-PKG_SHA256="f5a642d8c5699269ab62a68b296ff990767eb120f51e2e8f3d6afb16bdb57f4b"
+PKG_VERSION="1.1.0"
+PKG_SHA256="a36919599a9b7dc5d86a7a8988f23a9a3a3d083070023bab23d64f7f1d1e0a4b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/Mako"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
