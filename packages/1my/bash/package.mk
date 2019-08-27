483011da488366e91f351b24881b6f92eabbebad
AV <27rurk@gmail.com>
2019-08-28 02:55:41 +0300

    autocommit on change

--- packages/1my/bash/package.mk
+++ packages/1my/bash/package.mk
@@ -5,10 +5,20 @@ PKG_SHA256="acd35b78d07c1824b017574ed8c9fabec6d42010f531e82b0edfff69f4f25d76"
 #PKG_URL="http://ftp.gnu.org/gnu/bash/bash-$PKG_VERSION.tar.gz"
 PKG_URL="https://github.com/bminor/bash/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses readline"
+PKG_BUILD_FLAGS="-parallel"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-readline \
+PKG_CONFIGURE_OPTS_TARGET="bash_cv_func_sigsetjmp=present \
+			      bash_cv_printf_a_format=yes \
+			      bash_cv_getcwd_malloc=yes \
+			      bash_cv_job_control_missing=present \
+			      bash_cv_dev_fd=whacky \
+			      bash_cv_sys_named_pipes=present \
+			      --with-curses \
+			      --enable-readline \
+			      --without-bash-malloc \
 			      --with-installed-readline \
-			      --with-bash-malloc=no \
+			      --enable-direxpand-default \
+			      --enable-job-control \
 			      --disable-rpath"
 
 #post_makeinstall_target() {
