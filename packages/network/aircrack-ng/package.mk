57526d6386c607c2a1f6eba392d4bacf0ee935f4
AV <27rurk@gmail.com>
2019-08-27 22:17:21 +0300

    autocommit on change

--- packages/network/aircrack-ng/package.mk
+++ packages/network/aircrack-ng/package.mk
@@ -1,26 +1,8 @@
-prPKG_NAME="aircrack-ng"
-PKG_VERSION="1.2-rc4"
+PKG_NAME="aircrack-ng"
+PKG_VERSION="c2e20a4"
+PKG_SHA256=""
 PKG_URL="https://github.com/aircrack-ng/aircrack-ng/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl libdnet libnl libpcap openssl pcre usbutils iw"
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="autotools"
 
-pre_configure_target() {
-  cd $PKG_BUILD
-  rm -rf .$TARGET_NAME
-}
-
-#PKG_MAKE_OPTS_TARGET="prefix=/usr sqlite=true unstable=false ext_scripts=true pcre=true libnl=true"
-
-PKG_MAKE_OPTS_TARGET="prefix=/usr libnl=false sqlite=true unstable=false pcre=true ext_scripts=true"
-
-#PKG_MAKEINSTALL_OPTS_TARGET="$PKG_MAKE_OPTS_TARGET"
-
-make_target() {
-#  cd $PKG_BUILD
-  make
-}
-
-make_install_target() {
-#  cd $PKG_BUILD
-  make install
-}
+PKG_CONFIGURE_OPTS_TARGET="--disable-dependency-tracking"
--- packages/network/iw/package.mk
+++ packages/network/iw/package.mk
@@ -1,9 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
 PKG_NAME="iw"
-PKG_VERSION="4.14"
+PKG_VERSION="5.3"
+PKG_SHA256="04afe857bc8dea67e461946de30ae1b012954b6965839c5c3fda7d0ed15505d5"
+PKG_LICENSE="PUBLIC_DOMAIN"
 PKG_SITE="http://wireless.kernel.org/en/users/Documentation/iw"
 PKG_URL="https://www.kernel.org/pub/software/network/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libnl"
+PKG_LONGDESC="A new nl80211 based CLI configuration utility for wireless devices."
+# iw fails at runtime with lto enabled
 
-make_target() {
-  make PKG_CONFIG=$TOOLCHAIN/bin/pkg-config CC=$CC
+pre_configure_target() {
+  export LDFLAGS="$LDFLAGS -pthread"
 }
