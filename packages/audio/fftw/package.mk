1cc4edea55daf87e62c988c5a10eed4e05fc8275
AV <27rurk@gmail.com>
2019-08-27 21:23:01 +0300

    autocommit on change

--- packages/audio/fftw/package.mk
+++ packages/audio/fftw/package.mk
@@ -3,12 +3,11 @@ PKG_VERSION="3.3.8"
 PKG_SITE="ftp://ftp.fftw.org/pub/fftw/"
 PKG_URL="ftp://ftp.fftw.org/pub/fftw/fftw-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="service/system"
-PKG_TOOLCHAIN="cmake-make"
+#PKG_TOOLCHAIN="cmake-make"
 PKG_BUILD_FLAGS="+pic"
 
 #pre_configure_target() {
-#  export CFLAGS="$CFLAGS -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math -fopenmp -fPIC"
+#  export CFLAGS="$CFLAGS -malign-double -fstrict-aliasing -ffast-math -fopenmp -fPIC"
 #}
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
@@ -16,7 +15,7 @@ PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
 			  -DENABLE_AVX=0 \
 			  -DENABLE_AVX2=0 \
 			  -DENABLE_FLOAT=1 \
-			  -DENABLE_OPENMP=1 \
+			  -DENABLE_OPENMP=0 \
 			  -DENABLE_SSE=1 \
 			  -DENABLE_SSE2=2 \
 			  -DENABLE_THREADS=1"
