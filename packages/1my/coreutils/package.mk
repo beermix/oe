11be265c71c566a6c29286343f290e9b8d3c703f
AV <27rurk@gmail.com>
2019-08-28 03:03:59 +0300

    autocommit on change

--- packages/1my/coreutils/package.mk
+++ packages/1my/coreutils/package.mk
@@ -2,7 +2,7 @@ PKG_NAME="coreutils"
 PKG_VERSION="8.31"
 PKG_URL="http://ftpmirror.gnu.org/coreutils/coreutils-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="acl libcap pcre readline openssl"
-PKG_TOOLCHAIN="autotools"
+#PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="PERL=missing \
 			      MAKEINFO=missing \
