18f01693a42de511dad2faf3c0a3ba70e710de01
AV <27rurk@gmail.com>
2019-08-28 15:25:12 +0300

    autocommit on change

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -23,7 +23,3 @@ post_configure_host() {
 # $PATH. The following addition will correct this:
   echo '#define YYENABLE_NLS 1' >> lib/config.h
 }
-
-#post_makeinstall_host() {
-#  cp -r $PKG_DIR/scripts/yacc $TOOLCHAIN/bin/
-#}
--- packages/devel/mpfr/package.mk
+++ packages/devel/mpfr/package.mk
@@ -11,11 +11,10 @@ PKG_URL="http://ftpmirror.gnu.org/mpfr/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain gmp"
 PKG_DEPENDS_HOST="ccache:host gmp:host"
 PKG_LONGDESC="A C library for multiple-precision floating-point computations with exact rounding."
-PKG_BUILD_FLAGS="+pic:host +pic"
+#PKG_BUILD_FLAGS="+pic:host +pic"
 
 PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
                          --enable-static --disable-shared \
-                         --enable-thread-safe \
                          --prefix=$TOOLCHAIN \
                          --with-gmp-lib=$TOOLCHAIN/lib \
                          --with-gmp-include=$TOOLCHAIN/include"
