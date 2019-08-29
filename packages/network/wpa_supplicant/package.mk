8a9ce24d488b2fe476ea9384a923de068743421f
AV <27rurk@gmail.com>
2019-08-30 02:34:35 +0300

    autocommit on change

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -8,10 +8,10 @@ PKG_SHA256="fcbdee7b4a64bea8177973299c8c824419c413ec2e3a95db63dd6a5dc3541f17"
 PKG_LICENSE="GPL"
 PKG_SITE="https://w1.fi/releases/?C=M;O=D"
 PKG_URL="https://w1.fi/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain dbus libnl openssl"
+PKG_DEPENDS_TARGET="toolchain dbus libnl-tiny openssl"
 PKG_LONGDESC="A free software implementation of an IEEE 802.11i supplicant."
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="+lto"
+PKG_BUILD_FLAGS="+lto-parallel"
 
 PKG_MAKE_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 PKG_MAKEINSTALL_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin eapol_test"
@@ -20,7 +20,7 @@ configure_target() {
  LDFLAGS="$LDFLAGS -lpthread -lm"
 # export LIBS="$LIBS -lpthread -lm"
 
-# export CFLAGS="$CFLAGS -D_GNU_SOURCE -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
+export CFLAGS="$CFLAGS -DCONFIG_LIBNL20 -D_GNU_SOURCE -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
 
   cp $PKG_DIR/config/makefile.config wpa_supplicant/.config
 }
