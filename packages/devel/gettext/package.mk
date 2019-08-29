82ed6c967f2c94e9e8f06cbf727479acda9cc272
AV <27rurk@gmail.com>
2019-08-29 21:30:06 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -8,8 +8,9 @@ PKG_SHA256="ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
 PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host m4:host automake:host"
+PKG_DEPENDS_HOST="ccache:host m4:host automake:host libtool:host"
 PKG_LONGDESC="A program internationalization library and tools."
+PKG_TOOLCHAIN="autotools"
 
 configure_package() {
   PKG_CONFIGURE_SCRIPT="${PKG_BUILD}/gettext-tools/configure"
