671511f1f19c082120833a43a439465ba4e6f192
AV <27rurk@gmail.com>
2019-08-30 02:11:40 +0300

    autocommit on change

--- packages/audio/taglib/package.mk
+++ packages/audio/taglib/package.mk
@@ -2,13 +2,11 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="taglib"
-#PKG_VERSION="1.11.1"
-#PKG_SHA256="b6d1a5a610aae6ff39d93de5efd0fdc787aa9e9dc1e7026fa4c961b26563526b"
-PKG_VERSION="2918602"
+PKG_VERSION="1.11.1"
+PKG_SHA256="b6d1a5a610aae6ff39d93de5efd0fdc787aa9e9dc1e7026fa4c961b26563526b"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://taglib.github.com/"
 PKG_URL="http://taglib.github.io/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_URL="https://github.com/taglib/taglib/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host zlib"
 PKG_LONGDESC="TagLib is a library for reading and editing the meta-data of several popular audio formats."
 
