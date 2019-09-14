5e4838ca2c0099a4969d37395c11456638647114
AV <27rurk@gmail.com>
2019-09-14 18:54:24 +0300

    autocommit on change

---
+++ packages/devel/gettext-tiny/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+
+PKG_NAME="gettext-tiny"
+PKG_VERSION="a76f8ad"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/sabotage-linux/gettext-tiny"
+PKG_URL="https://github.com/sabotage-linux/gettext-tiny/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="ccache:host"
+
+make_host() {
+  make LIBINTL=FLAVOR
+}
+
+makeinstall_host() {
+  make LIBINTL=FLAVOR DESTDIR=$TOOLCHAIN prefix=/ all install
+}
