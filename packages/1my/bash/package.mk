5cf0ccc77b03461ac40842edf99ac0bba026612a
AV <27rurk@gmail.com>
2019-09-14 03:04:36 +0300

    autocommit on change

--- packages/1my/bash/package.mk
+++ packages/1my/bash/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="bash"
-PKG_VERSION="9f597fd"
-PKG_SHA256="acd35b78d07c1824b017574ed8c9fabec6d42010f531e82b0edfff69f4f25d76"
+PKG_VERSION="d894cfd"
+PKG_SHA256=""
 #PKG_SITE="https://github.com/bminor/bash/"
 #PKG_URL="http://ftp.gnu.org/gnu/bash/bash-$PKG_VERSION.tar.gz"
 PKG_URL="https://github.com/bminor/bash/archive/${PKG_VERSION}.tar.gz"
