1f6d811427a8895c015aecb8a5e60c9f6e0ef564
AV <27rurk@gmail.com>
2019-09-15 04:25:48 +0300

    autocommit on change

---
+++ packages/1my/uClibc-ng/package.mk
@@ -0,0 +1,7 @@
+PKG_NAME="uClibc-ng"
+PKG_VERSION="1.0.31"
+PKG_URL="https://downloads.uclibc-ng.org/releases/1.0.31/uClibc-ng-1.0.31.tar.gz"
+
+PKG_TOOLCHAIN="autotools"
+
+#PKG_CONFIGURE_OPTS_TARGET="--enable-static"
