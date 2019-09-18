44513557438b5a86e7dac1c6816e0d930bca3fb0
AV <27rurk@gmail.com>
2019-09-19 02:35:32 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -5,7 +5,7 @@
 PKG_NAME="glibc"
 #PKG_VERSION="2.30"
 #PKG_SHA256="e2c4114e569afbe7edbc29131a43be833850ab9a459d81beb2588016d2bbb8af"
-PKG_VERSION="02d35f0646050157d47f22f2f74347431d5ccbdd"
+PKG_VERSION="0f02b6cfc44af73d4d4363c46b3cbb18b8ff9171"
 PKG_LICENSE="GPL"
 PKG_SITE="https://sourceware.org/git/gitweb.cgi?p=glibc.git;a=shortlog"
 PKG_SITE="https://github.com/bminor/glibc/tree/release/2.30/master"
@@ -30,7 +30,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
                            --with-__thread \
                            --with-binutils=$BUILD/toolchain/bin \
                            --with-headers=$SYSROOT_PREFIX/usr/include \
-                           --enable-kernel=5.2 \
+                           --enable-kernel=5.3 \
                            --without-cvs \
                            --without-gd \
                            --disable-build-nscd \
