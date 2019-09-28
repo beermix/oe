1d0e986c5d2e48f981981b58315ec1fea0745710
AV <27rurk@gmail.com>
2019-09-28 18:09:24 +0300

    autocommit on change

--- packages/x11/lib/libXvMC/package.mk
+++ packages/x11/lib/libXvMC/package.mk
@@ -1,5 +1,9 @@
 PKG_NAME="libXvMC"
-PKG_VERSION="1.0.11"
+PKG_VERSION="1.0.12"
+PKG_SHA256="024c9ec4f001f037eeca501ee724c7e51cf287eb69ced8c6126e16e7fa9864b5"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.X.org"
+PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_URL="https://www.x.org/releases/individual/lib/libXvMC-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain glib libXv"
 
