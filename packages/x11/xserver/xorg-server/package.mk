aafdec39e767a0d09e812e67f1b34a94cc80352b
AV <27rurk@gmail.com>
2019-09-29 00:26:58 +0300

    autocommit on change

--- packages/x11/xserver/xorg-server/package.mk
+++ packages/x11/xserver/xorg-server/package.mk
@@ -30,7 +30,7 @@ else
 fi
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-debug \
-                           --disable-silent-rules \
+                           --enable-silent-rules \
                            --disable-strict-compilation \
                            --enable-largefile \
                            --enable-visibility \
