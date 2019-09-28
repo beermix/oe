f5412f7c896497c73572b8d5b4fb6e34f821d3d9
AV <27rurk@gmail.com>
2019-09-28 19:07:40 +0300

    autocommit on change

--- packages/python/system/simplejson/package.mk
+++ packages/python/system/simplejson/package.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="simplejson"
 PKG_VERSION="3.16.0"
