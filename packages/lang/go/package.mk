7ec43b635cdd13cb8f12fb4089522e4bd1749224
AV <27rurk@gmail.com>
2019-09-29 01:00:10 +0300

    autocommit on change

--- packages/lang/go/package.mk
+++ packages/lang/go/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="go"
-PKG_VERSION="1.13rc1"
+PKG_VERSION="1.13.1"
 PKG_SHA256=""
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/golang/go/releases"
