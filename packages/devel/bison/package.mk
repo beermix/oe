e8ad5544d57bf5912b64234affc64aa5bca9ab4d
AV <27rurk@gmail.com>
2019-09-28 17:34:04 +0300

    autocommit on change

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -3,8 +3,6 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bison"
-#PKG_VERSION="3.3.2"
-#PKG_SHA256="039ee45b61d95e5003e7e8376f9080001b4066ff357bde271b7faace53b9d804"
 PKG_VERSION="3.4.2"
 PKG_SHA256="27d05534699735dc69e86add5b808d6cb35900ad3fd63fa82e3eb644336abfa0"
 PKG_LICENSE="GPL"
@@ -15,13 +13,3 @@ PKG_LONGDESC="A general-purpose parser generator."
 
 PKG_CONFIGURE_OPTS_HOST="--disable-rpath --with-gnu-ld"
 
-#post_configure_host() {
-# The configure system causes Bison to be built without support for
-# internationalization of error messages if a bison program is not already in
-# $PATH. The following addition will correct this:
-#  echo '#define YYENABLE_NLS 1' >> lib/config.h
-#}
-
-#post_makeinstall_host() {
-#  cp -r $PKG_DIR/scripts/yacc $TOOLCHAIN/bin
-#}
--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -8,18 +8,17 @@ PKG_SHA256="13958243a01365b05652fa01b21d40fa834f70a9e30efa69c02604e64f58b8f5"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/download/"
 PKG_URL="http://www.cmake.org/files/v${PKG_VERSION%.*}/cmake-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="ccache:host openssl:host rhash:host"
+PKG_DEPENDS_HOST="ccache:host openssl:host"
 PKG_LONGDESC="A cross-platform, open-source make system."
 PKG_TOOLCHAIN="configure"
 
 configure_host() {
   ../configure --prefix=${TOOLCHAIN} \
-               --no-qt-gui --no-system-libs --no-system-expat \
+               --no-qt-gui --no-system-libs \
                -- \
                -DCMAKE_C_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
                -DCMAKE_CXX_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
                -DCMAKE_EXE_LINKER_FLAGS="${HOST_LDFLAGS}" \
                -DCMAKE_USE_OPENSSL=ON \
-               -DCMAKE_USE_LIBRHASH=ON \
                -DBUILD_CursesDialog=0
 }
