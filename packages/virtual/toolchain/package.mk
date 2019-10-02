fe532b5e546cbd310747eda4ce561ad416d98691
AV <27rurk@gmail.com>
2019-10-02 18:22:35 +0300

    autocommit on change

--- packages/virtual/toolchain/package.mk
+++ packages/virtual/toolchain/package.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv) gawk:host 
 
 PKG_NAME="toolchain"
 PKG_VERSION=""
