69e900c0b6d6a1eeeee108bcf98262e603efccfa
AV <27rurk@gmail.com>
2019-10-03 09:28:32 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -82,9 +82,9 @@ pre_configure_host() {
   unset CPP
 }
 
-pre_configure_bootstrap() {
-  export CCACHE_DISABLE=true
-}
+#pre_configure_bootstrap() {
+#  export CCACHE_DISABLE=true
+#}
 
 post_make_host() {
   # fix wrong link
