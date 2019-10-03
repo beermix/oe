f4d3e252c9e29543d344d2bbfc64d5ff5ca26338
AV <27rurk@gmail.com>
2019-10-03 21:06:23 +0300

    autocommit on change

--- packages/devel/gettext-tiny/package.mk
+++ packages/devel/gettext-tiny/package.mk
@@ -2,18 +2,15 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="gettext-tiny"
-PKG_VERSION="a76f8ad"
+PKG_VERSION="c6dcdcdef801127549d3906d153c061880d25a73"
 PKG_SITE="https://github.com/sabotage-linux/gettext-tiny"
 PKG_URL="https://github.com/sabotage-linux/gettext-tiny/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 
 make_host() {
-#  make LIBINTL=FLAVOR
-  make CFLAGS="$HOST_CFLAGS -fPIC" LIBINTL=NONE
+  make LIBINTL=NONE
 }
 
 makeinstall_host() {
-#  make LIBINTL=NONE DESTDIR=$TOOLCHAIN prefix=/ all install
-#  make DESTDIR=$TOOLCHAIN LIBINTL=NONE install
-  make LIBINTL=NONE DESTDIR=$TOOLCHAIN prefix=/ install
+  make LIBINTL=NONE DESTDIR=$TOOLCHAIN prefix=/ all install
 }
