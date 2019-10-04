2013aa5d14901d95891d2d964bc1f6aa43b067b4
AV <27rurk@gmail.com>
2019-10-05 02:38:35 +0300

    autocommit on change

--- packages/devel/libcroco/package.mk
+++ packages/devel/libcroco/package.mk
@@ -1,8 +1,8 @@
 PKG_NAME="libcroco"
-PKG_VERSION="0.6.12"
+PKG_VERSION="0.6.13"
 PKG_SITE="http://eigen.tuxfamily.org/index.php?title=Main_Page"
 PKG_URL="http://ftp.gnome.org/pub/GNOME/sources/libcroco/0.6/libcroco-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="libxml2:host pkg-config:host"
+PKG_DEPENDS_HOST="libxml2:host"
 PKG_BUILD_FLAGS="+pic:host"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
