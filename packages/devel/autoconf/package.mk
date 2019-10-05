9028bbdcfc04e933e41c302512e5bba9828f3ebf
AV <27rurk@gmail.com>
2019-10-05 12:15:51 +0300

    autocommit on change

--- packages/devel/autoconf/package.mk
+++ packages/devel/autoconf/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684"
 PKG_LICENSE="GPL"
 PKG_SITE="http://sources.redhat.com/autoconf/"
 PKG_URL="http://ftpmirror.gnu.org/autoconf/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="ccache:host m4:host"
+PKG_DEPENDS_HOST="ccache:host m4:host gettext:host"
 PKG_LONGDESC="A GNU tool for automatically configuring source code."
 
 PKG_CONFIGURE_OPTS_HOST="EMACS=no \
