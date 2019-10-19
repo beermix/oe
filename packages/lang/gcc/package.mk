b5f09b450570aedbea37f6b6fd37faf63aa4e684
AV <27rurk@gmail.com>
2019-10-19 22:27:39 +0300

    autocommit on change

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -39,6 +39,7 @@ GCC_COMMON_CONFIGURE_OPTS="--target=$TARGET_NAME \
                            --enable-checking=release \
                            --with-default-libstdcxx-abi=gcc4-compatible \
                            --with-diagnostics-color=always \
+                           --with-gcc-major-version-only \
                            --without-ppl \
                            --without-cloog \
                            --disable-libada \
