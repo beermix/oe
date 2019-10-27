dfc691d1bc41a5fdd2aa672d849575b748bc2019
AV <27rurk@gmail.com>
2019-10-27 22:32:36 +0300

    autocommit on change

--- packages/devel/pkgconf/package.mk
+++ packages/devel/pkgconf/package.mk
@@ -6,10 +6,10 @@ PKG_SHA256="61f0b31b0d5ea0e862b454a80c170f57bad47879c0c42bd8de89200ff62ea210"
 PKG_SITE="https://github.com/pkgconf/pkgconf/releases"
 PKG_URL="https://distfiles.dereferenced.org/pkgconf/pkgconf-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
-#PKG_TOOLCHAIN="autotools"
 PKG_TOOLCHAIN="configure"
+#PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --with-gnu-ld"
 
 post_makeinstall_host() {
   ln -sf pkgconf $TOOLCHAIN/bin/pkg-config
