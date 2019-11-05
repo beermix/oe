0693dcc47fd8f8f8681698d7c77c399665b4c994
AV <27rurk@gmail.com>
2019-11-05 17:06:48 +0300

    autocommit on change

--- packages/devel/icu/package - копия.mk
+++ packages/devel/icu/package - копия.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv) --with-data-packaging=archive \
 
 PKG_NAME="icu"
-PKG_VERSION="65.1"
-PKG_SHA256="53e37466b3d6d6d01ead029e3567d873a43a5d1c668ed2278e253b683136d948"
+PKG_VERSION="64.2"
+PKG_SHA256="627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
 PKG_LICENSE="Custom"
 PKG_SITE="http://download.icu-project.org/files/icu4c/?C=M;O=D"
 PKG_URL="http://download.icu-project.org/files/icu4c/${PKG_VERSION}/icu4c-${PKG_VERSION//./_}-src.tgz"
-PKG_URL="https://fossies.org/linux/misc/icu4c-65_1-src.tgz"
+#PKG_URL="https://fossies.org/linux/misc/icu4c-65rc-src.tgz"
 PKG_DEPENDS_TARGET="toolchain icu:host"
 PKG_LONGDESC="International Components for Unicode library."
 PKG_BUILD_FLAGS="+pic"
