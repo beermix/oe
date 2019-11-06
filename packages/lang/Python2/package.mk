758ca88e8426c373ba132f92425c82c283443beb
AV <27rurk@gmail.com>
2019-11-06 04:17:55 +0300

    autocommit on change

--- packages/lang/Python2/package.mk
+++ packages/lang/Python2/package.mk
@@ -24,8 +24,7 @@ PKG_CONFIGURE_OPTS_HOST="--cache-file=config.cache \
                          --without-cxx-main \
                          --with-threads \
                          --disable-ipv6 \
-                         --enable-unicode=ucs4 \
-                         --with-computed-gotos"
+                         --enable-unicode=ucs4"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
                            ac_cv_file_dev_ptmx=yes \
