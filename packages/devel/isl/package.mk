875c62a4bfb1a11110ea9a8f428cd1287b1a746f
AV <27rurk@gmail.com>
2019-11-11 15:48:17 +0300

    autocommit on change

--- packages/devel/isl/package.mk
+++ packages/devel/isl/package.mk
@@ -6,10 +6,9 @@ PKG_NAME="isl"
 #PKG_VERSION="0.21"
 #PKG_SHA256="777058852a3db9500954361e294881214f6ecd4b594c00da5eee974cd6a54960"
 PKG_VERSION="0.16.1"
-PKG_SHA256="412538bb65c799ac98e17e8cfcdacbb257a57362acfaaff254b0fcae970126d2"
+PKG_SHA256="45292f30b3cb8b9c03009804024df72a79e9b5ab89e41c94752d6ea58a1e4b02"
 PKG_SITE="http://isl.gforge.inria.fr/?C=M;O=D"
 PKG_URL="http://isl.gforge.inria.fr/isl-$PKG_VERSION.tar.xz"
-#PKG_URL="https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.16.1.tar.bz2"
 PKG_DEPENDS_HOST="libtool:host intltool:host gmp:host"
 
 pre_configure_host() {
