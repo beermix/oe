33b1fa018aafca1e16dbc58ec54fb2de71504a7f
AV <27rurk@gmail.com>
2019-11-15 00:57:54 +0300

    autocommit on change

--- packages/devel/automake15/package.mk
+++ packages/devel/automake15/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="automake"
-PKG_VERSION="1.16.1"
-PKG_SHA256="5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921"
+PKG_VERSION="1.15.1"
+PKG_SHA256="af6ba39142220687c500f79b4aa2f181d9b24e4f8d8ec497cea4ba26c64bedaf"
 PKG_LICENSE="GPL"
 PKG_SITE="http://sources.redhat.com/automake/"
 PKG_URL="http://ftpmirror.gnu.org/automake/$PKG_NAME-$PKG_VERSION.tar.xz"
