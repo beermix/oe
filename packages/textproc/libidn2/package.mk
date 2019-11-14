19489e10a3db212aa40e49d98c8d28d1412e5555
AV <27rurk@gmail.com>
2019-11-15 00:03:54 +0300

    autocommit on change

--- packages/textproc/libidn2/package.mk
+++ packages/textproc/libidn2/package.mk
@@ -7,12 +7,12 @@ PKG_SHA256="e1cb1db3d2e249a6a3eb6f0946777c2e892d5c5dc7bd91c74394fc3a01cab8b5"
 PKG_LICENSE="LGPL3"
 PKG_SITE="https://www.gnu.org/software/libidn/"
 PKG_URL="http://ftpmirror.gnu.org/gnu/libidn/libidn2-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Free software implementation of IDNA2008, Punycode and TR46."
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-doc \
-                           --enable-shared \
-                           --disable-static"
+PKG_CONFIGURE_OPTS_HOST="--disable-doc --enable-shared=no"
+PKG_CONFIGURE_OPTS_TARGET="--disable-doc --enable-shared=no"
 
 post_makeinstall_target() {
   safe_remove ${INSTALL}/usr/bin
