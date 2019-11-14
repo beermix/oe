02901f0fbff68999d0c2e35c0e54ec8002650ceb
AV <27rurk@gmail.com>
2019-11-14 21:02:23 +0300

    autocommit on change

--- packages/sysutils/psmisc/package.mk
+++ packages/sysutils/psmisc/package.mk
@@ -1,11 +1,9 @@
 PKG_NAME="psmisc"
-PKG_VERSION="23.2"
+PKG_VERSION="23.3"
 PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
-PKG_PRIORITY="optional"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
                            ac_cv_func_realloc_0_nonnull=yes \
                            --enable-static \
-                           --disable-shared \
-                           --with-sysroot=$SYSROOT_PREFIX"
+                           --disable-shared"
