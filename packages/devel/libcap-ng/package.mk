4ece703578e7fd220521892cc74a1fec6397e213
AV <27rurk@gmail.com>
2019-11-15 03:04:32 +0300

    autocommit on change

--- packages/devel/libcap-ng/package.mk
+++ packages/devel/libcap-ng/package.mk
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
 PKG_NAME="libcap-ng"
 PKG_VERSION="0.7.9"
 PKG_URL="http://people.redhat.com/sgrubb/libcap-ng/libcap-ng-$PKG_VERSION.tar.gz"
@@ -5,5 +8,7 @@ PKG_DEPENDS_TARGET="toolchain"
 #PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_swig_found=no --without-python --disable-shared --enable-static"
+
 PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_TARGET"
+
 PKG_CONFIGURE_OPTS_INIT="$PKG_CONFIGURE_OPTS_TARGET"
