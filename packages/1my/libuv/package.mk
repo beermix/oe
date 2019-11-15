1d1051aa4f96f49d6fadcfe97682830cd6eb63fc
AV <27rurk@gmail.com>
2019-11-15 03:19:19 +0300

    autocommit on change

--- packages/1my/libuv/package.mk
+++ packages/1my/libuv/package.mk
@@ -1,10 +1,15 @@
 PKG_NAME="libuv"
-PKG_VERSION="v1.32.0"
+PKG_VERSION="v1.33.1"
 PKG_SITE="https://dist.libuv.org/dist/"
 PKG_URL="http://dist.libuv.org/dist/$PKG_VERSION/libuv-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="autotools"
+PKG_BUILD_FLAGS="+pic:host +pic"
 
 post_unpack() {
   echo "m4_define([UV_EXTRA_AUTOMAKE_FLAGS], [serial-tests])" > $PKG_BUILD/m4/libuv-extra-automake-flags.m4
 }
+
+PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static --with-gnu-ld"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --with-gnu-ld"
