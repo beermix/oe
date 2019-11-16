a2c5a542acf258d2f73340cb6d931370a249cd01
AV <27rurk@gmail.com>
2019-11-16 20:14:38 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="ninja"
 #PKG_VERSION="1.9.0"
 #PKG_SHA256="5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9"
-PKG_VERSION="3ef623a"
+PKG_VERSION="8486a88"
 PKG_LICENSE="Apache"
 PKG_SITE="https://github.com/ninja-build/ninja"
 PKG_URL="https://github.com/ninja-build/ninja/archive/v$PKG_VERSION.tar.gz"
@@ -16,12 +16,12 @@ PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
 
 makeinstall_host() {
   cp $PKG_BUILD/.$HOST_NAME/ninja $TOOLCHAIN/bin
-  strip $TOOLCHAIN/bin/ninja
+  strip -s $TOOLCHAIN/bin/ninja
 }
 
 #make_host() {
-#  python3 configure.py --bootstrap
-# CXX=/bin/clang++ | $TOOLCHAIN/bin/python2
+#  CXX=clang++ python3 configure.py --bootstrap
+# CXX=clang++ | $TOOLCHAIN/bin/python2
 #}
 
 #makeinstall_host() {
