62845e4b7aaa1c11c5c27c610fa78bdebe14e58a
AV <27rurk@gmail.com>
2019-11-17 18:59:59 +0300

    autocommit on change

--- packages/1my/bash/package.mk
+++ packages/1my/bash/package.mk
@@ -1,11 +1,26 @@
 PKG_NAME="bash"
-PKG_VERSION="d894cfd"
-PKG_SHA256="65d34a7d87d7141a8877fa335c76a5f1dcfa56565363198c76791563d024d791"
+PKG_VERSION="5.0"
+PKG_SHA256=""
 #PKG_SITE="https://github.com/bminor/bash/"
 #PKG_URL="http://ftp.gnu.org/gnu/bash/bash-$PKG_VERSION.tar.gz"
 PKG_URL="https://github.com/bminor/bash/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses readline"
-PKG_BUILD_FLAGS="-parallel"
+
+pre_patch() {
+  cd $PKG_BUILD
+  patch -p0 -i $PKG_DIR/patches0/bash50-001.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-002.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-003.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-004.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-005.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-006.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-007.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-008.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-009.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-010.patch
+  patch -p0 -i $PKG_DIR/patches0/bash50-011.patch
+}
 
 PKG_CONFIGURE_OPTS_TARGET="--with-curses \
 			      --enable-readline \
--- packages/1my/file/package.mk
+++ packages/1my/file/package.mk
@@ -1,16 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
 PKG_NAME="file"
-PKG_VERSION="5.37"
-PKG_SHA256="e9c13967f7dd339a3c241b7710ba093560b9a33013491318e88e6b8b57bae07f"
-PKG_SITE="ftp://ftp.astron.com/pub/file/"
-PKG_URL="ftp://ftp.astron.com/pub/file/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain zlib file:host"
-PKG_SECTION="tools"
+PKG_VERSION="d1ff3af7a2c6b38bdbdde7af26b59e3c50a48fff"
+PKG_SHA256="acc16f878576ad0bb29bbb7c724e29d2827f14ddb39fe94d76c3a859d4a3d0d9"
+PKG_LICENSE="BSD"
+PKG_SITE="http://www.darwinsys.com/file/"
+PKG_URL="https://github.com/file/file/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="toolchain:host"
+PKG_DEPENDS_TARGET="toolchain file:host zlib"
+PKG_LONGDESC="The file utility is used to determine the types of various files."
+PKG_TOOLCHAIN="autotools"
 
-pre_configure_target() {
-  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-Os|"`
-  export CXXFLAGS=`echo $CXXFLAGS | sed -e "s|-O.|-Os|"`
-}
-
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static"
-
+PKG_CONFIGURE_OPTS_HOST="--enable-fsect-man5 --enable-static --disable-shared -disable-libseccomp --disable-rpath --disable-warnings"
 PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
