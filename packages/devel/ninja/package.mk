ae1261259d3161746c1533f984737358901e23e4
AV <27rurk@gmail.com>
2019-11-18 19:43:40 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="ninja"
 #PKG_VERSION="1.9.0"
 #PKG_SHA256="5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9"
-PKG_VERSION="8486a88"
+PKG_VERSION="2d15b04"
 PKG_LICENSE="Apache"
 PKG_SITE="https://github.com/ninja-build/ninja"
 PKG_URL="https://github.com/ninja-build/ninja/archive/v$PKG_VERSION.tar.gz"
