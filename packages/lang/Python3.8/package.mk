2ec34ddb18599f98e09b4d083c0fcb96690c34db
AV <27rurk@gmail.com>
2019-11-24 01:44:34 +0300

    autocommit on change

--- packages/lang/Python3.8/package.mk
+++ packages/lang/Python3.8/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="Python3"
 # When changing PKG_VERSION remember to sync PKG_PYTHON_VERSION!
-PKG_VERSION="3.7.5"
-PKG_SHA256="e85a76ea9f3d6c485ec1780fca4e500725a4a7bbc63c78ebc44170de9b619d94"
+PKG_VERSION="3.8.0"
+PKG_SHA256="b356244e13fb5491da890b35b13b2118c3122977c2cd825e3eb6e7d462030d84"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.python.org/"
 PKG_URL="http://www.python.org/ftp/python/$PKG_VERSION/${PKG_NAME::-1}-$PKG_VERSION.tar.xz"
@@ -14,7 +14,7 @@ PKG_LONGDESC="Python3 is an interpreted object-oriented programming language."
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+speed"
 
-PKG_PYTHON_VERSION="python3.7"
+PKG_PYTHON_VERSION="python3.8"
 
 PKG_PY_DISABLED_MODULES="_tkinter nis gdbm bsddb ossaudiodev"
 
@@ -45,6 +45,7 @@ PKG_CONFIGURE_OPTS_HOST="ac_cv_prog_HAS_HG=/bin/false
                          --without-pymalloc
                          --without-ensurepip
                          --with-computed-gotos
+                         --disable-ipv6
 "
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_HAS_HG=/bin/false
@@ -85,6 +86,7 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_HAS_HG=/bin/false
                            --without-ensurepip
                            --disable-ipv6
                            --with-computed-gotos
+                           --with-threads
 "
 
 pre_configure_host() {
@@ -126,7 +128,7 @@ post_makeinstall_target() {
 
   PKG_INSTALL_PATH_LIB=$INSTALL/usr/lib/$PKG_PYTHON_VERSION
 
-  for dir in config compiler sysconfigdata lib-dynload/sysconfigdata lib2to3 test; do
+  for dir in config compiler sysconfigdata lib-dynload/sysconfigdata lib2to3; do
     rm -rf $PKG_INSTALL_PATH_LIB/$dir
   done
 
