04890c8d31c28ea6a5e3f9b6f5c2a9892646eb53
AV <27rurk@gmail.com>
2019-11-23 17:40:30 +0300

    autocommit on change

--- packages/databases/mariadb-connector-c/package.mk
+++ packages/databases/mariadb-connector-c/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mariadb-connector-c"
-PKG_VERSION="3.1.3"
-PKG_SHA256="7afac2ef28aea7d71c95473c7a3da0f021a5ef2364480cd3e80eab83016341c2"
+PKG_VERSION="3.1.5"
+PKG_SHA256="a78b41b638e4e93df373bc10377523f85d455c09ff2fe87543fe7c111592a597"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/MariaDB/mariadb-connector-c/releases"
 PKG_URL="https://github.com/MariaDB/mariadb-connector-c/archive/v$PKG_VERSION.tar.gz"
