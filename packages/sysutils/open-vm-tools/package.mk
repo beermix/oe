507fa5a8db1687cfdbe0ad608e7ee1cf9c960291
AV <27rurk@gmail.com>
2019-11-24 02:35:35 +0300

    autocommit on change

--- packages/sysutils/open-vm-tools/package.mk
+++ packages/sysutils/open-vm-tools/package.mk
@@ -4,13 +4,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="open-vm-tools"
-PKG_VERSION="10.3.10-12406962"
-PKG_SHA256=""
+PKG_VERSION="stable-11.0.1"
+PKG_SHA256="99f1e3c5245bb002c1e66cbb7a1078e1c3567db5f92cc2e00ab08557e9df4758"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/vmware/open-vm-tools"
 PKG_URL="https://github.com/vmware/open-vm-tools/archive/${PKG_VERSION}.tar.gz"
-PKG_URL="https://github.com/vmware/open-vm-tools/releases/download/stable-10.3.10/open-vm-tools-10.3.10-12406962.tar.gz"
 PKG_DEPENDS_TARGET="toolchain fuse glib:host glib libdnet libtirpc"
 PKG_LONGDESC="open-vm-tools: open source implementation of VMware Tools"
 PKG_TOOLCHAIN="autotools"
@@ -30,7 +29,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-docs \
                            --with-sysroot=$SYSROOT_PREFIX"
 
 post_unpack() {
-#  mv $PKG_BUILD/$PKG_NAME/* $PKG_BUILD/
+  mv $PKG_BUILD/$PKG_NAME/* $PKG_BUILD/
 
   sed -e 's|.*common-agent/etc/config/Makefile.*||' -i $PKG_BUILD/configure.ac
 
