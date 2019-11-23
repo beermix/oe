6abcbdece8cd4d98574ba18221363e4a87492b8b
AV <27rurk@gmail.com>
2019-11-23 23:13:57 +0300

    autocommit on change

--- packages/devel/gmp/package.mk
+++ packages/devel/gmp/package.mk
@@ -11,11 +11,11 @@ PKG_URL="https://gmplib.org/download/gmp/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host m4:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating point numbers."
-PKG_BUILD_FLAGS="+pic:host +pic"
+PKG_BUILD_FLAGS="+pic:host"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-cxx --enable-static --disable-shared"
 
-PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_HOST"
+PKG_CONFIGURE_OPTS_TARGET="--enable-cxx"
 
 pre_configure_host() {
   export CPPFLAGS="$CPPFLAGS -fexceptions"
