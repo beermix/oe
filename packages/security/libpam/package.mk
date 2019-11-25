a8ce3054b5c046df0b0a678dd467876c2e7a1c6c
AV <27rurk@gmail.com>
2019-11-25 11:06:26 +0300

    autocommit on change

--- packages/security/libpam/package.mk
+++ packages/security/libpam/package.mk
@@ -4,10 +4,10 @@ PKG_SHA256="eff47a4ecd833fbf18de9686632a70ee8d0794b79aecb217ebd0ce11db4cd0db"
 PKG_URL="https://github.com/linux-pam/linux-pam/releases/download/v$PKG_VERSION/Linux-PAM-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="+pic"
+PKG_BUILD_FLAGS="+pic -hardening"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-pamlocking \
-			      --disable-shared \
+			      --enable-shared \
 			      --enable-static \
 			      --disable-audit \
 			      --disable-cracklib \
