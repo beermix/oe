e355a285ec314c14d9e1bf90d2ad1eba05a1269e
AV <27rurk@gmail.com>
2019-11-30 03:25:45 +0300

    autocommit on change

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -20,9 +20,6 @@ configure_package() {
                            --disable-java \
                            --disable-curses \
                            --with-included-libxml \
-                           --with-included-libcroco \
-                           --with-included-libunistring \
-                           --with-included-glib \
                            --disable-native-java \
                            --disable-csharp \
                            --without-emacs"
