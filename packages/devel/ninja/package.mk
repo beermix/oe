aaac2944c8219ab1b1d552ba0a1b8a38901e42eb
AV <27rurk@gmail.com>
2019-12-03 14:00:39 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -2,18 +2,18 @@
 # Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="ninja"
-PKG_VERSION="3b82f99"
+PKG_VERSION="ba156d0"
 PKG_SITE="https://github.com/ninja-build/ninja"
 PKG_URL="https://github.com/ninja-build/ninja/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="Python3:host re2c:host"
-#PKG_TOOLCHAIN="cmake-make"
-PKG_TOOLCHAIN="manual"
+PKG_DEPENDS_HOST="cmake:host re2c:host"
+PKG_TOOLCHAIN="cmake-make"
+#PKG_TOOLCHAIN="manual"
 
-#PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
+PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
 
-make_host() {
-  python3 $PKG_BUILD/configure.py --bootstrap
-}
+#make_host() {
+#  python3 $PKG_BUILD/configure.py --bootstrap
+#}
 
 makeinstall_host() {
   cp $PKG_BUILD/.$HOST_NAME/ninja $TOOLCHAIN/bin
