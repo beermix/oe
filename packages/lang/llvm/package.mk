28fb7bbe0437b078a8b27cedb1d928e41e9c07c7
AV <27rurk@gmail.com>
2019-12-12 22:22:45 +0300

    autocommit on change

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -25,7 +25,7 @@ PKG_CMAKE_OPTS_COMMON="-DLLVM_INCLUDE_TOOLS=ON \
                        -DLLVM_INCLUDE_DOCS=OFF \
                        -DLLVM_ENABLE_DOXYGEN=OFF \
                        -DLLVM_ENABLE_SPHINX=OFF \
-                       -DLLVM_TARGETS_TO_BUILD="X86" \
+                       -DLLVM_TARGETS_TO_BUILD="AMDGPU" \
                        -DLLVM_ENABLE_TERMINFO=OFF \
                        -DLLVM_ENABLE_ASSERTIONS=OFF \
                        -DLLVM_ENABLE_WERROR=OFF \
