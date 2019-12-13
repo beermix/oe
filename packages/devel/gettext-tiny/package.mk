a72b2cec24c1d0ea34e7fe90aad3a06684309bf7
AV <27rurk@gmail.com>
2019-12-13 13:50:41 +0300

    autocommit on change

--- packages/devel/gettext-tiny/package.mk
+++ packages/devel/gettext-tiny/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="gettext-tiny"
-PKG_VERSION="a76f8ad"
+PKG_VERSION="55a2119"
 PKG_SITE="https://github.com/sabotage-linux/gettext-tiny"
 PKG_URL="https://github.com/sabotage-linux/gettext-tiny/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
