5ee988b3abbc056f8a903503680fbb3dda97b9ac
AV <27rurk@gmail.com>
2019-12-21 03:43:26 +0300

    autocommit on change

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="configtools"
-PKG_VERSION="1912ca50411bb77fb2c610ef55dd91e332663de9"
-PKG_SHA256="f92fb314e16e37998d6b9c642eb0ef655fb54535ab6128a9edaab84b067f75e3"
+PKG_VERSION="706fbe5"
+PKG_SHA256="185511fd06ccbb892e14d6c593f68f0f1fd7c713b21a8d8bd3c7fd949e9c5979"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
-PKG_URL="http://git.savannah.gnu.org/cgit/config.git/snapshot/$PKG_VERSION.tar.gz"
+PKG_URL="http://git.savannah.gnu.org/cgit/config.git/snapshot/$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST=""
 PKG_LONGDESC="configtools"
 PKG_TOOLCHAIN="manual"
