3643090dec0db3fde88b82319775ddb66bd2d72b
AV <27rurk@gmail.com>
2020-02-10 23:30:05 +0300

    autocommit on change

--- packages/devel/ninja/package.mk
+++ packages/devel/ninja/package.mk
@@ -3,17 +3,16 @@
 
 PKG_NAME="ninja"
 PKG_VERSION="9fd5d3e"
+PKG_SHA256=""
 PKG_SITE="https://github.com/ninja-build/ninja"
 PKG_URL="https://github.com/ninja-build/ninja/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="cmake:host re2c:host"
-PKG_TOOLCHAIN="cmake-make"
-#PKG_TOOLCHAIN="manual"
+PKG_DEPENDS_HOST="Python3:host re2c:host"
+PKG_LONGDESC="Small build system with a focus on speed"
+PKG_TOOLCHAIN="manual"
 
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release"
-
-#make_host() {
-#  python3 $PKG_BUILD/configure.py --bootstrap
-#}
+make_host() {
+  python3 $PKG_BUILD/configure.py --bootstrap
+}
 
 makeinstall_host() {
   cp $PKG_BUILD/.$HOST_NAME/ninja $TOOLCHAIN/bin
