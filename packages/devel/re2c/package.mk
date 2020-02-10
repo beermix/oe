5681b58eee414100d5e417696762f05a8eda268c
AV <27rurk@gmail.com>
2020-02-10 20:12:39 +0300

    autocommit on change

--- packages/devel/re2c/package.mk
+++ packages/devel/re2c/package.mk
@@ -1,10 +1,9 @@
 PKG_NAME="re2c"
-PKG_VERSION="1.2.1"
-PKG_SHA256="1a4cd706b5b966aeffd78e3cf8b24239470ded30551e813610f9cd1a4e01b817"
+PKG_VERSION="1.3"
+PKG_SHA256="f37f25ff760e90088e7d03d1232002c2c2672646d5844fdf8e0d51a5cd75a503"
 PKG_SITE="https://github.com/skvadrik/re2c/releases"
 PKG_URL="https://github.com/skvadrik/re2c/releases/download/$PKG_VERSION/re2c-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="bison:host autotools:host"
-#PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_HOST="--disable-docs"
 
