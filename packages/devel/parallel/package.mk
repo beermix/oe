6fcd67897a660e2c3ae46e8926246f608a7579d4
AV <27rurk@gmail.com>
2020-02-29 18:02:11 +0300

    autocommit on change

--- packages/devel/parallel/package.mk
+++ packages/devel/parallel/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="parallel"
-PKG_VERSION="20120122"
-PKG_SHA256=""
+PKG_VERSION="20120222"
+PKG_SHA256="da315ae094bb670c4915f0ac28b74e28fb808013370cd55395e2aa833c4321d1"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://ftp.gnu.org/gnu/parallel/?C=M;O=A"
 PKG_URL="http://ftpmirror.gnu.org/parallel/$PKG_NAME-$PKG_VERSION.tar.bz2"
