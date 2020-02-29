c2acd5ae9978fd51483260a202bb10e185f9a55e
AV <27rurk@gmail.com>
2020-02-29 16:35:26 +0300

    autocommit on change

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -4,7 +4,7 @@
 
 PKG_NAME="cmake"
 PKG_VERSION="3.16.4"
-PKG_SHA256=""
+PKG_SHA256="9bcc8c114d9da603af9512083ed7d4a39911d16105466beba165ba8fe939ac2c"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/download"
 PKG_URL="http://www.cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
@@ -20,6 +20,5 @@ configure_host() {
                -DCMAKE_CXX_FLAGS="-O2 -Wall -pipe -Wno-format-security" \
                -DCMAKE_EXE_LINKER_FLAGS="${HOST_LDFLAGS}" \
                -DCMAKE_USE_OPENSSL=ON \
-               -DCMAKE_BUILD_TYPE=Release \
                -DBUILD_CursesDialog=0
 }
--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="configtools"
-PKG_VERSION="5256817"
-PKG_SHA256="e51f907cf9f1d615311c2b7bf5d17f5c5607fbaa9344fad0808cf0780fc49118"
+PKG_VERSION="5256817ace8493502ec88501a19e4051c2e220b0"
+PKG_SHA256="3856ff9a2a9382a3396549c047c28e0f05b7e4822239ffe91ce2e59b0a0284db"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
-PKG_URL="http://git.savannah.gnu.org/cgit/config.git/snapshot/$PKG_VERSION.tar.gz"
+PKG_URL="http://git.savannah.gnu.org/cgit/config.git/snapshot/config-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST=""
 PKG_LONGDESC="configtools"
 PKG_TOOLCHAIN="manual"
