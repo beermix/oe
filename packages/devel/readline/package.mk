ac353f43173d65253f62337bc28095f770f31059
AV <27rurk@gmail.com>
2020-03-18 18:46:05 +0300

    autocommit on change

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -11,7 +11,7 @@ PKG_URL="http://ftpmirror.gnu.org/readline/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in."
 
-PKG_CONFIGURE_OPTS_TARGET="bash_cv_wcwidth_broken=no --with-curses"
+PKG_CONFIGURE_OPTS_TARGET="bash_cv_wcwidth_broken=no --with-curses --disable-install-examples"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/share/readline
