0721fbb0ab47b98190154a9df39e03c020cb426d
AV <27rurk@gmail.com>
2020-04-08 17:50:51 +0300

    autocommit on change

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -28,7 +28,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
                            --with-__thread \
                            --with-binutils=$BUILD/toolchain/bin \
                            --with-headers=$SYSROOT_PREFIX/usr/include \
-                           --enable-kernel=5.5 \
+                           --enable-kernel=5.6 \
                            --without-cvs \
                            --without-gd \
                            --disable-build-nscd \
